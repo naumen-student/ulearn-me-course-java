2b2373261d7eb9b1e4686e566077bd8886412775
rabbit21xx13 <rabbit21xx13@users.noreply.github.com>
2020-09-15 12:47:45 +0500

    Шевлякова Алёна. Задание 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,19 +3,21 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        String result = "TRUE";
+        while (x > 0) {
+            int o = x % 10;
+            if (o % 2 == 0)
+                x = x / 10;
+            else {
+                result = "FALSE";
+                break;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution(1234);
         System.out.println(result);
-        */
     }
-
 }
