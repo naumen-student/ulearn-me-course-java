71179d81f627ec68556193a44323580610e06cde
HiiiiItsMe <zavyalovartem79@gmail.com>
2020-09-15 23:51:14 +0500

    Завьялов Артём. Задание 14

--- 02-java-types/task14/src/com/example/task14/Task14.java
+++ 02-java-types/task14/src/com/example/task14/Task14.java
@@ -4,10 +4,8 @@ public class Task14 {
 
 
     public static int reverse(int value) {
-
-        // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
-
-        return 0;
+        String reversed = new StringBuilder(Integer.toString(value)).reverse().toString();
+        return Integer.parseInt(reversed);
     }
 
     public static void main(String[] args) {
