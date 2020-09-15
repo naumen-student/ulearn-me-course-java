c080f4023d59b6377bd427fee2593af72cb9b658
Iulia <julia.chebykina.13@gmail.com>
2020-09-15 19:34:29 +0500

    Чебыкина Юлия. Задание 2.07

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -3,10 +3,9 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int n1 = n / k;
+        int m1 = m / k;
+        return n1 * m1;
     }
 
     public static void main(String[] args) {
