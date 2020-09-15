23a1cf0bd505328bc20cac5148b229250c45a947
Semyon <semen2k1@mail.ru>
2020-09-15 17:53:19 +0500

    Task 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,10 +3,14 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        long sum = x + y;
+        int counter = 0;
+        do {
+            sum /= 10;
+            counter++;
+        }
+        while (sum != 0);
+        return counter;
     }
 
     public static void main(String[] args) {
