7d146a3a0384fbebd0a93ad1b93f354ff1d95102
theSweater23 <geg.gegre123@gmail.com>
2020-09-16 16:00:40 +0500

    Сулейманов Эмиль. Задание 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -5,17 +5,17 @@ public class Task06 {
     public static int solution(int x, int y) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int num = x + y;
+        String res = Integer.toString(num);
+        return res.length();
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(12, 34);
+
+        int result = solution(523, 528);
         System.out.println(result);
-        */
     }
 
 }
