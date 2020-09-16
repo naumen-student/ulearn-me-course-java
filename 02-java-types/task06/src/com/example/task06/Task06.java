c6756d40a5996c4592088ffe6aef159223c7d54d
Lera <Valeria.Smirnova@urfu.me>
2020-09-16 13:01:14 +0500

    Смирнова_Валерия_task06

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -6,16 +6,12 @@ public class Task06 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return 0;
+        return String.valueOf(Math.abs(x + y)).length();
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
 
 }
