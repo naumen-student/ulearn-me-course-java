b05bd54d970accb0ac94cb76dae45864cf7aebca
bearsbon <bearsbon@gmail.com>
2020-09-16 19:19:04 +0500

    Task 2.2 Долматов

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -4,18 +4,22 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        long num = Long.parseLong(input);
+
+        if (num >= Byte.MIN_VALUE && num <= Byte.MAX_VALUE) return "byte";
+        if (num >= Short.MIN_VALUE && num <= Short.MAX_VALUE) return "short";
+        if (num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE) return "int";
+        if (num >= Long.MIN_VALUE && num <= Long.MAX_VALUE) return "long";
 
         return "";
     }
 
+
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution("12345");
         System.out.println(result);
-         */
+
     }
 
 }
