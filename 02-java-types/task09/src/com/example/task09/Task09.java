99c6da2e447f00d8cffb25348d6bf157591d8d2c
krepeland <krepeland@yandex.ru>
2020-09-16 22:50:05 +0500

    Буханов Евгений. Задание 2.9

--- 02-java-types/task09/src/com/example/task09/Task09.java
+++ 02-java-types/task09/src/com/example/task09/Task09.java
@@ -5,9 +5,7 @@ public class Task09 {
     public static double solution() {
         float a = 1.0f;
         float b = 3.0f;
-        double x = (a / b - 1.0f / 3.0f) * 1.0e9;
-
-        return x;
+        return ((double) a / b - 1.0 / 3.0) * 1.0e9;
     }
 
     public static void main(String[] args) {
