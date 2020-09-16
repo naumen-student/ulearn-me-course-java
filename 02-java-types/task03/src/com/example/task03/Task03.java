ae658785ac147e783d3d38eac8310776fc158192
bearsbon <bearsbon@gmail.com>
2020-09-16 20:44:25 +0500

    2.3

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -1,11 +1,16 @@
+package com.example.task03;
+
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
 
-        return centimetre / 100;
+        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+
+        return 0;
     }
 
     public static void main(String[] args) {
 
     }
+
 }
