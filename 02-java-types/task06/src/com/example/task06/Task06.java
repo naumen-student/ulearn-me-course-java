4185acaba1148eb9c88851c40c2d840864a7963e
Stanislav <youngfrezyx@gmail.com>
2020-09-16 06:32:30 +0300

    Ильин Станислав. Задача 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,17 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        return String.valueOf(checkNegative(x + y)).length();
+    }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+    public static int checkNegative(int val){
+        if(val < 0)val *= -1;
+        return val;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
 
 }
