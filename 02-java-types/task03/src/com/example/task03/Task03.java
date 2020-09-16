dee0ab5a0ce5ce18d22150a94f27825657f4f433
theSweater23 <geg.gegre123@gmail.com>
2020-09-16 14:41:56 +0500

    Сулейманов Эмиль. Задание 2.3

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -6,16 +6,15 @@ public class Task03 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return 0;
+        return centimetre/100;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = getMetreFromCentimetre(345);
+
+        int result = getMetreFromCentimetre(999999);
         System.out.println(result);
-         */
     }
 
 }
