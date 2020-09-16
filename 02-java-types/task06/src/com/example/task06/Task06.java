9bea309ad1a1c78ffff7a47dec368581dd971151
theSweater23 <geg.gegre123@gmail.com>
2020-09-16 16:03:26 +0500

    Сулейманов Эмиль. Задание 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -14,7 +14,7 @@ public class Task06 {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        int result = solution(523, 528);
+        int result = solution(12, 34);
         System.out.println(result);
     }
 
