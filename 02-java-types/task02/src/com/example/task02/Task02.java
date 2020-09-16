985722d5b93c5e33f8840f995401c73da268abb3
Gorr <gorrgorrob@gmail.com>
2020-09-16 17:46:39 +0500

    Бородин Владислав. Задание 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,19 +3,28 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+        long inputNumber = Long.parseLong(input);
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if (checkBounds(inputNumber, Byte.MIN_VALUE, Byte.MAX_VALUE))
+            return "byte";
+        if (checkBounds(inputNumber, Short.MIN_VALUE, Short.MAX_VALUE))
+            return "short";
+        if (checkBounds(inputNumber, Integer.MIN_VALUE, Integer.MAX_VALUE))
+            return "int";
+        if (checkBounds(inputNumber, Long.MIN_VALUE, Long.MAX_VALUE))
+            return "long";
 
-        return "";
+        return null;
+    }
+
+    private static boolean checkBounds(long number, long minValue, long maxValue) {
+        return (number >= minValue && number <= maxValue);
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
+        System.out.println(solution("1"));
+        System.out.println(solution("12345"));
+        System.out.println(solution("12345678"));
+        System.out.println(solution("123456789000"));
     }
-
 }
