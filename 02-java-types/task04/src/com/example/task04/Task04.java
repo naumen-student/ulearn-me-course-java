b5a1f37423fbbf8124acc9b8f4c1f290386aba83
bearsbon <bearsbon@gmail.com>
2020-09-17 14:06:24 +0500

    Задание 2.4 Долматов

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -3,18 +3,14 @@ package com.example.task04;
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
-        switch (operation) {
-            case ("+"):
-                return a + b;
-            case ("-"):
-                return a - b;
-            case ("*"):
-                return a * b;
-            case ("/"):
-                return (float)(a / b);
 
-        }
-        return 0;
+        float num1 = a, num2 = b;
+
+        if (operation == "+") return num1+num2;
+        else if (operation == "-") return num1-num2;
+        else if (operation == "/") return num1/num2;
+        else if (operation == "*") return num1*num2;
+        else return 0;
     }
 
     public static void main(String[] args) {
