164b7d8db693ed151bcb61ab058db0014ec58fa6
petrdiukin <petr.diukin@urfu.me>
2020-09-17 12:46:48 +0500

    Дюкин Петр. Раздел 2, задача 3

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -3,11 +3,19 @@ package com.example.task03;
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
+
+        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+
         return centimetre / 100;
     }
 
     public static void main(String[] args) {
-
+        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
+        int result = getMetreFromCentimetre(345);
+        System.out.println(result);
+         */
     }
 
 }
