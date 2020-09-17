be6b8c9feb015e2d347ffea8c0d635876688e3c2
gisinka <mrxenonmr@gmail.com>
2020-09-17 17:49:28 +0500

    task2.4 solved

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -4,6 +4,7 @@ public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
+<<<<<<< HEAD
         switch (operation) {
             case ("+"):
                 return a + b;
@@ -13,11 +14,20 @@ public class Task04 {
                 return (float) a / b ;
         }
         return a * b;
+=======
+        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+
+        return 0;
+>>>>>>> parent of 9697fa3... task2.4 solved
     }
 
     public static void main(String[] args) {
+        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
         float result = calculate(-25, 5, "/");
         System.out.println(result);
+        */
     }
 
 }
