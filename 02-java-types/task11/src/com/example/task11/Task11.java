89be54e398aa0d65f3f07f7aed62011fab1ed3de
Seeryoga <seeryoga@gmail.com>
2020-09-17 20:21:30 +0500

    Савченко Сергей 2.11

--- 02-java-types/task11/src/com/example/task11/Task11.java
+++ 02-java-types/task11/src/com/example/task11/Task11.java
@@ -10,18 +10,13 @@ public class Task11 {
             sum += sum * percent;
         }
         return sum;
-        return  (float) (sum * Math.pow(percent + 1.0, 12));
+        return (float) (sum * Math.exp(12 * Math.log(1.0 + percent)));
     }
 
     public static void main(String[] args) {
-
         float sum = 500; // 500 руб. на счете
         float percent = 0.00000001f; // 0.000001% ежемесячно
-
         sum = benefit(sum, percent);
-
         System.out.println("Сумма на счете через год: " + sum);
-
     }
-
 }
