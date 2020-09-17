6d839aa63010cc255493de03a370ff168de647a8
BoberPro <34961963+BoberPro@users.noreply.github.com>
2020-09-17 20:36:38 +0500

    Худяков Сергей 2.4

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,19 +3,21 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        for (int i = 0; i < 5; i++) {
+            int mod10 = x % 10;
+            x/=10;
+            if (mod10 % 2 != 0) {
+                return "FALSE";
+            }
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
