99e1dc8f2bff5418206c59295f5016a2d045272f
petrdiukin <petr.diukin@urfu.me>
2020-09-17 16:29:02 +0500

    Дюкин Петр 2.8

--- 02-java-types/task08/src/com/example/task08/Task08.java
+++ 02-java-types/task08/src/com/example/task08/Task08.java
@@ -3,8 +3,8 @@ package com.example.task08;
 public class Task08 {
 
     public static boolean solution() {
-        float x = Float.POSITIVE_INFINITY;
-        return x + 1 == x;
+        double x = Double.POSITIVE_INFINITY;
+        return x == x + 1;
     }
 
     public static void main(String[] args) {
