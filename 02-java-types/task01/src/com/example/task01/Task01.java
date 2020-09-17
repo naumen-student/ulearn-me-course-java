2cfecc639908375eb942c574cd185b4097132958
BoberPro <34961963+BoberPro@users.noreply.github.com>
2020-09-17 17:48:47 +0500

    Худяков Сергей 2.1

---
+++ 02-java-types/task01/src/com/example/task01/Task01.java
@@ -0,0 +1,18 @@
+package com.example.task01;
+
+public class Task01 {
+
+    public static String solution() {
+        return "Я думаю, быть программистом - это круто";
+    }
+
+    public static void main(String[] args) {
+        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
+        String result = solution();
+        System.out.println(result);
+         */
+    }
+
+}
