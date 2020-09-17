62e901d43ee627a691db51580b6f257612346cd9
gisinka <mrxenonmr@gmail.com>
2020-09-17 17:20:43 +0500

    task 2.2 solved

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -1,21 +1,28 @@
 package com.example.task02;
 
+import static java.lang.Long.parseLong;
+
 public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long value = parseLong(input);
+        if (isInBounds(Byte.MIN_VALUE, Byte.MAX_VALUE, value))
+            return "byte";
+        if (isInBounds(Short.MIN_VALUE, Short.MAX_VALUE, value))
+            return "short";
+        if (isInBounds(Integer.MIN_VALUE, Integer.MAX_VALUE, value))
+            return "int";
+        return "long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
+    }
+
+    private static boolean isInBounds(long lowerBound, long upperBound, long value) {
+        return value >= lowerBound && value <= upperBound;
     }
 
 }
