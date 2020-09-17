06c48ea609714d91b193eb07e28521b66f2e7d45
petrdiukin <petr.diukin@urfu.me>
2020-09-17 12:41:25 +0500

    Дюкин Петр. Раздел 2, задача 3

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -7,12 +7,7 @@ public class Task03 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = getMetreFromCentimetre(345);
-        System.out.println(result);
-         */
+
     }
 
 }
