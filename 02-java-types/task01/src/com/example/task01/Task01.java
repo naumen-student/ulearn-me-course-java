10f682e8bc231fbc9cb367a625a783c543a19f96
BoberPro <34961963+BoberPro@users.noreply.github.com>
2020-09-17 15:15:16 +0500

    Худяков Сергей Задача 0.1. модуль 2

--- 02-java-types/task01/src/com/example/task01/Task01.java
+++ 02-java-types/task01/src/com/example/task01/Task01.java
@@ -10,12 +10,8 @@ public class Task01 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution();
         System.out.println(result);
-         */
     }
 
 }
