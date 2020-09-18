cafe86115ccfcaf40aa3a52506c9f2adaa32a346
bearsbon <bearsbon@gmail.com>
2020-09-18 16:19:37 +0500

    Revert "Задание 2.4 Долматов"
    This reverts commit 3418eb9c

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -4,21 +4,18 @@ public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
-        float num1 = a, num2 = b;
+        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        if (operation == "+") return num1+num2;
-        else if (operation == "-") return num1-num2;
-        else if (operation == "/") return num1/num2;
-        else if (operation == "*") return num1*num2;
-        else return 0;
+        return 0;
     }
 
     public static void main(String[] args) {
-
+        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-
+        */
     }
 
 }
-
