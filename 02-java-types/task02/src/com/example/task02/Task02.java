7260896f836b3b039c14cc10dc4c839cc796beb7
Dima <d.d.demin@urfu.me>
2020-09-18 15:13:19 +0500

    02_Task02 is finished

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -4,10 +4,16 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long number = Long.parseLong(input);
+        if (Byte.MIN_VALUE <= number && number <= Byte.MAX_VALUE)
+            return "byte";
+        if (Short.MIN_VALUE <= number && number <= Short.MAX_VALUE)
+            return "short";
+        if (Integer.MIN_VALUE <= number && number <= Integer.MAX_VALUE)
+            return "int";
+        return "long";
     }
+
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
