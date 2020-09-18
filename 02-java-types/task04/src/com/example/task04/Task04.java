d934f96313726ed439019347380baa5a79efb473
517v3r <o-gybanov@mail.ru>
2020-09-18 21:29:16 +0500

    Губанов Олег. Задание 2.4

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -1,18 +1,16 @@
 package com.example.task04;
 
-import sun.tools.jstat.Operator;
-
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
         if (operation.equals("+"))
-            return a+b;
+            return a + b;
         if (operation.equals("-"))
-            return a-b;
+            return a - b;
         if (operation.equals("*"))
-            return a*b;
+            return a * b;
         if (operation.equals("/"))
-            return (float) a/b;
+            return (float) a / b;
         return 0;
     }
 
