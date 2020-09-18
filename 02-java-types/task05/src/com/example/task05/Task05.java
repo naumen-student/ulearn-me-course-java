1b191b828c870fd82c969f9ac4e15d9631095dff
bearsbon <bearsbon@gmail.com>
2020-09-18 16:18:09 +0500

    Задание 2.5 Долматов

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,18 +3,23 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
+
         while (x > 0) {
-            if (x % 10 % 2 != 0 ) return "FALSE";
+            if (x % 10 % 2 != 0) {
+                return "FALSE";
+            }
             x /= 10;
         }
         return "TRUE";
     }
 
     public static void main(String[] args) {
-
+        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
         String result = solution(1234);
         System.out.println(result);
-
+        */
     }
 
 }
