a0c41ac66fe8dad36db1de719c13ec1d422a92af
bearsbon <bearsbon@gmail.com>
2020-09-18 16:20:03 +0500

    Revert "Задание 2.5 Долматов"

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -14,12 +14,10 @@ public class Task05 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
