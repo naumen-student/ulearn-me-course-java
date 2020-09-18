57c7cd6be7082635235582eb94a05988904fa5e3
dmitry <dmitry.sushkov2000@mail.ru>
2020-09-18 22:11:32 +0500

    Сушков Дмитрий,задание 2.7

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -4,18 +4,14 @@ public class Task07 {
 
     public static int solution(int n, int m, int k) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int onSideN = n / k;
+        int onSideM = m / k;
+        int tents = onSideN * onSideM;
+        return tents;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(10, 20, 5);
-        System.out.println(result);
-        */
+
     }
 
 }
