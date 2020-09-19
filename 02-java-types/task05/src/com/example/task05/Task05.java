6890eeba3754d82fe92137bc4575776f2185cdd9
viha4ka <vikusha2012@mail.ru>
2020-09-19 14:45:39 +0500

    Буторина Виктория, Задания 2.4

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,19 +3,22 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
+        while (x != 0){
+            int c = x %10;
+            if(c % 2 !=0)
+                return "FALSE";
+            x = x/10;
+        }
+        return "TRUE";
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "FALSE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
