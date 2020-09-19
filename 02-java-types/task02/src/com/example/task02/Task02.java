847a42adcc2530251db6568f8c545adcc16058ca
Andrey Emelyanov <56556713+reshxs@users.noreply.github.com>
2020-09-19 15:27:00 +0500

    02-task02

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,19 +3,23 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+        long number = Long.parseLong(input);
+        if(number <= 127 && number >= -128) {
+            return "byte";
+        }
+        else if(number <= Math.pow(2 ,15) - 1 && number >= -1 * Math.pow(2, 15)){
+            return "short";
+        }
+        else if (number <= Math.pow(2, 31) - 1 && number >= -1 * Math.pow(2, 31)){
+            return "int";
+        }
+        return "long";
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
