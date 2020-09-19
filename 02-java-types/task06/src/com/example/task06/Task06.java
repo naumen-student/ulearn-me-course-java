d371529d3068e212e972496f2d8e7ae1a3c943cf
ReyyL <_Galaxy5500>
2020-09-19 21:34:19 +0500

    Лазарев Андрей. Задание 2.06

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,20 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int res = x + y;
+        int counter = 0;
+        while (res != 0) {
+            counter++;
+            res /= 10;
+        }
+        if (counter == 0)
+            return 1;
+        return counter;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
 
 }
