fa7feed2111c07eb33e1576f61befee08d262a8a
Елизавета Беломытцева <elzahaggard13@outlook.com>
2020-09-19 17:08:13 +0500

    Беломытцева Елизавета. Задание 2.13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -4,9 +4,7 @@ public class Task13 {
 
     public static char toUpperCase(char c) {
 
-        // TODO привести букву к верхнему регистру
-
-        return c;
+        return (char)(c - 32);
     }
 
     public static void main(String[] args) {
