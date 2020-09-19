489818f1c48c54576401f4dfc23bc8f36f022273
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-09-20 01:42:23 +0500

    Кузьмин Денис Задание 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -6,7 +6,15 @@ public class Task02 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "";
+        long x = Long.parseLong(input);
+        if (-128 <= x && x <= 127)
+            return "byte";
+        else if (-Math.pow(2,15) <= x && x <= Math.pow(2,15) - 1)
+            return "short";
+        else if (-Math.pow(2,31) <= x && x <= Math.pow(2,31) - 1)
+            return "int";
+        else
+            return "long";
     }
 
     public static void main(String[] args) {
