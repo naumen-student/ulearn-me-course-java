81c3999498d91cf6d0ecc58ae1cf1707c0c11bae
bearsbon <bearsbon@gmail.com>
2020-09-20 14:36:15 +0500

    Задание 2.14 Долматов

--- 02-java-types/task14/src/com/example/task14/Task14.java
+++ 02-java-types/task14/src/com/example/task14/Task14.java
@@ -5,18 +5,17 @@ public class Task14 {
 
     public static int reverse(int value) {
 
-        // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
+        int result = Integer.parseInt(new StringBuilder(Integer.toString(value)).reverse().toString());
 
-        return 0;
+
+        return result;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int result = reverse(345);
         System.out.println(result);
-         */
+
     }
 
 
