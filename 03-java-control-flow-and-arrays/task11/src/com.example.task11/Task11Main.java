7838ca7f9259a61b78345179ab491e2e2592f575
diukin9 <petr.diukin@urfu.me>
2020-09-20 20:42:01 +0500

    Дюкин Петр. 3.11

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -1,5 +1,8 @@
 package com.example.task11;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -12,7 +15,23 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null && arr.length > 1) {
+            int minIndex = getIndecesToReplace(arr);
+            int t = arr[0];
+            arr[0] = arr[minIndex];
+            arr[minIndex] = t;
+        }
     }
 
+    public static int getIndecesToReplace (int[] arr) {
+        int minValue = Integer.MAX_VALUE;
+        int minIndex = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= minValue) {
+                minValue = arr[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
 }
