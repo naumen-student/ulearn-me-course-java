553d4667b423e60e3fa210651199f4d96c33913b
Kirill <vvvw_00@mail.ru>
2020-09-20 14:44:07 +0500

    Ворожцов Кирилл, 2. Базовый синтаксис. Типы,Task04

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -1,21 +1,33 @@
 package com.example.task04;
 
+import com.github.javaparser.ast.expr.LambdaExpr;
+import jdk.dynalink.Operation;
+import jdk.nashorn.internal.ir.CallNode;
+
+import javax.swing.*;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 public class Task04 {
-
     public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        switch (operation)
+        {
+            case "*":
+                return  a*b;
+            case "/":
+                return 1.0f*a/b;
+            case "+":
+                return a+b;
+            case "-":
+                return a-b;
+            default:
+                throw new UnsupportedOperationException(operation + "not supported");
+        }
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        float result = calculate(-25, 5, "/");
-        System.out.println(result);
-        */
     }
 
 }
