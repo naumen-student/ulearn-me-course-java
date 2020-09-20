1a02cafa43b2a7ea654b9b929eaabafb54ee674e
AdIVIuH <42569158+AdIVIuH@users.noreply.github.com>
2020-09-20 21:17:11 +0300

    Квашнин Дмитрий. Задание 2.06

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -1,21 +1,22 @@
 package com.example.task06;
 
 public class Task06 {
-
     public static int solution(int x, int y) {
+        int sum = x + y;
+        if (sum == 0)
+            return 1;
+        int count = 0;
+        int integerPart = sum;
+        while (integerPart != 0) {
+            count++;
+            integerPart /= 10;
+        }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return count;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
-
 }
