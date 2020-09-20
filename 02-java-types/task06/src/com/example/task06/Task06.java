08717b008d1b01ad71191a953914304765717a46
kirillegorow <e.kirill.2001@gmail.com>
2020-09-20 21:44:42 +0500

    Task6 Егоров Кирилл

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,15 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int result = Integer.toString(x+y).length();
+        if (x+y < 0){
+            result -= 1;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
-        System.out.println(result);
-        */
+        System.out.print(solution(-111, -222));
     }
 
 }
