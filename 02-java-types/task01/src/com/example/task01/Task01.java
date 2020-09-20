d977649b1622ac681a6e96a12e63e198d1c83551
Chocolatelis <elizavetab123@mail.ru>
2020-09-20 17:20:39 +0500

    Быкова Елизавета Задание 2.1

--- 02-java-types/task01/src/com/example/task01/Task01.java
+++ 02-java-types/task01/src/com/example/task01/Task01.java
@@ -1,18 +1,11 @@
 package com.example.task01;
-
 public class Task01 {
-
     public static String solution() {
-
         return "Я думаю, быть программистом - это круто";
-
     }
 
     public static void main(String[] args) {
-
         String result = solution();
         System.out.println(result);
-
     }
-
 }
