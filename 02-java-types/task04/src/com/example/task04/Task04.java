efc031540c6f66f58d9f3ee08bcb5de0611d4aaf
djafar kurbanov <djafar.kurbanow@gmail.ru>
2020-09-20 19:19:43 +0500

    Курбанов Джафар. Задача 2.3

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -3,9 +3,14 @@ package com.example.task04;
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        if (operation.equals("+"))
+            return a+b;
+        if (operation.equals("-"))
+            return a-b;
+        if (operation.equals("/"))
+            return (float) a/b;
+        if (operation.equals("*"))
+            return a*b;
         return 0;
     }
 
