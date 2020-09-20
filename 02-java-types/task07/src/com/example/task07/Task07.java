cf86967d2e51d8440f26b02e214b80f672a44e10
djafar kurbanov <djafar.kurbanow@gmail.ru>
2020-09-20 20:24:36 +0500

    Курбанов Джафар. Задача 2.7

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -3,10 +3,12 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        /*int campingArea = n * m;
+        int tentSquare = k * k;
+        int tents = 0;
+        for(int i = 0; i < campingArea; i += tentSquare) tents++;
+        return tents;*/
+        return (n/k) * (m/k);
     }
 
     public static void main(String[] args) {
