1b19f7115333704a04d83bceb34bfe11f32366cc
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-09-20 12:20:55 +0500

    Update Task02.java

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -14,14 +14,4 @@ public class Task02 {
 	    return "int";
 	return "long";
     }
-
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
-    }
-
 }
