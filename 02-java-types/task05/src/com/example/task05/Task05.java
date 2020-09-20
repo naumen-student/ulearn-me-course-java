8762d3186f6295b5a1bd956a112e3677259a2af3
GallYana <71075410+GallYana@users.noreply.github.com>
2020-09-20 17:13:44 +0500

    Лесовская Яна, Задание 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,19 +3,21 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        String number = String.valueOf(x);
+        int length = number.length();
+        while(x % 2 == 0 && length > 1) {
+                x /= 10;
+                length--;
+        }
+        if(x % 2 == 0) return "TRUE";
         return "FALSE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
