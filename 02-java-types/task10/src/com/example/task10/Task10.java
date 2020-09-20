a92005a963c32b2ae1caf4a6e9b1a50110f84615
Kirill <vvvw_00@mail.ru>
2020-09-21 00:35:53 +0500

    Ворожцов Кирилл, 2. Базовый синтаксис. Типы,Task10

--- 02-java-types/task10/src/com/example/task10/Task10.java
+++ 02-java-types/task10/src/com/example/task10/Task10.java
@@ -1,15 +1,16 @@
 package com.example.task10;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
-
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
-
+         if ((!Float.isFinite(a) || !Float.isFinite(b)))
+            return  Float.compare(a,b)==0;
+        BigDecimal c = new BigDecimal(String.valueOf(a)).setScale(precision, RoundingMode.FLOOR);
+        BigDecimal d =new BigDecimal(String.valueOf(b)).setScale(precision,RoundingMode.FLOOR);
+        return c.floatValue()==d.floatValue();
     }
 
     public static void main(String[] args) {
