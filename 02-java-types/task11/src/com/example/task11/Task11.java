b72d6f46d112caa96bd4651fdb64d68d41cc3373
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-09-20 17:01:37 +0500

    Update Task11.java

--- 02-java-types/task11/src/com/example/task11/Task11.java
+++ 02-java-types/task11/src/com/example/task11/Task11.java
@@ -3,9 +3,6 @@ package com.example.task11;
 public class Task11 {
 
     public static float benefit(float sum, float percent) {
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
+        return (float) (sum * Math.pow(1.0 + percent, 12));
     }
 }
