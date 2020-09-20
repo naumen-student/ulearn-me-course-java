a3ea3888d6b01ea86e993c56ecc5bbcf151696dd
Andrew <tdr896@gmail.com>
2020-09-20 21:27:29 +0500

    Мусиенко Андрей. Задание 2.4

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -1,12 +1,22 @@
 package com.example.task04;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Task04 {
 
-    public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+    public static float calculate(int a, int b, String operation) throws OperationNotSupportedException {
+        switch (operation) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                throw new OperationNotSupportedException();
+        }
     }
 
     public static void main(String[] args) {
