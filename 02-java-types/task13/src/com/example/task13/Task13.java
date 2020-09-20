48f44a1cc0ef28b7251d47cb4f5b846dac989ae9
Danil Harlamtsev <harlamtsev.danil@gmail.com>
2020-09-20 17:30:26 +0500

    Харламцев Данил задача 2.13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -1,21 +1,24 @@
 package com.example.task13;
 
+import sun.nio.cs.UTF_8;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class Task13 {
 
     public static char toUpperCase(char c) {
-
-        // TODO привести букву к верхнему регистру
-
+        String str = Character.toString(c);
+        str = str.toUpperCase();
+        c = str.charAt(0);
         return c;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        char result = solution('x');
+        char result = toUpperCase('x');
         System.out.println(result);
-         */
     }
 
 }
