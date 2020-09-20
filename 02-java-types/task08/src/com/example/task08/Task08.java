830a0344f6324779e886e84d192dfd2066446e1f
AdIVIuH <42569158+AdIVIuH@users.noreply.github.com>
2020-09-20 21:34:01 +0300

    Квашнин Дмитрий. Задание 2.08

--- 02-java-types/task08/src/com/example/task08/Task08.java
+++ 02-java-types/task08/src/com/example/task08/Task08.java
@@ -1,7 +1,6 @@
 package com.example.task08;
 
 public class Task08 {
-
     public static boolean solution() {
         double x = Double.POSITIVE_INFINITY;
         return x == x + 1;
@@ -10,5 +9,4 @@ public class Task08 {
     public static void main(String[] args) {
         System.out.println(solution());
     }
-
 }
