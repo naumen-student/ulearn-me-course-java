490b3dad028b43fef18e033ecf959808dd8d02f6
kirillegorow <e.kirill.2001@gmail.com>
2020-09-20 21:25:24 +0500

    Task4 Егоров Кирилл

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -3,17 +3,15 @@ package com.example.task04;
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
-        switch (operation){
-            case "+":
-                return (float)a + (float)b;
-            case "-":
-                return (float)a - (float)b;
-            case "*":
-                return (float)a * (float)b;
-            case "/":
-                return (float)a / (float)b;
+        if (operation.equals("+")){
+            return (float)a + b;
+        } else if (operation.equals("-")){
+            return (float)a - b;
+        } else if (operation.equals("/")){
+            return (float)a / b;
+        } else{
+            return (float)a * b;
         }
-        return 0;
     }
 
     public static void main(String[] args) {
