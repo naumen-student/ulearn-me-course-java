3e1dede6999b77aae29b6324b686568684de6a9f
theonethei <57193492+theonethei@users.noreply.github.com>
2020-09-21 00:08:21 +0500

    Update Task02.java

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -4,9 +4,19 @@ public class Task02 {
 
     public static String solution(String input) {
 
+        long a = Long.parseLong(input);
+        if (a<=127 && a>=-128)
+            return "byte";
+        else
+            if (a>=Math.pow(-2,15) && a<-128 || a>127 && a<=Math.pow(2,15)-1)
+                return "short";
+            else
+                if (a>=Math.pow(-2,31) && a<Math.pow(-2,15) || a>Math.pow(2,15)-1 && a<=Math.pow(2,31)-1)
+                    return "int";
+                else
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        return "long";
 
-        return "";
     }
 
     public static void main(String[] args) {
