5eb51f76898d6f101d2f788c8ee9b0b5767d04f9
diukin9 <petr.diukin@urfu.me>
2020-09-20 19:27:19 +0500

    Дюкин Петр. 3.3

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -10,6 +10,11 @@ public class Task03Main {
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String[] days = new String[] {"такого дня недели не существует", "понедельник",
+                                      "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+        if (weekDaysNumber < 1 || weekDaysNumber > 7) {
+            return days[0];
+        }
+        else return days[weekDaysNumber];
     }
 }
