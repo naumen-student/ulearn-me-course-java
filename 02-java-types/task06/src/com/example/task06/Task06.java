577089f0daa3fe7f6a7e691adedf45be610a1e38
theonethei <57193492+theonethei@users.noreply.github.com>
2020-09-21 19:24:25 +0500

    Update Task06.java

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,10 +3,17 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int result = x+y;
+        int i=0;
+        for(int k = 0;result!=0;k++) {
+            result = result / 10;
+            i = i + 1;
+        }
+
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return 0;
+        return i;
     }
 
     public static void main(String[] args) {
