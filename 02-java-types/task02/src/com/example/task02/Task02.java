ac51aa4c777a1c4ce5596969055c7430c2b8dd0d
sharphurt <#ugs;37cxoi[97cpnx908z209>
2020-09-21 11:57:49 +0500

    Task 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,19 +3,59 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+        if (checkForByte(input))
+            return "byte";
+        if (checkForShort(input))
+            return "short";
+        if (checkForInt(input))
+            return "int";
+        if (checkForLong(input))
+            return "long";
+        return "Unknown type";
+    }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+    private static boolean checkForByte(String input) {
+        try {
+            Byte.parseByte(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
-        return "";
+    private static boolean checkForInt(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean checkForShort(String input) {
+        try {
+            Short.parseShort(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean checkForLong(String input) {
+        try {
+            Long.parseLong(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+        // например вот таk
         String result = solution("12345");
         System.out.println(result);
-         */
+
     }
 
 }
