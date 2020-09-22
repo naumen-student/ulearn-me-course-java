c9289e3572a73d8140fd5bddff6c1ed96e63d303
JokeI777 <danleo.2000@mail.ru>
2020-09-22 05:03:18 +0500

    Леонов Даниил, модуль 2, задание 2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,19 +3,12 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        return Math.abs(Long.parseLong(input)) - 1 > (long)Integer.MAX_VALUE ? "long" :
+                Math.abs(Integer.parseInt(input)) - 1 > (int)Short.MAX_VALUE ? "int" :
+                        Math.abs(Short.parseShort(input)) - 1 > (short)Byte.MAX_VALUE ? "short" : "byte";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
     }
 
 }
