c6a7c74d278ca420a74f8e677109d3ea8e565528
Жора <JoraKochkin@yandex.ru>
2020-09-22 23:50:05 +0500

    Кочкин Георгий 2-2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -15,12 +15,7 @@ public class Task02 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
+
     }
 
 }
