190cd7506898db16883154c7c81099be824ef101
JokeI777 <danleo.2000@mail.ru>
2020-09-22 10:53:26 +0500

    Леонов Даниил, модуль 2, задание 3

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -3,7 +3,7 @@ package com.example.task03;
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
-        return centimetre / 100;
+         return centimetre / 100;
     }
 
     public static void main(String[] args) {
