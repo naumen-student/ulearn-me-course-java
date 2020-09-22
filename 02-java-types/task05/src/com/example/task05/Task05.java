622f60df2f735c6db2de4def38978b1a6c950d8a
Жора <JoraKochkin@yandex.ru>
2020-09-23 01:31:46 +0500

    Кочкин Георгий 2-5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,19 +3,18 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        String string = Integer.toString(x);
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (c % 2 != 1)
+                return "TRUE";
+        }
         return "FALSE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution(1234);
         System.out.println(result);
-        */
     }
 
 }
