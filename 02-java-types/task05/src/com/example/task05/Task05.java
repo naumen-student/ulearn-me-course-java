b3aa6f0caef331fdc49c7a7debeac9b6505dd58b
Жора <JoraKochkin@yandex.ru>
2020-09-23 01:43:56 +0500

    Кочкин Георгий 2-5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -2,17 +2,21 @@ package com.example.task05;
 
 public class Task05 {
 
-    public static String solution(int x) {
-        String st = Integer.toString(x);
+    String st = Integer.toString(x);
         return "FALSE";
         for(int i=0;i<st.length();i++){
-            char c = st.charAt(i);
-            if (c=='1' || c=='3' || c=='5' || c=='7' || c=='9'){
-                    return "FALSE";
-            }
+        char c = st.charAt(i);
+        switch (c){
+            case '1':
+            case '3':
+            case '5':
+            case '7':
+            case '9':
+                return "FALSE";
         }
-        return "TRUE";
     }
+        return "TRUE";
+}
 
     public static void main(String[] args) {
         String result = solution(1234);
