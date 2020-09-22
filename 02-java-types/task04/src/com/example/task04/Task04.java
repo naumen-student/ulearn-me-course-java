2e8845c34440ffa141854d5ead353a4e8acbad4f
Олег Хейн <okhiein@mail.ru>
2020-09-22 13:34:03 +0500

    Хейн Олег 02_task04

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -1,21 +1,30 @@
 package com.example.task04;
 
+import org.graalvm.compiler.core.common.calc.FloatConvert;
+
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return 0;
+        switch (operation){
+            case "+": return (float) a+b;
+            case "-": return (float) a-b;
+            case "*": return (float) a*b;
+            case "/": return (float) a/b;
+            default: return 0;
+        }
+        //#return 0;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        float result = calculate(-25, 5, "/");
+
+        float result = calculate(-250, 5, "/");
         System.out.println(result);
-        */
+
     }
 
 }
