81b4e9432d8b525b886ad4cfc33d60b2a6283fd5
Arseny Smirnov <Arseny.Smirnov@urfu.me>
2020-09-22 22:20:31 +0500

    Смирнов Арсений 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -4,18 +4,19 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        long number = new Long(input);
 
-        return "";
+        if (number <= Byte.MAX_VALUE && number >= Byte.MIN_VALUE)
+            return "byte";
+        else if (number <= Short.MAX_VALUE && number >= Short.MIN_VALUE)
+            return "short";
+        else if (number <= Integer.MAX_VALUE && number >= Integer.MIN_VALUE)
+            return "int";
+        else return "long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
+
     }
 
 }
