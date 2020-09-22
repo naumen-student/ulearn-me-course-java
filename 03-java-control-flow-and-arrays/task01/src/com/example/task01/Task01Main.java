1d92e145691f5a6de0fb9b9ff1da31e65e52202c
sharphurt <#ugs;37cxoi[97cpnx908z209>
2020-09-22 11:42:01 +0500

    Task 3.1

--- 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
+++ 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
@@ -3,7 +3,6 @@ package com.example.task01;
 public class Task01Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
         /*
         boolean b = canGetDriverLicense(100);
         System.out.println(b);
