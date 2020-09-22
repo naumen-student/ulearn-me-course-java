532638777e44f17e5ba17e58763b1e50559081a0
Олег Хейн <okhiein@mail.ru>
2020-09-22 14:34:26 +0500

    Хейн Олег 02_task05

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -4,18 +4,30 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        String st = Integer.toString(x);
 
-        return "FALSE";
+        for(int i=0;i<st.length();i++){
+            char c = st.charAt(i);
+
+            switch (c){
+                case '1':
+                case '5':
+                case '3':
+                case '7':
+                case '9':
+                    return "FALSE";
+            }
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution(1234);
+
+        String result = solution(462);
         System.out.println(result);
-        */
+
     }
 
 }
