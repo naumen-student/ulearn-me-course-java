7d6512acd6f33c5f123c7b2327eb518b291eeacf
Олег Хейн <okhiein@mail.ru>
2020-09-22 09:44:12 +0500

    Хейн Олег Задание 2-3

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -5,17 +5,19 @@ public class Task03 {
     public static int getMetreFromCentimetre(int centimetre) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int met = (int) centimetre / 100;
 
-        return 0;
+
+        return met;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = getMetreFromCentimetre(345);
         System.out.println(result);
-         */
+
     }
 
 }
