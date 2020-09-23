f55f11f5713ac791a200647e5de38d22b35a7bf3
Kirill <vvvw_00@mail.ru>
2020-09-23 20:28:35 +0500

    Ворожцов Кирилл, Task 3.10

--- 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
+++ 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
@@ -1,5 +1,9 @@
 package com.example.task10;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -11,8 +15,12 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        IntStream stream = Arrays.stream(arr);
+        int min = stream.min().getAsInt();
+        for (int i = arr.length-1; i >=0 ; i--)
+            if (arr[i]==min)
+                return  i;
+        return  -1;
     }
 
 }
