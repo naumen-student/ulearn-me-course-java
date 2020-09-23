62971c92291817722ee2d161c482e3f6ae4014d1
andrewzvorygin <Andrei.Zvorygin@urfu.me >
2020-09-23 20:19:55 +0500

    Зворыгин Андрей Задача 2.3

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,16 +3,10 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-        long value = Long.parseLong(input);
-        if(value <= Byte.MAX_VALUE && value >= Byte.MIN_VALUE)
-            return "byte";
-        if(value <= Short.MAX_VALUE && value >= Short.MIN_VALUE)
-            return "short";
-        if(value <= Integer.MAX_VALUE && value >= Integer.MIN_VALUE)
-            return "int";
-        if(value <= Long.MAX_VALUE && value >= Long.MIN_VALUE)
-            return "long";
-        return "некорректный ввод";
+
+        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+
+        return "";
     }
 
     public static void main(String[] args) {
