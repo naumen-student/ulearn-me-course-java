8f21ea9e206df6de4e3a201c58671f046488846e
Kirill <vvvw_00@mail.ru>
2020-09-23 20:08:34 +0500

    Ворожцов Кирилл, Task 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -1,5 +1,7 @@
 package com.example.task08;
 
+import java.util.Arrays;
+
 public class Task08Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -14,7 +16,9 @@ public class Task08Main {
 
     static long mult(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        long result=1;
+        for (int j : arr) result *= j;
+        return arr.length==0?0:result;
     }
 
 }
