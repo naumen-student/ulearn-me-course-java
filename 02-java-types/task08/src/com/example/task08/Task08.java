aa7ee78e1d6a9063984f69b3be806fd3ee193e41
Жора <JoraKochkin@yandex.ru>
2020-09-23 16:41:04 +0500

    Кочкин Георгий Задание 2-8

--- 02-java-types/task08/src/com/example/task08/Task08.java
+++ 02-java-types/task08/src/com/example/task08/Task08.java
@@ -4,15 +4,11 @@ public class Task08 {
 
     public static boolean solution() {
         int x = 100;
-        double x = 1.0 / 0.0;
+        double x = Double.POSITIVE_INFINITY;
         return x == x + 1;
     }
 
     public static void main(String[] args) {
-
-
         System.out.println(solution());
-
     }
-
 }
