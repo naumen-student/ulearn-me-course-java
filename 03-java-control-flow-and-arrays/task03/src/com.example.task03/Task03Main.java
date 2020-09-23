4702bdcdb1242d21c00adc0b3068cecc01ed0123
Kirill <vvvw_00@mail.ru>
2020-09-23 18:09:43 +0500

    Ворожцов Кирилл, Task 3.3

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -1,5 +1,8 @@
 package com.example.task03;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+
 public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -10,6 +13,18 @@ public class Task03Main {
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        HashMap<Integer,String> dict = new HashMap<Integer,String>()
+        {{
+            put(1,"понедельник");
+            put(2,"вторник");
+            put(3,"среда");
+            put(4,"четверг");
+            put(5,"пятница");
+            put(6,"суббота");
+            put(7,"воскресенье");
+        }};
+        if (dict.containsKey(weekDaysNumber))
+            return dict.get(weekDaysNumber);
+        return "такого дня недели не существует";
     }
 }
