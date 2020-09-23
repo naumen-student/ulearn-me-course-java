a952e182c96cc0c680c9edec1f56f02b2bcd98ca
evanesenze <evanesenze@gamil.com>
2020-09-23 17:03:25 +0500

    Базуев Иван.02.Task06

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,16 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = x + y;
+        int res = 0;
+        do {
+            res++;
+            sum /= 10;
+        } while (sum != 0);
+        return res;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
-        System.out.println(result);
-        */
     }
 
 }
