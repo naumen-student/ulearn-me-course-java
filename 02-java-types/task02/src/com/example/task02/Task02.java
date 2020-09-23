82e3bcf661fa38f1df1d2f81175c944713840f98
And21rew <71344837+And21rew@users.noreply.github.com>
2020-09-23 23:10:47 +0500

    Update Task02.java

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -5,14 +5,19 @@ public class Task02 {
     public static String solution(String input) {
 
         long number = Long.parseLong(input);
+        
         if(number <= Byte.MAX_VALUE && number >= Byte.MIN_VALUE)
             return "byte";
+        
         if(number <= Short.MAX_VALUE && number >= Short.MIN_VALUE)
             return "short";
+        
         if(number <= Integer.MAX_VALUE && number >= Integer.MIN_VALUE)
             return "int";
+        
         if(number <= Long.MAX_VALUE && number >= Long.MIN_VALUE)
             return "long";
+        
         return "";
     }
 
