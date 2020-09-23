666fce3e32f694867e08db152ba785588683b143
Жора <JoraKochkin@yandex.ru>
2020-09-23 13:35:33 +0500

    Кочкин Георгий 2-8

--- 02-java-types/task08/src/com/example/task08/Task08.java
+++ 02-java-types/task08/src/com/example/task08/Task08.java
@@ -6,6 +6,7 @@ public class Task08 {
         // TODO измените тип и значение переменной x, чтобы возвращалось значение true
 
         int x = 100;
+        double x = Double.POSITIVE_INFINITY;
         return x == x + 1;
     }
 
