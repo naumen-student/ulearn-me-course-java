aad111f2a77ce9f6b14cc5ae5f441677952cbfae
Ayzhan Taalaybek <ayzhan312@live.ru>
2020-09-23 20:54:16 +0500

    2_task10

--- 02-java-types/task10/src/com/example/task10/Task10.java
+++ 02-java-types/task10/src/com/example/task10/Task10.java
@@ -4,13 +4,10 @@ public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
 
-        boolean result = (Float.isInfinite(a) && Float.isInfinite(b)
+        return (Float.isInfinite(a) && Float.isInfinite(b)
                 && !Float.isNaN(a + b)) // for +-inf
                 || (Float.isNaN(a) && Float.isNaN(b))
                 || (Math.abs(a - b) < Math.pow(10, -precision));
-
-        return result;
-
     }
 
     public static void main(String[] args) {
@@ -21,7 +18,6 @@ public class Task10 {
 
         boolean result = compare(sum, c, 2);
         System.out.println(result);
-
     }
 
 }
