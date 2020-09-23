32555eb6e3d16a1e4b4d0fa51671d69aab673724
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2020-09-23 19:03:23 +0500

    Зернюков Никита. Задача 2.3

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -2,20 +2,13 @@ package com.example.task03;
 
 public class Task03 {
 
-    public static int getMetreFromCentimetre(int centimetre) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+    public static int getMetreFromCentimetre(int centimetre){
+        return centimetre/100;
     }
 
+
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = getMetreFromCentimetre(345);
-        System.out.println(result);
-         */
+
     }
 
 }
