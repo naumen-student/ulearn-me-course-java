0f7164953cbe020a274359691bdff4f3cacb4b0c
theonethei <57193492+theonethei@users.noreply.github.com>
2020-09-23 22:47:29 +0500

    Update Task11.java

--- 02-java-types/task11/src/com/example/task11/Task11.java
+++ 02-java-types/task11/src/com/example/task11/Task11.java
@@ -3,13 +3,6 @@ package com.example.task11;
 public class Task11 {
 
     public static float benefit(float sum, float percent) {
-
-        // TODO исправьте функцию, чтобы избежать накопления ошибки
-
-        // Считаем проценты за год
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
         return (float)(sum * Math.pow(1.0+percent,12.0));
     }
 
