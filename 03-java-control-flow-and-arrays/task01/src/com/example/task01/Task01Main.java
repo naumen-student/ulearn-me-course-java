ef3c88d1e728ba4b9c8bb2907e2352e6527f7106
Kirill <vvvw_00@mail.ru>
2020-09-23 17:57:00 +0500

    Ворожцов Кирилл, Task 3.1

--- 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
+++ 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
@@ -15,7 +15,6 @@ public class Task01Main {
     static boolean canGetDriverLicense(int age) {
         if (age<=0)
             throw new InvalidParameterException("age<=0");
-
         return age>=18;
     }
 }
