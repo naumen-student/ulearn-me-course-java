12bc16209776aac6e50253a8f8fac6b65dc4b1ad
KozhevnikovAlexandr <kozhevnikov.alexandr8@yandex.ru>
2020-09-23 21:46:30 +0500

    Task 02.02 completed

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -4,7 +4,6 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
         if (Long.parseLong(input) == (byte)Long.parseLong(input))
             return "byte";
         if (Long.parseLong(input) == (short)Long.parseLong(input))
@@ -15,12 +14,6 @@ public class Task02 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String res  ult = solution("12345");
-        System.out.println(result);
-         */
     }
 
 }
