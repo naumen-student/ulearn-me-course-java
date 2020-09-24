1cde347c55fbea494da3d0322f2d2e933baf7645
djafar kurbanov <djafar.kurbanow@gmail.ru>
2020-09-24 22:21:45 +0500

    Курбанов Джафар Задача 2.13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -3,7 +3,7 @@ package com.example.task13;
 public class Task13 {
 
     public static char toUpperCase(char c) {
-        return toUpperCase(c);
+        return Character.toUpperCase(c);
     }
 
     public static void main(String[] args) {
