055370201df5770dd3d3730d20a1c81c01546d51
djafar kurbanov <djafar.kurbanow@gmail.ru>
2020-09-24 22:18:31 +0500

    Курбанов Джафар Задача 2.13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -3,9 +3,6 @@ package com.example.task13;
 public class Task13 {
 
     public static char toUpperCase(char c) {
-
-        // TODO привести букву к верхнему регистру
-
         return toUpperCase(c);
     }
 
