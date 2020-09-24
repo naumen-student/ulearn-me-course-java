3185a358b0082b9ea7c57d8d82872bd4bfc673c0
Protages <k.stif@mail.ru>
2020-09-24 14:10:39 +0500

    Трошин Вадим Задание 2.1

--- 02-java-types/task01/src/com/example/task01/Task01.java
+++ 02-java-types/task01/src/com/example/task01/Task01.java
@@ -1,21 +1,14 @@
 package com.example.task01;
+import System;
 
 public class Task01 {
 
     public static String solution() {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "здесь какая-то неправильная строка";
+        return "Я думаю, быть программистом - это круто";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution();
-        System.out.println(result);
-         */
+            System.out.println(solution());
     }
 
 }
