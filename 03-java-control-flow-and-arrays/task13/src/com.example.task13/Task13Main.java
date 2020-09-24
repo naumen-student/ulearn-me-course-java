8eda12d613c365cb4b6f24afa4f16ee26b3fb076
djafar kurbanov <djafar.kurbanow@gmail.ru>
2020-09-24 22:13:59 +0500

    Курбанов Джафар Задача 3.13

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -1,19 +1,36 @@
 package com.example.task13;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {9, 1100, 7, 8};
+        int[] arr = {1001, 1100, 7, 8};
         removeMoreThen1000(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        ArrayList <Integer> newArr = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 1000) {
+                newArr.add(arr[i]);
+            }
+        }
+        int[] res = new int[newArr.size()];
+        for (int i=0; i < res.length; i++)
+            res[i] = newArr.get(i);
+        return res;
+    }
+
+    public static int[] convertIntegers(ArrayList<Integer> integers)
+    {
+        int[] res = new int[integers.size()];
+        for (int i=0; i < res.length; i++)
+            res[i] = integers.get(i);
+        return res;
     }
 
 }
