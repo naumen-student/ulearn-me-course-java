99a3b3e4d29c18c16b6ae969d5fb8529dfe7173f
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-09-24 14:49:05 +0500

    Update Task05.java

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,19 +3,16 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while(true) {
+            if (x % 2 == 0) {
+                x /= 10;
+                if (0 < x)
+                    continue;
+                else
+                    return "TRUE";
+            }
+            else
+                return "FALSE";
+        }
     }
-
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
-        System.out.println(result);
-        */
-    }
-
 }
