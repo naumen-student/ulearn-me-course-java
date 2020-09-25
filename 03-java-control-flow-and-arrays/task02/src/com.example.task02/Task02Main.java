e085ababe91e37ca404447169640713243ddc9e3
Stanislav <youngfrezyx@gmail.com>
2020-09-26 01:15:24 +0300

    Ильин Станислав. Задача 3.2

--- 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
+++ 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
@@ -1,15 +1,23 @@
 package com.example.task02;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Task02Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getSeason(12));
-         */
+        System.out.println(getSeason(15));
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        List<String> seasons = Arrays.asList("весна","лето", "осень", "зима");
+        int offset = 1;
+        int seasonLengthInMonths = 3;
+        int seasonsCount = seasons.toArray().length;
+        for (int i = offset; i < seasonsCount + offset; i++){
+            int monthNumberLeftSide = (i * seasonLengthInMonths) -offset;
+            if(monthNumber > monthNumberLeftSide && monthNumber < monthNumberLeftSide+4)
+                return seasons.get(i-offset);
+        }
+        return seasons.get(seasonsCount-1);
     }
 }
