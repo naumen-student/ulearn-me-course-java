b23c3cb41eba7319a296b174f2f37754e2428c2d
djafar kurbanov <djafar.kurbanow@gmail.ru>
2020-09-25 20:12:47 +0500

    Курбанов Джафар. Задача 3.13

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -13,8 +13,8 @@ public class Task13Main {
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        if (arr == null)
-            return null;
+        if (arr == null || arr.length == 0)
+            return arr;
         ArrayList <Integer> newArr = new ArrayList<>();
         for (int j : arr) {
             if (j <= 1000) {
