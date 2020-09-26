8dec9776153d663ed0f827065f50a7e0a85d924a
Stanislav <youngfrezyx@gmail.com>
2020-09-26 20:42:26 +0300

    Ильин Станислав. Задача 3.12

--- 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
+++ 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
@@ -12,7 +12,17 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0) return;
+        for (int i = 0; i < arr.length; i++) {
+            int position = i;
+            int min = arr[i];
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[j] < min) {
+                    position = j;
+                    min = arr[j];
+                }
+            arr[position] = arr[i];
+            arr[i] = min;
+        }
     }
-
 }
