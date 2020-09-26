4ee7ab5dc1391aa8fa266e93a4700c5d29fdde6b
suppor14 <suppor14@yandex.ru>
2020-09-26 16:42:45 +0500

    Лицин Владислав. Задача 2.13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -3,19 +3,10 @@ package com.example.task13;
 public class Task13 {
 
     public static char toUpperCase(char c) {
-
-        // TODO привести букву к верхнему регистру
-
-        return c;
+        return Character.toUpperCase(c);
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        char result = solution('x');
-        System.out.println(result);
-         */
     }
 
 }
