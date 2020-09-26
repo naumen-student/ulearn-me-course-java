8f7626dc494b5501ac7e69fcd4a85b33e589465a
diukin9 <petr.diukin@urfu.me>
2020-09-26 18:03:32 +0500

    Дюкин Петр. 3.11

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -5,13 +5,7 @@ import java.util.Arrays;
 
 public class Task11Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
-        swap(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void swap(int[] arr) {
