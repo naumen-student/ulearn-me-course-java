8bb001563cba12c039941029644cca9501cffd24
Fuzuki <temniakovslava@yandex.ru>
2020-09-26 12:20:10 +0500

    Темняков Вячеслав. Задание 2.12

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -1,15 +1,12 @@
 package com.example.task12;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Task12 {
 
     public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
-
-        for (int i = 1; i <= 12; i++) {
-            sum = sum.add(sum.multiply(percent));
-        }
-        return sum.setScale(9,BigDecimal.ROUND_HALF_UP)
+        return sum.multiply((percent.add(new BigDecimal(1.0))).pow(12)).setScale(9, RoundingMode.HALF_UP);
     }
 
     public static void main(String[] args) {
