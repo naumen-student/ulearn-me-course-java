aaecd014ddd9c281ec18e98eaaaad79abd1827df
diukin9 <petr.diukin@urfu.me>
2020-09-26 18:01:19 +0500

    Дюкин Петр. 3.5

--- 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
+++ 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
@@ -2,11 +2,7 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getMin(5,4,3));
-         */
+
     }
 
     static int getMin(int a, int b, int c) {
