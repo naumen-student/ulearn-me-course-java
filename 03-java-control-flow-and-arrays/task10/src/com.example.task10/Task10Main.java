459477cf76e388a87949a372a2bae0feadaa2c64
RoyalPiano <zelyoniyoleg@yandex.ru>
2020-09-26 16:17:49 +0500

    task3.1

--- 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
+++ 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
@@ -1,5 +1,7 @@
 package com.example.task10;
 
+import java.util.Arrays;
+
 public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -11,8 +13,13 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int minIndex = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int newNum = arr[i];
+            if ((newNum <= arr[minIndex])) {
+                minIndex = i;
+            }
+        }
+        return minIndex;
     }
-
 }
