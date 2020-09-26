c55831cd4d9d485c08e88f3902427e56e1b05671
Chocolatelis <elizavetab123@mail.ru>
2020-09-26 19:13:04 +0500

    Быкова Елизавета Задание 2.12

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -6,18 +6,11 @@ public class Task12 {
 
     public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
 
-        // TODO раскомментируйте и исправьте код
-
-        // Считаем проценты за год
-
-        /*
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+            sum = sum.add(sum.multiply(percent));
         }
-        return sum;
-        */
+        return sum.setScale(9, BigDecimal.ROUND_HALF_UP);
 
-        return BigDecimal.ZERO;
     }
 
     public static void main(String[] args) {
