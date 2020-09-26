349a04a1e1940f4fa65f28b708b4e74d66404aeb
andrewzvorygin <Andrei.Zvorygin@urfu.me >
2020-09-26 09:18:30 +0500

    Зворыгин Андрей Задача 3.02

--- 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
+++ 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
@@ -2,14 +2,16 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getSeason(12));
-         */
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
+        if(1 <= monthNumber && monthNumber <=2 || monthNumber == 12)
+            return "зима";
+        if(3 <= monthNumber && monthNumber <=5)
+            return "весна";
+        if(6 <= monthNumber && monthNumber <=8)
+            return "лето";
+        if(9 <= monthNumber && monthNumber <=11)
+            return "осень";
+        return "";}
 }
