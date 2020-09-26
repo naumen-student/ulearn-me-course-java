61bba4dbe665aaea074425773ed34fc95176b643
diukin9 <petr.diukin@urfu.me>
2020-09-26 18:00:56 +0500

    Дюкин Петр. 3.4

--- 03-java-control-flow-and-arrays/task04/src/com.example.task04/Task04Main.java
+++ 03-java-control-flow-and-arrays/task04/src/com.example.task04/Task04Main.java
@@ -2,11 +2,7 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        from0to10000();
-         */
+
     }
 
     static void from0to10000() {
