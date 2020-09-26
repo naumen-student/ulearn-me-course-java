0d268b51013ba4a47e42ab67cceee491991e81b9
Stanislav <youngfrezyx@gmail.com>
2020-09-26 12:31:26 +0300

    Ильин Станислав. Задача 3.3

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -1,15 +1,23 @@
 package com.example.task03;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task03Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         System.out.println(getNameOfWeekDays(7));
-         */
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String response = "такого дня недели не существует";
+        Map<Integer, String> days = new HashMap<Integer, String>(){{
+            put( 1 , "понедельник"); put( 2 , "вторник");
+            put( 3 , "среда");       put( 4 , "четверг");
+            put( 5 , "пятница");     put( 6 , "суббота");
+            put( 7 , "воскресенье");
+        }};
+        if (days.containsKey(weekDaysNumber))
+            response = days.get(weekDaysNumber);
+        return response;
     }
 }
