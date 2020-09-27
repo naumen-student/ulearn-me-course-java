502657147ad03230a35303189080acd9ae18a45b
dnrkvn <diyllina@mail.ru>
2020-09-27 16:41:35 +0500

    Хабибуллина Диана. Задача 2.7

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -3,19 +3,13 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int side1 = n / k;
+        int side2 = m / k;
+        return side1 * side2;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(10, 20, 5);
         System.out.println(result);
-        */
     }
-
 }
