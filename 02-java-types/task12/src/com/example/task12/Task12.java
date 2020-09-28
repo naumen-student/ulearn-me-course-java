6a84e6ac3898062864159cc525020f78879f497e
kirillegorow <e.kirill.2001@gmail.com>
2020-09-28 17:22:46 +0500

    Task12 Егоров Кирилл

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -5,19 +5,9 @@ import java.math.BigDecimal;
 public class Task12 {
 
     public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
-
-        // TODO раскомментируйте и исправьте код
-
-        // Считаем проценты за год
-
-        /*
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
-        */
-
-        return BigDecimal.ZERO;
+        for (int i = 1; i <= 12; i++)
+            sum = sum.add(sum.multiply(percent));
+        return sum.setScale(9,BigDecimal.ROUND_HALF_UP);
     }
 
     public static void main(String[] args) {
