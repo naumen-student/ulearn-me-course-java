cdd6aedc7afb5e39e1a4a3ba5b0d21a78f0d8ad6
sharphurt <#ugs;37cxoi[97cpnx908z209>
2020-09-28 11:13:05 +0500

    Task 3.12

--- 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
+++ 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
@@ -12,7 +12,17 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
+        if (arr == null || arr.length == 0)
+            return;
 
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
