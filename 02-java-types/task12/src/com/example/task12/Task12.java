036faf3f1bde50294d32228164dc6b07e4f91b3a
dnrkvn <diyllina@mail.ru>
2020-09-28 20:02:56 +0500

    Хабибуллина Диана. Задача 2.12

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -5,19 +5,11 @@ import java.math.BigDecimal;
 public class Task12 {
 
     public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
-
-        // TODO раскомментируйте и исправьте код
-
-        // Считаем проценты за год
-
-        /*
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+            sum = sum.add(sum.multiply(percent));
         }
-        return sum;
-        */
 
-        return BigDecimal.ZERO;
+        return sum.setScale(9,BigDecimal.ROUND_HALF_UP);
     }
 
     public static void main(String[] args) {
@@ -28,7 +20,5 @@ public class Task12 {
         sum = benefit(sum, percent);
 
         System.out.println("Сумма на счете через год: " + sum);
-
     }
-
 }
