90d5126c7fe156d2b1aa7f035f2b782899d86c3f
Dima <d.d.demin@urfu.me>
2020-09-28 18:44:55 +0500

    03-task13 is finished

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -1,19 +1,23 @@
 package com.example.task13;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task13Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        return arr == null || arr.length == 0 ? arr
+                : Arrays.stream(arr).filter(number -> number < 1000).toArray();
+//        ArrayList<Integer> numbers = new ArrayList<Integer>();
+//        for (int i : arr){
+//            if (i < 1000)
+//                numbers.add(i);
+//        }
+//        return numbers.stream().mapToInt(e -> e).toArray();
     }
 
 }
