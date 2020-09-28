b3216986355c750e6a5b4f5211cc5edfbb2ba602
And21rew <Andrei.Bubnov@at.urfu.ru>
2020-09-28 22:49:42 +0500

    Бубнов Андрей. Задание 3.6

--- 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
+++ 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
@@ -1,17 +1,16 @@
 package com.example.task06;
 
+import java.util.Arrays;
+
 public class Task06Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getMax(1, 2, 3, 4));
-         */
+        System.out.println(getMax(8, 3, 1, 22));
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] numbers = {a, b, c, d};
+        Arrays.sort(numbers);
+        return numbers[3];
     }
 
 }
