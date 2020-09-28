32cafe466c3e13603d229dba77eb10d77e4d4bf3
grekova-web <71182152+grekova-web@users.noreply.github.com>
2020-09-28 21:23:42 +0500

    Грекова Елена. Задание 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -13,8 +13,12 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        if (arr.length == 0) return 0;
+        long result = 1;
+        for (int i=0; i < arr.length; i++) {
+            result = result * arr[i];
+        }
+        return result;
     }
 
 }
