5b19879d5cbb312345b31eba5ef43d8b19f86e17
Сухраб <Sukhrab2001@mail.ru>
2020-09-29 00:57:58 +0500

    Курбанов Сухраб, задача 2 07

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -3,10 +3,9 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int blindCountVertical = n/k;
+        int blindCountHorisontal = m/k;
+        return blindCountHorisontal*blindCountVertical;
     }
 
     public static void main(String[] args) {
