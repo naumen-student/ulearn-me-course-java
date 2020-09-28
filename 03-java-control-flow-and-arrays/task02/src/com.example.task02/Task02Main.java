9128f251fefe4c3c3946794d730254b9333e3086
sharphurt <#ugs;37cxoi[97cpnx908z209>
2020-09-28 10:32:12 +0500

    Task 3.2

--- 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
+++ 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
@@ -10,6 +10,16 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (inBetween(monthNumber, 3, 5))
+            return "весна";
+        if (inBetween(monthNumber, 6, 8))
+            return "лето";
+        if (inBetween(monthNumber, 9, 11))
+            return "весна";
+        return "зима";
+    }
+
+    private static boolean inBetween(int value, int start, int end){
+        return value >= start && value <= end;
     }
 }
