b987ffe5dbe453ae23203b987d3fe267c7ed9ef1
gisinka <mrxenonmr@gmail.com>
2020-09-28 14:23:36 +0500

    task3.13 solved

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -12,8 +12,22 @@ public class Task13Main {
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if (arr == null) return null;
+        int[] newArr = new int[arr.length - countMoreThen1000(arr)];
+        int i = 0;
+        for (int item: arr) {
+            if (item <= 1000)
+                newArr[i++] = item;
+        }
+        return  newArr;
+    }
+
+    static int countMoreThen1000(int[] arr){
+        int number = 0;
+        for (int item: arr) {
+           if (item > 1000) number++;
+        }
+        return number;
     }
 
 }
