ecb5d12866b4ee36b827095122def4db8790209d
chek539 <chek539camo@gmail.com>
2020-09-28 12:21:46 +0500

    Лазарев Иван. Задание 2.06

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,19 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int sum = (x + y) / 10;
+        int count = 1;
+        while (sum != 0)
+        {
+            count++;
+            sum /= 10;
+        }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return count;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
-        System.out.println(result);
-        */
+
     }
 
 }
