734846b3f5096b3c622b53e8f40031405d209180
Сухраб <Sukhrab2001@mail.ru>
2020-09-28 21:06:29 +0500

    Курбанов Сухраб, задача 2 02

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -5,7 +5,15 @@ public class Task02 {
     public static String solution(String input) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        long a = Long.parseLong(input);
+        if (a <= Byte.MAX_VALUE && a >= Byte.MIN_VALUE)
+            return "byte";
+        if (a <= Short.MAX_VALUE && a >= Short.MIN_VALUE)
+            return "short";
+        if (a <= Integer.MAX_VALUE && a >= Integer.MIN_VALUE)
+            return "int";
+        if (a <= Long.MAX_VALUE && a >= Long.MIN_VALUE)
+            return "long";
         return "";
     }
 
