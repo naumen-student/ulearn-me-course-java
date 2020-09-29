3b116f5bcfbf5ad06964b2706565ead462043667
theonethei <57193492+theonethei@users.noreply.github.com>
2020-09-29 12:35:26 +0500

    Update Task01Main.java

--- 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
+++ 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
@@ -11,6 +11,9 @@ public class Task01Main {
     }
 
     static boolean canGetDriverLicense(int age) {
-        return true;//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(age >= 18)
+            return true;
+        else
+            return false;//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
