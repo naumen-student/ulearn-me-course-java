afbfd6b293406b43340681924ac7010d9f021092
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-09-30 00:38:48 +0500

    Update Task03Main.java

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -1,15 +1,8 @@
 package com.example.task03;
 
 public class Task03Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getNameOfWeekDays(7));
-         */
-    }
-
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String[] week = new String[] { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+        return week[weekDaysNumber - 1];
     }
 }
