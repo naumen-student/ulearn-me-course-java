f4cb9f1ed68f6d679661ae6d9fe4addd27587409
Danil Harlamtsev <harlamtsev.danil@gmail.com>
2020-09-29 21:33:16 +0500

    Харламцев Данил задача 3.9

--- 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
+++ 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
@@ -14,8 +14,11 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int minimum = Integer.MAX_VALUE;
+        for (int num: arr)
+            if (num < minimum)
+                minimum = num;
+        return minimum;
     }
 
 }
