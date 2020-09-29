dcf1a08be83e6988c54522f836ec9078c347df14
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-09-29 21:48:48 +0500

    Update Task13Main.java

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -1,18 +1,11 @@
 package com.example.task13;
 
 public class Task13Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
-    }
-
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        int[] result;
+        for (int i = 0; i < arr.length; i++){
+            arr
+        }
         return  null;
     }
 
