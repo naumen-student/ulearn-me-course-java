227f31db58b7e64a85df2e8afab2c4121c1764af
Andrey Emelyanov <56556713+reshxs@users.noreply.github.com>
2020-09-29 19:47:14 +0400

    03-task05

--- 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
+++ 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
@@ -10,7 +10,9 @@ public class Task05Main {
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        if (a >= b) {
+            return Math.min(b, c);
+        }
+        return Math.min(a, c);
     }
 }
