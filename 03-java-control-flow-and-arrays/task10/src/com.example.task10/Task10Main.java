56b756a9283a4020a144c116f090e7702ff96a52
Danil Harlamtsev <harlamtsev.danil@gmail.com>
2020-09-29 21:53:52 +0500

    Харламцев Данил задача 3.10

--- 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
+++ 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,15 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int index = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+        return index;
     }
 
 }
