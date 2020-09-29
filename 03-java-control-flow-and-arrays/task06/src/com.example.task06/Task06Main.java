3778c0e37baba545f454be655c8e5ff6e23daa01
Andrey Emelyanov <56556713+reshxs@users.noreply.github.com>
2020-09-29 20:10:21 +0400

    03-task06

--- 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
+++ 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
@@ -1,5 +1,6 @@
 package com.example.task06;
 
+
 public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -10,8 +11,16 @@ public class Task06Main {
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+
+        // Не бейте пожайлуста за такое решение, я понимаю что через условия будет быстрее
+
+        int max = Integer.MIN_VALUE;
+        for(int current : new int[] {a, b, c, d}) {
+            if (current > max) {
+                max = current;
+            }
+        }
+        return max;
     }
 
 }
