9ebe50682a1d502ea9e3222a8a64b83cb2e64336
Andrey Emelyanov <56556713+reshxs@users.noreply.github.com>
2020-09-29 22:30:53 +0400

    03-task12

--- 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
+++ 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
@@ -11,8 +11,38 @@ public class Task12Main {
          */
     }
 
+    static int numMin(int[] arr, int start_index) {
+        int min = Integer.MAX_VALUE;
+        int index = -1;
+        for(int i = start_index; i < arr.length; i++){
+            if(arr[i] <= min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+    static void swap(int[] arr, int index) {
+        if (arr == null){
+            return;
+        }
+        int min_index = numMin(arr, index);
+        if (min_index > 0) {
+            int temporary = arr[min_index];
+            arr[min_index] = arr[index];
+            arr[index] = temporary;
+        }
+    }
+
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            swap(arr, i);
+        }
     }
 
 }
