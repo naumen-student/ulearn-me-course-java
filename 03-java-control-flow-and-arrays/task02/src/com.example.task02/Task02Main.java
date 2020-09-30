1299217da11db8ea294b6c21452f8026d0d08719
BoberPro <34961963+BoberPro@users.noreply.github.com>
2020-09-30 19:04:53 +0500

    Худяков Сергей. Задача 3.2

--- 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
+++ 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
@@ -10,6 +10,18 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if ((monthNumber >= 1 && monthNumber <= 2) || monthNumber == 12) {
+            return "зима";
+        }
+        if (monthNumber >= 3 && monthNumber <= 5) {
+            return "весна";
+        }
+        if (monthNumber >= 6 && monthNumber <= 8) {
+            return "лето";
+        }
+        if (monthNumber >= 9 && monthNumber <= 11) {
+            return "осень";
+        }
+        return null;
     }
 }
