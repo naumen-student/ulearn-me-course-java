5b75e9045b0074b5909b16a35764bc70abd43bbe
JIiCeHoK <foxs29546@gmail.com>
2020-09-30 12:22:11 +0500

    Моисеев Михаил. Задача 2.02

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,19 +3,22 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long num = Long.parseLong(input);
+        if (num <= Integer.MAX_VALUE && num >= Integer.MIN_VALUE)
+            return ("int");
+        if (num <= Byte.MAX_VALUE && num >= Byte.MIN_VALUE)
+            return ("byte");
+        if (num <= Short.MAX_VALUE && num >= Short.MIN_VALUE)
+            return ("short");
+        return ("long");
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите число ");
+        String num = console.nextLine();
+        String result = solution(num);
+        System.out.println("Тип ");
         System.out.println(result);
-         */
     }
-
 }
