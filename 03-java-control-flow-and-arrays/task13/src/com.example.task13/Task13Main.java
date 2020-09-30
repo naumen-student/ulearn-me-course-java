74ac7cc4cc6b01327acf1748453da67b3aeab90f
dariasavvina <dariasavvina@mail.ru>
2020-09-30 13:27:52 +0500

    Саввина Дарья. Задача 13.

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -2,18 +2,38 @@ package com.example.task13;
 
 public class Task13Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int[] arr = {9, 1100, 7, 8};
         removeMoreThen1000(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if (arr == null || arr.length == 0)
+            return arr;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 1000) {
+                continue;
+            }
+            count++;
+        }
+        int[] result = new int[count];
+        int amount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 1000) {
+                amount += 1;
+                continue;
+            }
+            result[i - amount] = arr[i];
+        }
+        return result;
     }
-
 }
+
+
+
+
+
+
