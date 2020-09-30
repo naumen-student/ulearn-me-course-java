c0a466173b9bddea8f84132b0cda130d750eacbc
Andrey Emelyanov <56556713+reshxs@users.noreply.github.com>
2020-09-30 12:20:27 +0400

    03-task13

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -11,9 +11,35 @@ public class Task13Main {
          */
     }
 
+    private static int countLessThen1000(int[] arr){
+        int count = 0;
+
+        for (int number: arr) {
+            if (number <= 1000) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if (arr == null) {
+            return arr;
+        }
+
+        int length = countLessThen1000(arr);
+        int[] result = new int[length];
+        int index = 0;
+
+        for (int number: arr) {
+            if (number <= 1000) {
+                result[index] = number;
+                index++;
+            }
+        }
+
+        return result;
     }
 
 }
