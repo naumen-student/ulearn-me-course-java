d961999dde82f48c87b0658f26730fd3556077ea
arturha4 <sadbooys.2001@gmail.com>
2020-09-30 14:51:48 +0500

    Давлетшин Артур. Задание 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,26 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int res=x+y;
+        int k =0;
+        if (x+y==0){
+            return 1;
+        }
+        else {
+            while (res != 0) {
+                res = res / 10;
+                k += 1;
+            }
+        }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return k;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
