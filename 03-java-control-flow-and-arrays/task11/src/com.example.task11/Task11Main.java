12061b946fa46349c809f6174ff67cfe9f7033a6
BoberPro <34961963+BoberPro@users.noreply.github.com>
2020-09-30 20:54:16 +0500

    Худяков Сергей. Задача 3.11

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -2,17 +2,29 @@ package com.example.task11;
 
 public class Task11Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        int c = Integer.MAX_VALUE;
+        int pozition = 0;
+        if (arr == null || arr.length == 0)
+            return;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < c) {
+                c = arr[i];
+                pozition = i;
+            }
+            if (i + 1 == arr.length) {
+                int b = arr[0];
+                arr[0] = arr[pozition];
+                arr[pozition] = b;
+            }
+        }
     }
 
 }
