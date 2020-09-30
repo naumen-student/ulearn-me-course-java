2ec56cc3ef5d77f7032243a8415ba053faf7bcb6
BoberPro <34961963+BoberPro@users.noreply.github.com>
2020-09-30 21:20:40 +0500

    Худяков Сергей. Задача 3.12

--- 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
+++ 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
@@ -2,17 +2,39 @@ package com.example.task12;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0)
+            return;
+        int min = Integer.MAX_VALUE;
+        int count = 0;
+        int pozition = 0;
+        int i = 0;
+        for (; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                pozition = i;
+            }
+            if (i + 1 == arr.length) {
+                arr[pozition] = arr[count];
+                arr[count] = min;
+                count++;
+                min = Integer.MAX_VALUE;
+                i = count;
+            }
+            if (arr[arr.length - 2] > arr[arr.length - 1]) {
+                int k = arr[arr.length - 2];
+                int dz = arr[arr.length - 1];
+                arr[arr.length - 1] = k;
+                arr[arr.length - 2] = dz;
+            }
+        }
     }
 
 }
