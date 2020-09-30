0f44d566bcc017e58d1910a9acd88b9a1c7a9d3c
Елизавета Беломытцева <elzahaggard13@outlook.com>
2020-09-30 22:17:47 +0500

    Беломытцева Елизавета. Задание 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -1,5 +1,7 @@
 package com.example.task08;
 
+import java.util.Arrays;
+
 public class Task08Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -13,8 +15,16 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
-    }
 
+        if (arr.length == 0) {
+            return 0;
+        }
+
+        long sum = 1;
+        for (int e : arr) {
+            sum *= e;
+        }
+
+        return sum;
+    }
 }
