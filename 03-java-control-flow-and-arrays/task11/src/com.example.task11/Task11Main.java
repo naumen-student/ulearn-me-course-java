bb9fb7a2500e368959d812380377c1f02c62109a
justvaclav <just.vaclav@gmail.com>
2020-09-30 21:06:29 +0500

    Шимонек Вацлав, задача 3.11

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -8,6 +8,8 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return;
         int minNum = 0;
         for (int i = 0; i < arr.length; i++)
             if (arr[i] <= arr[minNum]) {
