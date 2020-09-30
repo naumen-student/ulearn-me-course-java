b65e9b95d71b3db712f63d9373d9b5f9c8ae962f
Елизавета Беломытцева <elzahaggard13@outlook.com>
2020-09-30 22:35:19 +0500

    Беломытцева Елизавета. Задание 3.11

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -1,5 +1,7 @@
 package com.example.task11;
 
+import java.util.Arrays;
+
 public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -12,7 +14,21 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
+        if (arr == null || arr.length == 0) {
+            return;
+        }
 
+        int minNumber = Integer.MAX_VALUE;
+        int indexMinNumber = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= minNumber) {
+                minNumber = arr[i];
+                indexMinNumber = i;
+            }
+        }
+
+        arr[indexMinNumber] = arr[0];
+        arr[0] = minNumber;
+    }
 }
