2ea91425bbfef730a83d5a20c5d8c16a8240914c
JIiCeHoK <foxs29546@gmail.com>
2020-09-30 17:49:53 +0500

    Моисеев Михаил. Задача 2.04

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -25,10 +25,10 @@ public class Task04 {
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
+
     }
 
 }
