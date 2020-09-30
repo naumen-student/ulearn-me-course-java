89bcc53007b5a59fd245b11dabfebb899cdce42b
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2020-10-01 01:26:04 +0500

    Мусиенко Андрей. Задание 3.2

--- 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
+++ 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
@@ -2,20 +2,6 @@ package com.example.task02;
 
 public class Task02Main {
 
-    private  static String[] months= new String[]{
-            "Январь",
-            "Февраль",
-            "Март",
-            "Апрель",
-            "Май",
-            "Июнь",
-            "Июль",
-            "Август",
-            "Сентябрь",
-            "Октябрь",
-            "Ноябрь",
-            "Декабрь"
-    };
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
@@ -25,6 +11,6 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return months[monthNumber-1];//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        return (monthNumber<=2||monthNumber==12)?"зима":(monthNumber<=5)?"весна":(monthNumber<=8)?"лето":"осень";
     }
 }
