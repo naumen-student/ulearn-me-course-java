f82d265a8a911f09c565fd79b02748f57802fafa
Андрей <andryfka_3rd@mail.ru>
2020-10-01 03:13:57 +0500

    Тыщенко Андрей. Task 3_06

--- 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
+++ 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
@@ -9,9 +9,9 @@ public class Task06Main {
          */
     }
 
-    static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+    static int getMax(int a, int b, int c, int d)
+    {
+        return Math.max(Math.max(Math.max(a,b),c),d);
     }
 
 }
