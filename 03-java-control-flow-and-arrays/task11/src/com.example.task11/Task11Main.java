eaf82792f4b90abac0131303c41a7d0748f78a9c
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2020-10-02 01:12:14 +0500

    Зернюков Никита. 3 тема

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,24 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
+        if(arr == null || arr.length == 0) return;
+        int temp = arr[0];
+        int indexToSwitch = numMin(arr);
+        arr[0] = arr[indexToSwitch];
+        arr[indexToSwitch] = temp;
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+    }
+
+    static int numMin(int[] arr) {
+        int index = 0;
+        int value = Integer.MAX_VALUE;
+        for (int i = 0; i<arr.length; i++){
+            if(arr[i]<=value){
+                value = arr[i];
+                index = i;
+            }
+        }
+        return index;
     }
 
 }
