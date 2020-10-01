a5742bed16f79995729220e059ac32c6a3c195cf
avetluginaa <a.vetlugina1@yandex.ru>
2020-10-01 15:09:08 +0500

    Ветлугина Анастасия. Задание 3.3

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -4,9 +4,7 @@ public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         System.out.println(getNameOfWeekDays(7));
-         */
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
