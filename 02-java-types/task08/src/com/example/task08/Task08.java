c6ec0b27f69b2fe79511a907bb9a7dcabe9c1912
JIiCeHoK <foxs29546@gmail.com>
2020-10-01 14:23:43 +0500

    Моисеев Михаил. Задача 2.08

--- 02-java-types/task08/src/com/example/task08/Task08.java
+++ 02-java-types/task08/src/com/example/task08/Task08.java
@@ -5,15 +5,15 @@ public class Task08 {
     public static boolean solution() {
         // TODO измените тип и значение переменной x, чтобы возвращалось значение true
 
-        double x = double.POSITIVE_INFINITY;
+        double x = Double.POSITIVE_INFINITY;
         return x == x + 1;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение
-        /*
+
         System.out.println(solution());
-        */
+
     }
 
 }
