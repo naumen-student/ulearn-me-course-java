e2920f01418f931753f1bd33aa7227012b78174f
rabbit21xx13 <rabbit21xx13@users.noreply.github.com>
2020-10-01 12:11:46 +0500

    Шевлякова Алёна. Задание 4.4

---
+++ 04-java-oop-basic/task04/src/com/example/task04/Point.java
@@ -0,0 +1,25 @@
+package com.example.task04;
+
+public class Point {
+
+    final int x;
+    final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void print() {
+        String pointToString = String.format("(%d, %d)", x, y);
+        System.out.println(pointToString);
+    }
+
+    double distance(Point point) {
+        return Math.sqrt((x - point.x) * (x - point.x) + (y - point.y) * (y - point.y));
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+}
