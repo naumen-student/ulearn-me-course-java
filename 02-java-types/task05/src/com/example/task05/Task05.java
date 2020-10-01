60c5cef6565bd975363c3041d7d4030bb8d6546e
nukit181 <nik_babin@inbox.ru>
2020-10-01 23:38:25 +0500

    02_task05

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,19 +3,18 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        int length = (x+"").length();
+        for (int number = 0; number < length; number++) {
+            if (x % 2 != 0)
+                return "FALSE";
+            x /= 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
+        String result = solution(10);
         System.out.println(result);
-        */
     }
 
 }
