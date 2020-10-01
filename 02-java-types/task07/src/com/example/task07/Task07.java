a87574eb9b637006a6761500b8de9005af5a11b9
JIiCeHoK <foxs29546@gmail.com>
2020-10-01 14:01:39 +0500

    Моисеев Михаил. Задача 2.07

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -11,8 +11,7 @@ public class Task07 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
+
 
         int result = solution(20, 50, 3);
         System.out.println(result);
