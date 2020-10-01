6bae34aa926817ac91e882ff98c760c42eebdbd8
JIiCeHoK <foxs29546@gmail.com>
2020-10-01 13:24:32 +0500

    Моисеев Михаил. Задача 2.06

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -4,9 +4,14 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int number = x + y;
+        int result = 0;
 
-        return 0;
+        while (number != 0) {
+            result++;
+            number /= 10;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
