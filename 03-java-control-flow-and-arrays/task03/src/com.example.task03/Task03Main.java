4716194c31c3eca39581128d500a0b28e5f31b44
AdIVIuH <42569158+AdIVIuH@users.noreply.github.com>
2020-10-01 12:32:42 +0500

    Квашнин Дмитрий. Задание 3.03

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -1,15 +1,20 @@
 package com.example.task03;
 
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class Task03Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         System.out.println(getNameOfWeekDays(7));
-         */
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (weekDaysNumber < 1 || weekDaysNumber > 7)
+            return "такого дня недели не существует";
+        DayOfWeek month = DayOfWeek.of(weekDaysNumber);
+        Locale loc = Locale.forLanguageTag("ru");
+
+        return month.getDisplayName(TextStyle.FULL_STANDALONE, loc);
     }
 }
