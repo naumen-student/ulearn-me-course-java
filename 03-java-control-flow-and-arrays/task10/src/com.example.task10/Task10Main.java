aebdb94deac39b79cd456e1a668929884bfe4923
theSweater23 <geg.gegre123@gmail.com>
2020-10-02 19:55:05 +0500

    Сулейманов Эмиль. Задание 3.10

--- 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
+++ 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
@@ -1,5 +1,7 @@
 package com.example.task10;
 
+import java.util.Arrays;
+
 public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -11,8 +13,14 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = 0;
+        int res = -1;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] <= min || min == 0) {
+                min = arr[i];
+                res = i;
+            }
+        }
+        return res;
     }
-
 }
