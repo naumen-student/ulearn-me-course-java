ee618bcbcd55f40fcead802fd5f55ae43292b15c
дом <anton89222910261123@yandex.ru>
2020-10-02 16:10:29 +0500

    Петров Антон Задача 2.12

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -6,24 +6,19 @@ public class Task12 {
 
     public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
 
-        // TODO раскомментируйте и исправьте код
-
-        // Считаем проценты за год
-
-        /*
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+            sum=sum.add(sum.multiply(percent));
         }
+        sum= sum.setScale(9, BigDecimal.ROUND_HALF_UP);
         return sum;
-        */
 
-        return BigDecimal.ZERO;
+       // return BigDecimal.ZERO;
     }
 
     public static void main(String[] args) {
 
         BigDecimal sum = new BigDecimal(500).setScale(9, BigDecimal.ROUND_HALF_UP); // 500 руб. на счете
-        BigDecimal percent = new BigDecimal(0.00000001f).setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
+        BigDecimal percent = new BigDecimal("0.00000001").setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
 
         sum = benefit(sum, percent);
 
