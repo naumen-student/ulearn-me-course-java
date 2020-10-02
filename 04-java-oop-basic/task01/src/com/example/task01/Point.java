073c0403469b6dca2520a4f7efb92b93de6a8e1f
Stanislav <youngfrezyx@gmail.com>
2020-10-02 03:04:30 +0300

    Ильин Станислав. Задача 4.1

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -7,8 +7,28 @@ public class Point {
     int x;
     int y;
 
+    Point(int x, int y ){
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int buffer = x;
+        x = -y;
+        y = -buffer;
+    }
+
+    double distance(Point point) {
+        int xDistance = point.x - x;
+        int yDistance = point.y - y;
+        return Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+        System.out.println(toString());
     }
 }
