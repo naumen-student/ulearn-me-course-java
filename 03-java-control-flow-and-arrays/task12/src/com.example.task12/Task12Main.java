a68eccdb029fb93c6b00ae69790042611c5c1037
theSweater23 <geg.gegre123@gmail.com>
2020-10-02 20:43:08 +0500

    Сулейманов Эмиль. Задание 3.12

--- 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
+++ 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
@@ -1,18 +1,37 @@
 package com.example.task12;
 
+import java.util.Arrays;
+
 public class Task12Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {9, 11, 7, 8};
+
+        int[] arr = {7, 5, 9};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (Arrays.equals(arr, null) || Arrays.equals(arr, new int[]{})) return;
+        else
+        {
+            for (int i = 0; i < arr.length; i++)
+            {
+                int min = 0, index = 0;
+                for (int j = i; j < arr.length; j++)
+                {
+                    if (min == 0 || arr[j] < min)
+                    {
+                        min = arr[j];
+                        index = j;
+                    }
+                }
+                arr[index] = arr[i];
+                arr[i] = min;
+            }
+        }
     }
 
 }
