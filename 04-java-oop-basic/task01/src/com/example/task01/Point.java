bbe01e97685d9237733c17ccda8e7a5a8f9ec6a1
Dima <d.d.demin@urfu.me>
2020-10-03 18:27:31 +0500

    04_task01 is finished

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -4,8 +4,27 @@ package com.example.task01;
  * Класс точки на плоскости
  */
 public class Point {
-    int x;
-    int y;
+    public int x;
+    public int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip(){
+        int temp = -x;
+        x = -y;
+        y = temp;
+    }
+
+    public double distance(Point point){
+        return Math.sqrt((point.x-x) * (point.x-x) + (point.y-y) * (point.y-y));
+    }
+
+    public String toString(){
+        return "({x}, {y})";
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
