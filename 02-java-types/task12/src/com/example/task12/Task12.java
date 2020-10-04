619e5536c1eaa17fef56d1585be788c5aef10e6b
JIiCeHoK <foxs29546@gmail.com>
2020-10-04 14:21:53 +0500

    Моисеев Михаил. Задача 2.12

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -12,9 +12,6 @@ public class Task12 {
 
     public static void main(String[] args) {
 
-        BigDecimal sum = new BigDecimal(500).setScale(9, BigDecimal.ROUND_HALF_UP); // 500 руб. на счете
-        BigDecimal percent = new BigDecimal(0.00000001f).setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
-        sum = benefit(sum, percent);
         System.out.println("Сумма на счете через год: " + sum);
 
     }
