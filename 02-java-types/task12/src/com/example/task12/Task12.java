237d274afb5c7c16ab6f2e630903ddaef62521fd
JIiCeHoK <foxs29546@gmail.com>
2020-10-04 14:13:14 +0500

    Моисеев Михаил. Задача 2.12

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -1,32 +1,20 @@
 package com.example.task12;
 
+import java.math.RoundingMode;
 import java.math.BigDecimal;
 
 public class Task12 {
 
-    public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
-
-        // TODO раскомментируйте и исправьте код
-
-        // Считаем проценты за год
-
-        /*
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
-        */
-
-        return BigDecimal.ZERO;
+    public static float benefit(float sum, float percent) {
+        final BigDecimal decPercent = new BigDecimal(percent);
+        return sum.multiply(decPercent.pow(12)).setScale(9, RoundingMode.HALF_UP);
     }
 
     public static void main(String[] args) {
 
         BigDecimal sum = new BigDecimal(500).setScale(9, BigDecimal.ROUND_HALF_UP); // 500 руб. на счете
         BigDecimal percent = new BigDecimal(0.00000001f).setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
-
         sum = benefit(sum, percent);
-
         System.out.println("Сумма на счете через год: " + sum);
 
     }
