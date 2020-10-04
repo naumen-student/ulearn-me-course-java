e5ee6fb4b074fc39e5e380b6ea06184136452a06
Danil Konkov <buroroll@gmail.com>
2020-10-04 16:40:46 +0500

    Коньков Данил. Задание 4.4

---
+++ 04-java-oop-basic/task04/src/com/example/task04/Point.java
@@ -0,0 +1,20 @@
+package com.example.task04;
+
+/**
+ * Класс точки на плоскости
+ */
+public class Point {
+    final int x;
+    final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+}
