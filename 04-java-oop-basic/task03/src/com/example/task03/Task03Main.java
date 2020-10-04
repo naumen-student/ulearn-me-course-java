14432a05310fcf59b3a763a28676e56de3020ac7
Haggard13 <alexey.kozedubov@urfu.me>
2020-10-04 16:09:41 +0500

    Козедубов Алексей. Задание 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber firstNumber = new ComplexNumber(7, -10);
+        ComplexNumber secondNumber = new ComplexNumber(-4, 9);
+        ComplexNumber sum = firstNumber.sum(secondNumber);
+        ComplexNumber mulriply = firstNumber.multiply(secondNumber);
     }
 }
