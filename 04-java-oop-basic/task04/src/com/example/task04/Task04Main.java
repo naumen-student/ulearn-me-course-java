cf093c8bffac658f2b60ac0877a92af1764d1327
andrewzvorygin <Andrei.Zvorygin@urfu.me >
2020-10-04 10:39:18 +0500

    Зворыгин Андрей Задача 4.4

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,7 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Line l = new Line(new Point(1,1), new Point(2,2));
+        System.out.println(l);
     }
 }
