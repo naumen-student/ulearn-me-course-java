87c216ed424529c896a2cc889b2ffa8c912d11b6
Елизавета Беломытцева <elzahaggard13@outlook.com>
2020-10-04 17:10:34 +0500

    Беломытцева Елизавета. Задание 4.1

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -7,6 +7,27 @@ public class Point {
     int x;
     int y;
 
+    public Point(){}
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int x1 = x;
+        x = -y;
+        y = - x1;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt((x - point.x) * (x - point.x) + (y - point.y) * (y - point.y));
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
