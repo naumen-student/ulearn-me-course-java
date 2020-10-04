fb52424658ef834f5570b9816a341a68d6185850
Chekebreek <shura.dushkin@yandex.ru>
2020-10-04 17:27:08 +0500

    Душкин Александр. 03_task07

--- 03-java-control-flow-and-arrays/task07/src/com.example.task07/Task07Main.java
+++ 03-java-control-flow-and-arrays/task07/src/com.example.task07/Task07Main.java
@@ -12,10 +12,7 @@ public class Task07Main {
 
     static int sum(int[] arr) {
         int sum = 0;
-        for (int i = 0; i < arr.length; i++)
-        {
-            sum += arr[i];
-        }
+        for (int i = 0; i < arr.length; i++) sum += arr[i];
         return sum;
     }
 
