7ecb53ea2ae3db0545e769c273259b638a34a306
nukit181 <nik_babin@inbox.ru>
2020-10-05 22:07:23 +0500

    Бабин Никита. Задание 3.10

--- 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
+++ 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
@@ -2,17 +2,20 @@ package com.example.task10;
 
 public class Task10Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         System.out.println(numMin(arr));
-         */
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = Integer.MAX_VALUE;
+        int number = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= min) {
+                min = arr[i];
+                number = i;
+            }
+        }
+        return number;
     }
 
 }
