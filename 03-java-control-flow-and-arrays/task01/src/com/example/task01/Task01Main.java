fa8b881cafe878ce6ec9175c0c2103f390950368
kirillegorow <e.kirill.2001@gmail.com>
2020-10-05 15:57:36 +0500

    Task 3.1 Егоров Кирилл

--- 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
+++ 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,7 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
+        System.out.print(canGetDriverLicense(20));
     }
 
     static boolean canGetDriverLicense(int age) {
