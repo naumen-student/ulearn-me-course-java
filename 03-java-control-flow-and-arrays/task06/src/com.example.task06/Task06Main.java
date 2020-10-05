268ade15e73161ad2c0e1db7996246984a68b3f7
kirillegorow <e.kirill.2001@gmail.com>
2020-10-05 17:50:15 +0500

    Task 3.6 Егоров Кирилл

--- 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
+++ 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
@@ -2,16 +2,17 @@ package com.example.task06;
 
 public class Task06Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getMax(1, 2, 3, 4));
-         */
+        System.out.print(getMax(1,6,2,1));
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        if(b>a)
+            a = b;
+        if(c>a)
+            a = c;
+        if(d>a)
+            a = d;
+        return a;
     }
 
 }
