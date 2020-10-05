7fd920e110b3319aade9b628652f3ba8603e6a93
kirillegorow <e.kirill.2001@gmail.com>
2020-10-05 18:10:11 +0500

    Task 3.10 Егоров Кирилл

--- 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
+++ 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
@@ -2,17 +2,17 @@ package com.example.task10;
 
 public class Task10Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
-        System.out.println(numMin(arr));
-         */
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int minI = 0;
+        int min = 2147483647;
+        for(int i = 0;i<arr.length;i++)
+            if (arr[i]<=min) {
+                min = arr[i];
+                minI = i;
+            }
+        return minI;
     }
 
 }
