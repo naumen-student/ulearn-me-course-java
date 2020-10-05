60cd43e781967816ca517acd90643f06ee70cd00
Meh <zavyalovartem79@gmail.com>
2020-10-05 23:52:34 +0500

    Завьялов Артём. Задача 4.02

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,10 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
-
+        TimeSpan test = new TimeSpan(10, 20, 0);
+        test.add(new TimeSpan(1, 40, 0));
+        System.out.println(test);
+        test.subtract(new TimeSpan(1, 3, 59));
+        System.out.println(test);
     }
 }
