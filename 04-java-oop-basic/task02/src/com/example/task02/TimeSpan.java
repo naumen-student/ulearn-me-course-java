76699c851afeb6f2b72556737e8848cdef52c7fa
gisinka <mrxenonmr@gmail.com>
2020-10-06 19:28:14 +0500

    task4.2 solved

---
+++ 04-java-oop-basic/task02/src/com/example/task02/TimeSpan.java
@@ -0,0 +1,82 @@
+package com.example.task02;
+
+public class TimeSpan {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public TimeSpan(int hours, int minutes, int seconds){
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+        Normalize();
+    }
+
+    public int getHours(){
+        return hours;
+    }
+
+    public void setHours(int hours){
+        this.hours = hours;
+    }
+
+    public int getMinutes(){
+        return minutes;
+    }
+
+    public void setMinutes(int minutes){
+        this.minutes = minutes;
+    }
+
+    public int getSeconds(){
+        return seconds;
+    }
+
+    public void setSeconds(int seconds){
+        this.seconds = seconds;
+    }
+
+    public String toString(){
+        return String.format("%d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    void add(TimeSpan time){
+        doArithmetic(time, +1);
+    }
+
+    void subtract(TimeSpan time){
+        doArithmetic(time, -1);
+    }
+
+    private void doArithmetic(TimeSpan time, int sign){
+        hours+=sign * time.hours;
+        minutes+=sign * time.minutes;
+        seconds+=sign * time.seconds;
+        Normalize();
+    }
+
+    private void Normalize() {
+        int tmp = 0;
+        if (seconds > 59){
+            tmp = seconds / 60;
+            minutes+=tmp;
+            seconds-=tmp * 60;
+        }
+        if (seconds < 0){
+            tmp = (int) Math.ceil((double) -seconds / 60);
+            minutes-=tmp;
+            seconds+=tmp * 60;
+        }
+        if (minutes > 59){
+            tmp =minutes / 60;
+            hours+=tmp;
+            minutes-=tmp * 60;
+        }
+        if (minutes < 0){
+            tmp = (int) Math.ceil((double) -minutes / 60);
+            hours-=tmp;
+            minutes+=tmp * 60;
+        }
+    }
+
+}
