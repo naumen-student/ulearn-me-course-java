af23ce1fec5a81caa9dd16937ad2c3f3389296d4
evanesenze <evanesenze@gamil.com>
2020-10-06 21:38:01 +0500

    Базуев Иван.04.Task03

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,11 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber n1 = new ComplexNumber(2,5);
+        ComplexNumber n2 = new ComplexNumber(4,3);
+        ComplexNumber r1 = n1.sum(n2);
+        ComplexNumber r2 = n1.multiply(n2);
+        System.out.println(r1.toString());
+        System.out.println(r2.toString());
     }
 }
