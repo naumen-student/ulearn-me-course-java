5b57a55206f207e7d58620529a0c636bf2423ccb
diukin9 <petr.diukin@urfu.me>
2020-10-06 11:54:39 +0500

    Дюкин Петр 4.1

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -1,13 +1,10 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
 public class Point {
     int x;
     int y;
 
-    public Point(int x, int y) {
+    Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -15,11 +12,16 @@ public class Point {
     void flip() {
         x = x + y;
         y = -(y - x);
-        x = x - y;
+        x = -(x - y);
+        y = -y;
     }
 
     double distance(Point point) {
         return Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2));
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
     }
 
     void print() {
