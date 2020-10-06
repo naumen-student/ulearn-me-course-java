99592514c32baae231e741bc0f19b840aa65aef6
Traiz0r4 <64859204+Traiz0r4@users.noreply.github.com>
2020-10-06 21:19:36 +0500

    Потапов Андрей task4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,37 @@
 package com.example.task03;
 
+class ComplexNumber {
+
+    private double realAxis;
+    private double imaginaryAxis;
+
+    public ComplexNumber(double realAxis, double imaginaryAxis) {
+        this.imaginaryAxis = imaginaryAxis;
+        this.realAxis = realAxis;
+    }
+
+    public ComplexNumber sum(ComplexNumber number) {
+        return new ComplexNumber(realAxis + number.realAxis, imaginaryAxis + number.imaginaryAxis);
+    }
+
+    public ComplexNumber multiply(ComplexNumber number) {
+        return new ComplexNumber(
+                realAxis * number.realAxis - imaginaryAxis * number.imaginaryAxis,
+                realAxis * number.imaginaryAxis + imaginaryAxis * number.realAxis);
+    }
+
+    public String toString() {
+        return String.format("%f + %fi", realAxis, imaginaryAxis);
+    }
+
+
+}
 public class Task03Main {
     public static void main(String[] args) {
 
+        ComplexNumber a = new ComplexNumber(5, 9);
+        ComplexNumber b = new ComplexNumber(2, -14);
+        System.out.println((a.sum(b)).toString());
+        System.out.println((a.multiply(b)).toString());
     }
 }
