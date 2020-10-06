bf62261ff03f72183830f2fe895fff701f02cdf7
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2020-10-06 20:41:28 +0500

    Create Task03.java
    Кашкин Сергей Сергеевич 2.3

---
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03.java
@@ -0,0 +1,18 @@
+package com.example.task03;
+
+public class Task03 {
+
+    public static int getMetreFromCentimetre(int centimetre) {
+        return centimetre / 100;
+    }
+
+    public static void main(String[] args) {
+        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
+        int result = getMetreFromCentimetre(345);
+        System.out.println(result);
+         */
+    }
+
+}
