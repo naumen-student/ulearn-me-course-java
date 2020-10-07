94e9d60d7884e07e4fc1964200ec3e8eda2abb3d
gorr <gorrgorrob@gmail.com>
2020-10-07 20:16:50 +0500

    Бородин Владислав. Задание 4.2

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,11 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
-
+        TimeSpan timeSpan = new TimeSpan(1, 0, 0);
+        System.out.println(timeSpan);
+        timeSpan.subtract(new TimeSpan(0, 0, 1));
+        System.out.println(timeSpan);
+        timeSpan.add(new TimeSpan(0, 1, 1));
+        System.out.println(timeSpan);
     }
 }
