b37bc1ca8e402d562211b5a158280ebe75900a8b
BoberPro <34961963+BoberPro@users.noreply.github.com>
2020-10-07 17:58:19 +0500

    Худяков Сергей 4.1.

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -7,8 +7,34 @@ public class Point {
     int x;
     int y;
 
+    Point() {
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int positionY = y;
+        int positionX = x;
+        x = -positionY;
+        y = -positionX;
+    }
+
+    double distance(Point point) {
+        int positionX = x - point.x;
+        int positionY = y - point.y;
+        return Math.sqrt(positionX * positionX + positionY * positionY);
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
+
 }
