1ac5de6f2ada9adaf8cbb7f59b784994921c5960
justvaclav <39595190+justvaclav@users.noreply.github.com>
2020-10-07 14:56:17 +0500

    Шимонек Вацлав, задание 4.4

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,6 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        System.out.println(new Line(new Point(0,0), new Point(-10, 10)).toString());
     }
 }
