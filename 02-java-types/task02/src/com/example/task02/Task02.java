39c36c01121cff3db75bdfedcc8af5ad2de5d290
Veronika Muray <muray.veronika@mail.ru>
2020-10-07 23:07:43 +0500

    Мурай Вероника. Задание 02

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -4,18 +4,20 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        long num = Long.parseLong(input);
+        if (num >= -128 && num <= 127)
+            return "byte";
+        if (num >= Short.MIN_VALUE && num <= Short.MAX_VALUE)
+            return "short";
+        if (num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE)
+            return "int";
+        return "long";
 
-        return "";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
