a429752f228d32d18a7bb0b5436b42c6d9bc4c42
Admin <sc2mar4enko@yandex.ru>
2020-10-07 19:51:02 +0500

    Марченко Константин. Задание 04.02

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,30 @@
 package com.example.task02;
 
-public class Task02Main {
-    public static void main(String[] args) {
+class TimeSpan {
 
+    private int seconds;
+    private int minutes;
+    private int hours;
+
+    public TimeSpan(int hours, int minutes, int second) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = second;
+    }
+
+    public void add(TimeSpan time) {
+        seconds += time.seconds;
+        minutes += time.minutes;
+        hours += time.hours;
+    }
+
+    public void subtract(TimeSpan time) {
+        seconds -= time.seconds;
+        minutes -= time.minutes;
+        hours -= time.hours;
+    }
+
+    public String toString() {
+        return String.format("(%dч:%dм:%dс)", hours, minutes, seconds);
     }
 }
