1312fb8ff67367ed230e74a63231e1866019f2e8
gorr <gorrgorrob@gmail.com>
2020-10-07 21:20:04 +0500

    Бородин Владислав. Задание 4.4

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,9 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Line line = new Line(new Point(), new Point(4, 4));
+        System.out.println(line);
+        System.out.println(line.isCollinearLine(new Point(2, 2)));
+        System.out.println(line.isCollinearLine(new Point(2, 3)));
     }
 }
