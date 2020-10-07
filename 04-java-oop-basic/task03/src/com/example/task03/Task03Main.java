f22533e532401b3bd24ecf2481f1e48debc3356a
gorr <gorrgorrob@gmail.com>
2020-10-07 20:53:20 +0500

    Бородин Владислав. Задание 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,10 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber num1 = new ComplexNumber(2, 3);
+        ComplexNumber num2 = new ComplexNumber(5, 9);
+        System.out.println(num1);
+        System.out.println(num1.sum(num2));
+        System.out.println(num1.multiply(num2));
     }
 }
