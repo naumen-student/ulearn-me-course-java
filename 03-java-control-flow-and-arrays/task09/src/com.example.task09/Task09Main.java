4759742d1c6a40ce1734ee7d92bf87c9adf7905d
theonethei <57193492+theonethei@users.noreply.github.com>
2020-10-07 14:44:44 +0500

    Update Task09Main.java

--- 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
+++ 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
@@ -15,7 +15,11 @@ public class Task09Main {
 
     static int min(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i+1]<arr[0])
+                arr[0] = arr[i + 1];
+        }
+        return arr[0];
     }
 
 }
