d40be346095250c29994a55607375a347e50ff04
Андрей <andryfka_3rd@mail.ru>
2020-10-07 19:49:40 +0500

    Тыщенко Андрей. Task 4.03

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,14 @@
 package com.example.task03;
 
-public class Task03Main {
-    public static void main(String[] args) {
-
+public class Task03Main
+{
+    public static void main(String[] args)
+    {
+        ComplexNumber number1 = new ComplexNumber(1,8);
+        ComplexNumber number2 = new ComplexNumber(4,8);
+        ComplexNumber result1 = number1.Summary(number2);
+        ComplexNumber result2 = number1.Multiply(number2);
+        System.out.println(result1.toString());
+        System.out.println(result2.toString());
     }
 }
