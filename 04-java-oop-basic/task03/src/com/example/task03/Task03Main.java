44b30ae53a4afb12810bb86ac77a52bf77f9d683
Admin <sc2mar4enko@yandex.ru>
2020-10-07 20:39:34 +0500

    Марченко Константин. Задание 04.03

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumbers a = new ComplexNumbers(1, 3);
+        ComplexNumbers b = new ComplexNumbers(25, 10);
+        System.out.println((a.sum(b)).toString());
+        System.out.println((a.mult(b)).toString());
     }
 }
