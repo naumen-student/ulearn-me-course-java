cb9366f04ad19653812ea0ab2ee8a9da9f7f603a
theonethei <57193492+theonethei@users.noreply.github.com>
2020-10-07 17:54:42 +0500

    Update Point.java

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -6,6 +6,23 @@ package com.example.task01;
 public class Point {
     int x;
     int y;
+    public Point(){}
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+
+    }
+    void flip(){
+        int k = x;
+        x = -y;
+        y = -k;
+    }
+    double distance(Point point){
+        return Math.sqrt(Math.pow(point.x-x,2)+Math.pow(point.y-y,2));
+    }
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
