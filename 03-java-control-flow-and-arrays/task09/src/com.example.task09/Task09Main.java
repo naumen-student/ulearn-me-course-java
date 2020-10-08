703f7c402b9dba7162e6450f82b0a68e336a96c2
avetluginaa <a.vetlugina1@yandex.ru>
2020-10-08 16:12:06 +0500

    Ветлугина Анастасия. Задание 3.9

--- 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
+++ 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
@@ -4,18 +4,19 @@ public class Task09Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         int[] arr = new int[3];
         arr[0] = 2;
         arr[1] = -1;
         arr[2] = 3;
         System.out.println(min(arr));
-         */
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int minValue=arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < minValue)
+                minValue = arr[i];
+        }
+        return minValue;
     }
-
 }
