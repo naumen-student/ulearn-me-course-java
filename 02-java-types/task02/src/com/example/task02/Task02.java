188322a57ddfe5a8594378bbe0997199f67231a0
Traiz0r4 <64859204+Traiz0r4@users.noreply.github.com>
2020-10-08 17:32:20 +0500

    Потапов Андрей task2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,19 +3,19 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        long c = Long.parseLong(input);
+        if (c >= -128 && c <= 127) {
+            return "byte";
+        }
+        if (c >= -32768 && c <= 32767) {
+            return "short";
+        }
+        if (c >= -2147483648 && c <= 2147483647) {
+            return "int";
+        }
+        if (c <= -2147483648 || c > 2147483647) {
+            return "long";
+        }
         return "";
     }
-
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
-    }
-
 }
