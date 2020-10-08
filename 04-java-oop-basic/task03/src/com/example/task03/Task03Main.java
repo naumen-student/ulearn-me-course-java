cf6eb537445915451be2e6d3a0317db47cf6e59f
grekova-web <71182152+grekova-web@users.noreply.github.com>
2020-10-08 22:40:56 +0500

    Грекова Елена. Задание 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber c1 = new ComplexNumber(1,1);
+        ComplexNumber c2 = new ComplexNumber(2,2);
+        System.out.println(c1.multiple(c2));
+        System.out.println(c1.add(c2));
     }
 }
