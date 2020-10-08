93b0678461477621d470421c5c490f871bd97d94
Andrew <tdr896@gmail.com>
2020-10-08 05:12:24 +0500

    Мусиенко Андрей. Задание 4.3

---
+++ 04-java-oop-basic/task04/src/com/example/task04/Point.java
@@ -0,0 +1,24 @@
+package com.example.task04;
+
+/**
+ * Класс точки на плоскости
+ */
+public class Point {
+    private final int x;
+    private final int y;
+
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void print() {
+        String pointToString = String.format("(%d, %d)", x, y);
+        System.out.println(pointToString);
+    }
+
+    public String toString() {
+        return String.format("(%1$,%2$)", x, y);
+    }
+}
