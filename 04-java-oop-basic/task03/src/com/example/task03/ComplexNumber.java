0ec1b21cc799a659fc0851783487b7e7b814a97c
RoyalPiano <zelyoniyoleg@yandex.ru>
2020-10-09 18:12:23 +0500

    Чесноков Олег Задание 4.2

--- 04-java-oop-basic/task03/src/com/example/task03/ComplexNumber.java
+++ 04-java-oop-basic/task03/src/com/example/task03/ComplexNumber.java
@@ -1,26 +1,47 @@
 package com.example.task03;
 
 public class ComplexNumber {
-    private double a;
-    private double bi;
+    private double re;
+    private double im;
 
     ComplexNumber() {
 
     }
 
-    ComplexNumber(double a, double bi) {
-        this.a = a;
-        this.bi = bi;
+    ComplexNumber(double realNum, double imNum) {
+        this.re = realNum;
+        this.im = imNum;
     }
 
-    public ComplexNumber sum(ComplexNumber num) {
-        return new ComplexNumber(this.a + num.a, this.bi + num.bi);
+    public ComplexNumber sum(ComplexNumber complexNum) {
+        return new ComplexNumber(this.re + complexNum.re, this.im + complexNum.im);
     }
 
-    public ComplexNumber multiply(ComplexNumber num) {
-        return new ComplexNumber(this.a * num.a, - this.bi * num.bi);
+    public ComplexNumber multiply(ComplexNumber complexNum) {
+        return new ComplexNumber(re * complexNum.re - im * complexNum.im,
+                re * complexNum.im + complexNum.re * im);
     }
 
+    public double getIm() {
+        return this.im;
+    }
 
+    public void setIm(double im) {
+        this.im = im;
+    }
 
+    public double getRe() {
+        return this.re;
+    }
+
+    public void setRe(double re) {
+        this.re = re;
+    }
+
+    public String toString() {
+        return "ComplexNumber{" +
+                "a=" + re +
+                ", bi=" + im +
+                '}';
+    }
 }
