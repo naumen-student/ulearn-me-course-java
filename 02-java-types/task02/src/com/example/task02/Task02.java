1d72e026d54b6467857b268acccdd2cf9b509666
IlyaRussia23 <56472066+IlyaRussia23@users.noreply.github.com>
2020-10-09 19:24:29 +0500

    Илья Гребенюк. 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -5,8 +5,12 @@ public class Task02 {
     public static String solution(String input) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        Long num = new Long(input);
+        if(num >= -128 && num <= 127) return "byte";
+        else if(num >= -32768 && num <= 32767) return "short";
+        else if(num >= -2147483648 && num <= 2147483647) return "int";
+        else return "long";
+        //return "";
     }
 
     public static void main(String[] args) {
