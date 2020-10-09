cebb24d4423b740f32a70456a300f2e93509a09e
Veronika Muray <muray.veronika@mail.ru>
2020-10-09 19:56:41 +0500

    Мурай Вероника. Задание 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,19 +3,19 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        int num;
+        for(int i = 0; i < String.valueOf(x).length() + 1; i++){
+            num = x % 10;
+            x = x / 10;
+            if (num % 2 != 0)
+                return "FALSE";
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution(1234);
         System.out.println(result);
-        */
     }
 
 }
