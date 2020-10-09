d2624a32b1c685551905a1c3e67d585086d60497
LekoevNVK <lekoev.nvk@gmail.com>
2020-10-09 17:11:12 +0500

    Лебедкин Константин. Задание 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -4,14 +4,6 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        long number = Long.parseLong(input);
-        if (Byte.MAX_VALUE >= number && Byte.MIN_VALUE <= number)
-            return "byte";
-        else if (Short.MAX_VALUE >= number && Short.MIN_VALUE <= number)
-            return "short";
-        else if (Integer.MAX_VALUE >= number && Integer.MIN_VALUE <= number)
-            return "int";
-        else return "long";
 
     }
     public static void main(String[] args) {
