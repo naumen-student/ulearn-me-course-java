193bf26810c37bd2cb8b52af10fbe4f1cc88b068
regina <regina.agafonova.2001@gmail.com>
2020-10-09 12:40:13 +0500

    Агафонова Регина 02_task01

--- 02-java-types/task01/src/com/example/task01/Task01.java
+++ 02-java-types/task01/src/com/example/task01/Task01.java
@@ -3,19 +3,12 @@ package com.example.task01;
 public class Task01 {
 
     public static String solution() {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "здесь какая-то неправильная строка";
+        return "Я думаю, быть программистом - это круто";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution();
         System.out.println(result);
-         */
     }
 
 }
