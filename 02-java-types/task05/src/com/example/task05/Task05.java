7b2e023db395da2d024bca89b285d4e8c47729dc
nvit29 <gainetdinoa-nastya@ya.ru>
2020-10-09 19:45:07 +0500

    Гайнетдинова Анастасия. Задание 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,19 +3,18 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        for (int i = 0; i <= String.valueOf(x).length(); i++) {
+            int num = x % 10;
+            if (num % 2 != 0)
+                return "FALSE";
+            x = x / 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
+        String result = solution(2322);
         System.out.println(result);
-        */
     }
 
 }
