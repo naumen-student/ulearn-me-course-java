2e975c5dbd02d9b4d069e0710def6acc4365db8c
Foxovsky-bit <mihalov.k@mail.ru>
2020-10-10 18:18:38 +0500

    Михалев Кирилл. Задача 4.5

--- 04-java-oop-basic/task05/src/com/example/task05/Task05Main.java
+++ 04-java-oop-basic/task05/src/com/example/task05/Task05Main.java
@@ -1,7 +1,16 @@
 package com.example.task05;
 
+import java.util.ArrayList;
+
 public class Task05Main {
     public static void main(String[] args) {
-
+        Point[] poin = {
+                new Point(1, 2),
+                new Point(4, 6),
+                new Point(7, 5)
+        };
+       PolygonalLine pline = new PolygonalLine();
+       pline.setPoints(poin);
+       System.out.println(pline.getLength());
     }
 }
