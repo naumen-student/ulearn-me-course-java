6dad3b8a8c521416b2344c4cfb9d42098a60551c
Олег Хейн <okhiein@mail.ru>
2020-10-10 20:57:07 +0500

    03_task03

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -4,12 +4,15 @@ public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getNameOfWeekDays(7));
-         */
+
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (weekDaysNumber>=1 && weekDaysNumber<=7)
+            return (new String[] {"понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"})[weekDaysNumber-1];
+
+        return "такого дня недели не существует";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
