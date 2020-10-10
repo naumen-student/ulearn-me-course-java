49523eb2b33674f036d5123102a3f8ecc44241a8
Veronika Muray <muray.veronika@mail.ru>
2020-10-10 16:50:47 +0500

    Мурай Вероника. Задание 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,16 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = x + y;
+        String numbers = Integer.toString(sum);
+        if (sum < 0)
+            return numbers.length() - 1;
+        return numbers.length();
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
 
 }
