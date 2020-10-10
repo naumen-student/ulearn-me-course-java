904f87ee07813c5e232b853eb7b27b4fdcd9a695
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-10-10 10:13:35 +0500

    Update Task02.java

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,19 +3,25 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+        if (isByt(input))
+            return "byte";
+        else if(isShor(input))
+            return "short";
+        else if (isIn(input))
+            return "int";
+        return "long";
+    }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+    // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+    public static boolean isShor(String input){
+        return Long.parseLong(input) <= Short.MAX_VALUE && Long.parseLong(input) >= Short.MIN_VALUE;
+    }
 
         return "";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
+
     }
 
 }
