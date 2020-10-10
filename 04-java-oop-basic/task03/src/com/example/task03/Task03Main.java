3ddb230d3037bd8a45ed2a0419e9c29dbb38a553
dnrkvn <diyllina@mail.ru>
2020-10-10 19:03:29 +0500

    Хабибуллина Диана. Задача 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,15 @@
 package com.example.task03;
 
+import sun.awt.geom.AreaOp;
+
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumber num1 = new ComplexNumber(5, 14);
+        ComplexNumber num2 = new ComplexNumber(9.04, 7.03);
 
+        System.out.println("The first complex number: " + num1);
+        System.out.println("The second complex number: " + num2);
+        System.out.println("Sum: " + num1.calculateSum(num2));
+        System.out.println("Multiplication: " + num1.multiply(num2));
     }
 }
