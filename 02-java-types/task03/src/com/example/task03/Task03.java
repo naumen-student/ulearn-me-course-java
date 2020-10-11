64cb94c7ca39e18d6cdbd5e753c249c5ecfa3a0b
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-10-11 12:01:39 +0500

    Update Task03.java

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -3,15 +3,12 @@ package com.example.task03;
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
-<<<<<<< Updated upstream
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
         return 0;
-=======
         
         return centimetre / 100;
->>>>>>> Stashed changes
     }
 
     public static void main(String[] args) {
