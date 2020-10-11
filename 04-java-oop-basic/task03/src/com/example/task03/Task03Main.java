850b010ab8400292508ff7288242c2a29565321a
k1n5-bt <k1n5lobal@gmail.com>
2020-10-11 22:42:24 +0500

    Андреев Иван. Задача 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        Complex a = new Complex(1, 2);
+        Complex b = new Complex(3, 5);
+        Complex c = a.add(b);
+        System.out.println(c);
     }
 }
