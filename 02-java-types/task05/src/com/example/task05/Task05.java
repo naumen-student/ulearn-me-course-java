f1f1fef9ef20bfaea8b6b7b4d2a8a12d62cace05
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2020-10-13 03:02:09 +0500

    Update Task05.java

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,10 +3,11 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while (x != 0) {
+            if(x % 2 == 1)  return "FALSE";
+            x /= 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
