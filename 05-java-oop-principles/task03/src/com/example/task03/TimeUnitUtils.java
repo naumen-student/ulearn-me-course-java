50bebae39ecc96af5c49feae01e484a0de01bd28
MaximFrancevich <FranzuaAirMax@yandex.ru>
2020-10-12 20:40:59 +0500

    Францевич Максим. Задание 05-03

--- 05-java-oop-principles/task03/src/com/example/task03/TimeUnitUtils.java
+++ 05-java-oop-principles/task03/src/com/example/task03/TimeUnitUtils.java
@@ -24,4 +24,60 @@ public class TimeUnitUtils {
     public static Seconds toSeconds(Milliseconds millis) {
         return new Seconds(millis.toSeconds());
     }
+
+    public static Milliseconds toMillis(Milliseconds millis) {
+        return new Milliseconds(millis.toMillis());
+    }
+
+    public static Milliseconds toMillis(Minutes minutes) {
+        return new Milliseconds(minutes.toMillis());
+    }
+
+    public static Milliseconds toMillis(Hours hours) {
+        return new Milliseconds(hours.toMillis());
+    }
+
+    public static Seconds toSeconds(Seconds seconds) {
+        return new Seconds(seconds.toSeconds());
+    }
+
+    public static Seconds toSeconds(Minutes minutes) {
+        return new Seconds(minutes.toSeconds());
+    }
+
+    public static Seconds toSeconds(Hours hours) {
+        return new Seconds(hours.toSeconds());
+    }
+
+    public static Minutes toMinutes(Milliseconds millis) {
+        return new Minutes(millis.toMinutes());
+    }
+
+    public static Minutes toMinutes(Seconds seconds) {
+        return new Minutes(seconds.toMinutes());
+    }
+
+    public static Minutes toMinutes(Minutes minutes) {
+        return new Minutes(minutes.toMinutes());
+    }
+
+    public static Minutes toMinutes(Hours hours) {
+        return new Minutes(hours.toMinutes());
+    }
+
+    public static Hours toHours(Milliseconds millis) {
+        return new Hours(millis.toHours());
+    }
+
+    public static Hours toHours(Seconds seconds) {
+        return new Hours(seconds.toHours());
+    }
+
+    public static Hours toHours(Minutes minutes) {
+        return new Hours(minutes.toHours());
+    }
+
+    public static Hours toHours(Hours hours) {
+        return new Hours(hours.toHours());
+    }
 }
