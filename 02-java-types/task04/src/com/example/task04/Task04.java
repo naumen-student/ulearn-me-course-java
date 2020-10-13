5626c152553083aa0d06ed8d976112dd3f7024a7
TobeeM <mark.p200110@gmail.com>
2020-10-14 00:37:01 +0500

    Поташников Марк, задание 2.4

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -1,10 +1,16 @@
 package com.example.task04;
-
 public class Task04 {
-
     public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        switch (operation){
+            case "+":
+                return (float)a + b;
+            case "-":
+                return (float)a - b;
+            case "/":
+                return (float)a / b;
+            case "*":
+                return (float)a * b;
+        }
 
         return 0;
     }
@@ -17,5 +23,4 @@ public class Task04 {
         System.out.println(result);
         */
     }
-
 }
