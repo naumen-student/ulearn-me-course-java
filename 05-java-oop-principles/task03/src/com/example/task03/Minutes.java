02b833ce004d1891d6883a8d3463396e966d75fd
Chekebreek <shura.dushkin@yandex.ru>
2020-10-13 14:00:23 +0500

    Душкин Александр. 05_task03

--- 05-java-oop-principles/task03/src/com/example/task03/Minutes.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Minutes.java
@@ -2,26 +2,29 @@ package com.example.task03;
 
 public class Minutes implements TimeUnit {
 
-    public Minutes(long amount) {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
+    private final long num;
+
+    public Minutes(long num) {
+        this.num = num;
     }
 
     @Override
     public long toMillis() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
+        return num * 60 * 1000;
     }
 
     @Override
     public long toSeconds() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
+        return num * 60;
     }
 
     @Override
     public long toMinutes() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
+        return num;
+    }
+
+    @Override
+    public long getHours() {
+        return Math.round((double) num / 60);
     }
 }
