5dc08f7daf4b6b7efc89cbc1ebacb5a5c1f98ebc
Traiz0r4 <64859204+Traiz0r4@users.noreply.github.com>
2020-10-14 01:47:57 +0500

    Потапов Андрей task2.10

--- 02-java-types/task10/src/com/example/task10/Task10.java
+++ 02-java-types/task10/src/com/example/task10/Task10.java
@@ -1,25 +1,34 @@
 package com.example.task10;
 
+
+import static java.lang.Float.NaN;
+
 public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
 
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
+        double scale = Math.pow(10, precision);
+        if ((a != a && b !=b) || (a == b))
+            return  true;
+        //if (Math.round(a * scale * 10) % 10 > 4)
+           // a = (float) ((Math.round(a * scale * 10) - 5) / (10 * scale));
+        //if (Math.round(b * scale * 10) % 10 > 4)
+           //b = (float) ((Math.round(b * scale * 10) - 5) / (10 * scale));
+
+        //return (Math.round(a * scale) / scale == Math.round(scale * b) / scale);
+        return Math.abs(a - b) < Math.pow(0.1,precision);
 
     }
 
     public static void main(String[] args) {
-        float a = 0.3f;
-        float b = 0.4f;
+        float a = 0.0f;
+        float b = NaN;
         float sum = a + b;
         float c = 0.7f;
 
-        boolean result = compare(sum, c, 2);
+        boolean result = compare(a, b, 10);
         System.out.println(result);
+        System.out.println(sum);
 
     }
 
