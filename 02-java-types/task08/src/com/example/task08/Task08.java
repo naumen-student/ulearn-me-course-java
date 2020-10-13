65b6ed24dcfb184ff0befee71563654d80c61aee
IlyaRussia23 <56472066+IlyaRussia23@users.noreply.github.com>
2020-10-14 00:13:04 +0500

    Илья Гребенюк. 2.8

--- 02-java-types/task08/src/com/example/task08/Task08.java
+++ 02-java-types/task08/src/com/example/task08/Task08.java
@@ -5,15 +5,15 @@ public class Task08 {
     public static boolean solution() {
         // TODO измените тип и значение переменной x, чтобы возвращалось значение true
 
-        int x = 100;
+        double x = 1.0/0;
         return x == x + 1;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение
-        /*
-        System.out.println(solution());
-        */
+
+        //System.out.println(solution());
+
     }
 
 }
