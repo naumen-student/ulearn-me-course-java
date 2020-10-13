0c6cce68871c3d2aba89e27a57a3db5098282f45
Chekebreek <shura.dushkin@yandex.ru>
2020-10-13 14:22:38 +0500

    Душкин Александр. 05_task03

--- 05-java-oop-principles/task03/src/com/example/task03/Milliseconds.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Milliseconds.java
@@ -7,17 +7,17 @@ public class Milliseconds implements TimeUnit {
 
     private final long num;
 
-    public Milliseconds(long num) { this.num = num; }
+    public Milliseconds(long num) {this.num = num;}
 
     @Override
-    public long toMillis() { return num; }
+    public long toMillis() {return num;}
 
     @Override
-    public long toSeconds() { return Math.round((double) num / 1000); }
+    public long toSeconds() {return Math.round((double) num / 1000);}
 
     @Override
-    public long toMinutes() { return Math.round((double) num / 1000 / 60); }
+    public long toMinutes() {return Math.round((double) num / 1000 / 60);}
 
     @Override
-    public long getHours() { return Math.round((double) num / 1000 / 60 / 60); }
+    public long getHours() {return Math.round((double) num / 1000 / 60 / 60);}
 }
