7fa71a728cc0746fb336c4aa0ce9c947241a4267
TobeeM <mark.p200110@gmail.com>
2020-10-13 23:54:59 +0500

    Поташников Марк, задание 2.3

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -3,12 +3,11 @@ package com.example.task03;
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
-        int result = (int)(centimetre / 100);
-        return result;
+        return centimetre / 100;
     }
 
     public static void main(String[] args) {
-        int result = getMetreFromCentimetre(999999);
+        int result = getMetreFromCentimetre(478);
         System.out.println(result);
     }
 
