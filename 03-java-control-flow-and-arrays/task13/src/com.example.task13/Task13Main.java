0e9ecc0bb03e8f1d9e55ed1d332f1068bc687f56
Chocolatelis <elizavetab123@mail.ru>
2020-10-13 16:37:33 +0500

    Быкова Елизавета Задание 3.13

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -2,11 +2,6 @@ package com.example.task13;
 
 public class Task13Main {
     public static void main(String[] args) {
-
-        int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-
     }
 
     static int[] removeMoreThen1000(int[] arr) {
