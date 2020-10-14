d0343f38c0a9af1f082f326766cbe87d934bcbdf
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2020-10-14 15:18:28 +0500

    Update Task06Main.java

--- 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
+++ 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
@@ -1,16 +1,9 @@
 package com.example.task06;
 
-public class Task06Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        new Task06Main().printMethodName();
-         */
-    }
+public class Task06Main{
 
-    void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+    void printMethodName(){
+        StackTraceElement element = new Exception().getStackTrace()[1];
+        System.out.print(element.getMethodName());
     }
-
 }
