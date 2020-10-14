d580bb3aea08ba732e62426d61a0696102b48a1b
MaximFrancevich <FranzuaAirMax@yandex.ru>
2020-10-14 19:50:08 +0500

    Францевич Максим. Задание 06-07

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -7,18 +7,20 @@ public class Task07Main {
     public static final String NONE = "none";
 
     public static void main(String[] args) {
+
     }
 
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+            processor.process();
+        } catch (Error | RuntimeException e) {
+            return UNCHECKED;
         } catch (Exception e) {
-
+            return CHECKED;
         }
-        return null;
+        return NONE;
     }
 
 }
