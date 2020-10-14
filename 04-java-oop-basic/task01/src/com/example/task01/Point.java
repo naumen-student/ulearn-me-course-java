22f849f88d4e95aa9d392ba30b9e9b5f1c924172
KozhevnikovAlexandr <kozhevnikov.alexandr8@yandex.ru>
2020-10-14 12:05:04 +0500

    Task 0401 completed

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -7,6 +7,26 @@ public class Point {
     int x;
     int y;
 
+    public void flip(){
+        int savedX = x;
+        x=-y;
+        y=-savedX;
+    }
+    public Point(){}
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point point){
+        return Math.sqrt((x-point.x)*(x- point.x) + (y- point.y)*(y- point.y));
+    }
+
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
