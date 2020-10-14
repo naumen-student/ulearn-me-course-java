11e4a5fdcbba7ec4f4629e77618c602439314ef2
evanesenze <evanesenze@gamil.com>
2020-10-14 23:08:35 +0500

    Базуев Иван.06.Task02

--- 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
+++ 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Task02Main {
-
     public static void main(String[] args) {
     }
 
     static String getSeason(int monthNumber) {
         List<String> seasons = Arrays.asList("зима", "весна", "лето", "осень");
-        if (monthNumber < 0 || monthNumber > 12)
-            throw new IllegalArgumentException(String.format("monthNumber %d is invalid, month number should be between 1..12", monthNumber));
+        if (monthNumber < 1 || monthNumber > 12)
+            throw new IllegalArgumentException
+                    (String.format("monthNumber %d is invalid, month number should be between 1..12", monthNumber));
         return seasons.get(monthNumber == 12 ? 0 : monthNumber / 3);
     }
 
