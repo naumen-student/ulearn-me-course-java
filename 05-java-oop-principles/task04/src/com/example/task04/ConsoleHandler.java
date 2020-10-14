856c71c8dee140513d454a141592327779c37d1e
Daniella-Abdimalik-kyzy <70878729+Daniella-Abdimalik-kyzy@users.noreply.github.com>
2020-10-15 03:13:01 +0500

    Абдималик кызы Даниэлла. Задание 5.04

---
+++ 05-java-oop-principles/task04/src/com/example/task04/ConsoleHandler.java
@@ -0,0 +1,9 @@
+package com.example.task04;
+
+public class ConsoleHandler extends MessageHandler {
+
+    @Override
+    public void log(String message) {
+        System.out.println(message);
+    }
+}
