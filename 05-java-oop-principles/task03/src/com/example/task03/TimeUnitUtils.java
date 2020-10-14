84918662cf1b55c8b4843113b50e765f1087eb7b
HiiiiItsMe <zavyalovartem79@gmail.com>
2020-10-14 17:20:21 +0500

    Завьялов Артём. Задание 5.03

--- 05-java-oop-principles/task03/src/com/example/task03/TimeUnitUtils.java
+++ 05-java-oop-principles/task03/src/com/example/task03/TimeUnitUtils.java
@@ -8,20 +8,24 @@ public class TimeUnitUtils {
     /**
      * Конвертирует интервал в секундах в интервал в миллисекундах
      *
-     * @param seconds интервал в секундах
+     * @param units
      * @return интервал в миллисекундах
      */
-    public static Milliseconds toMillis(Seconds seconds) {
-        return new Milliseconds(seconds.toMillis());
+    public static Milliseconds toMillis(TimeUnit units) {
+        return new Milliseconds(units.toMillis());
     }
 
     /**
      * Конвертирует интервал в миллисекундах в интервал в секундах
      *
-     * @param millis интервал в миллисекундах
+     * @param units
      * @return интервал в секундах
      */
-    public static Seconds toSeconds(Milliseconds millis) {
-        return new Seconds(millis.toSeconds());
+    public static Seconds toSeconds(TimeUnit units) {
+        return new Seconds(units.toSeconds());
     }
+
+    public static Minutes toMinutes(TimeUnit units) { return new Minutes(units.toMinutes()); }
+
+    public static Hours toHours(TimeUnit units) { return new Hours(units.toHours()); }
 }
