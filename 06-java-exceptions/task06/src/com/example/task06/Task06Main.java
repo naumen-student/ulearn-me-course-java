9e0e05b896578f10c574ca1fb96a69e5f5c3a6f1
NASTYA090701 <nastya90701@gmail.com>
2020-10-15 11:13:44 +0500

    Мокрова Анастасия,6.06

--- 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
+++ 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
@@ -2,9 +2,7 @@ package com.example.task06;
 
 public class Task06Main {
     public static void main(String[] args) {
-
         new Task06Main().printMethodName();
-
     }
 
     void printMethodName() {
