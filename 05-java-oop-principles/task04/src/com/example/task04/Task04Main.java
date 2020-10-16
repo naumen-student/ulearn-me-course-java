809c18fc50612f7295fc6f5c98422263aa969bd5
Fuzuki <temniakovslava@yandex.ru>
2020-10-16 09:10:20 +0500

    Темняков Вячеслав. Задание 5.4

--- 05-java-oop-principles/task04/src/com/example/task04/Task04Main.java
+++ 05-java-oop-principles/task04/src/com/example/task04/Task04Main.java
@@ -3,7 +3,6 @@ package com.example.task04;
 import java.time.temporal.ChronoUnit;
 
 public class Task04Main {
-
     public static void main(String[] args) {
 
         Logger test = new Logger("test");
