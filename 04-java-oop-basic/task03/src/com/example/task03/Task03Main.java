36e02fa5821001767905832651126d782da7444a
KozhevnikovAlexandr <kozhevnikov.alexandr8@yandex.ru>
2020-10-16 17:29:46 +0500

    Task 0403 completed

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,10 @@
 package com.example.task03;
 
 public class Task03Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        ComplexNumber a = new ComplexNumber(5, 10);
+        ComplexNumber b = new ComplexNumber(6, 7);
+        System.out.println(a.sum(b).toString());
+        System.out.println(a.multiplication(b).toString());
     }
 }
