c5de695b286d41a3dbe6481bff5471664551e5e1
kirillegorow <e.kirill.2001@gmail.com>
2020-10-16 18:30:55 +0500

    Егоров Кирилл task4.5

--- 04-java-oop-basic/task05/src/com/example/task05/Task05Main.java
+++ 04-java-oop-basic/task05/src/com/example/task05/Task05Main.java
@@ -2,6 +2,15 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
-
+        Point[] points = {
+                new Point(0, 0),
+                new Point(0, 1),
+                new Point(1, 1)
+        };
+        PolygonalLine pLine = new PolygonalLine();
+        pLine.setPoints(points);
+        System.out.println(pLine.getLength());
+        pLine.addPoint(3, 1);
+        System.out.println(pLine.getLength());
     }
 }
