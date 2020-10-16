9197787f1456de1e434dc4fa11f00383b04e2efb
Fuzuki <temniakovslava@yandex.ru>
2020-10-16 09:08:39 +0500

    Темняков Вячеслав. Задание 5.4

--- 05-java-oop-principles/task04/src/com/example/task04/MessageHandler.java
+++ 05-java-oop-principles/task04/src/com/example/task04/MessageHandler.java
@@ -1,5 +1,6 @@
 package com.example.task04;
 
-public interface MessageHandler{
-    void HandleMessage(String template, Object... messageArgs);
+public interface MessageHandler {
+    void log(Logger.Level level, String message);
+    void log(Logger.Level level, String pattern, Object... args);
 }
