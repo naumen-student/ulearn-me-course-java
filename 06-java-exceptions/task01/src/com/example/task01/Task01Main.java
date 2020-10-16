bafd23ea4577e3db4ef0a9a527e29fc7a89d1ea0
Semyon <semen2k1@mail.ru>
2020-10-16 20:50:35 +0500

    Task 6.1

--- 06-java-exceptions/task01/src/com/example/task01/Task01Main.java
+++ 06-java-exceptions/task01/src/com/example/task01/Task01Main.java
@@ -1,5 +1,7 @@
 package com.example.task01;
 
+import java.lang.reflect.Array;
+
 public class Task01Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -10,6 +12,7 @@ public class Task01Main {
     }
 
     static void codeWithNPE() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        Integer[] numbers = null;
+        System.out.println(numbers[0]);
     }
 }
