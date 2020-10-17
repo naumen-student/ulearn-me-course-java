1d546773231ae9ddb95b7817dd1339d541f926be
дом <anton89222910261123@yandex.ru>
2020-10-17 17:55:32 +0500

    Петров Антон Задача 4.41

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -3,5 +3,14 @@ package com.example.task04;
 public class Task04Main {
     public static void main(String[] args) {
 
+    public Line(Point p1, Point p2)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.length = Math.sqrt((p1.x - p2.y) * (p1.x - p2.y) + (p1.y - p2.y) * (p1.y - p2.y));
     }
-}
+
+    public Point getP1()
+    {
+        return p1;
+    }
