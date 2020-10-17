e071d931334798fcc82f5694f4f3717dba6d60a7
дом <anton89222910261123@yandex.ru>
2020-10-17 22:39:58 +0500

    Петров Антон Задача 4.5

--- 04-java-oop-basic/task05/src/com/example/task05/Point.java
+++ 04-java-oop-basic/task05/src/com/example/task05/Point.java
@@ -1,49 +1,29 @@
 package com.example.task05;
 
-/**
- * Точка в двумерном пространстве
- */
 public class Point {
 
-    /**
-     * Конструктор, инициализирующий координаты точки
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
-    public Point(double x, double y) {
-        throw new AssertionError();
+    private final double x;
+    private final double y;
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
     }
 
-    /**
-     * Возвращает координату точки по оси абсцисс
-     *
-     * @return координату точки по оси X
-     */
-    public double getX() {
-        // TODO: реализовать
-        throw new AssertionError();
+    public double getX()
+    {
+        return this.x;
     }
 
-    /**
-     * Возвращает координату точки по оси ординат
-     *
-     * @return координату точки по оси Y
-     */
-    public double getY() {
-        // TODO: реализовать
-        throw new AssertionError();
+    public double getY()
+    {
+        return this.y;
     }
 
-    /**
-     * Подсчитывает расстояние от текущей точки до точки, переданной в качестве параметра
-     *
-     * @param point вторая точка отрезка
-     * @return расстояние от текущей точки до переданной
-     */
-    public double getLength(Point point) {
-        // TODO: реализовать
-        throw new AssertionError();
+    public double getLength(Point point)
+    {
+        return Math.sqrt((this.x - point.x)*(this.x - point.x) + (this.y - point.y)*(this.y - point.y));
     }
 
 }
