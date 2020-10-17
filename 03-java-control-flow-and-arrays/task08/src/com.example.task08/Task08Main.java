75f73144efcfb9bd38270508db3f48bb7ac02f09
Konstantin <super1414@list.ru>
2020-10-17 17:05:44 +0500

    Князев Константин. Задание 3.08

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -13,8 +13,16 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        if (arr.length == 0) {
+            return 0;
+        }
+
+        long result = 1;
+        for(int e : arr){
+            result *= e;
+        }
+
+        return result;
     }
 
 }
