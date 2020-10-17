bbde56ba732a911ebfefb43d01084460183f0703
bearsbon <bearsbon@gmail.com>
2020-10-18 03:37:07 +0500

    3.9

--- 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
+++ 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
@@ -14,8 +14,14 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+
+        int min = arr[0];
+        for (int i : arr) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
     }
 
 }
