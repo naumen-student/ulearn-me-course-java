d7189a2470ecd827f18a55ec5a54b379a0b52331
дом <anton89222910261123@yandex.ru>
2020-10-17 17:44:37 +0500

    Петров Антон Задача 4.4

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -1,7 +1,58 @@
 package com.example.task04;
 
-public class Task04Main {
-    public static void main(String[] args) {
+class Point
+{
+    final int x;
+    final int y;
 
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString()
+    {
+        return String.format("(%d; %d)", x, y);
     }
 }
+
+class Line
+{
+    private Point p1;
+    private Point p2;
+    private double length;
+
+    public Line(Point p1, Point p2)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.length = Math.sqrt((p1.x - p2.y) * (p1.x - p2.y) + (p1.y - p2.y) * (p1.y - p2.y));
+    }
+
+    public Point getP1()
+    {
+        return p1;
+    }
+
+    public Point getP2()
+    {
+        return p2;
+    }
+
+    public String toString()
+    {
+        return String.format("[%str, %str] - length: %str", p1.toString(), p2.toString(), length);
+    }
+
+    public boolean isCollinearLine(Point p)
+    {
+        int dx1 = p2.x - p1.x;
+        int dy1 = p2.y - p1.y;
+        int dx = p.x - p1.x;
+        int dy = p.y - p1.y;
+        int s = dx1 * dy - dx * dy1;
+        return s == 0;
+    }
+}
+
