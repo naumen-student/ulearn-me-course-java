d651a77029e3a8d6668ad22c4cabf3a968f98c44
Ayzhan Taalaybek <ayzhan312@live.ru>
2020-10-18 01:27:22 +0500

    5_task04

---
+++ 05-java-oop-principles/task04/src/com/example/task04/ConsoleHandler.java
@@ -0,0 +1,9 @@
+package com.example.task04;
+
+public class ConsoleHandler implements MessageHandler{
+
+    @Override
+    public void logMes(String message) {
+        System.out.println(message);
+    }
+}
---
+++ 05-java-oop-principles/task04/src/com/example/task04/MemoryHandler.java
@@ -0,0 +1,26 @@
+package com.example.task04;
+
+import java.util.ArrayList;
+
+public class MemoryHandler implements MessageHandler {
+
+    private final long size;
+    private final ArrayList<String> messageList = new ArrayList<>();
+    private final MessageHandler handler;
+
+    public MemoryHandler(long size, MessageHandler handler) {
+        this.size = size;
+        this.handler = handler;
+    }
+
+    @Override
+    public void logMes(String message) {
+        messageList.add(message);
+        if (messageList.size() >= size) {
+            for (String savedMessage : messageList) {
+                handler.logMes(savedMessage);
+            }
+            messageList.clear();
+        }
+    }
+}
