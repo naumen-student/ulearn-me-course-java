de45f632407a85f5353be9256646fb18d92e65aa
IlyaRussia23 <56472066+IlyaRussia23@users.noreply.github.com>
2020-10-18 14:21:18 +0500

    Илья Гребенюк. 2.12

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -10,14 +10,11 @@ public class Task12 {
 
         // Считаем проценты за год
 
-        /*
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
-        */
 
-        return BigDecimal.ZERO;
+        for (int i = 1; i <= 12; i++) {
+            sum = sum.add(sum.multiply(percent));
+        }
+        return sum.setScale(9, BigDecimal.ROUND_HALF_UP);
     }
 
     public static void main(String[] args) {
