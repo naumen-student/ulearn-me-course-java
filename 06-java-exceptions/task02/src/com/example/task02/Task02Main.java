254c6340bd7268004931e1fbfb08ac910c66913f
diukin9 <petr.diukin@urfu.me>
2020-10-19 02:19:20 +0500

    Дюкин Петр 6.2

--- 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
+++ 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
@@ -10,8 +10,8 @@ public class Task02Main {
     static String getSeason(int monthNumber) {
         String[] seasons = new String[]{"зима", "весна", "лето", "осень"};
         if (monthNumber < 1 || monthNumber > 12)
-            throw new IllegalArgumentException(MessageFormat.format(
-                    "monthNumber {0} is invalid, month number should be between 1..12",
+            throw new IllegalArgumentException(String.format(
+                    "monthNumber %d is invalid, month number should be between 1..12",
                     monthNumber));
         else if (monthNumber == 12)
             return seasons[0];
