fa4314c38edd9365a19d0c0940044248b8a6e22e
TobeeM <mark.p200110@gmail.com>
2020-10-19 21:54:19 +0500

    Поташников Марк, Задание 2.11

--- 02-java-types/task11/src/com/example/task11/Task11.java
+++ 02-java-types/task11/src/com/example/task11/Task11.java
@@ -8,13 +8,6 @@ public class Task11 {
     }
 
     public static void main(String[] args) {
-
-        float sum = 500; // 500 руб. на счете
-        float percent = 0.00000001f; // 0.000001% ежемесячно
-
-        sum = benefit(sum, percent);
-
-        System.out.println("Сумма на счете через год: " + sum);
-
+        //
     }
 }
