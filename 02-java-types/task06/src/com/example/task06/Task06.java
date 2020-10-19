8013d7bc9d3d5820d0704ba48f7dc92ca524fa0c
nvit29 <gainetdinoa-nastya@ya.ru>
2020-10-19 18:48:20 +0500

    Гайнетдинова Анастасия. Задание 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -5,17 +5,18 @@ public class Task06 {
     public static int solution(int x, int y) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = x + y;
+        int result = 0;
+        while(sum != 0) {
+            result += 1;
+            sum /= 10;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
+        int result = solution(625, 5324);
         System.out.println(result);
-        */
     }
 
 }
