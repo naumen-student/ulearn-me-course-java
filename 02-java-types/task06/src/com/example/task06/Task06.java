bf5e6d2f307579a0d9bbb11a888a2bcf7f963151
nvit29 <gainetdinoa-nastya@ya.ru>
2020-10-19 19:38:23 +0500

    Гайнетдинова Анастасия. Задание 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,20 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int num = x + y;
+        String sum = Integer.toString(num);
+        int result = 0;
+        for (int i = 0; i < sum.length(); i++) {
+            result += 1;
+        }
 
-        return 0;
+        return result;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
+        int result = solution(625, 5324);
         System.out.println(result);
-        */
     }
 
 }
