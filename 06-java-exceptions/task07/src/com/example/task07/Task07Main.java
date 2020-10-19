eea35f537002fcaadcbefb3caefd45ceb8a9e7ad
Andrey Emelyanov <56556713+reshxs@users.noreply.github.com>
2020-10-19 16:12:03 +0500

    06-task07

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -12,13 +12,14 @@ public class Task07Main {
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+            processor.process();
+        } catch (RuntimeException e) {
+            return "unchecked";
         } catch (Exception e) {
-
+            return "checked";
         }
-        return null;
+        return "none";
     }
 
 }
