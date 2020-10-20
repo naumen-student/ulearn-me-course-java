bbd93233339c4a911223caae2bb0b59a738bb24c
Fuzuki <temniakovslava@yandex.ru>
2020-10-20 18:00:35 +0500

    Темняков Вячеслав. Задание 6.4

--- 06-java-exceptions/task04/src/com/example/task04/MyException.java
+++ 06-java-exceptions/task04/src/com/example/task04/MyException.java
@@ -1,8 +1,7 @@
 package com.example.task04;
 
 public class MyException extends IllegalArgumentException {
-
-    public MyException(String message) { super(message); }
-
-    public MyException(String message, Throwable cause) { super(message, cause); }
+    public MyException(String message) {
+        super(message);
+    }
 }
