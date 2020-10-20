bb6715d989a9fe0f69c2bb5528f3085b70933b5e
Asus <vya4eslav.rublev@yandex.ru>
2020-10-20 11:28:11 +0500

    Рублев Вячеслав. Задание 6.7

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -12,13 +12,13 @@ public class Task07Main {
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+            processor.process();
         } catch (Exception e) {
-
+            if (e instanceof RuntimeException) return UNCHECKED;
+            return CHECKED;
         }
-        return null;
+        return NONE;
     }
 
 }
