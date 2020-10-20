079572bb8f5ec111238cf5ed6aec6614bfb8c65b
Жора <JoraKochkin@yandex.ru>
2020-10-20 23:07:21 +0500

    Кочкин Георгий Задание 6-3

--- 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
+++ 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
@@ -2,14 +2,8 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        throwCheckedException();
-         */
     }
 
-    //todo напишите здесь свою корректную реализацию задания
     public static void throwCheckedException() throws Exception {
         throw new Exception("");
     }
