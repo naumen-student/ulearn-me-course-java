6e50359078262148dff087414d6966fe10ac972b
TobeeM <mark.p200110@gmail.com>
2020-10-20 19:52:44 +0500

    Поташников Марк, Задание 3.13

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -1,19 +1,41 @@
 package com.example.task13;
 
+import com.sun.tools.javac.util.ArrayUtils;
+import sun.security.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task13Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int[] arr = {9, 1100, 7, 8};
         removeMoreThen1000(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+
+        if (arr == null) return null;
+
+        int wrongNumbersCount = 0;
+        int currentIndex = 0;
+
+        for (int number : arr) {
+            if (number > 1000) wrongNumbersCount++;
+        }
+
+        int[] resArr = new int[arr.length - wrongNumbersCount];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 1000) {
+                resArr[currentIndex] = arr[i];
+                currentIndex++;
+            }
+        }
+        return resArr;
     }
 
 }
