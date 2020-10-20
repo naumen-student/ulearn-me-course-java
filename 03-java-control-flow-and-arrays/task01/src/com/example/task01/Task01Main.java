ffcaefcec0c338705ff0c3e11bcee6841ebadaea
TobeeM <mark.p200110@gmail.com>
2020-10-20 16:29:45 +0500

    Поташников Марк, Задание 3.1

--- 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
+++ 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
@@ -3,7 +3,7 @@ package com.example.task01;
 public class Task01Main {
     public static void main(String[] args) {
         //
-
+        //
     }
 
     static boolean canGetDriverLicense(int age) {
