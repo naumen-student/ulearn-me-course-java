9db48cc236a2313c805f0c7fc082c7c13c5ede30
TobeeM <mark.p200110@gmail.com>
2020-10-20 18:04:53 +0500

    Поташников Марк, Задание 3.3

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -10,6 +10,8 @@ public class Task03Main {
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String[] namesOfWeekDays = new String[] {"понедельник", "вторник", "среда", "четверг",
+                "пятница", "суббота", "воскресенье"};
+        return namesOfWeekDays[weekDaysNumber - 1];
     }
 }
