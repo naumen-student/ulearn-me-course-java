1496fbde230631ba10b088f3bb0bddae9670802a
Lera <Valeria.Smirnova@urfu.me>
2020-10-21 21:25:30 +0500

    Смирнова Лера 04_05

--- 04-java-oop-basic/task05/src/com/example/task05/Task05Main.java
+++ 04-java-oop-basic/task05/src/com/example/task05/Task05Main.java
@@ -2,6 +2,14 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
-
+        Point[] point =
+                {
+                        new Point(1, 2),
+                        new Point(8, 9),
+                        new Point(11, 42)
+                };
+        PolygonalLine pointline = new PolygonalLine();
+        pointline.setPoints(point);
+        System.out.println(pointline.getLength());
     }
 }
