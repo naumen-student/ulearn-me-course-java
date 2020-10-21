6d9c3e04c51593a0dc97c0e7ff90158143f070ce
Gorr <gorrgorrob@gmail.com>
2020-10-21 21:48:46 +0500

    Бородин Владислав. Задание 6.7

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -7,18 +7,20 @@ public class Task07Main {
     public static final String NONE = "none";
 
     public static void main(String[] args) {
+        System.out.println(new Task07Main().getExceptionType());
     }
 
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+            processor.process();
+            return NONE;
+        } catch (RuntimeException e) {
+            return UNCHECKED;
         } catch (Exception e) {
-
+            return CHECKED;
         }
-        return null;
     }
 
 }
