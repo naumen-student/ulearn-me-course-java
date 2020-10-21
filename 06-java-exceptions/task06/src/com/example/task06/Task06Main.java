c73a294a094123add9b522656be24aa6734c6c9b
Gorr <gorrgorrob@gmail.com>
2020-10-21 21:40:07 +0500

    Бородин Владислав. Задание 6.6

--- 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
+++ 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
@@ -2,15 +2,12 @@ package com.example.task06;
 
 public class Task06Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         new Task06Main().printMethodName();
-         */
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        StackTraceElement[] stackTrace = new Exception().getStackTrace();
+        System.out.print(stackTrace[1].getMethodName());
     }
 
 }
