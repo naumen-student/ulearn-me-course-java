ddf19e54104ccfc777c1893e604d940c8dd6f32e
Traiz0r4 <64859204+Traiz0r4@users.noreply.github.com>
2020-10-21 23:09:05 +0500

    Потапов Андрей 6.4

--- 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
+++ 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
@@ -10,8 +10,7 @@ public class Task04Main {
 
     static String getSeason(int monthNumber) {
         if ( monthNumber > 12 || monthNumber < 1) {
-            throw new MyException(("monthNumber " + monthNumber
-                    + " is invalid, month number should be between 1..12"));
+            throw new MyException(String.format("monthNumber %d is invalid, month number should be between 1..12", monthNumber));
         }
 
         return new String[] {"зима", "весна", "лето", "осень"}[monthNumber == 12? 0: monthNumber / 3];
