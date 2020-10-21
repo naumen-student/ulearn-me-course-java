911eed93f05a5a91d0d0052b60857a413bbd9b70
Gorr <gorrgorrob@gmail.com>
2020-10-21 20:41:13 +0500

    Бородин Владислав. Задание 6.2

--- 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
+++ 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
@@ -1,16 +1,60 @@
 package com.example.task02;
 
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task02Main {
 
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getSeason(-5));
-         */
+        getSeason(-5);
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber < 1 || monthNumber > 12)
+            throw new IllegalArgumentException(
+                MessageFormat.format("monthNumber {0} is invalid, month number should be between 1..12", monthNumber)
+            );
+
+        String result = "";
+
+        switch (Seasons.getSeason(monthNumber)) {
+            case WINTER:
+                result = "зима";
+                break;
+            case SPRING:
+                result = "весна";
+                break;
+            case SUMMER:
+                result = "лето";
+                break;
+            case AUTUMN:
+                result = "осень";
+                break;
+        }
+
+        return result;
+    }
+
+    private enum Seasons {
+        WINTER(12, 1, 2),
+        SPRING(3, 4, 5),
+        SUMMER(6, 7, 8),
+        AUTUMN(9, 10, 11),
+        ;
+
+        private final List<Integer> months;
+        Seasons(Integer... months) {
+            this.months = Arrays.asList(months);
+        }
+
+        public static Seasons getSeason(int monthNum) {
+            for (Seasons season : Seasons.values()) {
+                if (season.months.contains(monthNum))
+                    return season;
+            }
+
+            throw new IllegalArgumentException("Month number should be in (1..12) range");
+        }
     }
 }
