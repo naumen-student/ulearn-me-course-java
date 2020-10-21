5b175886907361b822633754b1e5fbfdd81cf226
Андрей <andryfka_3rd@mail.ru>
2020-10-21 18:52:32 +0500

    Тыщенко Андрей. Task 6.07

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -1,24 +1,35 @@
 package com.example.task07;
 
-public class Task07Main {
+public class Task07Main
+{
 
     public static final String CHECKED = "checked";
     public static final String UNCHECKED = "unchecked";
     public static final String NONE = "none";
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
     }
 
     public Processor processor;
 
-    public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
-
+    public String getExceptionType()
+    {
+        try
+        {
+            processor.process();
         }
-        return null;
-    }
 
+        catch (RuntimeException e)
+        {
+            return UNCHECKED;
+        }
+
+        catch (Exception e)
+        {
+            return CHECKED;
+        }
+        return NONE;
+    }
 }
