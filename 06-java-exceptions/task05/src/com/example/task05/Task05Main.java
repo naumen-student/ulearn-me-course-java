f668ced89947d5b638abf96c922117a90674a4cc
S1ide <kopotev.nick@icloud.com>
2020-10-21 15:40:25 +0500

    Копотев Никита, задание 6.5

--- 06-java-exceptions/task05/src/com/example/task05/Task05Main.java
+++ 06-java-exceptions/task05/src/com/example/task05/Task05Main.java
@@ -1,28 +1,56 @@
 package com.example.task05;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileSystemNotFoundException;
 
 public class Task05Main {
-    public static void main(String[] args) throws IOException {
-        String pathToFile = args[0]; // "/home/user/file.txt"
+    public static void main(String[] args){
+        String pathToFile = null,s = null;
+        try {
+            pathToFile = args[0]; // "/home/user/file.txt"
+            throw new FileNotFoundException();
+        } catch (FileNotFoundException e) {
+            System.out.println(String.format("файл %d не найден", pathToFile));
+        }
 
-        String s = readFile(pathToFile);
+        try{
+            s = readFile(pathToFile);
+            throw new IOException();
+        }
+        catch (IOException exception){
+            System.out.println(String.format("произошла ошибка при чтении файла %d", pathToFile));
+        }
         System.out.println(s);
     }
 
-    public static String readFile(String pathToFile) throws IOException {
-        FileReader fileReader = new FileReader(pathToFile);
+    public static String readFile(String pathToFile){
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(pathToFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
         StringBuilder stringBuilder = new StringBuilder();
-        String currentLine;
-        while ((currentLine = bufferedReader.readLine()) != null) {
+        String currentLine = null;
+        while (true) {
+            try {
+                if (!((currentLine = bufferedReader.readLine()) != null)) break;
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
             stringBuilder.append(currentLine);
             stringBuilder.append("\n");
         }
-        bufferedReader.close();
+        try {
+            bufferedReader.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
 
         return stringBuilder.toString();
     }
