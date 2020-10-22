c1f9ac345053f0508299c9c55b2e2decd12bed36
Haggard13 <alexey.kozedubov@urfu.me>
2020-10-22 14:37:42 +0500

    Козедубов Алексей. Задание 6.1

--- 06-java-exceptions/task01/src/com/example/task01/Task01Main.java
+++ 06-java-exceptions/task01/src/com/example/task01/Task01Main.java
@@ -10,6 +10,7 @@ public class Task01Main {
     }
 
     static void codeWithNPE() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String s = null;
+        s.toString();
     }
 }
