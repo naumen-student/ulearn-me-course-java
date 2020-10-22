b7885b6b37e2de1589ca9abceeb95e85bdc49848
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2020-10-22 08:41:07 +0500

    Update Logger.java

--- 05-java-oop-principles/task01/src/com/example/task01/Logger.java
+++ 05-java-oop-principles/task01/src/com/example/task01/Logger.java
@@ -1,8 +1,7 @@
 package com.example.task01;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Date;
 
 enum Level {
