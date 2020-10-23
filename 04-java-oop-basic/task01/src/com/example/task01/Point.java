0d48caa892cee6314e5fa1a7bc7c1437d4753db5
djafar kurbanov <djafar.kurbanow@gmail.ru>
2020-10-23 13:16:13 +0500

    Курбанов Джафар. Задача 4.1

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -7,6 +7,23 @@ public class Point {
     int x;
     int y;
 
+    void flip() {
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+
+    double distance(Point point){
+        int deltaX = point.x - x;
+        int deltaY = point.y - y;
+        return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
