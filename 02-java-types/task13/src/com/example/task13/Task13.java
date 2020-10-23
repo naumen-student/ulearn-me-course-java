365a333bf9692cf51a645328f4a647869d0a586f
ArbyzikYA <66357650+ArbyzikYA@users.noreply.github.com>
2020-10-23 11:24:12 +0500

    Update Task13.java

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -2,20 +2,19 @@ package com.example.task13;
 
 public class Task13 {
 
+    
     public static char toUpperCase(char c) {
 
-        // TODO привести букву к верхнему регистру
+        
 
-        return c;
+        return "c".toUpperCase().charAt(0);
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        char result = solution('x');
+        
+        char result = toUpperCase('x');
         System.out.println(result);
-         */
+       
     }
 
 }
