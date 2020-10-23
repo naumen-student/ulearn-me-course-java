715d4601018c44ea687a6d4689900fb92abe81a5
Protages <k.stif@mail.ru>
2020-10-23 15:30:04 +0500

    Трошин Вадим Задание 3.01

--- 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
+++ 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
@@ -4,13 +4,13 @@ public class Task01Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         boolean b = canGetDriverLicense(100);
         System.out.println(b);
-         */
+
     }
 
     static boolean canGetDriverLicense(int age) {
-        return true;//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        return age >= 18;
     }
 }
