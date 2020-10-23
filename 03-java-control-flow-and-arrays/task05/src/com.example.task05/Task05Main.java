910bdb0a9e4ef67972a49abaee100efce6c735a8
JIiCeHoK <foxs29546@gmail.com>
2020-10-23 19:36:12 +0500

    Моисеев Михаил. Задача 3.05

--- 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
+++ 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
@@ -5,8 +5,8 @@ public class Task05Main {
         static int getMin ( int a, int b, int c)
         {
             if (a < b && a < c) return a;
-            if (b < a && b < c) return b;
-            return c;
+            else if (b < a && b < c) return b;
+            else return c;
 
         }
 
