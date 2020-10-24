803c3506a21a4981aa1539d78b846901c1112ae0
OperationIntruder313 <71594631+OperationIntruder313@users.noreply.github.com>
2020-10-25 01:25:58 +0500

    Курбанов Джафар. Задача 4.1

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -4,8 +4,16 @@ package com.example.task01;
  * Класс точки на плоскости
  */
 public class Point {
+
     int x;
     int y;
+
+    public  Point(){}
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 
     void flip() {
         int temp = x;
@@ -19,13 +27,11 @@ public class Point {
         return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
     }
 
-    @Override
     public String toString() {
-        return String.format("(%d %d)", x, y);
+        return String.format("(%d, %d)", x, y);
     }
 
     void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+        System.out.println(toString());
     }
 }
