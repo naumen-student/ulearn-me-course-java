ff1c458a117555499948d72a5e034cf0cd0222e1
IlyaRussia23 <56472066+IlyaRussia23@users.noreply.github.com>
2020-10-25 11:07:59 +0500

    Илья Гребенюк. 4.04

---
+++ 04-java-oop-basic/task04/src/com/example/task04/Point.java
@@ -0,0 +1,15 @@
+package com.example.task04;
+
+public class Point {
+    final double x, y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(){
+        x = 0;
+        y = 0;
+    }
+}
--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,10 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Line line1 = new Line(-2, 0, 2, 0);
+        Point p1 = new Point(0, 1);// Не на одной прямой
+        Point p2 = new Point(0, 0);// На одной прямой
+        System.out.println(line1.isCollinearLine(p1));
+        System.out.println(line1.isCollinearLine(p2));
     }
 }
