337c62bfd12e6b77b09cfeaec3ca31a43110435e
OperationIntruder313 <71594631+OperationIntruder313@users.noreply.github.com>
2020-10-25 23:01:03 +0500

    Курбанов Джафар. Задача 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,14 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber Z1 = new ComplexNumber(15.3, 10.234);
+        ComplexNumber Z2 = new ComplexNumber(16, 8.17);
+        Z1.printComplexNumber();
+        Z2.printComplexNumber();
+        System.out.println(Z1.addComplex(Z1, Z2));
+        System.out.println(Z1.subtractComplex(Z1, Z2));
+        System.out.println(Z1.multiplyComplex(Z1, Z2));
     }
 }
+
+
