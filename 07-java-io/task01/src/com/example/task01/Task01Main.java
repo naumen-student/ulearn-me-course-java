9df760b778d54072b792d8a71df62845188159cf
And21rew <Andrei.Bubnov@at.urfu.ru>
2020-10-26 14:06:02 +0500

    Бубнов Андрей. Задание 7.1

--- 07-java-io/task01/src/com/example/task01/Task01Main.java
+++ 07-java-io/task01/src/com/example/task01/Task01Main.java
@@ -2,20 +2,22 @@ package com.example.task01;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
-
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null)
+            throw new IllegalArgumentException();
+        int controlSum = 0;
+        int lastByte = inputStream.read();
+        while (lastByte >= 0) {
+            controlSum = Integer.rotateLeft(controlSum, 1) ^ lastByte;
+            lastByte = inputStream.read();
+        }
+        return controlSum;
     }
 }
