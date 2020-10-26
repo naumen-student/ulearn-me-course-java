1576465e7ac47dd05b8c9c39e8424bdd718fa301
S1ide <kopotev.nick@icloud.com>
2020-10-26 10:40:11 +0500

    Копотев Никита, задание 6.7

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -1,5 +1,8 @@
 package com.example.task07;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
 public class Task07Main {
 
     public static final String CHECKED = "checked";
@@ -12,13 +15,16 @@ public class Task07Main {
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
-
+            processor.process();
         }
-        return null;
+        catch (RuntimeException e){
+            return UNCHECKED;
+        }
+        catch (Exception e){
+            return CHECKED;
+        }
+        return NONE;
     }
 
 }
