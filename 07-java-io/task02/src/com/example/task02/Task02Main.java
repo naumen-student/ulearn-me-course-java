955e93b06d526a8a8188631ab27b2a22d825378a
And21rew <Andrei.Bubnov@at.urfu.ru>
2020-10-26 14:41:00 +0500

    Бубнов Андрей. Задание 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -4,11 +4,6 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
         int previousByte = System.in.read();
         int currentByte;
         while (previousByte >= 0) {
