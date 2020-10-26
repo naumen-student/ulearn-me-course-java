d5ffd2d2102ab4b03da410f24b96a73d424ae214
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2020-10-27 00:44:52 +0500

    Кашкин Сергей 3.6

--- 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
+++ 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
@@ -10,8 +10,7 @@ public class Task06Main {
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return Math.max(Math.max(a, b), Math.max(c, d));
     }
 
 }
