af975a6d9da8bed24ee4f70602866f7bf43fbadb
Meh <zavyalovartem79@gmail.com>
2020-10-27 08:25:30 +0500

    Завьялов Артём. Задание 7.03

--- 07-java-io/task03/src/com/example/task03/Task03Main.java
+++ 07-java-io/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,8 @@ package com.example.task03;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.Charset;
 
 public class Task03Main {
@@ -15,7 +17,17 @@ public class Task03Main {
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null || charset == null){
+            throw new IllegalArgumentException();
+        }
+        Reader reader = new InputStreamReader(inputStream, charset);
+        int charsRead;
+        int bufferSize = 1024;
+        char[] buffer = new char[bufferSize];
+        StringBuilder stringBuilder = new StringBuilder();
+        while((charsRead = reader.read(buffer, 0, bufferSize)) >= 0){
+            stringBuilder.append(buffer, 0, charsRead);
+        }
+        return stringBuilder.toString();
     }
 }
