e24ac64c7b1e53322bb44eb1580288cdc8284afd
Kirill <vvvw_00@mail.ru>
2020-10-27 22:42:02 +0500

    Ворожцов Кирилл Task 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,34 @@
 package com.example.task02;
 
-import java.io.IOException;
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+import java.io.*;
+import java.util.Scanner;
 
 public class Task02Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+    public static void main(String[] args) throws IOException
+    {
+        BufferedInputStream reader = new BufferedInputStream(System.in);
+
+        int first=reader.read();
+        int second = reader.read();
+
+        while (first!=-1)
+        {
+            if (first==13 && second==10)
+            {
+                 System.out.write(10);
+                 first=reader.read();
+            }
+            else
+            {
+                System.out.write(first);
+                first=second;
+            }
+            second = reader.read();
+        }
+
+        reader.close();
+        System.out.flush();
     }
 }
