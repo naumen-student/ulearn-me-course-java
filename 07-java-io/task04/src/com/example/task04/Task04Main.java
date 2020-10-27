82f5d670a3744963e3f8e9c3fc03b8c53433f8b7
Semyon <semen2k1@mail.ru>
2020-10-27 19:20:08 +0500

    Task 7.4

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,13 @@
 package com.example.task04;
 
-import java.io.IOException;
+import org.assertj.core.util.Files;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.Scanner;
+
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +15,12 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        Reader reader = new InputStreamReader(System.in);
+        Scanner scanner = new Scanner(reader).useLocale(Locale.US);
+        double sum = 0;
+        while (scanner.hasNextDouble())
+            sum += scanner.nextDouble();
+        System.out.printf(Locale.US, "%.6f", sum);
 
-        System.out.println("0.0");
     }
 }
