881784c2987acdb657fb895245f50474b229b577
Semyon <semen2k1@mail.ru>
2020-10-27 16:40:26 +0500

    Task 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,9 @@
 package com.example.task02;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +12,14 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        int previousByte = System.in.read();
+        int currentByte;
+        while(previousByte != -1){
+            currentByte = System.in.read();
+            if (!(previousByte == 13 && currentByte == 10))
+                System.out.write(previousByte);
+            previousByte = currentByte;
+        }
+        System.out.flush();
     }
 }
