b77639dba806f8a558384a9e98d5ca8f3cb24615
Iulia Chebykina <julia.chebykina.13@gmail.com>
2020-10-27 12:42:41 +0500

    Чебыкина Юлия. Задание 7.03

--- 07-java-io/task03/src/com/example/task03/Task03Main.java
+++ 07-java-io/task03/src/com/example/task03/Task03Main.java
@@ -1,5 +1,6 @@
 package com.example.task03;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
@@ -9,13 +10,16 @@ public class Task03Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
         System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null || charset == null) {
+            throw new IllegalArgumentException();
+        }
+        byte[] arr = new byte[inputStream.available()];
+        int inputChar = inputStream.read(arr);
+        inputStream.close();
+        return new String(arr, charset);
     }
 }
