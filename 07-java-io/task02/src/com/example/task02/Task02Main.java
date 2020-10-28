49c8e4de5e3ed3adac4042100213a6efbea766d7
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-10-28 20:30:25 +0500

    Update Task02Main.java

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,28 @@
 package com.example.task02;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        BufferedInputStream reader = new BufferedInputStream(System.in);
+        int first = reader.read();
+        int second = reader.read();
+        while (first != -1)
+        {
+            if (first == 13 && second == 10)
+            {
+                System.out.write(10);
+                first = reader.read();
+            }
+            else
+            {
+                System.out.write(first);
+                first = second;
+            }
+            second = reader.read();
+        }
+        reader.close();
+        System.out.flush();
     }
 }
