d7be0c156c723d795f479fe89db7b70caaa86eb2
nukit181 <nik_babin@inbox.ru>
2020-10-28 21:13:03 +0500

    Бабин Никита. Задание 4.4

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -1,7 +1,34 @@
 package com.example.task04;
 
+import java.awt.*;
+
 public class Task04Main {
     public static void main(String[] args) {
+    }
+}
 
+class Line{
+    public Line (Point p1, Point p2){
+        this.point1 = p1;
+        this.point2 = p2;
+    }
+
+    private final Point point1;
+    private final Point point2;
+
+    public Point getP1(){
+        return this.point1;
+    }
+
+    public Point getP2(){
+        return this.point2;
+    }
+
+    public String toString(){
+        return String.format("P1: "+ point1.toString()+ "\nP2: "+ point2.toString());
+    }
+
+    public boolean isCollinearLine(Point p){
+        return (p.x - point1.x)/(point2.x - point1.x) == (p.y - point1.y) / (point2.y - point1.y);
     }
 }
