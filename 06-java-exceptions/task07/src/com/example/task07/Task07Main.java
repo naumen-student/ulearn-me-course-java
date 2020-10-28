0da01ec447ddbd3b84b62673f092097e44bf34c5
RoyalPiano <zelyoniyoleg@yandex.ru>
2020-10-29 02:51:27 +0500

    з

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -1,5 +1,8 @@
 package com.example.task07;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Task07Main {
 
     public static final String CHECKED = "checked";
@@ -12,13 +15,16 @@ public class Task07Main {
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
-
+            processor.process();
         }
-        return null;
-    }
+        catch (IOException e) {
+            return CHECKED;
+        }
+        catch (Error | Exception e) {
+            return UNCHECKED;
+        }
 
+        return NONE;
+    }
 }
