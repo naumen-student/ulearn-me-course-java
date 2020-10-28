5b699df8d5026fa6754d31ace427124643749240
Traiz0r4 <64859204+Traiz0r4@users.noreply.github.com>
2020-10-28 12:51:07 +0500

    Потапов Андрей Task7.4

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,18 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+        Reader reader = new InputStreamReader(System.in);
+        Scanner scanner = new Scanner(reader).useLocale(Locale.US);
+        double sum = 0;
+        while (scanner.hasNextDouble())
+            sum += scanner.nextDouble();
+        System.out.printf(Locale.US, "%.6f", sum);
     }
 }
