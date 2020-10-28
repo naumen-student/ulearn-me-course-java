17a53943fe619ed07f1b77f71c1dc0769d506ae1
NASTYA090701 <nastya90701@gmail.com>
2020-10-28 11:18:15 +0500

    Мокрова Анастасия,7.01

--- 07-java-io/task01/src/com/example/task01/Task01Main.java
+++ 07-java-io/task01/src/com/example/task01/Task01Main.java
@@ -1,21 +1,27 @@
 package com.example.task01;
-
 import java.io.IOException;
 import java.io.InputStream;
 
 public class Task01Main {
-    public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
+        public static void main(String[] args)  {
+            //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+            // например вот так:
 
         /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
         */
+        }
 
+        public static int checkSumOfStream(InputStream inputStream) throws IOException {
+            if (inputStream==null)
+                throw  new IllegalArgumentException();
+            int first = inputStream.read();
+            int sum=0;
+            while (first!=-1 )
+            {
+                sum = Integer.rotateLeft(sum,1)^first;
+                first=inputStream.read();
+            }
+            return sum;
+        }
     }
-
-    public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
-    }
-}
