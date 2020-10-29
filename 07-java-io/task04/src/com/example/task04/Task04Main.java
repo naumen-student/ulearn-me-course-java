a865ba4cda0c57126950490ebd888b19c16ac13a
Danil Konkov <buroroll@gmail.com>
2020-10-30 00:41:07 +0500

    Коньков Данил. Задание 7.4

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,20 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+        double res = 0;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        while(scanner.hasNext()){
+            if(scanner.hasNextDouble())
+                res += scanner.nextDouble();
+            else
+                scanner.next();
+        }
+        scanner.close();
+        System.out.printf(Locale.ENGLISH, "%.6f", res);
     }
 }
