2bcd0fe1e7129ba3a5396c7ebc4c64022c6f3147
Danil Harlamtsev <harlamtsev.danil@gmail.com>
2020-10-29 22:20:31 +0500

    Харламцев Данил задача 5.3

--- 05-java-oop-principles/task03/src/com/example/task03/Milliseconds.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Milliseconds.java
@@ -17,12 +17,11 @@ public class Milliseconds implements TimeUnit {
     }
 
     @Override
-    public long toSeconds() {
-        return amount / 1000;
-    }
+    public long toSeconds() { return Math.round((double) amount / 1000); }
 
     @Override
-    public long toMinutes() {
-        return amount / 1000 * 60;
-    }
+    public long toMinutes() { return Math.round((double) amount / (1000 * 60)); }
+
+    @Override
+    public long getHours() { return amount / (1000 * 60 * 60); }
 }
--- 05-java-oop-principles/task03/src/com/example/task03/Minutes.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Minutes.java
@@ -1,27 +1,27 @@
 package com.example.task03;
 
+/**
+ * Интервал в минутах
+ */
 public class Minutes implements TimeUnit {
 
+    private final long amount;
+
     public Minutes(long amount) {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
+        this.amount = amount;
     }
 
     @Override
-    public long toMillis() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
-    }
+    public long toMillis()  { return amount * 60 * 1000; }
 
     @Override
-    public long toSeconds() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
-    }
+    public long toSeconds()  { return amount * 60; }
 
     @Override
-    public long toMinutes() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
+    public long toMinutes()  { return amount; }
+
+    @Override
+    public long getHours()   {
+        return amount / 60;
     }
 }
