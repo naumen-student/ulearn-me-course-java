62fb978131962375c19a5f7725129fb75c0bed08
Kristina Sadykova <kristina1997g@mail.ru>
2020-10-29 14:00:13 +0500

    Садыков Владислав. Задание 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,10 +3,14 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long numbers = Long.parseLong(input);
+        if (Byte.MAX_VALUE >= numbers & numbers >= Byte.MIN_VALUE)
+            return "byte";
+        else if (Short.MAX_VALUE >= numbers & numbers >= Short.MIN_VALUE)
+            return "short";
+        else if (Integer.MAX_VALUE >= numbers & numbers >= Integer.MIN_VALUE)
+            return "int";
+        else return "long";
     }
 
     public static void main(String[] args) {
