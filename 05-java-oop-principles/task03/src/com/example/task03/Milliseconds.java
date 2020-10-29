d79d1610caabc6469e698f201ca482b275d8d495
kirillegorow <e.kirill.2001@gmail.com>
2020-10-29 13:08:16 +0500

    task5.3 Егоров Кирилл

--- 05-java-oop-principles/task03/src/com/example/task03/Milliseconds.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Milliseconds.java
@@ -1,28 +1,26 @@
 package com.example.task03;
 
-/**
- * Интервал в миллисекундах
- */
 public class Milliseconds implements TimeUnit {
-
     private final long amount;
-
     public Milliseconds(long amount) {
         this.amount = amount;
     }
-
     @Override
     public long toMillis() {
         return amount;
     }
-
     @Override
     public long toSeconds() {
-        return amount / 1000;
+        return Math.round(amount / 1000.0);
     }
 
     @Override
     public long toMinutes() {
-        return amount / 1000 * 60;
+        return Math.round(amount / (1000.0 * 60.0));
+    }
+
+    @Override
+    public long toHours() {
+        return Math.round(amount / (1000.0 * 60.0 * 60.0));
     }
 }
