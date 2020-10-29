f3693aa39f50b9fa964f5d78856438d74966ed85
LilStupidPig20 <mistersaxar@gmail.com>
2020-10-29 16:46:11 +0500

    Волгарев Артем. Задание 2.7

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -3,20 +3,16 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-        n /= k;
-        m /= k;
-        n *= k;
-        m *= k;
-        int area = n * m;
-        int tent = k * k;
-        return (area / tent);
+        n = n / k;
+        m = m / k;
+        return (n * m);
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        int result = solution(20, 50, 3);
+        int result = solution(1000000, 1000000, 50);
         System.out.println(result);
 
     }
