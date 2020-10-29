a743855e80f6aaf844c037ef1521d19e9d755916
LilStupidPig20 <mistersaxar@gmail.com>
2020-10-29 16:39:35 +0500

    Волгарев Артем. Задание 2.7

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -3,19 +3,22 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        n /= k;
+        m /= k;
+        n *= k;
+        m *= k;
+        int area = n * m;
+        int tent = k * k;
+        return (area / tent);
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(10, 20, 5);
+
+        int result = solution(20, 50, 3);
         System.out.println(result);
-        */
+
     }
 
 }
