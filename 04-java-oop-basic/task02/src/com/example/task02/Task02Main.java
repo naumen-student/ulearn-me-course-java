ec00051ad927a478a062ae9eb2706ce9f9ef3d96
517v3r <o-gybanov@mail.ru>
2020-10-29 21:24:21 +0500

    Губанов Олег. Задание 4.2

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,77 @@
 package com.example.task02;
 
-public class Task02Main {
-    public static void main(String[] args) {
+class TimeSpan{
+    private int hours;
+    private int minutes;
+    private int seconds;
 
+    public TimeSpan(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void add(TimeSpan time) {
+        this.seconds += time.seconds;
+        if (seconds >= 60) {
+            seconds -= 60;
+            minutes += 1;
+        }
+
+        this.minutes += time.minutes;
+        if (minutes >= 60) {
+            minutes -= 60;
+            hours += 1;
+        }
+
+        this.hours += time.hours;
+        if (hours >= 24)
+            hours -= 24;
+    }
+
+    public void subtract(TimeSpan time) {
+        this.seconds -= time.seconds;
+        if (seconds < 0) {
+            seconds += 60;
+            minutes -= 1;
+        }
+
+        this.minutes -= time.minutes;
+        if (minutes < 0) {
+            minutes += 60;
+            hours -= 1;
+        }
+
+        this.hours -= time.hours;
+        if (hours < 0)
+            hours += 24;
+    }
+
+    public String toString() {
+        return String.format("%d:%d:%d", hours, minutes, seconds);
     }
 }
