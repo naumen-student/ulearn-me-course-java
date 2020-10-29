922a645a4afda712b3a0f1122161f545449c8faa
evanesenze <evanesenze@gamil.com>
2020-10-30 01:52:48 +0500

    Базуев Иван.07.Task02

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -5,9 +5,10 @@ import java.io.IOException;
 public class Task02Main {
     public static void main(String[] args) throws IOException {
         int first = System.in.read();
+        int second;
         while (first != -1) {
-            int second = System.in.read();
-            if(first != 13 || second != 10) System.out.write(second);
+            second = System.in.read();
+            if(first != 13 || second != 10) System.out.write(first);
             first = second;
         }
         System.out.flush();
