dbf0c93586f5dbd7de8146d8453f24941397aba7
517v3r <o-gybanov@mail.ru>
2020-10-29 21:45:59 +0500

    Губанов Олег. Задание 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,40 @@
 package com.example.task03;
 
+class Complex {
+    public int real;
+    public int imaginary;
+
+    public Complex(int real, int imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public Complex sum(Complex complex) {
+        return new Complex(real + complex.real, imaginary + complex.imaginary);
+    }
+
+    public Complex multiply(Complex complex) {
+        return new Complex((real * complex.real - imaginary * complex.imaginary),
+                (real * complex.imaginary + imaginary * complex.real));
+    }
+
+    public String toString() {
+        if (imaginary >= 0)
+            return String.format("%d + %dj", real, imaginary);
+        else
+            return String.format("%d - %dj", real, Math.abs(imaginary));
+    }
+}
+
 public class Task03Main {
     public static void main(String[] args) {
-
+        Complex first = new Complex(3, -2);
+        Complex second = new Complex(4, 1);
+        Complex sum = first.sum(second);
+        Complex multiply = first.multiply(second);
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(sum);
+        System.out.println(multiply);
     }
 }
