2b9e463d3fd6c520115b98f68bd14d1d2a30d7f4
LilStupidPig20 <mistersaxar@gmail.com>
2020-10-29 16:23:21 +0500

    Волгарев Артем. Задание 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,16 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return (String.valueOf(x + y).length());
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(12, 34);
+
+        int result = solution(12, 88);
         System.out.println(result);
-        */
+
     }
 
 }
