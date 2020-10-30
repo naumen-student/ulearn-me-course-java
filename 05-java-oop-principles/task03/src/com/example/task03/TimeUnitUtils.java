32c6d486ebf8ae7575ff823bc6b0dc62977ec31d
Danil Harlamtsev <harlamtsev.danil@gmail.com>
2020-10-30 11:46:57 +0500

    Харламцев Данил задача 5.3

--- 05-java-oop-principles/task03/src/com/example/task03/TimeUnitUtils.java
+++ 05-java-oop-principles/task03/src/com/example/task03/TimeUnitUtils.java
@@ -1,75 +1,20 @@
 package com.example.task03;
 
-/**
- * Класс, в котором собраны методы для работы с {@link TimeUnit}
- */
 public class TimeUnitUtils {
 
-    /** все типы конвертируются в миллисекунды **/
-    public static Milliseconds toMillis(Milliseconds millis) {
-        return new Milliseconds(millis.toMillis());
+    public static Milliseconds toMillis(TimeUnit timeUnit) {
+        return new Milliseconds(timeUnit.toMillis());
     }
 
-    public static Milliseconds toMillis(Seconds seconds) {
-        return new Milliseconds(seconds.toMillis());
+    public static Seconds toSeconds(TimeUnit timeUnit) {
+        return new Seconds(timeUnit.toSeconds());
     }
 
-    public static Milliseconds toMillis(Minutes minutes) {
-        return new Milliseconds(minutes.toMillis());
+    public static Minutes toMinutes(TimeUnit timeUnit) {
+        return new Minutes(timeUnit.toMinutes());
     }
 
-    public static Milliseconds toMillis(Hours hours) {
-        return new Milliseconds(hours.toMillis());
-    }
-
-    /** все типы конвертируются в секунды **/
-    public static Seconds toSeconds(Milliseconds millis) {
-        return new Seconds(millis.toSeconds());
-    }
-
-    public static Seconds toSeconds(Seconds seconds) {
-        return new Seconds(seconds.toSeconds());
-    }
-
-    public static Seconds toSeconds(Minutes minutes) {
-        return new Seconds(minutes.toSeconds());
-    }
-
-    public static Seconds toSeconds(Hours hours) {
-        return new Seconds(hours.toSeconds());
-    }
-
-    /** все типы конвертируются в минуты **/
-    public static Minutes toMinutes(Milliseconds millis) {
-        return new Minutes(millis.toMinutes());
-    }
-
-    public static Minutes toMinutes(Seconds seconds) {
-        return new Minutes(seconds.toMinutes());
-    }
-
-    public static Minutes toMinutes(Minutes minutes) {
-        return new Minutes(minutes.toMinutes());
-    }
-
-    public static Minutes toMinutes(Hours hours) {
-        return new Minutes(hours.toMinutes());
-    }
-
-    /** все типы конвертируются в часы **/
-    public static Hours getHours(Milliseconds millis) {
-        return new Hours(millis.getHours());
-    }
-
-    public static Hours getHours(Seconds seconds) {
-        return new Hours(seconds.getHours());
-    }
-
-    public static Hours getHours(Minutes minutes) {
-        return new Hours(minutes.getHours());
-    }
-
-    public static Hours getHours(Hours hours) {
-        return new Hours(hours.getHours());
+    public static Hours getHours(TimeUnit timeUnit) {
+        return new Hours(timeUnit.getHours());
     }
 }
