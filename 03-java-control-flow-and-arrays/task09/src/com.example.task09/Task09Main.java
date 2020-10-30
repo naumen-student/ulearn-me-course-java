12cabb5d5de349b31706cb1da7d47e3c81f2240d
Марк <markshubat@gmail.com>
2020-10-30 14:10:21 +0500

    Шубат задача 3.9

--- 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
+++ 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
@@ -14,8 +14,11 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++){
+            if (min > arr[i]) min = arr[i];
+        }
+        return min;
     }
 
 }
