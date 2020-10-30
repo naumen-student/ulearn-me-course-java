6e89734dc620a0ae5afc3287011e74b1eace9236
ssss123-byte <shardakovea@mail.ru>
2020-10-30 15:01:57 +0300

    Шардаков Егор. Задача 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -4,9 +4,8 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        if (x + y < 0) return (String.valueOf(x + y).length() - 1);
+        return (String.valueOf(x + y).length());
     }
 
     public static void main(String[] args) {
