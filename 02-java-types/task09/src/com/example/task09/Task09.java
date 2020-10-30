39af9b011bf85824e9325cff3dcdf7a01517258f
ssss123-byte <shardakovea@mail.ru>
2020-10-30 15:30:04 +0300

    Шардаков Егор. Задача 2.09

--- 02-java-types/task09/src/com/example/task09/Task09.java
+++ 02-java-types/task09/src/com/example/task09/Task09.java
@@ -3,6 +3,8 @@ package com.example.task09;
 public class Task09 {
 
     public static double solution() {
+        float a = 1.0f;
+        float b = 3.0f;
         return ((a / b - 1.0f / 3.0f) * 1.0e9);
     }
 
