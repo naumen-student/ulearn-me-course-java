b401188fdea722eb5fe94158b578085e605e737c
Марк <markshubat@gmail.com>
2020-10-30 13:50:59 +0500

    Шубат задача 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,10 +3,8 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int result = (Integer.toString(x + y).replaceAll("-", "")).length();
+        return result;
     }
 
     public static void main(String[] args) {
