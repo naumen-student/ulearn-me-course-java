e5e782f7e481562dec88ced479390289f7f8f33d
evanesenze <evanesenze@gamil.com>
2020-10-30 20:01:11 +0500

    Базуев Иван.07.Task03

--- 07-java-io/task03/src/com/example/task03/Task03Main.java
+++ 07-java-io/task03/src/com/example/task03/Task03Main.java
@@ -2,20 +2,20 @@ package com.example.task03;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null) throw new IllegalArgumentException();
+        StringWriter res = new StringWriter();
+        Scanner scanner = new Scanner(inputStream, charset);
+        while (scanner.hasNext())
+            res.write(scanner.next());
+        return res.toString();
     }
 }
