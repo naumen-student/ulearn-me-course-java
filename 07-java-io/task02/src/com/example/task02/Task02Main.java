96776e5dbcd194dc75b5213b9dbeabcdf60ede1f
diukin9 <petr.diukin@urfu.me>
2020-10-30 19:10:37 +0500

    Дюкин Петр 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 public class Task02Main {
     public static void main(String[] args) throws IOException {
         int previousByte = System.in.read();
-        int currentByte = System.in.read();
-        while (currentByte != -1) {
+        int currentByte;
+        while (previousByte != -1) {
+            currentByte = System.in.read();
             if (currentByte == 10 & previousByte == 13) {
+                previousByte = currentByte;
                 continue;
             }
             System.out.write(previousByte);
             previousByte = currentByte;
-            currentByte = System.in.read();
         }
-        System.out.write(previousByte);
         System.out.flush();
     }
 
