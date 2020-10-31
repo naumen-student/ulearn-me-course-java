aebb9d45792436248967eb39470042e9d7601b7a
diukin9 <petr.diukin@urfu.me>
2020-11-01 01:19:37 +0500

    Дюкин Петр 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,7 @@ package com.example.task02;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -11,15 +12,13 @@ public class Task02Main {
     public static List<Path> listFiles = new ArrayList<>();
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println(listFiles(Paths.get("C:\\Users\\petrd\\IdeaProjects\\ulearn-me-course-java\\08-java-io2\\task02\\src\\main\\resources")));
-
     }
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        File rootDirFile = rootDir.toFile();
+    public static List<Path> listFiles(Path rootDirectory) throws IOException, InterruptedException {
+        File rootDirFile = rootDirectory.toFile();
         return showFilesAndDirectoryes(rootDirFile);
     }
-    public static List<Path> showFilesAndDirectoryes (File rootDirFile) {
-        File[] files = rootDirFile.listFiles();
+    public static List<Path> showFilesAndDirectoryes (File rootDirectory) throws IOException {
+        File[] files = rootDirectory.listFiles();
         for (File file : files) {
             if (!file.isDirectory()) {
                 Path path = Paths.get(file.getName());
