e54a9e6e1be583ef06f53f06604ce85a0000e653
andrewzvorygin <Andrei.Zvorygin@urfu.me >
2020-10-31 19:27:46 +0500

    Зворыгин Андрей Задача 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -5,11 +5,10 @@ import java.io.IOException;
 public class Task02Main {
     public static void main(String[] args) throws IOException {
         int value1 = System.in.read();
-        while (value1 != -1){
+        while (value1 != -1) {
             int value2 = System.in.read();
-            if(value1 == 13 && value2 == 10)
-                continue;
-            System.out.write(value1);
+            if (!(value1 == 13 && value2 == 10))
+                System.out.write(value1);
             value1 = value2;
         }
         System.out.flush();
