085cd81b6c7c2b8870b58fd1a010ef862468c91b
Лялин Артём <maii122@mail.ru>
2020-11-01 00:52:16 +0500

    04_Task03 Done

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber first = new ComplexNumber(2,4);
+        ComplexNumber second = new ComplexNumber(4,3);
+        ComplexNumber sum = first.sum(second);
+        ComplexNumber multiply = second.multiply(first);
     }
 }
