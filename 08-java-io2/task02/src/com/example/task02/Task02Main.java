85169e5fef49266228ae5efaefdbd203870b664d
JokeI777 <danleo.2000@mail.ru>
2020-11-01 18:15:59 +0500

    Леонов Даниил, модуль 8, задача 2.

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,10 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystem;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
@@ -12,12 +15,24 @@ public class Task02Main {
         /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
         */
-
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+        List<Path> paths = new ArrayList<Path>();
+        File directory = rootDir.toFile();
+        if(!directory.isDirectory()){
+            paths.add(directory.toPath());
+            return paths;
+        }
+        else {
+            for (File file : directory.listFiles()) {
+                if (file.isFile()) {
+                    paths.add(file.toPath());
+                } else {
+                    paths.addAll(listFiles(file.toPath()));
+                }
+            }
+        }
+        return paths;
     }
 }
