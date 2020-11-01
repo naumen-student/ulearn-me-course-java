ff69497b2eeb041131f236f1edf1b6796b63040c
Polina <kiramik55@gmail.com>
2020-11-01 21:13:05 +0500

    02_task_02

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -5,8 +5,14 @@ public class Task02 {
     public static String solution(String input) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long number = Long.parseLong(input);
+        if(Byte.MAX_VALUE >= number && Byte.MIN_VALUE <= number)
+            return "byte";
+        if(Short.MAX_VALUE >= number && Short.MIN_VALUE <= number)
+            return "short";
+        if(Integer.MAX_VALUE >= number && Integer.MIN_VALUE <= number)
+            return "int";
+        return "long";
     }
 
     public static void main(String[] args) {
