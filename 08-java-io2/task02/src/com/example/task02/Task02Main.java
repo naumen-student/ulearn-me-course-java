80b9f0523fcc918735426ec2665084f5dab9f089
diukin9 <petr.diukin@urfu.me>
2020-11-01 18:47:10 +0500

    Дюкин Петр 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -9,19 +11,17 @@ import java.util.List;
 public class Task02Main {
     public static List<Path> listFiles = new ArrayList<>();
 
-    public static void main(String[] args) {
-        System.out.println(listFiles(Paths.get("08-java-io2/task02/src/main/resources/")));
+    public static void main(String[] args) throws IOException, InterruptedException {
     }
-    public static List<Path> listFiles(Path rootDirectory){
+    public static List<Path> listFiles(Path rootDirectory) throws IOException, InterruptedException {
         File rootDirFile = rootDirectory.toFile();
         return showFilesAndDirectoryes(rootDirFile);
     }
-    public static List<Path> showFilesAndDirectoryes (File rootDirectory) {
+    public static List<Path> showFilesAndDirectoryes (File rootDirectory) throws IOException {
         File[] files = rootDirectory.listFiles();
         for (File file : files) {
-            if (!file.isDirectory()) {
+            if (!file.isDirectory())
                 listFiles.add(Paths.get(file.getPath()));
-            }
             if (file.isDirectory ())
                 try {
                     showFilesAndDirectoryes(file);
