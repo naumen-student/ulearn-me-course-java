fbf7a8c7076acb20a62107902643f2c2791332fb
Белов Артём. Задача 1.1 <theflash.0320@gmail.com>
2020-11-01 18:50:36 +0500

    Белов Артём. Задание 6.07.

--- 06-java-exceptions/task07/src/com/example/task07/Processor.java
+++ 06-java-exceptions/task07/src/com/example/task07/Processor.java
@@ -1,10 +1,11 @@
 package com.example.task07;
 
+import java.io.IOException;
+
 public class Processor {
 
-    public Object process() throws Exception {
-        //todo вы можете заменить реализацию этого метода для ручного дебага
-        return null;
+    public Object process() throws IOException {
+        throw new IOException();
     }
 
 }
