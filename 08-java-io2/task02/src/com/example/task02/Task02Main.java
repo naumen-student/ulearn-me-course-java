b55db252e365b7af712be7c187d4c6bdce83576c
diukin9 <petr.diukin@urfu.me>
2020-11-01 19:10:59 +0500

    Дюкин Петр 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,33 +1,23 @@
 package com.example.task02;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
-    public static List<Path> listFiles = new ArrayList<>();
-
-    public static void main(String[] args) throws IOException, InterruptedException {
+    private static List<Path> listFiles = new ArrayList<>();
+    public static void main(String[] args) {
+        System.out.println(listFiles(Paths.get("08-java-io2/task02/src/main/resources/")));
     }
-    public static List<Path> listFiles(Path rootDirectory) throws IOException, InterruptedException {
-        File rootDirFile = rootDirectory.toFile();
-        return showFilesAndDirectoryes(rootDirFile);
-    }
-    public static List<Path> showFilesAndDirectoryes (File rootDirectory) throws IOException {
-        File[] files = rootDirectory.listFiles();
+    public static List<Path> listFiles(Path rootDirectory) {
+        File directory = rootDirectory.toFile();
+        File[] files = directory.listFiles();
         for (File file : files) {
             if (!file.isDirectory())
                 listFiles.add(Paths.get(file.getPath()));
-            if (file.isDirectory ())
-                try {
-                    showFilesAndDirectoryes(file);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+            else listFiles(Paths.get(file.getPath()));
         }
         return listFiles;
     }
