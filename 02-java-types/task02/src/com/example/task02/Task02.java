4137fa3e1b8c18cf5a30100db8ba08ffd93f2d70
Polina <kiramik55@gmail.com>
2020-11-01 21:48:55 +0500

    02_task_02

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,10 +3,20 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        try {
+            long number = Long.parseLong(input);
+            if(Byte.MAX_VALUE >= number && Byte.MIN_VALUE <= number)
+                return "byte";
+            if(Short.MAX_VALUE >= number && Short.MIN_VALUE <= number)
+                return "short";
+            if(Integer.MAX_VALUE >= number && Integer.MIN_VALUE <= number)
+                return "int";
+            return "long";
+        }
+        catch (NumberFormatException i){
+            //System.out.println(i.getStackTrace());
+            return null;
+        }
     }
 
     public static void main(String[] args) {
