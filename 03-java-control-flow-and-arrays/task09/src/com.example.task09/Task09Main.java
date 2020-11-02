32cddba7844e75b0601c57f1002af7d76fba5f97
regina <regina.agafonova.2001@gmail.com>
2020-11-02 15:19:35 +0500

    Агафонова Регина 03_task09

--- 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
+++ 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
@@ -2,20 +2,21 @@ package com.example.task09;
 
 public class Task09Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = new int[3];
         arr[0] = 2;
         arr[1] = -1;
         arr[2] = 3;
         System.out.println(min(arr));
-         */
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
     }
 
 }
