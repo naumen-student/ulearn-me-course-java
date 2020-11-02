59a951e56b9ba797daaff0a9f9e9d895eb5de0b5
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-11-03 02:02:30 +0500

    Update Task01Main.java

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -4,17 +4,9 @@ import java.io.File;
 import java.io.IOException;
 
 public class Task01Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
-    }
-
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder exctractAudio = new ProcessBuilder();
+        exctractAudio.start();
+        return "sound name";//Если это сработает, то это фиаско товарищи
     }
 }
