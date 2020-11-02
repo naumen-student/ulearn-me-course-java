fbcef19d0d132fe1bc65078f2baf460df6a4b487
Danil Konkov <buroroll@gmail.com>
2020-11-03 00:24:22 +0500

    Коньков Данил. Задание 8.1

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,29 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+        System.out.println(extractSoundName(new File("C:\\Users\\User\\Ulearn\\ulearn-me-course-java\\08-java-io2\\task01\\src\\main\\resources\\3726.mp3")));
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath())
+                .directory(new File(file.getAbsolutePath()))
+                .redirectOutput(Redirect.PIPE);
+        Process process = processBuilder.start();
+        try(BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()))){
+            for (Object s: reader.lines().toArray()) {
+                if(s.toString().startsWith("format.tags.title"))
+                    return s.toString().replace("format.tags.title=", "");
+            }
+        }
+        return null;
     }
 }
