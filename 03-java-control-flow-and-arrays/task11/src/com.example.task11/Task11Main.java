6d7763990872ca518986c30311678f8f85232e2e
nvit29 <gainetdinoa-nastya@ya.ru>
2020-11-02 19:40:33 +0500

    Гайнетдинова Анастасия. Задание 3.11

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -4,15 +4,24 @@ public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void swap(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        int min = arr[0];
+        int result = 0;
+        if (arr == null || arr.length == 0) return;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                result = i;
+                min = arr[i];
+            }
+        }
+        arr[result] = arr[0];
+        arr[0] = min;
     }
 
 }
