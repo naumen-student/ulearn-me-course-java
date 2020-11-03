bddc9d912e558d6a82c63ca9d7915dd16e71c274
LilStupidPig20 <mistersaxar@gmail.com>
2020-11-03 18:23:27 +0500

    Волгарев Артем. Задание 3.5

--- 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
+++ 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
@@ -1,16 +1,19 @@
 package com.example.task05;
 
+import java.util.Arrays;
+
 public class Task05Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        System.out.println(getMin(5,4,3));
-         */
+
+        System.out.println(getMin(2,2,11));
+
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] arr = new int[] {a, b, c};
+        Arrays.sort(arr);
+        return arr[0];
     }
 }
