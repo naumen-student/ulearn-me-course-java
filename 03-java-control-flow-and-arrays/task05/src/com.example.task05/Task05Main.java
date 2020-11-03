04f18fe6d3681a00014d3e83b130b45b69e81393
ssss123-byte <shardakovea@mail.ru>
2020-11-03 19:02:42 +0300

    Шардаков Егор. Задание 3.5

--- 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
+++ 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
@@ -10,7 +10,8 @@ public class Task05Main {
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] arr = new int[] {a, b, c};
+        Arrays.sort(arr);
+        return arr[0];
     }
 }
