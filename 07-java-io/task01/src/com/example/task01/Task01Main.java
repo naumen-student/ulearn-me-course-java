38cf6ce51147715f89cd9bced868c3daa081fd95
Haggard13 <alexey.kozedubov@urfu.me>
2020-11-03 19:46:58 +0500

    Козедубов Алексей. Задание 7.1

--- 07-java-io/task01/src/com/example/task01/Task01Main.java
+++ 07-java-io/task01/src/com/example/task01/Task01Main.java
@@ -1,21 +1,26 @@
 package com.example.task01;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
 
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) throw new IllegalArgumentException();
+        byte[] data = inputStream.readAllBytes();
+        return data.length == 0 ? 0 : getControlSum(data);
+    }
+
+    private static int getControlSum(byte[] data) {
+        int result = data[0];
+        for (int i = 1; i < data.length; i++) {
+            result = Integer.rotateLeft(result, 1) ^ data[i];
+        }
+        return result;
     }
 }
