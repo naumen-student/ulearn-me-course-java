aeaecdc2879efb2ec48084052e589cf41eb874ad
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2020-11-03 18:42:08 +0500

    Update

--- 08-java-io2/task03/src/com/example/task03/Task03Main.java
+++ 08-java-io2/task03/src/com/example/task03/Task03Main.java
@@ -3,8 +3,6 @@ package com.example.task03;
 import java.io.*;
 
 public class Task03Main{
-
-
     public static SampleData deserialize(InputStream inputStream) throws IOException, ClassNotFoundException{
         try (ObjectInputStream stream = new ObjectInputStream(inputStream)) {
             return (SampleData) stream.readObject();
