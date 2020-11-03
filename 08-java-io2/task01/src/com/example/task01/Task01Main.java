2180a7a32388404e899444b4f79f65eb8633efbe
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2020-11-03 17:40:00 +0500

    Update Task01Main.java

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,31 @@
 package com.example.task01;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-public class Task01Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
+public class Task01Main{
 
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
-    }
-
-    public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+    public static String extractSoundName(File file) throws IOException, InterruptedException{
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format");
+        processBuilder.directory(file);
+        try (Scanner scanner = new Scanner(processBuilder.start().getInputStream())) {
+            Pattern pattern = Pattern.compile("format.tags.title=\"*\"");
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                Matcher matcher = pattern.matcher(line);
+                if ( matcher.find() )
+                    return matcher.group();
+            }
+        }
+        return null;
     }
 }
