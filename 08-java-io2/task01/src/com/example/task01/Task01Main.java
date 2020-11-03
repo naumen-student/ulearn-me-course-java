704b07b6bb5ab896b568c2177ed6191f66ce854f
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2020-11-03 18:05:47 +0500

    Update Task01Main.java

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -1,29 +1,19 @@
 package com.example.task01;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
-
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Task01Main{
-
     public static String extractSoundName(File file) throws IOException, InterruptedException{
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format");
         processBuilder.directory(file);
         try (Scanner scanner = new Scanner(processBuilder.start().getInputStream())) {
-            Pattern pattern = Pattern.compile("format.tags.title=\"*\"");
-            while (scanner.hasNext()) {
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                Matcher matcher = pattern.matcher(line);
-                if ( matcher.find() )
-                    return matcher.group();
+                if ( line.contains("format.tags.title=") )
+                    return line.substring(19, line.length() - 1);
             }
         }
         return null;
