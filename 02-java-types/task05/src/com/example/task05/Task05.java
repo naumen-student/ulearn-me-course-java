57a18ac570059adf46770d8fcbd788ca8b4265bc
Polina <kiramik55@gmail.com>
2020-11-04 00:46:09 +0500

    02_task_05

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -4,9 +4,15 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        while (x > 0){
+            int y = x % 10;
+            x = x/10;
+            if(y % 2 !=0)
+                return "FALSE";
+        }
 
-        return "FALSE";
+
+        return "TRUE";
     }
 
     public static void main(String[] args) {
