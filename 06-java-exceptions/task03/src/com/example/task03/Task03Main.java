2372a9fea2f27c1c0be5261409af0e72e6481293
Frezyx <youngfrezyx@gmail.com>
2020-11-03 21:52:54 +0300

    Ильин Станислав. Задача 6.3

--- 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
+++ 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
@@ -2,15 +2,13 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        throwCheckedException();
-         */
+        try {
+            throwCheckedException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-    //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
-
+    public static void throwCheckedException() throws Exception {
+        throw new Exception();
     }
 }
