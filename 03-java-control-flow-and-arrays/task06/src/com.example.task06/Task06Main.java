68c706066957fedb72c1fcba851318f12f2bac24
ssss123-byte <shardakovea@mail.ru>
2020-11-03 19:04:32 +0300

    Шардаков Егор. Задание 3.6

--- 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
+++ 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
@@ -10,8 +10,9 @@ public class Task06Main {
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] arr = new int[] {a, b, c, d};
+        Arrays.sort(arr);
+        return arr[arr.length - 1];
     }
 
 }
