0918044768ea37047fc5fed0c3659f3471c5df24
Haggard13 <alexey.kozedubov@urfu.me>
2020-11-03 20:13:47 +0500

    Козедубов Алексей. Задание 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,13 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        byte[] inBytes = System.in.readAllBytes();
+        for (int i = 0; i < inBytes.length; i++) {
+            if (i != inBytes.length - 1 && inBytes[i] == 13 && inBytes[i + 1] == 10) {
+                System.out.write(10);
+                i++;
+            }
+            else System.out.write(inBytes[i]);
+        }
     }
 }
