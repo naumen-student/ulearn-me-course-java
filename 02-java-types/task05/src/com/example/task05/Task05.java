b0fa8b4af706dfc1613db9b3ae229a0bd9081c75
Polina <kiramik55@gmail.com>
2020-11-04 00:59:08 +0500

    02_task_05

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,10 +3,13 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while (x>0){
+            int y = x% 10;
+            x = x /10;
+            if (y % 2 != 0)
+                return "FALSE";
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
