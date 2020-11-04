444207c869cbe5eb042144ee4cdb8b2b7be19f85
grekova-web <71182152+grekova-web@users.noreply.github.com>
2020-11-04 11:05:35 +0500

    Грекова Елена. Задание 7.4

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +10,16 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        double result = 0;
+        Locale en = Locale.ENGLISH;
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.useLocale(en).hasNext()) {
+            if (scanner.hasNextDouble())
+                result += scanner.nextDouble();
+            else scanner.next();
+        }
 
-        System.out.println("0.0");
+        System.out.printf(Locale.ENGLISH, "%.6f", result);
+
     }
 }
