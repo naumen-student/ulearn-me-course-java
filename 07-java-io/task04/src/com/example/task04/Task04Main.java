879fd56def575b299f0e72178172a94e0ce8b11d
dariasavvina <dariasavvina@mail.ru>
2020-11-04 21:06:24 +0500

    Саввина Дарья.Задача 7.04

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,20 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0.0;
+        while(scanner.hasNextLine()){
+            String[] inputs = scanner.nextLine().split(" ");
+            for(int i = 0; i < inputs.length; i++) {
+                try {
+                    sum += Double.parseDouble(inputs[i]);
+                } catch (NumberFormatException e) {}
+            }
+        }
+        System.out.println(String.format("%.6f", sum).replace(",", "."));
     }
 }
