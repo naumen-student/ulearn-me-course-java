a5681b62558be76c442d579b6286b4b320427744
Polina <kiramik55@gmail.com>
2020-11-04 21:44:28 +0500

    02_task_13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -4,9 +4,9 @@ public class Task13 {
 
     public static char toUpperCase(char c) {
 
-        // TODO привести букву к верхнему регистру
+        return (char)(c - 32);
+        //return Character.toLowerCase(c);
 
-        return c;
     }
 
     public static void main(String[] args) {
