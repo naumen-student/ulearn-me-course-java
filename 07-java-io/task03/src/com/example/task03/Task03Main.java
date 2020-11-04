ff5d8be17657f6c15c5213f3c5a93f2541950b1d
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2020-11-04 12:15:51 +0500

    Задача 7.3

--- 07-java-io/task03/src/com/example/task03/Task03Main.java
+++ 07-java-io/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,7 @@ package com.example.task03;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 
 public class Task03Main {
@@ -15,7 +16,7 @@ public class Task03Main {
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if(inputStream == null) throw new IllegalArgumentException();
+        return (charset.decode(ByteBuffer.wrap(inputStream.readAllBytes()))).toString();
     }
 }
