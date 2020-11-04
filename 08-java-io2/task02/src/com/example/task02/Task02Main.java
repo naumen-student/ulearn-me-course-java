1b31c5cbe9c1edf12e761fedf3f25008a3bb1622
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-11-04 13:52:57 +0500

    Update Task02Main.java

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,30 @@
 package com.example.task02;
 
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+        RootVisitor visitor = new RootVisitor();
+        Files.walkFileTree(rootDir, new RootVisitor());
+        return visitor.filesRoot;
+    }
+}
 
-        return null;
+class  RootVisitor extends SimpleFileVisitor<Path>
+{
+    public  final List<Path> filesRoot = new ArrayList<>();
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
+    {
+        filesRoot.add(( file.normalize()));
+        return super.visitFile(file, attrs);
     }
 }
