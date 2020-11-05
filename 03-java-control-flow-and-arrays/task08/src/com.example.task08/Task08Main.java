05b475ad1f144da1bf07043fd48f1013c178ffcb
arturha4 <sadbooys.2001@gmail.com>
2020-11-05 11:10:18 +0500

    Давлетшин Артур. Задание 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -3,18 +3,19 @@ package com.example.task08;
 public class Task08Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = new int[2];
-        arr[0] = 2;
-        arr[1] = 3;
-        System.out.println(mult(arr));
-         */
+
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        long res=1;
+        if (arr.length==0){
+            return 0;
+        }
+        else{
+            for  (int i:arr)
+                res*=i;
+        }
+        return res;
     }
 
 }
