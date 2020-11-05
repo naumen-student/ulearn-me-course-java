e95bf0f6fe4ecde9f9e358451c9796e0eeb20a4f
0verv0ice <theznoy@gmail.com>
2020-11-05 10:07:18 +0500

    Машанов роман 04task01

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -4,8 +4,31 @@ package com.example.task01;
  * Класс точки на плоскости
  */
 public class Point {
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     int x;
     int y;
+
+    void flip() {
+        int z = x;
+        x = -y;
+        y = -z;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
+    }
+
+    public String toString() {
+        return String.format("{0}, {1}", x, y);
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
