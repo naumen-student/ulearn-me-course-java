fc740fd8c771cc4af9cc8907c2d851b443a8be25
kirillegorow <e.kirill.2001@gmail.com>
2020-11-05 11:38:43 +0500

    task6.4 Егоров Кирилл

--- 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
+++ 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
@@ -2,16 +2,29 @@ package com.example.task04;
 
 public class Task04Main {
 
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getSeason(-5));
-         */
-    }
+    public static void main(String[] args) {}
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        switch (monthNumber) {
+            case (1):
+            case (2):
+            case (12):
+                return "зима";
+            case (3):
+            case (4):
+            case (5):
+                return "весна";
+            case (6):
+            case (7):
+            case (8):
+                return "лето";
+            case (9):
+            case (10):
+            case (11):
+                return "осень";
+            default:
+                throw new MyException("monthNumber " + monthNumber + " is invalid, month number should be between 1..12");
+        }
     }
 
 }
