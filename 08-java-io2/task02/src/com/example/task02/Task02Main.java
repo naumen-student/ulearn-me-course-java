cbb40cf54eaccf98a6a690c3235f913a7ae7c35f
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-11-06 15:48:14 +0500

    Update Task02Main.java

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -2,25 +2,20 @@ package com.example.task02;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        MyVisitor visitor = new MyVisitor();
-        Files.walkFileTree(rootDir, visitor);
-        return visitor.allVisitedFiles;
-    }
-}
-
-class  MyVisitor extends SimpleFileVisitor<Path>
-{
-    public  final List<Path> allVisitedFiles = new ArrayList<>();
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
-    {
-        allVisitedFiles.add((file.normalize()));
-        return super.visitFile(file, attrs);
+        ArrayList<Path> result = new ArrayList<>();
+        DirectoryStream<Path> dirStream = Files.newDirectoryStream(rootDir);
+        for (Path child : dirStream) {
+            if (Files.isRegularFile(child)) {
+                result.add(child);
+            } else {
+                result.addAll(listFiles(child));
+            }
+        }
+        return result;
     }
 }
