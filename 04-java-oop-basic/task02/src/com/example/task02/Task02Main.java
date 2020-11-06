e7dd0500598cc5c0915749af5e6d4f0ec9f77181
nvit29 <gainetdinoa-nastya@ya.ru>
2020-11-07 03:39:49 +0500

    Гайнетдинова Анастасия. Задание 4.2

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -4,4 +4,52 @@ public class Task02Main {
     public static void main(String[] args) {
 
     }
+    public class TimeSpan {
+        private int seconds, minutes, hours;
+        void add(TimeSpan time) {
+            this.seconds += time.seconds;
+            this.minutes += time.minutes;
+            this.hours += time.hours;
+        }
+
+        void subtract(TimeSpan time) {
+            this.seconds -= time.seconds;
+            this.minutes -= time.minutes;
+            this.hours -= time.hours;
+        }
+
+        public String toString() {
+            return String.format("%d, %d, %d", hours, minutes, seconds);
+        }
+
+        public int getSeconds() {
+            return seconds;
+        }
+
+        public int getMinutes() {
+            return minutes;
+        }
+
+        public int getHours() {
+            return hours;
+        }
+
+        public void setSeconds(int seconds) {
+            this.seconds = seconds;
+        }
+
+        public void setMinutes(int minutes) {
+            this.seconds = minutes;
+        }
+
+        public void setHours(int hours) {
+            this.seconds = hours;
+        }
+
+        public TimeSpan(int seconds, int minutes, int hours) {
+            this.seconds = seconds;
+            this.minutes = minutes;
+            this.hours = hours;
+        }
+    }
 }
