249656e5671773b6cbe55df26127a9deb2a34e21
Заид Омар <omar.m.zaid@hotmail.com>
2020-11-09 00:12:46 +0500

    Заид Омар 02_Task09

--- 02-java-types/task09/src/com/example/task09/Task09.java
+++ 02-java-types/task09/src/com/example/task09/Task09.java
@@ -6,12 +6,10 @@ public class Task09 {
 
         float a = 1.0f;
         float b = 3.0f;
-
         return (a / b - 1.0f / 3.0f) * 1.0e9;
     }
 
     public static void main(String[] args) {
 
     }
-
 }
