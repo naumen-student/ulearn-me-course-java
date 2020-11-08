fc0350b462b99801fb94999d24df39731c68eaee
Заид Омар <omar.m.zaid@hotmail.com>
2020-11-09 00:06:31 +0500

    Заид Омар 02_Task08

--- 02-java-types/task08/src/com/example/task08/Task08.java
+++ 02-java-types/task08/src/com/example/task08/Task08.java
@@ -9,6 +9,10 @@ public class Task08 {
     }
 
     public static void main(String[] args) {
+        // Здесь вы можете вручную протестировать ваше решение
+        /*
+        System.out.println(solution());
+        */
     }
 
 }
