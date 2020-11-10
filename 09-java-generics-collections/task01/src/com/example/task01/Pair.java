323ecaef4e457fdf8be099b7cd1ac8019759abe7
Chekebreek <shura.dushkin@yandex.ru>
2020-11-10 20:30:13 +0500

    Душкин Александр. 09_task01

--- 09-java-generics-collections/task01/src/com/example/task01/Pair.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Pair.java
@@ -1,42 +1,5 @@
 package com.example.task01;
 
-import java.util.Optional;
-import java.util.function.BiConsumer;
-import java.util.Objects;
-
-public class Pair<FirstType, SecondType>{
-    public static <T, S> Pair<T, S> of(T first, S second){
-        return new Pair<T, S>(first, second);
-    }
-
-    private final Optional<FirstType> first;
-    private final Optional<SecondType> second;
-
-    private Pair(FirstType first, SecondType second){
-        this.first = Optional.ofNullable(first);
-        this.second = Optional.ofNullable(second);
-    }
-
-    public FirstType getFirst(){
-        return first.orElse(null);
-    }
-
-    public SecondType getSecond(){
-        return second.orElse(null);
-    }
-
-    public void ifPresent(BiConsumer<? super FirstType, ? super SecondType> consumer){
-        if ( first.isPresent() && second.isPresent() ) consumer.accept(first.get(), second.get());
-    }
-
-    @Override
-    public boolean equals(Object o){
-        if ( this == o ) return true;Pair<FirstType, SecondType> pair = (Pair<FirstType, SecondType>) o;
-        if ( pair == null ) return false;return first.equals(pair.first) && second.equals(pair.second);
-    }
-
-    @Override
-    public int hashCode(){
-        return Objects.hash(first, second);
-    }
+public class Pair {
+    // TODO напишите реализацию
 }
