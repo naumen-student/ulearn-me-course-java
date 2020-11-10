a756eba2b8f912f025fe8b7b696cf1827d753543
Mohamed <mohamedtarek21763@gmail.com>
2020-11-10 15:10:26 +0500

    Хассан Мохамед Тарек Хассан 1.1

--- 01-java-intro/task01/src/Tests.java
+++ 01-java-intro/task01/src/Tests.java
@@ -1,50 +1,5 @@
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.text.MessageFormat;
-import java.util.Arrays;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-
-public class Tests {
-
-    private static boolean isMainMethod(Method method) {
-        int modifiers = method.getModifiers();
-        return method.getName().equals("main") && Modifier.isStatic(modifiers) && Modifier.isPublic(modifiers) && method.getReturnType() == void.class;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
-
-    @Test
-    public void testEven() {
-        try {
-            Class<?> clazz = Class.forName("HelloWorld");
-            Method main = Arrays.stream(clazz.getDeclaredMethods()).filter(Tests::isMainMethod).findFirst().orElseThrow(() -> new AssertionError("Не найден main метод"));
-            try {
-                PrintStream sout = System.out;
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                Field field = System.class.getField("out");
-                field.setAccessible(true);
-                Field modifiersField = Field.class.getDeclaredField("modifiers");
-                modifiersField.setAccessible(true);
-                modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-                try {
-                    field.set(System.class, new PrintStream(baos));
-                    main.invoke(clazz, new Object[]{ new String[0] });
-                } finally {
-                    field.set(System.class, sout);
-                }
-                String value = baos.toString().trim();
-                Assert.assertEquals("Hello, World!", value);
-            } catch (Exception e) {
-                throw new AssertionError(e);
-            }
-        } catch (ClassNotFoundException e) {
-            throw new AssertionError("Класс HelloWorld не найден");
-        }
-    }
-
 }
