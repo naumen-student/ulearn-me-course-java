fabb4f902f803596a3d03319818660ef03c8e762
Semyon <semen2k1@mail.ru>
2020-11-10 20:36:30 +0500

    Task 9.1

--- 09-java-generics-collections/task01/src/com/example/task01/Task01Main.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Task01Main.java
@@ -1,14 +1,9 @@
 package com.example.task01;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-
-        // TODO С корректно реализованным классом Pair должен компилироваться и успешно работать следующий код:
-
-        /*
         Pair<Integer, String> pair = Pair.of(1, "hello");
         Integer i = pair.getFirst(); // 1
         String s = pair.getSecond(); // "hello"
@@ -21,8 +16,6 @@ public class Task01Main {
         Pair<Integer, String> pair2 = Pair.of(1, "hello");
         boolean mustBeTrue = pair.equals(pair2); // true!
         boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
-        */
-
     }
 
 }
