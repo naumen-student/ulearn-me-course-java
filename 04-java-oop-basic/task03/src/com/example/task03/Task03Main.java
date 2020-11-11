02951620b5710546687b8d91d1063e97c70afcc2
Chocolatelis <elizavetab123@mail.ru>
2020-11-11 17:11:05 +0500

    Быкова Елизавета Задание 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,12 @@
 package com.example.task03;
 
-public class Task03Main {
-    public static void main(String[] args) {
-
+public class Task03Main{
+    public static void main(String[] args){
+        Complex number1 = new Complex(3,1);
+        Complex number2 = new Complex(5,4);
+        Complex result2 = number1.Multiply(number2);
+        Complex result1 = number1.Sum(number2);
+        System.out.println(result1.toString());
+        System.out.println(result2.toString());
     }
 }
