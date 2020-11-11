c90f6253aca734f8c7d59bdf2b1f39a686e370ef
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2020-11-11 20:15:23 +0500

    Зернюков Никита. Задача 8.3

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -10,14 +10,14 @@ public class Task01Main {
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        ProcessBuilder processBuilder = new ProcessBuilder().command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+        ProcessBuilder processBuilder = new ProcessBuilder("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
         String res = "";
 
         Scanner sc = new Scanner(processBuilder.start().getInputStream());
         while (sc.hasNextLine()) {
             String curr = sc.nextLine();
-            if (curr.startsWith("format.tags.title")) res = curr.replace("format.tags.title", "");
+            if (curr.startsWith("format.tags.title")) return curr.split("\"")[1];
         }
-        return res.substring(1, res.length() - 1);
+        return null;
     }
 }
