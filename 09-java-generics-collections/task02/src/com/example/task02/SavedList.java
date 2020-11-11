c14afcaa40215e95f05a25e29e73987ce26a9aa8
rabbit21xx13 <rabbit21xx13@users.noreply.github.com>
2020-11-11 18:00:51 +0500

    Шевлякова Алёна. Задание 9.2

--- 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
+++ 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,64 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private List<E> list;
+    private final File file;
 
     public SavedList(File file) {
+        this.file = file;
+        boolean flag = false;
+        try {
+            FileInputStream inputFile = new FileInputStream(file);
+            try (ObjectInputStream stream = new ObjectInputStream(inputFile)) {
+                list = (List<E>) stream.readObject();
+                flag = true;
+            }
+        } catch (Exception ignored) {
+        }
+        if (!flag)
+            list = new ArrayList<>();
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        list.set(index, element);
+        updateFile();
+        return element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        updateFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E result = list.get(index);
+        list.remove(index);
+        updateFile();
+        return result;
+    }
+
+    private void updateFile() {
+        try (ObjectOutputStream data = (new ObjectOutputStream(new FileOutputStream(file)))) {
+            data.writeObject(list);
+        } catch (Exception ignored) {
+        }
     }
 }
