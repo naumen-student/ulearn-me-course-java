6baedd2b18f83ac1f252e57cd948f06ae7f524e2
dmitry <dmitry.sushkov2000@mail.ru>
2020-11-11 17:06:50 +0500

    Сушков Дмитрий,задание 9.3

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -3,22 +3,41 @@ package com.example.task03;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
-        for (Set<String> anagram : anagrams) {
+        List<Set<String>> list = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
+        for (Set<String> anagram : list) {
             System.out.println(anagram);
         }
 
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset)).useDelimiter("\n");
+        Map<String, TreeSet<String>> lists = new TreeMap<>();
+        while (scanner.hasNext()) {
+            String words = scanner.next().toLowerCase();
+            if (words.matches("[а-я]+") && words.length() > 2) {
+                char[] arr = words.toCharArray();
+                Arrays.sort(arr);
+                String sortedWord = new String(arr);
+                if (!lists.containsKey(sortedWord)) {
+                    lists.put(sortedWord, new TreeSet<>());
+                }
+                lists.get(sortedWord).add(words);
+            }
+        }
+        return lists
+                .values()
+                .stream()
+                .filter(v -> v.size() > 1)
+                .collect(Collectors.toList());
     }
 }
