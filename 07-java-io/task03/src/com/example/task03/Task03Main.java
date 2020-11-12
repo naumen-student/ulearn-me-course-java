1b356917d1daebc9d4b38313ca0b32480e0b8a46
Белов Артём. Задача 1.1 <theflash.0320@gmail.com>
2020-11-12 11:31:48 +0500

    Белов Артём. Задание 7.03.

--- 07-java-io/task03/src/com/example/task03/Task03Main.java
+++ 07-java-io/task03/src/com/example/task03/Task03Main.java
@@ -2,7 +2,9 @@ package com.example.task03;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
@@ -15,7 +17,14 @@ public class Task03Main {
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null || charset == null)
+            throw new IllegalArgumentException();
+        InputStreamReader reader = new InputStreamReader(inputStream, charset);
+        Scanner scanner = new Scanner(reader);
+        StringBuilder builder = new StringBuilder();
+        while (scanner.hasNext()) {
+            builder.append(scanner.next());
+        }
+        return builder.toString();
     }
 }
