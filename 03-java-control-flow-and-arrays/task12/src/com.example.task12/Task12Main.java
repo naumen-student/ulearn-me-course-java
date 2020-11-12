b4dbfe5f09b80dadf546b98ab38b3fd579f71b7f
Kirill <vvvw_00@mail.ru>
2020-11-13 01:27:45 +0500

    Ворожцов Кирилл, Task 3.12

--- 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
+++ 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
@@ -1,22 +1,18 @@
 package com.example.task12;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Task12Main {
     public static void main(String[] args) {
     }
 
-    private  static int getIndexOfMin(int[]  arr,int startIndex)
-    {
-        IntStream stream = Arrays.stream(arr).skip(startIndex);
-        int min = stream.min().getAsInt();
-
-        for (int i = arr.length-1; i >=startIndex ; i--)
-            if (arr[i]==min)
-                return  i;
-
-        return  -1;
+    private  static int getIndexOfMin(int[]  arr,int startIndex) {
+        List<Integer> list =Arrays.stream(arr).boxed().collect(Collectors.toList());
+        int min = Arrays.stream(arr).skip(startIndex).min().getAsInt();
+        return list.lastIndexOf(min);
     }
 
     static void swap(int[] arr,int startIndex) {
@@ -27,9 +23,8 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        if (!(arr==null||arr.length==0) )
+        if ((arr!=null&&arr.length!=0) )
             for (int i = 0; i <arr.length ; i++)
                 swap(arr, i);
     }
-
 }
