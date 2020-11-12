44052524ded57abfbef44f4183bb7e78e2a95013
Белов Артём. Задача 1.1 <theflash.0320@gmail.com>
2020-11-12 11:23:01 +0500

    Белов Артём. Задание 7.02.

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int thisByte = System.in.read();
+        while (thisByte != -1) {
+            int nextByte = System.in.read();
+            if (!(thisByte == 13 && nextByte == 10)) {
+                System.out.write(thisByte);
+            }
+            thisByte = nextByte;
+        }
+        System.out.flush();
     }
 }
