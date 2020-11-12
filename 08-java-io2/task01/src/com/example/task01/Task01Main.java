1d7bbcdffba64a2febd8037d6e833030ee9030f7
dariasavvina <dariasavvina@mail.ru>
2020-11-12 22:00:35 +0500

    Саввина Дарья. Задача 8.01

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -2,19 +2,50 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.Buffer;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Task01Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(extractSoundName(new File("task01/src/main/resources/3724.mp3")));
     }
 
-    public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+    public static String extractSoundName(File file) throws  InterruptedException {
+        String[] parseNameFile = file.getName().split("/");
+        String nameFile = parseNameFile[parseNameFile.length - 1];
+        String pathToFile = ClassLoader.getSystemClassLoader().getResource(nameFile ).getPath().substring(1);
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        File fileRunProcess = new File("C:\\ffmpeg\\bin");
+        String pathToRunProcess = "C:\\ffmpeg\\bin\\ffprobe.exe";
+        processBuilder.directory(fileRunProcess);
+        String propertySound = "";
+        String nameSound = "";
+        try{
+            processBuilder.command(pathToRunProcess,
+                    "-of",
+                    "flat",
+                    "-show_format",
+                    pathToFile);
+            Process process = processBuilder.start();
+            Scanner scanner  = new Scanner(process.getInputStream());
+            while (scanner.hasNextLine()){
+                propertySound = scanner.nextLine();
+                if (propertySound.indexOf("format.tags.title") != -1){
+                    nameSound = getTittleProperty(propertySound);
+                    break;
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return nameSound;
+    }
+
+    public static String getTittleProperty(String properties){
+        return properties.split("format.tags.title")[1]
+                .replace("=", "").replace("\"", "");
     }
 }
