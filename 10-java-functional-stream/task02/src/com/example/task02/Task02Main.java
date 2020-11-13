dd33dbb156a5b7ad4e08b7564bbd1d4de2fb40e8
Kirill <vvvw_00@mail.ru>
2020-11-13 17:04:33 +0500

    Ворожцов Кирилл, Task 10.02

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -1,11 +1,12 @@
 package com.example.task02;
 
+import java.util.function.IntSupplier;
 import java.util.stream.IntStream;
 
 public class Task02Main {
 
     public static void main(String[] args) {
-
+        cycleGrayCode(2);
         /*
         cycleGrayCode(2)
                 .limit(10)
@@ -15,9 +16,26 @@ public class Task02Main {
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
-
+        return IntStream.generate(new GrayCodeSupplier(n));
     }
 
+    static class GrayCodeSupplier implements IntSupplier
+    {
+        private  final int max;
+        private  int current=-1;
+
+        public GrayCodeSupplier(int n)
+        {
+            if (n<1 || n>16)
+                throw  new IllegalArgumentException();
+            this.max=(int)Math.pow(2,n);
+        }
+        @Override
+        public int getAsInt() {
+            current++;
+            if (current>=max)
+                current=0;
+            return current^ current>>1;
+        }
+    }
 }
