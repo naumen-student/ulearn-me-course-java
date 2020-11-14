a06eede74516025c325084e8c76854efc55588bc
Lera <Valeria.Smirnova@urfu.me>
2020-11-14 11:02:42 +0500

    Смирнова Лера 07_04

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.Locale;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +10,13 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+        double sum = 0;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        while (scanner.hasNext())
+            if (scanner.hasNextDouble())
+                sum += scanner.nextDouble();
+            else
+                scanner.next();
+        System.out.format(Locale.ENGLISH, "%.6f", sum);
     }
 }
