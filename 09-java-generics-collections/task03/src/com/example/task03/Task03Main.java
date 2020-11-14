e1d159b4fb9fa4b7535aa6b2cb08604fd542a427
Dima <d.d.demin@urfu.me>
2020-11-14 19:37:18 +0500

    09_task03

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -3,9 +3,11 @@ package com.example.task03;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
@@ -19,6 +21,24 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset));
+        Map<String, TreeSet<String>> anagrams = new TreeMap<>();
+        while (scanner.hasNextLine()) {
+            String word = scanner.next().toLowerCase();
+            if (!word.matches("[а-я]{3,}"))
+                continue;
+            String sortedWord = getSortedWord(word);
+            if (!anagrams.containsKey(sortedWord))
+                anagrams.put(sortedWord, new TreeSet<>());
+            anagrams.get(sortedWord).add(word);
+        }
+
+        return (anagrams.values().stream().filter(a -> a.size() > 1)).collect(Collectors.toList());
+    }
+
+    private static String getSortedWord(String word){
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        return Arrays.toString(chars);
     }
 }
