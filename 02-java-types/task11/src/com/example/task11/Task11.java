7f1ecc29d401843e9d8f66626bc97f0b06ca29cb
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-11-16 19:43:15 +0500

    Update Task11.java

--- 02-java-types/task11/src/com/example/task11/Task11.java
+++ 02-java-types/task11/src/com/example/task11/Task11.java
@@ -10,18 +10,14 @@ public class Task11 {
         for (int i = 1; i <= 12; i++) {
             sum += sum * percent;
         }
-        return (float) (sum * Math.pow(percent + 1.0, 12));
+        
+        return (float) (sum * Math.pow(1.0 + percent, 12));
     }
 
     public static void main(String[] args) {
-
         float sum = 500; // 500 руб. на счете
         float percent = 0.00000001f; // 0.000001% ежемесячно
-
         sum = benefit(sum, percent);
-
         System.out.println("Сумма на счете через год: " + sum);
-
     }
-
 }
