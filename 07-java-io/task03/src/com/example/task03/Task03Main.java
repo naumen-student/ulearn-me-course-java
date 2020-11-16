e0f163630fb07114d5471e8e4877fd67b47b58fc
MaximFrancevich <FranzuaAirMax@yandex.ru>
2020-11-16 17:58:46 +0500

    Францевич Максим. Задание 07-03

--- 07-java-io/task03/src/com/example/task03/Task03Main.java
+++ 07-java-io/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,6 @@
 package com.example.task03;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 
 public class Task03Main {
@@ -15,7 +14,9 @@ public class Task03Main {
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null || charset == null) {
+            throw new IllegalArgumentException("parameter is null");
+        }
+        return new BufferedReader(new InputStreamReader(inputStream, charset)).readLine();
     }
 }
