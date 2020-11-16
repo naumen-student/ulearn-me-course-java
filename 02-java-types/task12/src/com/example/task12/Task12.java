728f6e3721d5e78b5cfd6ffac49363536f52acca
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-11-16 20:30:19 +0500

    Update Task12.java

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -1,5 +1,6 @@
 package com.example.task12;
 
+import java.math.RoundingMode;
 import java.math.BigDecimal;
 
 public class Task12 {
@@ -17,16 +18,16 @@ public class Task12 {
         return sum;
         */
 
-        return BigDecimal.ZERO;
+
+        BigDecimal number = percent.add(BigDecimal.valueOf(1));
+        BigDecimal result = sum.multiply(number.pow(12));
+        return result.setScale(9, RoundingMode.HALF_UP);
     }
 
     public static void main(String[] args) {
-
         BigDecimal sum = new BigDecimal(500).setScale(9, BigDecimal.ROUND_HALF_UP); // 500 руб. на счете
         BigDecimal percent = new BigDecimal(0.00000001f).setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
-
         sum = benefit(sum, percent);
-
         System.out.println("Сумма на счете через год: " + sum);
 
     }
