87ca2b4be08604c7ae5245811754f68c7b968352
0verv0ice <theznoy@gmail.com>
2020-11-17 18:45:24 +0500

    Машанов роман 04task05

--- 04-java-oop-basic/task05/src/com/example/task05/PolygonalLine.java
+++ 04-java-oop-basic/task05/src/com/example/task05/PolygonalLine.java
@@ -1,9 +1,15 @@
 package com.example.task05;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
+
+    private final ArrayList<Point> points = new ArrayList<>();
+    private double length = 0;
 
     /**
      * Устанавливает точки ломаной линии
@@ -11,7 +17,8 @@ public class PolygonalLine {
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        this.points.addAll(Arrays.asList(points));
+        Length();
     }
 
     /**
@@ -20,7 +27,8 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        points.add(point);
+        Length();
     }
 
     /**
@@ -30,7 +38,8 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        points.add(new Point(x, y));
+        Length();
     }
 
     /**
@@ -39,8 +48,14 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        return this.length;
+    }
+
+    private void Length() {
+        if (this.length != 0) this.length = 0;
+        for (int i = 1; i < this.points.size(); i++) {
+            this.length += this.points.get(i - 1).getLength(this.points.get(i));
+        }
     }
 
 }
