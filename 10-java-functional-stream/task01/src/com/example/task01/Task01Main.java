fff5145f72533eb5d883d61d97ad59af9ffb8fd5
deska13kuzmin <49113558+deska13kuzmin@users.noreply.github.com>
2020-11-18 17:47:12 +0500

    Update Task01Main.java

--- 10-java-functional-stream/task01/src/com/example/task01/Task01Main.java
+++ 10-java-functional-stream/task01/src/com/example/task01/Task01Main.java
@@ -1,8 +1,5 @@
 package com.example.task01;
 
-import jdk.nashorn.internal.ir.TernaryNode;
-import org.assertj.core.error.ShouldNotBeNull;
-import java.beans.Expression;
 import java.io.IOException;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -15,8 +12,9 @@ public class Task01Main {
             Predicate<? super T> condition,
             Function<? super T, ? extends U> ifTrue,
             Function<? super T, ? extends U> ifFalse) {
-        if (condition == null || ifFalse = null || ifTrue == null)
+        if (condition == null || ifFalse == null || ifTrue == null) {
             throw new NullPointerException();
+        }
         return (x)->condition.test(x)?ifTrue.apply(x):ifFalse.apply(x);
     }
 }
