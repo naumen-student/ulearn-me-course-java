0a4cc879d35fcb0788f7942cafc3d2b4493051bb
Danil Konkov <buroroll@gmail.com>
2020-11-18 23:13:49 +0500

    Коньков Данил. Задание 10.3

--- 10-java-functional-stream/task03/src/com/example/task03/Task03Main.java
+++ 10-java-functional-stream/task03/src/com/example/task03/Task03Main.java
@@ -1,6 +1,6 @@
 package com.example.task03;
 
-import java.util.Comparator;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
@@ -21,7 +21,18 @@ public class Task03Main {
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-
-        // your implementation here
+        Map<String, T> maxAndMinEl = new HashMap<>();
+        final boolean[] first = {true};
+        stream.forEach(x -> {
+            if (first[0]){
+                maxAndMinEl.put("Min", x);
+                maxAndMinEl.put("Max", x);
+                first[0] = false;
+            } else if(order.compare(x, maxAndMinEl.get("Min")) < 0)
+                maxAndMinEl.put("Min", x);
+            else if (order.compare(x, maxAndMinEl.get("Max")) > 0)
+                maxAndMinEl.put("Max", x);
+        });
+        minMaxConsumer.accept(maxAndMinEl.get("Min"), maxAndMinEl.get("Max"));
     }
 }
