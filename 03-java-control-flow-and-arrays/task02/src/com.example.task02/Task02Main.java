3e3f5c1ffc828d293296db3c3f3ad12c60ac7f9a
grekova-web <71182152+grekova-web@users.noreply.github.com>
2020-11-18 11:59:29 +0500

    Грекова Елена. Задание 3.2

--- 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
+++ 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
@@ -10,24 +10,9 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        switch (monthNumber) {
-            case 12:
-            case 1:
-            case 2:
-                return "зима";
-            case 3:
-            case 4:
-            case 5:
-                return "весна";
-            case 6:
-            case 7:
-            case 8:
-                return "лето";
-            case 9:
-            case 10:
-            case 11:
-                return "осень";
-            default: return "Несуществующий месяц";
-        }
+        if(monthNumber < 3 || monthNumber == 12) return "зима";
+        if(monthNumber < 6) return "весна";
+        if(monthNumber < 9) return "лето";
+        return "осень";
     }
 }
