b1ed0306c298344d70b5690af3047a987f4024a6
MaximFrancevich <FranzuaAirMax@yandex.ru>
2020-11-18 17:32:35 +0500

    Францевич Максим. Задание 07-04

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,29 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+        StringBuilder value = new StringBuilder();
+        double sum = 0;
+        int character;
+        while ((character = System.in.read()) != -1) {
+            value.append((char) character);
+            if (Character.isWhitespace((char) character)) {
+                sum = addSum(sum, value);
+                value.setLength(0);
+            }
+        }
+        sum = addSum(sum, value);
+        System.out.printf(Locale.US, "%.6f", sum);
+    }
 
-        System.out.println("0.0");
+    static double addSum(double sum, StringBuilder value) {
+        try {
+            sum += Double.parseDouble(value.toString());
+        } catch (NumberFormatException e) {
+        }
+        return sum;
     }
 }
