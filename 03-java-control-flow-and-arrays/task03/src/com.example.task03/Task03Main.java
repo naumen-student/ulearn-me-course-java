609e4c6c83e3d060a0295897d6acd1710e316081
grekova-web <71182152+grekova-web@users.noreply.github.com>
2020-11-18 12:08:08 +0500

    Грекова Елена. Задание 3.3

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -10,15 +10,11 @@ public class Task03Main {
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        switch (weekDaysNumber) {
-            case 1: return "понедельник";
-            case 2: return "вторник";
-            case 3: return "среда";
-            case 4: return "четверг";
-            case 5: return "пятница";
-            case 6: return "суббота";
-            case 7: return "воскресенье";
-            default: return "такого дня недели не существует";
+        if (weekDaysNumber < 1 || weekDaysNumber > 7) {
+            return "такого дня недели не существует";
         }
+        String[] days = new String[]{"понедельник", "вторник", "среда",
+                "четверг", "пятница", "суббота", "воскресенье"};
+        return days[weekDaysNumber - 1];
     }
 }
