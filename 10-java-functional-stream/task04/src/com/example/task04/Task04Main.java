7226a797e907969c19a4aebe9d3a0cc8017ddbf2
zavyalovartem58@gmail.com <A75VRmQ652IRK>
2020-11-18 08:05:36 +0500

    Завьялов Артём. Задание 10.04

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,29 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Task04Main {
 
     public static void main(String[] args) {
-
-        // your implementation here
-
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            br.lines().
+                    flatMap(line -> Arrays.stream(line.split("[\\P{L}]")))
+                    .map(String::toLowerCase)
+                    .filter(word -> word.length() > 0)
+                    .collect(Collectors.toMap(w -> w, w -> 1, Integer::sum))
+                    .entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByKey())
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .limit(10)
+                    .forEach(entry -> System.out.print(entry.getKey() + "\n"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
