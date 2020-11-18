b3da0322458b53c7febd73196960fde8a6f38233
apterousteen <ayzhan312@live.ru>
2020-11-18 20:58:50 +0500

    10_task03

--- 10-java-functional-stream/task03/src/com/example/task03/Task03Main.java
+++ 10-java-functional-stream/task03/src/com/example/task03/Task03Main.java
@@ -1,6 +1,7 @@
 package com.example.task03;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
@@ -22,6 +23,25 @@ public class Task03Main {
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
 
-        // your implementation here
+        if (stream == null || order == null || minMaxConsumer == null)
+            throw new NullPointerException();
+
+        Iterator<? extends T> iterator = stream.iterator();
+
+        if(!iterator.hasNext()){
+            minMaxConsumer.accept(null, null);
+            return;
+        }
+
+        T max = iterator.next();
+        T min = max;
+
+        while (iterator.hasNext()){
+            T temp = iterator.next();
+            if(order.compare(min, temp) > 0) min = temp;
+            if(order.compare(max,temp) < 0) max = temp;
+        }
+
+        minMaxConsumer.accept(min, max);
     }
 }
