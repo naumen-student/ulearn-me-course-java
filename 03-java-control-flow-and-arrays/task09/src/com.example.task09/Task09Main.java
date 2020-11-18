d857964ff20d9b0c7d3309157360864f7d26f66a
grekova-web <71182152+grekova-web@users.noreply.github.com>
2020-11-18 12:34:28 +0500

    Грекова Елена. Задание 3.9

--- 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
+++ 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
@@ -1,5 +1,7 @@
 package com.example.task09;
 
+import java.util.Arrays;
+
 public class Task09Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -14,10 +16,7 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length; i++)
-            min = Math.min(min, arr[i]);
-        return min;
+        return Arrays.stream(arr).min().getAsInt();
     }
 
 }
