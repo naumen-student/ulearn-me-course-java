cea8fc113b3b90e30b5a872889e998ff1d0f6e2e
grekova-web <71182152+grekova-web@users.noreply.github.com>
2020-11-18 12:31:46 +0500

    Грекова Елена. Задание 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -1,5 +1,7 @@
 package com.example.task08;
 
+import java.util.Arrays;
+
 public class Task08Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -13,12 +15,9 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        if (arr.length == 0) return 0;
-        long result = 1;
-        for (int i=0; i < arr.length; i++) {
-            result = result * arr[i];
-        }
-        return result;
+        return Arrays.stream(arr).asLongStream()
+                .reduce((x, y) -> x * y)
+                .orElse(0);
     }
 
 }
