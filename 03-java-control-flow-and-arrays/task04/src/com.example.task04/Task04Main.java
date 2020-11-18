aee731aa515cbabfabf69945ea89c07446e6cac0
grekova-web <71182152+grekova-web@users.noreply.github.com>
2020-11-18 12:12:32 +0500

    Грекова Елена. Задание 3.4

--- 03-java-control-flow-and-arrays/task04/src/com.example.task04/Task04Main.java
+++ 03-java-control-flow-and-arrays/task04/src/com.example.task04/Task04Main.java
@@ -10,11 +10,8 @@ public class Task04Main {
     }
 
     static void from0to10000() {
-        int i = 0;
-        while (i < 10000) {
+        for (int i = 0; i < 10000; i++) {
             System.out.println(i);
-            i++;
         }
     }
-
 }
