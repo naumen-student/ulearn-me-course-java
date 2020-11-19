3136dcb83de7a6b210e0419742f922a6d4021c11
MaximFrancevich <FranzuaAirMax@yandex.ru>
2020-11-19 17:21:19 +0500

    Францевич Максим. Задание 08-01

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,33 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task01Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
+    private static String title;
 
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+    public static void main(String[] args) throws IOException, InterruptedException {
+//        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        new BufferedReader(
+                new InputStreamReader(
+                        new ProcessBuilder()
+                                .command("C:\\ffmpeg\\bin\\ffmpeg", "-i", file.getAbsoluteFile().toString())
+                                .start()
+                                .getErrorStream()))
+                .lines()
+                .forEach(Task01Main::getTitle);
+        return title;
+    }
+
+    private static void getTitle(String line) {
+        if (line.indexOf("title") > 0) {
+            int indexOfTitle = line.indexOf(":") + 2;
+            title = line.substring(indexOfTitle);
+        }
     }
 }
