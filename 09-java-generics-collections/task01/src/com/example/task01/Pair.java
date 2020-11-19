525b532222745448884b79638911e8ebd1714057
JokeI777 <danleo.2000@mail.ru>
2020-11-19 14:06:54 +0500

    Леонов Даниил, Модуль 9, задание 1.

--- 09-java-generics-collections/task01/src/com/example/task01/Pair.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Pair.java
@@ -1,5 +1,41 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.function.BiConsumer;
+
+public class Pair <T, Z> {
+
+    private final T firstValue;
+    private final Z secondValue;
+
+    private Pair(T firstValue, Z secondValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
+
+    public static <T, Z> Pair<T, Z> of(T firstValue, Z secondValue) {
+        Pair<T, Z> pair = new Pair(firstValue, secondValue);
+        return pair;
+    }
+
+    public void ifPresent(BiConsumer pair) {
+        if (firstValue != null && secondValue != null) {
+            pair.accept(firstValue, secondValue);
+        }
+    }
+
+    public T getFirst() {
+        return firstValue;
+    }
+
+    public Z getSecond() {
+        return secondValue;
+    }
+
+    public boolean equals(Pair value) {
+        return firstValue.equals(value.getFirst()) && secondValue.equals(value.secondValue);
+    }
+
+    public int hashCode() {
+        return firstValue.hashCode() + secondValue.hashCode();
+    }
 }
