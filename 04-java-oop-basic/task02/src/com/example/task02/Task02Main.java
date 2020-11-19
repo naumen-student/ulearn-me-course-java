3d8d569df6b0b45ece6eca623f80876abd047e5a
dimagarn <sheldingcoder4@gmail.com>
2020-11-19 13:51:54 +0500

    4.2

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,57 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
+    }
+}
 
+class TimeSpan {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes(){
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public TimeSpan(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    void add(TimeSpan time) {
+        hours += time.hours;
+        minutes += time.minutes;
+        seconds += time.seconds;
+    }
+
+    void subtract(TimeSpan time) {
+        hours -= time.hours;
+        minutes -= time.minutes;
+        seconds -= time.seconds;
+    }
+
+    public String toString() {
+        return String.format("Time is: %d:%d:%d", hours, minutes, seconds);
     }
 }
