2ec87dda2fc287fe8f87ba586b65237359373868
JokeI777 <danleo.2000@mail.ru>
2020-11-19 14:55:39 +0500

    Леонов Даниил, Модуль 9, задание 3.

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -4,21 +4,70 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
-
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
-        for (Set<String> anagram : anagrams) {
-            System.out.println(anagram);
-        }
-
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Scanner scanner = new Scanner(inputStream, charset.name());
+        Map<String, Set<String>> newDict = new HashMap<>();
+        Pattern pattern = Pattern.compile("[" + "а-яА-ЯёЁ" + "]" + "*");
+
+        while (scanner.hasNextLine()){
+            String str = scanner.nextLine().toLowerCase();
+
+            if(str.length() > 2 && pattern.matcher(str).matches()){
+                if(newDict.size() == 0){
+                    Set<String> set = new HashSet<>();
+                    set.add(str);
+                    newDict.put(str, set);
+                    continue;
+                }
+            }
+            else {
+                continue;
+            }
+
+            boolean added = false;
+
+            for(String key : newDict.keySet()){
+                if(algorithmAnagrams(str, key)){
+                    Set<String> list = newDict.get(key);
+                    list.add(str);
+                    newDict.put(key, list);
+                    added = true;
+                    break;
+                }
+            }
+
+            if(!added){
+                Set<String> set = new HashSet<>();
+                set.add(str);
+                newDict.put(str, set);
+            }
+
+        }
+
+        List<Set<String>> needList = new ArrayList<>();
+
+        for (Set<String> value : newDict.values()){
+            if(value.size() > 1){
+                needList.add(new TreeSet<String>(value));
+            }
+        }
+
+        return needList;
+    }
+
+    public static boolean algorithmAnagrams(String str1, String str2){
+        char[] firstArray = str1.toCharArray();
+        char[] secondArray = str2.toCharArray();
+        Arrays.sort(firstArray);
+        Arrays.sort(secondArray);
+        return (new String(firstArray)).equals(new String(secondArray));
     }
 }
