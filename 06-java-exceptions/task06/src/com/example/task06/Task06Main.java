a35aeb6034e092e4682ac18a6c39d4b2a624e181
IlyaRussia23 <ilya.grebenyuk.01@mail.ru>
2020-11-19 05:49:39 +0300

    Гребенюк Илья 6.6

--- 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
+++ 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
@@ -4,13 +4,13 @@ public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         new Task06Main().printMethodName();
-         */
+
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        System.out.print(Thread.currentThread().getStackTrace()[2].getMethodName());
     }
 
 }
