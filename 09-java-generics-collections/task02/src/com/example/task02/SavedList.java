dc77c44dce378523d7df400352a6a2a3cb1843a7
JokeI777 <danleo.2000@mail.ru>
2020-11-19 14:08:47 +0500

    Леонов Даниил, Модуль 9, задание 2.

--- 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
+++ 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,70 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
-import java.util.AbstractList;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
-    public SavedList(File file) {
+    public File file;
+    public List<E> list;
+
+    public SavedList(File file) throws IOException {
+        this.list = new ArrayList<E>();
+        if (file.exists()) {
+            try {
+                ObjectInputStream object = new ObjectInputStream(Files.newInputStream(file.toPath()));
+                list = (List<E>) object.readObject();
+                this.file = file;
+
+            } catch (Exception e) {
+            }
+        } else {
+            this.file = new File(file.getAbsolutePath());
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E listObject = list.set(index, element);
+        try {
+            ObjectOutputStream object = new ObjectOutputStream(Files.newOutputStream(file.toPath()));
+            object.writeObject(list);
+        } catch (Exception e) {
+        }
+        return listObject;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        try {
+            ObjectOutputStream object = new ObjectOutputStream(Files.newOutputStream(file.toPath()));
+            object.writeObject(list);
+        } catch (Exception e) {
+        }
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E removedObject = list.remove(index);
+        try {
+            ObjectOutputStream object = new ObjectOutputStream(Files.newOutputStream(file.toPath()));
+            object.writeObject(list);
+        } catch (Exception e) {
+        }
+        return removedObject;
     }
 }
