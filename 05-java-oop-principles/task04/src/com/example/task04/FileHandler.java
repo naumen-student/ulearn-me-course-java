fb8c89dfc7a215c0b38346ecfa20ca5b2915ddd2
regina <regina.agafonova.2001@gmail.com>
2020-11-20 20:28:31 +0500

    Агафонова Регина 05_task04

---
+++ 05-java-oop-principles/task04/src/com/example/task04/FileHandler.java
@@ -0,0 +1,18 @@
+package com.example.task04;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler implements MessageHandler {
+
+    @Override
+    public void logMessage(String message) {
+        try {
+            FileWriter writer = new FileWriter("log.txt", false);
+            writer.write(message + "\n");
+            writer.flush();
+        } catch (IOException e){
+            System.out.println(e.toString());
+        }
+    }
+}
---
+++ 05-java-oop-principles/task04/src/com/example/task04/MemoryHandler.java
@@ -0,0 +1,26 @@
+package com.example.task04;
+
+import java.util.ArrayList;
+
+public class MemoryHandler implements MessageHandler {
+
+    private final long size;
+    private final ArrayList<String> messageSaver = new ArrayList<>();
+    private final MessageHandler handler;
+
+    public MemoryHandler(long size, MessageHandler handler) {
+        this.size = size;
+        this.handler = handler;
+    }
+
+    @Override
+    public void logMessage(String message){
+        messageSaver.add(message);
+        if (messageSaver.size() >= size){
+            for (String savedMessage : messageSaver){
+                handler.logMessage(savedMessage);
+            }
+            messageSaver.clear();
+        }
+    }
+}
