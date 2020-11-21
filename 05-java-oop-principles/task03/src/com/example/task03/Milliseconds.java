0436ece93c0bf0b90a6aef8dedaf29bc49364b60
Konovalova Lera <lera.penguin01@mail.ru>
2020-11-21 17:15:10 +0500

    Коновалова Лера. Задание 5.3

--- 05-java-oop-principles/task03/src/com/example/task03/Milliseconds.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Milliseconds.java
@@ -18,11 +18,15 @@ public class Milliseconds implements TimeUnit {
 
     @Override
     public long toSeconds() {
-        return amount / 1000;
+        return Math.round(amount / 1000.0);
     }
 
     @Override
     public long toMinutes() {
-        return amount / 1000 * 60;
+        return Math.round(amount / 1000 * 60);
+    }
+
+    public long toHours() {
+        return Math.round(toMinutes() / 60.0);
     }
 }
