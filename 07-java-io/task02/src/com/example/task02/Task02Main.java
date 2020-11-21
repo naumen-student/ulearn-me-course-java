586f73d9e2c9c0865a507ec6e56a109c06d6247a
TobeeM <mark.p200110@gmail.com>
2020-11-22 00:45:55 +0500

    Поташников Марк, Задание 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Task02Main {
     public static void main(String[] args) throws IOException {
         int current = System.in.read();
-        while (current != -1) {
+        while (current >= -1) {
             int next = System.in.read();
             if (next != 10 && current != 13)
                 System.out.write(current);
