131b33c2cf2e4bef7f3623166eed59eb5ad8d7fa
TobeeM <mark.p200110@gmail.com>
2020-11-22 00:27:56 +0500

    Поташников Марк, Задание 7.1

--- 07-java-io/task01/src/com/example/task01/Task01Main.java
+++ 07-java-io/task01/src/com/example/task01/Task01Main.java
@@ -15,7 +15,13 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) throw new IllegalArgumentException();
+        int result = 0;
+        int currentSum = inputStream.read();
+        while (currentSum != -1) {
+            result = Integer.rotateLeft(result, 1) ^ currentSum;
+            currentSum = inputStream.read();
+        }
+        return result;
     }
 }
