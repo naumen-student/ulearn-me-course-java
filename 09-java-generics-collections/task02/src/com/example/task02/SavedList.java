de2f56de5d2792290ec2ba7a3af1d8f8e6cedbeb
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2020-11-22 19:56:18 +0500

    Зернюков Никита. Задача 9.2

--- 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
+++ 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,65 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private ArrayList<E> list = new ArrayList<>();
+    private final File file;
 
-    public SavedList(File file) {
+    public SavedList(File file) throws IOException {
+        if (file.exists()) {
+            try (
+                    FileInputStream inputStream = new FileInputStream(file);
+                    ObjectInputStream stream = new ObjectInputStream(inputStream)) {
+                list = (ArrayList<E>) stream.readObject();
+            } catch (ClassNotFoundException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else
+            file.createNewFile();
+        this.file = file;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E newbie = list.set(index, element);
+        update();
+        return newbie;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        update();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E corpse = list.remove(index);
+        update();
+        return corpse;
+    }
+
+    private void update() {
+        try (FileOutputStream fos = new FileOutputStream(file);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
