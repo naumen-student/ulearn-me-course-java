804636120c73e7701199d088d0410003f39d5e6a
дом <anton89222910261123@yandex.ru>
2020-11-22 20:49:17 +0500

    Петров Антон Задача 5.4

---
+++ 05-java-oop-principles/task04/src/com/example/task04/MemoryHandler.java
@@ -0,0 +1,36 @@
+package com.example.task04;
+
+import java.util.ArrayList;
+
+public class MemoryHandler implements MessageHandler
+{
+    private final ArrayList<String> buffer = new ArrayList<>();
+    private final long capacity;
+    private final MessageHandler handler;
+
+    public MemoryHandler(MessageHandler handler, long capacity)
+    {
+        this.capacity = capacity;
+        this.handler = handler;
+    }
+
+    @Override
+    public void log(Logger.Level level, String message)
+    {
+        buffer.add(message);
+        if (buffer.size() >= capacity)
+        {
+            for (String msg : buffer)
+            {
+                handler.log(level, msg);
+            }
+            buffer.clear();
+        }
+    }
+
+    @Override
+    public void log(Logger.Level level, String pattern, Object... args)
+    {
+        log(level, String.format(pattern, args));
+    }
+}
--- 05-java-oop-principles/task04/src/com/example/task04/Task04Main.java
+++ 05-java-oop-principles/task04/src/com/example/task04/Task04Main.java
@@ -1,7 +1,16 @@
 package com.example.task04;
 
-public class Task04Main {
-    public static void main(String[] args) {
+import java.time.temporal.ChronoUnit;
 
+public class Task04Main
+{
+    public static void main(String[] args)
+    {
+        Logger test = new Logger("test");
+        test.addHandler(new ConsoleHandler(test));
+        test.addHandler(new FileHandler(test, "info.txt"));
+        test.addHandler(new RotationFileHandler(test, "log", ChronoUnit.MINUTES));
+        test.error("Error");
+        test.debug("Debug");
     }
 }
