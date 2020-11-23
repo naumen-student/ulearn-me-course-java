64c0383f01d5edcd327ccc29e35b1ca1ade48861
Frezyx <youngfrezyx@gmail.com>
2020-11-23 21:19:06 +0300

    Ильин Станислав. Задача 9.2

--- 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
+++ 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,65 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SavedList<E extends Serializable> extends AbstractList<E> {
+public class SavedList<E extends Serializable> extends AbstractList<E>{
+    private final File file;
+    private final List<E> items = new ArrayList<E>();
 
-    public SavedList(File file) {
+    public SavedList(File file){
+        this.file = file;
+       tryReadItemsFromFile();
     }
 
     @Override
-    public E get(int index) {
-        return null;
+    public E get(int index){
+        return items.get(index);
     }
 
     @Override
-    public E set(int index, E element) {
-        return null;
+    public void add(int index, E e){
+        items.add(index, e);
+        tryWriteItemsToFile();
     }
 
     @Override
-    public int size() {
-        return 0;
+    public E remove(int index){
+        E e = items.remove(index);
+        tryWriteItemsToFile();
+        return e;
     }
 
     @Override
-    public void add(int index, E element) {
+    public int size(){
+        return items.size();
     }
 
     @Override
-    public E remove(int index) {
-        return null;
+    public E set(int index, E el){
+        E e = items.set(index, el);
+        tryWriteItemsToFile();
+        return e;
+    }
+
+    private void tryReadItemsFromFile(){
+        try {
+            FileInputStream inputStream = new FileInputStream(file);
+            try (ObjectInputStream stream = new ObjectInputStream(inputStream)) {
+                items.addAll((List<E>) stream.readObject());
+            }
+        } catch (Exception ignored) {}
+
+    }
+
+    private void tryWriteItemsToFile(){
+        try {
+            FileOutputStream stream = new FileOutputStream(file);
+            try (ObjectOutputStream inputStream = new ObjectOutputStream(stream)) {
+                inputStream.writeObject(items);
+            }
+        } catch (IOException ignored) {}
     }
 }
