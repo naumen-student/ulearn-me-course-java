7a4f0ba505cd93430285060143ddc6bdbe420313
ssss123-byte <55254118+ssss123-byte@users.noreply.github.com>
2020-11-24 14:58:44 +0300

    Шардаков Егор. Задание 3.10

--- 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
+++ 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
@@ -1,8 +1,8 @@
-package com.example.task09;
+package com.example.task10;
 
 import java.util.Arrays;
 
-public class Task09Main {
+public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
