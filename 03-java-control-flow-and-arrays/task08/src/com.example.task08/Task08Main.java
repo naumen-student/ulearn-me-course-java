ffd0ac2149451a1271e9c3a2d8ef7bb33dfb39f1
ssss123-byte <55254118+ssss123-byte@users.noreply.github.com>
2020-11-24 14:42:00 +0300

    Шардаков Егор. Задание 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -13,8 +13,11 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        if (arr.length == 0)
+            return 0;
+        long op = 1;
+        for (int i : arr) op *= i;
+        return op;
     }
 
 }
