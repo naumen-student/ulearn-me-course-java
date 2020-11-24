25d3a6108766b651694128832518ae0acf5018ee
Haggard13 <alexey.kozedubov@urfu.me>
2020-11-24 15:42:34 +0500

    Козедубов Алексей. Задание 9.2

--- 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
+++ 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,75 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
-    public SavedList(File file) {
+    public SavedList(File file){
+        List<E> list;
+
+        if (!file.exists()){
+            list = new ArrayList<>();
+        } else {
+            try (ObjectInputStream is = new ObjectInputStream(new FileInputStream(file))) {
+                list = (List<E>) is.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                list = new ArrayList<>();
+            }
+        }
+
+        this.list = list;
+        this.file = file;
     }
+
+    private final File file;
+    private final List<E> list;
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        list.set(index, element);
+        updateFile();
+        return list.get(index);
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        updateFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E e = list.remove(index);
+        updateFile();
+        return e;
+    }
+
+    private void updateFile() {
+        if (!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(file))) {
+            os.writeObject(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
