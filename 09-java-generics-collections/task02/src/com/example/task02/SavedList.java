a5d4e3fe69dd856e24881695be7da728553ad74d
Asus <vya4eslav.rublev@yandex.ru>
2020-11-24 12:08:29 +0500

    Рублев Вячеслав. Задание 9.2

--- 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
+++ 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,63 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private final File file;
+    private List<E> list = new ArrayList<E>();
 
-    public SavedList(File file) {
+    public SavedList(File file) throws IOException {
+        if (file.exists()) {
+            try (FileInputStream fileInputStream = new FileInputStream(file);
+                 ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+                list = (ArrayList<E>)objectInputStream.readObject();
+            } catch (ClassNotFoundException | IOException exception) { exception.printStackTrace(); }
+        }
+        else
+            file.createNewFile();
+        this.file = file;
+    }
+
+    private void writeListToFile() {
+        try(FileOutputStream fileOutputStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+            objectOutputStream.writeObject(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E e = list.set(index, element);
+        writeListToFile();
+        return e;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        writeListToFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E e = list.remove(index);
+        writeListToFile();
+        return e;
     }
 }
