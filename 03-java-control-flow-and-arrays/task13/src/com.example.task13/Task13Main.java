457aebb941d9ce93252d2032a5edcb14fb58aba8
ssss123-byte <55254118+ssss123-byte@users.noreply.github.com>
2020-11-24 15:11:12 +0300

    Шардаков Егор. Задание 3.13

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -1,5 +1,9 @@
 package com.example.task13;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -12,8 +16,17 @@ public class Task13Main {
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if (arr == null || arr.length == 0) return arr;
+        List<Integer> list = new ArrayList<>();
+        for (int i : arr) {
+            if (i < 1000) {
+                list.add(i);
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int j = 0; j < list.size(); j++)
+            res[j] = list.get(j);
+        return res;
     }
 
 }
