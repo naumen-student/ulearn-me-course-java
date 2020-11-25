558ae5dd59b4d548628259ec97bf8dd2206ea9b4
JIiCeHoK <foxs29546@gmail.com>
2020-11-25 20:15:38 +0500

    Моисеев Михаил. 4.2

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -4,4 +4,63 @@ public class Task02Main {
     public static void main(String[] args) {
 
     }
-}
+
+    class TimeSpan {
+        private int hour, minute, second;
+
+        public TimeSpan(int hour, int minute, int second) {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public int get_hour() {
+            return this.hour;
+        }
+
+        public int set_hour(int hour) {
+            this.hour = hour;
+        }
+
+        public int get_minute() {
+            return this.minute;
+        }
+
+        public int set_minute(int minute) {
+            this.minute = minute;
+        }
+
+        public int get_second() {
+            return this.second;
+        }
+
+        public int set_second(int second) {
+            this.second = second;
+        }
+
+        void add(TimeSpan time) {
+            this.hour += time.hour;
+            this.minute += time.minute;
+            this.second += time.second;
+            toFormat();
+        }
+
+        void subtract(TimeSpan time) {
+            this.hour -= time.hour;
+            this.minute -= time.minute;
+            this.second -= time.second;
+            toFormat();
+        }
+
+        private void toFormat() {
+            this.minute += this.second / 60;
+            this.second %= 60;
+            this.hour += this.minute / 60;
+            this.minute %= 60;
+            this.hour %= 24;
+        }
+
+        public String toString() {
+            return String.format("Час: {0}, Минута: {1}, Секунда: {2}", this.hour, this.minute, this.second);
+        }
+    }
