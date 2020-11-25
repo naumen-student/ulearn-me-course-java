5fc572d6e4de2802ee557f7e5c20ecf05b302a46
JIiCeHoK <foxs29546@gmail.com>
2020-11-25 21:38:48 +0500

    Моисеев Михаил. 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -1,6 +1,7 @@
 package com.example.task03;
 
 public class Task03Main {
+
     public static void main(String[] args) {
         KompNumbers number_1 = new CNumber(2.0, 0.0);
         KompNumbers number_2 = new CNumber(2.0, 2.0);
