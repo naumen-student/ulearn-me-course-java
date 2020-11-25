c2e0c515b46e792d5d5c7468e0f06d56ebd98919
Mckupe <65507746+Mckupe@users.noreply.github.com>
2020-11-25 21:54:09 +0500

    Нохрин Егор Задание 4.03

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,37 @@
 package com.example.task03;
 
 public class Task03Main {
-    public static void main(String[] args) {
+    private double x;
+    private double y;
 
+    public Task03Main(double re, double im)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getImPart() {
+        return x;
+    }
+
+    public double getRealPart() {
+        return y;
+    }
+
+    public Task03Main Sum(Task03Main number)
+    {
+        return new Task03Main(x + number.x, y + number.y);
+    }
+
+    public Task03Main Multiply(Task03Main complexNumber)
+    {
+        return new Task03Main(x * complexNumber.x, y * complexNumber.y);
+    }
+
+    public String toString() {
+        return "ComplexNumbers{" +
+                "re=" + x +
+                ", im=" + y +
+                '}';
     }
 }
