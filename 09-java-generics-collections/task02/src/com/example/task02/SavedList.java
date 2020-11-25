d3df919301e1aadcfc0fc177772308ad6f803463
krepeland <krepeland@yandex.ru>
2020-11-25 15:59:46 +0500

    Буханов Евгений. Задание 9.2

--- 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
+++ 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,61 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private final File file;
+    private List<E> list = new ArrayList<>();
 
-    public SavedList(File file) {
+    public SavedList(File file) throws IOException {
+        if (file.exists()) {
+            try (FileInputStream fileInputStream = new FileInputStream(file);
+                 ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+                list = (ArrayList<E>) objectInputStream.readObject();
+            } catch (Exception e) {
+            }
+        } else
+            file.createNewFile();
+        this.file = file;
+    }
+
+    private void saveListToFile() {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+            objectOutputStream.writeObject(list);
+        } catch (Exception e) {
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        list.set(index, element);
+        saveListToFile();
+        return element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        saveListToFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E e = list.remove(index);
+        saveListToFile();
+        return e;
     }
 }
