71d9cdc09c6812f8eb4afd6e11036b4ed23fbab1
JIiCeHoK <foxs29546@gmail.com>
2020-11-26 13:51:25 +0500

    Моисеев Михаил. 04.05

--- 04-java-oop-basic/task05/src/com/example/task05/Point.java
+++ 04-java-oop-basic/task05/src/com/example/task05/Point.java
@@ -1,51 +1,25 @@
 package com.example.task05;
 
-/**
- * Точка в двумерном пространстве
- */
 public class Point {
-
     private double x, y;
 
-    /**
-     * Конструктор, инициализирующий координаты точки
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
-     */
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    /**
-     * Возвращает координату точки по оси абсцисс
-     *
-     * @return координату точки по оси X
-     */
     public double getX() {
         return x;
     }
 
-    /**
-     * Возвращает координату точки по оси ординат
-     *
-     * @return координату точки по оси Y
-     */
     public double getY() {
         return y;
     }
 
-    /**
-     * Подсчитывает расстояние от текущей точки до точки, переданной в качестве параметра
-     *
-     * @param point вторая точка отрезка
-     * @return расстояние от текущей точки до переданной
-     */
     public double getLength(Point point) {
-        double result_x = point.x - x;
-        double result_y = point.x - y;
-        return Math.sqrt(result_x * result_x + result_y * result_y);
+        double X = point.x - x;
+        double Y = point.y - y;
+        return Math.sqrt(X * X + Y * Y);
     }
 
 }
