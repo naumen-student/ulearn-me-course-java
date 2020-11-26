42e1c14404ba061b3da9f218194a4b47a8ff4f72
JIiCeHoK <foxs29546@gmail.com>
2020-11-26 12:43:21 +0500

    Моисеев Михаил. 04.04

---
+++ 04-java-oop-basic/task04/src/com/example/task04/Point.java
@@ -0,0 +1,18 @@
+package com.example.task04;
+
+public class Point {
+    private final x,y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int get_x() {
+        return x;
+    }
+
+    public int get_y() {
+        return y;
+    }
+}
