b8c4fc89cb0c4e5796821d496fee00d39e43acb6
JokeI777 <danleo.2000@mail.ru>
2020-11-26 20:54:52 +0500

    Леонов Даниил, модуль 10, задача 3.

--- 10-java-functional-stream/task03/src/com/example/task03/Task03Main.java
+++ 10-java-functional-stream/task03/src/com/example/task03/Task03Main.java
@@ -1,27 +1,33 @@
 package com.example.task03;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
 public class Task03Main {
 
     public static void main(String[] args) {
-
-        findMinMax(
-                Stream.of(2, 9, 5, 4, 8, 1, 3),
-                Integer::compareTo,
-                (min, max) ->
-                        System.out.println("min: " + min + " / max: " + max)
-        );
-
     }
 
     public static <T> void findMinMax(
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-
-        // your implementation here
+        Integer[] counterIterations = new Integer[]{0};
+        Object[] minMax = new Object[2];
+        stream.forEach(x ->{
+            if(counterIterations[0] == 0) {
+                counterIterations[0] = 1;
+                minMax[0] = x;
+                minMax[1] = x;
+            }
+            else if(order.compare((T)minMax[0], x) > 0)
+                minMax[0] = x;
+            else if(order.compare((T) minMax[1], x) < 0)
+                minMax[1] = x;});
+        minMaxConsumer.accept((T) minMax[0], (T) minMax[1]);
     }
 }
