b0f27849e040ed0867b63fc425ee58478c299ec4
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-11-27 13:28:47 +0500

    Update Task02Main.java

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -1,57 +1,39 @@
 package com.example.task02;
 
-public class Task02Main {
-    public static void main(String[] args) {
-        class TimeSpan {
-            private int hours;
-            private int minutes;
-            private int seconds;
+class TimeSpan {
+    private int seconds, minutes, hours;
 
-            public TimeSpan(int hours, int minutes, int seconds) {
-                this.hours = hours;
-                this.minutes = minutes;
-                this.seconds = seconds;
-            }
-
-            public int getHours() {
-                return hours;
-            }
-
-            public void setHours(int hours) {
-                this.hours = hours;
-            }
-
-            public int getMinutes() {
-                return minutes;
-            }
-
-            public void setMinutes(int minutes) {
-                this.minutes = minutes;
-            }
-
-            public int getSeconds() {
-                return seconds;
-            }
-
-            public void setSeconds(int seconds) {
-                this.seconds = seconds;
-            }
-
-            void add(TimeSpan time) {
-                hours += time.hours;
-                minutes += time.minutes;
-                seconds += time.seconds;
-            }
-
-            void subtract(TimeSpan time) {
-                hours -= time.hours;
-                minutes -= time.minutes;
-                seconds -= time.seconds;
-            }
-
-            public String toString() {
-                return String.format("%d; %d; %d", hours, minutes, seconds);
-            }
-        }
+    public TimeSpan(int seconds, int minutes, int hours) {
+        this.seconds = seconds;
+        this.minutes = minutes;
+        this.hours = hours;
     }
-}
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+    public int getHours()
+    {
+        return hours;
+    }
+    void add(TimeSpan time)
+    {
+        hours += time.hours;
+        minutes += time.minutes;
+        seconds += time.seconds;
+    }
+    void subtract(TimeSpan time)
+    {
+        hours -= time.hours;
+        minutes -= time.minutes;
+        seconds -= time.seconds;
+    }
+    public String toString()
+    {
+        return String.format("",hours,minutes,seconds);
+    }
+} 
