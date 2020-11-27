973590b40f9eb8c253852cbc3406f4478a6895c4
Timoromish <55537915+Timoromish@users.noreply.github.com>
2020-11-27 12:33:23 +0300

    Танатаров Тимур. задание 3.1

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -7,6 +7,28 @@ public class Point {
     int x;
     int y;
 
+    public Point(){
+
+    }
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip(){
+        int c;
+        c = x;
+        x = -y;
+        y = -c;
+    }
+    double distance(Point point){
+        return Math.sqrt(Math.pow(x-point.x, 2) + Math.pow(y-point.y, 2));
+    }
+
+    @Override
+    public String toString(){
+        return "(" + x + "," + y +")";
+    }
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
