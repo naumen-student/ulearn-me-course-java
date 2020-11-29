78f85c40922c68158503472325dd6c43bb43340b
Polina <kiramik55@gmail.com>
2020-11-29 23:36:41 +0500

    04_task_04

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,8 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Line line = new Line(new Point(9,5), new Point(7, 6));
+        boolean str = line.isCollinearLine(new Point(9,8));
+        System.out.println(line);
     }
 }
