8aa15452f43e6d8c0ca20e516a3b3f34d9dd3343
andrewzvorygin <Andrei.Zvorygin@urfu.me >
2020-11-29 11:58:20 +0500

    Зворыгин Андрей Задача 9.3

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -1,24 +1,45 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
 
-    public static void main(String[] args) throws IOException {
-
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
-        for (Set<String> anagram : anagrams) {
-            System.out.println(anagram);
-        }
-
+    public static void main(String[] args) {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset));
+        Map<String, SortedSet<String>> map = new TreeMap<>();
+        while (scanner.hasNextLine()){
+            String word = scanner.nextLine().toLowerCase();
+            if(!checkLenghtLanguage(word))
+                continue;
+            String sortedChar = getSortString(word);
+            if (!map.containsKey(sortedChar))
+                map.put(sortedChar, new TreeSet<>());
+            map.get(sortedChar).add(word);
+        }
+        return getListAnagram(map);
+    }
+
+    private static boolean checkLenghtLanguage(String word){
+        return word.matches("[а-яА-Я]{3,}");
+    }
+
+    private static String getSortString(String word){
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    private static List<Set<String>> getListAnagram(Map<String, SortedSet<String>> map){
+        List<Set<String>> result = new ArrayList<>();
+        for (Set setWord : map.values())
+            if (setWord.size() > 1)
+                result.add(setWord);
+        return result;
     }
 }
