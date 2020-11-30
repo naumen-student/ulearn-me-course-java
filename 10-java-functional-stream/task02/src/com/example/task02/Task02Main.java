0c3d1e9f54ab3f8bda9f721691817a39288ff279
Lera <Valeria.Smirnova@urfu.me>
2020-11-30 12:28:09 +0500

    Смирнова Лера 10.02

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -6,17 +6,19 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
+        if (n < 1 || n > 16)
+            throw new IllegalArgumentException();
+        int max = (int) Math.pow(2, n);
+        return IntStream
+                .iterate(0, x -> x + 1)
+                .map(x -> (x % max) ^ ((x % max) >> 1)); // your implementation here
 
     }
 
