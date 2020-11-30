2f06713c1122db8bf84db378c31e01ba367b5338
KozhevnikovAlexandr <kozhevnikov.alexandr8@yandex.ru>
2020-11-30 17:59:22 +0500

    Task 0601 completed

--- 06-java-exceptions/task01/src/com/example/task01/Task01Main.java
+++ 06-java-exceptions/task01/src/com/example/task01/Task01Main.java
@@ -2,14 +2,15 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        codeWithNPE();
-         */
+        try{
+            codeWithNPE();
+        }
+        catch (NullPointerException e){
+            System.out.println("error caught");
+        }
     }
 
     static void codeWithNPE() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        throw new NullPointerException();
     }
 }
