640e48696621a4c2b6981c6ce72d231a86442ee5
aboyma <d.abaim1706@gmail.com>
2020-11-30 14:25:09 +0500

    Абаимов Дмитрий. Задание 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -4,18 +4,25 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        long l = Long.parseLong(input);
 
-        return "";
+        if (l >= Byte.MIN_VALUE && l <= Byte.MAX_VALUE)
+            return "byte";
+        if (l >= Short.MIN_VALUE && l <= Short.MAX_VALUE)
+            return "short";
+        if (l >= Integer.MIN_VALUE && l <= Integer.MAX_VALUE)
+            return "int";
+
+        return "long";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         String result = solution("12345");
         System.out.println(result);
-         */
+
     }
 
 }
