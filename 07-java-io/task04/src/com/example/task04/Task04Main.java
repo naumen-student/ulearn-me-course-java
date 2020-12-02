1885c3cab20ed67c643cf71393185950eecff629
Лялин Артём <maii122@mail.ru>
2020-12-02 15:45:24 +0500

    07_Task04 Done

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +11,12 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        Double sum = 0.0;
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)){
+            while (scanner.hasNextDouble()){
+                sum += scanner.nextDouble();
+            }
+        }
+        System.out.format(Locale.US, "%6f", sum);
     }
 }
