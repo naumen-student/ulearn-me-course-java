009cddd5849a7a9608e06ded6177ff513d58a2de
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2020-12-03 01:39:49 +0500

    Кашкин Сергей 6.4

--- 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
+++ 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
@@ -10,8 +10,34 @@ public class Task04Main {
          */
     }
 
-    static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+    static String getSeason(int monthNumber) throws MyException {
+        switch (monthNumber){
+            case 1:
+            case 2:
+            case 12:
+                return "зима";
+            case 3:
+            case 4:
+            case 5:
+                return "весна";
+            case 6:
+            case 7:
+            case 8:
+                return "лето";
+            case 9:
+            case 10:
+            case 11:
+                return "осень";
+            default:
+                throw new MyException ("monthNumber " + monthNumber +
+                        " is invalid, month number should be between 1..12");
+        }
     }
 
+}
+
+class MyException extends IllegalArgumentException{
+    public MyException(String message){
+        super(message);
+    }
 }
