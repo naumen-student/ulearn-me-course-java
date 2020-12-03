25bf44346a95b5b96e38f36187133d3fc61d7834
mckupe <nohrin09072001@yandex.ru>
2020-12-04 04:42:27 +0500

    Нохрин Егор Задание 5.04

---
+++ 05-java-oop-principles/task04/src/com/example/task04/MemoryHandler.java
@@ -0,0 +1,30 @@
+package com.example.task04;
+
+import java.util.ArrayList;
+
+public class MemoryHandler extends MessageHandler {
+    private final ArrayList<String> buffer = new ArrayList<>();
+    private final long capacity;
+    private final MessageHandler handler;
+
+    public MemoryHandler(MessageHandler handler, long capacity) {
+        this.capacity = capacity;
+        this.handler = handler;
+    }
+
+    @Override
+    public void log(Level level, String message) {
+        buffer.add(message);
+        if (buffer.size() >= capacity) {
+            for (String msg : buffer) {
+                handler.log(level, msg);
+            }
+            buffer.clear();
+        }
+    }
+
+    @Override
+    public void log(Level level, String pattern, Object... args) {
+        log(level, String.format(pattern, args));
+    }
+}
---
+++ 05-java-oop-principles/task04/src/com/example/task04/MessageHandler.java
@@ -0,0 +1,7 @@
+package com.example.task04;
+
+public class MessageHandler {
+    void log(Level level, String message, Object... args) {}
+    void log(Level level, String message) {}
+    void log(String message){}
+}
