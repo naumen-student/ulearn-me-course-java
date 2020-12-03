7b11263e403ee655c1ccc3f39305e08d30b5c24c
S1ide <kopotev.nick@icloud.com>
2020-12-03 17:43:51 +0500

    Копотев Никита, задание 9.1

--- 09-java-generics-collections/task01/src/com/example/task01/Pair.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Pair.java
@@ -1,5 +1,44 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class Pair<T, S> {
+    private T key;
+    private S value;
+
+    private Pair(T key, S value){ this.key = key; this.value = value;}
+
+
+
+    public static <T,S> Pair<T,S> of(T key, S value) {
+        return new Pair<>(key, value);
+    }
+
+    public T getFirst() {
+        return this.key;
+    }
+
+    public S getSecond() {
+        return this.value;
+    }
+
+
+    public boolean equals(Pair<T, S> second){
+        return this.key.equals(second.key) && this.value.equals(second.value);
+    }
+
+    public int hashCode(){
+        return this.key.hashCode() + this.value.hashCode();
+    }
+
+    public void ifPresent(BiConsumer<? super T, ? super S> action){
+        if (!(this.key == null) && !(this.value == null)){
+            action.accept(this.key, this.value);
+        }
+    }
+
+
 }
