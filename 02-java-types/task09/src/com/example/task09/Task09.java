1a80163a6ea19db335fff8f594b4b9d54cd0dd55
Polina <kiramik55@gmail.com>
2020-12-04 18:01:25 +0500

    02_task_09

--- 02-java-types/task09/src/com/example/task09/Task09.java
+++ 02-java-types/task09/src/com/example/task09/Task09.java
@@ -7,7 +7,7 @@ public class Task09 {
 
         float a = 1.0f;
         float b = 3.0f;
-        double x = (float)(a / b - 1.0 / 3.0) * 0;
+        double x = (a / b - 1.0f / 3.0f) * 1.0e9;
 
         return x;
     }
