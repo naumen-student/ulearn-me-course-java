1b779461e4cd72c85a88babce30d2b0c65019c20
Сухраб <Sukhrab2001@mail.ru>
2020-12-05 18:25:32 +0500

    Курбанов Сухраб, задача 3 08

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -13,8 +13,13 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        if(arr.length == 0)
+            return 0;
+        long result = 1L;
+        for (int val:arr) {
+            result *= val;
+        }
+        return result;
     }
 
 }
