7fcd74e7914ae06e523413552aedf7a4a763b34b
suppor14 <suppor14@yandex.ru>
2020-12-05 18:04:22 +0500

    Лицин Владислав. Задача 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,8 +1,11 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -16,8 +19,29 @@ public class Task02Main {
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+        List<Path> aPath = new ArrayList<>();
+        return FileExplore(rootDir.toFile());
+    }
 
-        return null;
+    private static List<Path> FileExplore(File rootDir) {
+        List<Path> res = new ArrayList<>();
+        for (File f : Objects.requireNonNull(rootDir.listFiles())) {
+            if (f.isDirectory()) {
+                FileExplore(f, res);
+            } else {
+                res.add(f.toPath());
+            }
+        }
+        return res;
+    }
+
+    private static void FileExplore(File rootDir, List<Path> paths) {
+        for (File f : Objects.requireNonNull(rootDir.listFiles())) {
+            if (f.isDirectory()) {
+                FileExplore(f, paths);
+            } else {
+                paths.add(f.toPath());
+            }
+        }
     }
 }
