70f76ec8417cf441723ddf2a2f1cab7d8e4bcec4
RoyalPiano <71338871+RoyalPiano@users.noreply.github.com>
2020-12-07 02:38:25 +0500

    Update Task02Main.java

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,11 +1,10 @@
 package com.example.task02;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -13,17 +12,14 @@ public class Task02Main {
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
         List<Path> result = new ArrayList<>();
-        Walk(rootDir.toFile(), result);
-        return result;
-    }
+        Files.walkFileTree(rootDir, new SimpleFileVisitor<Path>() {
 
-    private static void Walk(File rootDir, List<Path> result) {
-        for (File f : Objects.requireNonNull(rootDir.listFiles())) {
-            if (f.isDirectory()) {
-                Walk(f, result);
-            } else {
-                result.add(f.toPath());
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                result.add(file);
+                return super.preVisitDirectory(file, attrs);
             }
-        }
+        });
+        return result;
     }
 }
