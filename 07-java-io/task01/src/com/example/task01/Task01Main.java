1a1ad6f93339d0c00a9549bedec2f84514997603
Polina <kiramik55@gmail.com>
2020-12-07 03:41:25 +0500

    07_task_01

--- 07-java-io/task01/src/com/example/task01/Task01Main.java
+++ 07-java-io/task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,8 @@ package com.example.task01;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
@@ -15,7 +17,17 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int elementCurr = inputStream.read();
+        int contrSum = 0;
+        while (elementCurr != -1) {
+            contrSum = Integer.rotateLeft(contrSum, 1) ^ elementCurr;
+            elementCurr = inputStream.read();
+        }
+        return contrSum;
     }
 }
+
