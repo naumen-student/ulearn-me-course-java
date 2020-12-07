572603ecd81e5b36e3e7b7797198cd7ccdc590c0
RoyalPiano <zelyoniyoleg@yandex.ru>
2020-12-07 21:16:07 +0500

    ыва

--- 08-java-io2/task03/src/com/example/task03/Task03Main.java
+++ 08-java-io2/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,8 @@ package com.example.task03;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -15,7 +17,7 @@ public class Task03Main {
     }
 
     public static SampleData deserialize(InputStream inputStream) throws IOException, ClassNotFoundException {
-        // your implementation here
-        return null;
+        ObjectInputStream input = new ObjectInputStream(inputStream);
+        return (SampleData)input.readObject();
     }
 }
