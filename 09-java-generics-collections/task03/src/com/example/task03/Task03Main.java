2f684bcd329890fabd8b8c19ce5c1efdeed1c2cf
S1ide <kopotev.nick@icloud.com>
2020-12-07 21:34:12 +0500

    Копотев Никита, задание 9.3

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -18,22 +18,22 @@ public class Task03Main {
         Scanner scanner = new Scanner(inputStream);
         TreeMap<String, TreeSet<String>> strings= new TreeMap<String, TreeSet<String>>();
         while (scanner.hasNext()){
-            String word = scanner.nextLine().toLowerCase();
-            if (word.matches("[а-я]+") && word.length() > 3){
-                char[] chars = word.toCharArray();
-                Arrays.sort(chars);
-                String sortStr = new String(chars);
-                if (!strings.containsKey(sortStr)){
-                    strings.put(sortStr, new TreeSet<String>(){{
+            String word = scanner.nextLine().toLowerCase(); //приводим слова к нижнему регистру
+            if (word.matches("[а-я]+") && word.length() > 3){ // проверяем длину слова и соответствие русскому алфавиту
+                char[] chars = word.toCharArray(); // создаем массив символов
+                Arrays.sort(chars); //сортируем все символы
+                String sortStr = new String(chars); // собираем обратно в строку                   // все анаграммы состоят из одинаковых букв
+                if (!strings.containsKey(sortStr)){ // смотрим есть ли в нашем словаре такое слово //и при сортировке мы получим одинаковые значения
+                    strings.put(sortStr, new TreeSet<String>(){{ //кладем наше сортированное слово и само слово в словарь
                         add(word);
                     }
                     });
                 }
-                else strings.get(sortStr).add(word);
+                else strings.get(sortStr).add(word); // если его нет в словаре, добавляем новый
             }
         }
         for(TreeSet<String> combos : strings.values()){
-            if (combos.size() > 1){
+            if (combos.size() > 1){ // если более одной анаграммы добавляем в итоговый лист
                 list.add(combos);
             }
         }
