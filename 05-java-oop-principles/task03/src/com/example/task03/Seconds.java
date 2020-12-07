1655fda9986306a6101573fe4e54ad2e2183b2e7
Arnaud225K <kouakanarnaud@gmail.com>
2020-12-07 23:17:33 +0500

    Аданэн куакан задание 5.3

--- 05-java-oop-principles/task03/src/com/example/task03/Seconds.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Seconds.java
@@ -1,21 +1,16 @@
 package com.example.task03;
-
 /**
  * Интервал в секундах
  */
 public class Seconds implements TimeUnit {
-
     private final long amount;
-
     public Seconds(long amount) {
         this.amount = amount;
     }
-
     @Override
     public long toMillis() {
         return amount * 1000;
     }
-
     @Override
     public long toSeconds() {
         return amount;
@@ -24,5 +19,11 @@ public class Seconds implements TimeUnit {
     @Override
     public long toMinutes() {
         return Math.round(amount / 60);
+        return Math.round((double) amount / 60);
+    }
+
+    @Override
+    public long toHours() {
+        return  Math.round((double) amount / 3600);
     }
 }
