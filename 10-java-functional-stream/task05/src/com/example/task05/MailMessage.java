f401ec46992e5718616447f99335091c8d843129
Haggard13 <alexey.kozedubov@urfu.me>
2020-12-07 11:53:35 +0500

    Козедубов Алексей. Задание 10.5

---
+++ 10-java-functional-stream/task05/src/com/example/task05/MailMessage.java
@@ -0,0 +1,26 @@
+package com.example.task05;
+
+public class MailMessage implements Message<String> {
+    private String from;
+    private String to;
+    private String content;
+
+    public MailMessage(String from, String to, String content) {
+        this.from = from;
+        this.to = to;
+        this.content = content;
+    }
+
+    @Override
+    public String getFrom() {
+        return from;
+    }
+    @Override
+    public String getTo() {
+        return to;
+    }
+    @Override
+    public String getContent() {
+        return content;
+    }
+}
---
+++ 10-java-functional-stream/task05/src/com/example/task05/MailService.java
@@ -0,0 +1,27 @@
+package com.example.task05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class MailService<T> implements Consumer<Message<T>> {
+
+    private Map<String, List<T>> mailBox;
+
+    public MailService() {
+        mailBox = new HashMap<>();
+    }
+
+    public Map<String, List<T>> getMailBox() {
+        return mailBox;
+    }
+
+    @Override
+    public void accept(Message<T> tMessage) {
+        if (!mailBox.containsKey(tMessage.getTo()))
+            mailBox.put(tMessage.getTo(), new ArrayList<>());
+        mailBox.get(tMessage.getTo()).add(tMessage.getContent());
+    }
+}
