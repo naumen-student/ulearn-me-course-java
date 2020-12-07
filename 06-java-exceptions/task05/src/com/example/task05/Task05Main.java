ee18b74e9295e2722cfd60a3ca5b908fc715bd4e
ssss123-byte <55254118+ssss123-byte@users.noreply.github.com>
2020-12-07 11:06:29 +0300

    Шардаков Егор. Задание 6.5

--- 06-java-exceptions/task05/src/com/example/task05/Task05Main.java
+++ 06-java-exceptions/task05/src/com/example/task05/Task05Main.java
@@ -3,19 +3,26 @@ package com.example.task05;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileNotFoundException;
+import static java.lang.System.*;
 
 public class Task05Main {
-    public static void main(String[] args) throws IOException {
-        String pathToFile = args[0]; // "/home/user/file.txt"
-
-        String s = readFile(pathToFile);
-        System.out.println(s);
+    public static void main(String[] args){
+        String pathToFile = args[0];
+        try {
+            out.println(readFile(pathToFile));
+        }
+        catch (FileNotFoundException e){
+            out.printf("файл \"%s\" не найден\n", pathToFile);
+        }
+        catch (IOException e){
+            out.printf("произошла ошибка при чтении файла %s\n", pathToFile);
+        }
     }
 
     public static String readFile(String pathToFile) throws IOException {
         FileReader fileReader = new FileReader(pathToFile);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-
         StringBuilder stringBuilder = new StringBuilder();
         String currentLine;
         while ((currentLine = bufferedReader.readLine()) != null) {
@@ -23,7 +30,6 @@ public class Task05Main {
             stringBuilder.append("\n");
         }
         bufferedReader.close();
-
         return stringBuilder.toString();
     }
 }
