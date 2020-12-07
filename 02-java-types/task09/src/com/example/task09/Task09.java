81a9b17d495b84df17a7e0ae0063ae9ab0b8eb11
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2020-12-07 18:14:50 +0500

    Update Task09.java

--- 02-java-types/task09/src/com/example/task09/Task09.java
+++ 02-java-types/task09/src/com/example/task09/Task09.java
@@ -5,7 +5,7 @@ public class Task09 {
     public static double solution() {
         float a = 1.0f;
         float b = 3.0f;
-        double x = (int)(a / b - (float) 1.0 / 3.0) * 1.0e9;
+        double x = (a / b -  1.0f / 3.0f) * 1.0e9;
         return x;
     }
 }
