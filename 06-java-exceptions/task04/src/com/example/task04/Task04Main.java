8aa6bd1451e42bce6bea609ded66a6e950f64d41
ssss123-byte <55254118+ssss123-byte@users.noreply.github.com>
2020-12-07 10:57:36 +0300

    Шардаков Егор. Задание 6.4

--- 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
+++ 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
@@ -11,7 +11,16 @@ public class Task04Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(monthNumber < 1 || monthNumber > 12){
+            throw new MyException(
+                    "monthNumber "+
+                            monthNumber+" is invalid, month number should be between 1..12"
+            );
+        }
+        if(monthNumber >= 3 && monthNumber <= 5 ) return "весна";
+        if(monthNumber >= 6 && monthNumber <= 8) return  "лето";
+        if(monthNumber >= 9 && monthNumber <= 11) return "осень";
+        return "зима";
     }
 
 }
