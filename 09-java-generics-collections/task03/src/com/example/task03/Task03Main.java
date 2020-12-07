35e114abc4cf2dea3a535f61a50e02607af82920
S1ide <kopotev.nick@icloud.com>
2020-12-07 21:29:02 +0500

    Копотев Никита, задание 9.3

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -1,24 +1,42 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
 
         List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
-        for (Set<String> anagram : anagrams) {
-            System.out.println(anagram);
-        }
+        System.out.println(anagrams);
 
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        ArrayList<Set<String>> list = new ArrayList<Set<String>>();
+        Scanner scanner = new Scanner(inputStream);
+        TreeMap<String, TreeSet<String>> strings= new TreeMap<String, TreeSet<String>>();
+        while (scanner.hasNext()){
+            String word = scanner.nextLine().toLowerCase();
+            if (word.matches("[а-я]+") && word.length() > 3){
+                char[] chars = word.toCharArray();
+                Arrays.sort(chars);
+                String sortStr = new String(chars);
+                if (!strings.containsKey(sortStr)){
+                    strings.put(sortStr, new TreeSet<String>(){{
+                        add(word);
+                    }
+                    });
+                }
+                else strings.get(sortStr).add(word);
+            }
+        }
+        for(TreeSet<String> combos : strings.values()){
+            if (combos.size() > 1){
+                list.add(combos);
+            }
+        }
+        return list;
     }
 }
