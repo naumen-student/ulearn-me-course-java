2d14d2d3832b3a1e5391b5dc6746f946024202d1
s17v3rr <oleg.gubanov@urfu.me>
2020-12-08 20:57:00 +0500

    Губанов Олег. Задание 5.4

---
+++ 05-java-oop-principles/task04/src/com/example/task04/ConsoleHandler.java
@@ -0,0 +1,9 @@
+package com.example.task04;
+
+public class ConsoleHandler implements MessageHandler {
+
+    @Override
+    public void log(String message) {
+        System.out.println(message);
+    }
+}
