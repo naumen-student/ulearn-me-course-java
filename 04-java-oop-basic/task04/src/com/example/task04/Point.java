1110c80071420aef40cc4a95172aad66f5ad1aab
fitsuli <danilyuk_2011@bk.ru>
2020-12-09 00:15:22 +0500

    Task04 done

---
+++ 04-java-oop-basic/task04/src/com/example/task04/Point.java
@@ -0,0 +1,15 @@
+package com.example.task04;
+
+public class Point {
+    public final int x;
+    public final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+}
