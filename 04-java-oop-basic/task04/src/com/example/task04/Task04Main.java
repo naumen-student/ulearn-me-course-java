2c8da3c4ce16259d99347250a8fc37ae11ec6fbc
Максим Савков <savkovmaksim6@gmail.com>
2020-12-08 13:50:34 +0500

    Максим Савков Задание 4.4

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -1,7 +1,10 @@
 package com.example.task04;
 
+import java.awt.*;
+
 public class Task04Main {
     public static void main(String[] args) {
-
+        Line line = new Line(new Point(0,1), new Point(0,5));
+        System.out.println(line.isCollinearLine(new Point(0,3)));
     }
 }
