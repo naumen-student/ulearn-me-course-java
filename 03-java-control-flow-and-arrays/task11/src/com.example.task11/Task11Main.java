76a0570c95c4fe5d6e43352133a7f6eee406c30f
vlad2001g <Vladislav.Sadykov@urfu.me>
2020-12-08 14:45:19 +0500

    Садыков Владислав. Задание 3.11

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,20 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        int min = Integer.MAX_VALUE;
+        int first = arr[0];
+        int minIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                minIndex = i;
+            }
+        }
+        arr[0] = min;
+        arr[minIndex] = first;
     }
 
 }
