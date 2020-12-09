ae0f688f8cc6679afbe9d3b72067cfd4da16c534
vlad2001g <Vladislav.Sadykov@urfu.me>
2020-12-09 16:03:05 +0500

    Садыков Владислав. Задание 4.1

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -11,4 +11,26 @@ public class Point {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt((point.x - x) * (point.x - x) + (point.y - y) * (point.y - y));
+    }
+
+    void flip() {
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
 }
