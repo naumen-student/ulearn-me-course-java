ad150cb7c5dd72e863a9526c0964c5324514707c
0verv0ice <theznoy@gmail.com>
2020-12-09 16:42:08 +0500

    Машанов роман 06task06

--- 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
+++ 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
@@ -4,13 +4,18 @@ public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         new Task06Main().printMethodName();
-         */
+
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            System.out.print(e.getStackTrace()[1].getMethodName());
+
+        }
     }
 
 }
