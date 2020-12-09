0c2b801a56cebc4c909ed799aecce9a44d00323b
IlyaRussia23 <ilya.grebenyuk.01@mail.ru>
2020-12-09 18:12:37 +0500

    Илья Гребенюк 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,34 @@
 package com.example.task02;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
-        System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
+
+        System.out.println(listFiles(Paths.get("/home/ilya/IdeaProjects/ulearn-me-course-java/08-java-io2/task02/src/main/resources")));
+
 
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+        List<Path> res = new ArrayList<>();
+        try(Stream<Path> walk = Files.walk(rootDir)){
+            res = walk.filter(Files::isRegularFile).collect(Collectors.toList());
+        }
+        return res;
     }
 }
