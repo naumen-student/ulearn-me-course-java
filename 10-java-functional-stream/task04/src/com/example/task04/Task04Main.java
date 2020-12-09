8ba4a4490df436cef3ab5a25b77b3ecf9549a562
diukin9 <petr.diukin@urfu.me>
2020-12-09 18:29:00 +0500

    Дюкин Петр 10.4

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,21 @@
 package com.example.task04;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
 public class Task04Main {
 
     public static void main(String[] args) {
-
-        // your implementation here
-
+        HashMap<String, Integer> wordsCount = new HashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+        br.lines().map(String::toLowerCase).flatMap(y -> Arrays.stream(y.split("[^a-zа-яё0-9]+").clone()))
+                .filter(str -> !str.isEmpty()).forEach(currentKey -> {
+            if (!wordsCount.containsKey(currentKey))
+                wordsCount.put(currentKey, 1);
+            else wordsCount.put(currentKey, wordsCount.get(currentKey) + 1);
+        });
+        wordsCount.entrySet().stream().sorted(Map.Entry.comparingByKey()).sorted(Map.Entry
+        .<String, Integer>comparingByValue().reversed()).limit(10).forEach(x -> System.out.print(x.getKey() + '\n'));
     }
-
 }
