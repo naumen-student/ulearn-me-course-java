1acd3ef91c2acf090c7d1ce5f5ab7e1e57df3726
Arseny Smirnov <Arseny.Smirnov@urfu.me>
2020-12-09 13:19:17 +0500

    Смирнов Арсений 6.2

--- 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
+++ 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,22 @@ package com.example.task02;
 
 public class Task02Main {
 
+    private static final String[] seasons = {
+            "зима",
+            "зима",
+            "весна",
+            "весна",
+            "весна",
+            "лето",
+            "лето",
+            "лето",
+            "осень",
+            "осень",
+            "осень",
+            "зима",
+    };
+
+
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
@@ -11,6 +27,12 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+
+        if(monthNumber < 1 || monthNumber > 12) {
+            throw new IllegalArgumentException(String.format(
+                    "monthNumber %s is invalid, month number should be between 1..12",
+                    monthNumber));
+        }
+        return seasons[monthNumber - 1];
     }
 }
