972667250fa81c02d5719dcc2c96290ef0b88c9c
Жора <JoraKochkin@yandex.ru>
2020-12-09 15:12:14 +0500

    Жданов Сергей Задание 2-9

--- 02-java-types/task09/src/com/example/task09/Task09.java
+++ 02-java-types/task09/src/com/example/task09/Task09.java
@@ -3,7 +3,6 @@ package com.example.task09;
 public class Task09 {
 
     public static double solution() {
-        // TODO Устраните ошибку вычисления выражения, не изменяя типы данных у переменных
 
         float a = 1.0f;
         float b = 3.0f;
