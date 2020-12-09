738e7b8092c63212fc9589687a6336f7984f19f0
Жора <JoraKochkin@yandex.ru>
2020-12-09 14:44:34 +0500

    Жданов Сергей Задание 2-5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,10 +3,13 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while(x!= 0){
+            int number = x%10;
+            if(number % 2 != 0)
+                return "FALSE";
+            x /= 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
