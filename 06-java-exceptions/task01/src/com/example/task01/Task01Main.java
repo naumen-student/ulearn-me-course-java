fc8b855dca996898af365d6dedcf5840f2ee0269
0verv0ice <theznoy@gmail.com>
2020-12-09 15:25:41 +0500

    Машанов роман 06task01

--- 06-java-exceptions/task01/src/com/example/task01/Task01Main.java
+++ 06-java-exceptions/task01/src/com/example/task01/Task01Main.java
@@ -4,12 +4,13 @@ public class Task01Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         codeWithNPE();
-         */
+
     }
 
     static void codeWithNPE() {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        throw null;
     }
 }
