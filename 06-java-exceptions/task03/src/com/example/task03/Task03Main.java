a2452cf75285835e3921918f4dd17a1435e994cd
JIiCeHoK <foxs29546@gmail.com>
2020-12-10 22:13:29 +0500

    Моисеев Михаил. 06.03

--- 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
+++ 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
@@ -4,13 +4,14 @@ public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        throwCheckedException();
-         */
-    }
+        public static void main (String[]args) throws Exception {
+            throwCheckedException();
 
-    //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
+        }
 
+        public static void throwCheckedException () throws Exception {
+            throw new Exception();
+
+        }
     }
 }
