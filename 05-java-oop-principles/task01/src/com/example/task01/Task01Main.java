873db5091787ca49c8f5665742901035dbbd1c4a
JIiCeHoK <foxs29546@gmail.com>
2020-12-10 13:45:35 +0500

    Моисеев Михаил. 05.01

--- 05-java-oop-principles/task01/src/com/example/task01/Task01Main.java
+++ 05-java-oop-principles/task01/src/com/example/task01/Task01Main.java
@@ -1,6 +1,7 @@
 package com.example.task01;
 
 public class Task01Main {
+
     public static void main(String[] args) {
         Logger logg = new Logger("logger");
         logg.log(Logger.Level.ERROR, "xxxxx");
