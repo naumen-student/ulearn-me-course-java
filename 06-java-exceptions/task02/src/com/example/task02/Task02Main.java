cfbac96f1043c50b995b26fe2ef624843c19eaab
JIiCeHoK <foxs29546@gmail.com>
2020-12-10 20:20:20 +0500

    Моисеев Михаил. 06.02

--- 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
+++ 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
@@ -1,8 +1,6 @@
 public class Task02Main {
 
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
         /*
         System.out.println(getSeason(-5));
          */
