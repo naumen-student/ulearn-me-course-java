7c4d36fef937e654ab7376fa0d2ad6d476635862
arturha4 <sadbooys.2001@gmail.com>
2020-12-10 13:48:23 +0500

    Давлетшин Артур. Задание 8.3

--- 08-java-io2/task03/src/com/example/task03/Task03Main.java
+++ 08-java-io2/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,7 @@ package com.example.task03;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -15,7 +16,9 @@ public class Task03Main {
     }
 
     public static SampleData deserialize(InputStream inputStream) throws IOException, ClassNotFoundException {
-        // your implementation here
-        return null;
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
+            return (SampleData) objectInputStream.readObject();
+        }
     }
 }
+
