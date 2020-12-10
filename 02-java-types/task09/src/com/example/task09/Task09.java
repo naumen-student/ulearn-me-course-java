eb933bd55a88f6ecdf23037718bf4b56e9b614aa
Mohamed <mohamedtarek21763@gmail.com>
2020-12-11 00:02:32 +0500

    Хассан Мохамед Тарек Хассан 2.9

--- 02-java-types/task09/src/com/example/task09/Task09.java
+++ 02-java-types/task09/src/com/example/task09/Task09.java
@@ -3,20 +3,13 @@ package com.example.task09;
 public class Task09 {
 
     public static double solution() {
-        // TODO Устраните ошибку вычисления выражения, не изменяя типы данных у переменных
-
         float a = 1.0f;
         float b = 3.0f;
-        double x = (a / b - 1.0 / 3.0) * 1.0e9;
-
-        return x;
+        return (a / b - 1.0f / 3.0f) * 1.0e9;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение
-        /*
         System.out.println(solution());
-        */
     }
 
 }
