b2910e3f96cd26b933b241276cf316bbc313b4c8
dariasavvina <dariasavvina@mail.ru>
2020-12-10 18:06:52 +0500

    Саввина Дарья. Задача 10.5

---
+++ 10-java-functional-stream/task05/src/com/example/task05/Salary.java
@@ -0,0 +1,27 @@
+package com.example.task05;
+
+public class Salary implements IMessages<Integer> {
+
+    private String from;
+    private String to;
+    private Integer salary;
+
+    public Salary(String from, String to, Integer salary){
+        this.from = from;
+        this.to = to;
+        this.salary = salary;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    @Override
+    public Integer getContent() {
+        return salary;
+    }
+}
