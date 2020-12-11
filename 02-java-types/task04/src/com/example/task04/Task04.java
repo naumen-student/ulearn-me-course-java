5ed87bc7707e4745590bbe596e552044ed8031cf
Snilcher <m.mo.80@mail.ru>
2020-12-11 18:22:24 +0500

    Павленко Сергей 2.4

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -1,21 +1,24 @@
 package com.example.task04;
-
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        if (operation.equals("+"))
+            return a + b;
+        if (operation.equals("-"))
+            return a - b;
+        if (operation.equals("*"))
+            return a * b;
+        if (operation.equals("/"))
+            return (float) a / b;
         return 0;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
+
     }
 
 }
