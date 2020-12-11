4e68f6983096661bae5179a85bc3bbf0a3835867
Жора <JoraKochkin@yandex.ru>
2020-12-11 14:27:37 +0500

    Жданов Сергей Задание 1-1

--- 01-java-intro/task01/src/Helloworld.java
+++ 01-java-intro/task01/src/Helloworld.java
@@ -1,5 +1,5 @@
-public class Helloworld {
-    public static void main(String[] args) {
+public class HelloWorld {
+    public static void main (String[] args) {
         System.out.println("Hello, World!");
     }
 }
