b394ee0680ef91a7c8337f757d47022e05bceb86
Snilcher <m.mo.80@mail.ru>
2020-12-11 18:26:25 +0500

    Павленко Сергей 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,19 +3,20 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while (x != 0)
+        {
+            if (x%2 == 1)
+                return "FALSE";
+            x = x/10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
+
+        String result = solution(6801);
         System.out.println(result);
-        */
+
     }
 
 }
