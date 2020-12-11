e10ff9ea55ff6f1fa5a1d5cdce5215b77c769308
Weak <skandal_dota@mail.ru>
2020-12-11 20:23:59 +0500

    Task 2.02

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -1,12 +1,18 @@
 package com.example.task02;
-
 public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long num = Long.parseLong(input);
+        if (Byte.MIN_VALUE <= num && num <= Byte.MAX_VALUE)
+            return "byte";
+        if (Short.MIN_VALUE <= num && num <= Short.MAX_VALUE)
+            return "short";
+        if (Integer.MIN_VALUE <= num && num <= Integer.MAX_VALUE)
+            return "int";
+        if (Long.MIN_VALUE <= num && num <= Long.MAX_VALUE)
+            return "long";
+        else return "Not a number";
     }
 
     public static void main(String[] args) {
@@ -16,6 +22,15 @@ public class Task02 {
         String result = solution("12345");
         System.out.println(result);
          */
+        String result2 = solution("100");
+        System.out.println(result2);
+        String result3 = solution("8");
+        System.out.println(result3);
+        String result4 = solution("1000000000000");
+        System.out.println(result4);
+        String result5 = solution("1000");
+        System.out.println(result5);
+
     }
 
 }
