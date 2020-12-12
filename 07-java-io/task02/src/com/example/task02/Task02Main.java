0ae0874594ee00b49431f377c8a14a74bbc8e812
s17v3rr <oleg.gubanov@urfu.me>
2020-12-12 23:21:02 +0500

    Губанов Олег. Задание 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int current = System.in.read();
+        while (current != -1) {
+            int next = System.in.read();
+            if (!(current == 13 && next == 10))
+                System.out.write(current);
+            current = next;
+            System.out.flush();
+        }
     }
 }
