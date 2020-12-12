451af561d743676a3c8c83ee1af3aa8472c3f372
Snilcher <m.mo.80@mail.ru>
2020-12-12 18:56:02 +0500

    Павленко Сергей 8.1

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,31 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
 
-        /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(processBuilder.start().getInputStream()))){
+            String res;
+            while ((res = reader.readLine()) != null){
+                if(res.contains("title")){
+                    return res.split("\"")[1];
+                }
+            }
+        } catch (IOException e){
+            return null;
+        }
+        return null;
     }
 }
