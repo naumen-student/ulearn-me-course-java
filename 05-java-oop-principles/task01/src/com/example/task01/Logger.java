c0f4025865cf7bb7732624679d41f13dc639d0fb
Сухраб <Sukhrab2001@mail.ru>
2020-12-12 23:56:22 +0500

    Курбанов Сухраб, задача 5 01

--- 05-java-oop-principles/task01/src/com/example/task01/Logger.java
+++ 05-java-oop-principles/task01/src/com/example/task01/Logger.java
@@ -1,24 +1,25 @@
 package com.example.task01;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.HashMap;
 import java.util.Date;
+import java.util.HashMap;
+
+enum Level {
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR
+}
 
 public class Logger {
-    enum Level {
-        DEBUG,
-        INFO,
-        WARNING,
-        ERROR
-    }
-
-    private String name;
+    private final String name;
     private Level level;
-    private static final HashMap<String, Logger> loggerHashMap = new HashMap<>();
+    private static final HashMap<String, Logger> loggers = new HashMap<>();
 
-    public Logger (String name) {
+    public Logger(String name) {
         this.name = name;
-        loggerHashMap.put(name, this);
+        loggers.put(name, this);
     }
 
     public String getName() {
@@ -26,57 +27,58 @@ public class Logger {
     }
 
     public static Logger getLogger(String name) {
-        if (loggerHashMap.containsKey(name)) return loggerHashMap.get(name);
+        if (loggers.containsKey(name))
+            return loggers.get(name);
         return new Logger(name);
-    }
-
-    private String log(Level level, String message) {
-        Date date = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss");
-        return String.format("[%d], %d, %d", level, dateFormat.format(new Date()), name, message);
-    }
-
-    private void log(Level level, String message, Object ... args) {
-        log(level, String.format(message, args));
-    }
-
-    public Level getLevel() {
-        return level;
     }
 
     public void setLevel(Level level) {
         this.level = level;
     }
 
+    public Level getLevel() {
+        return level;
+    }
+
+    public void log(Level level, String message) {
+        Date date = new Date();
+        DateFormat dformat = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss");
+        System.out.printf("[%s] %s %s - %s%n%n", level, dformat.format(date), name, message);
+    }
+
+    public void log(Level level, String template, Object... messageArgs) {
+        log(level, String.format(template, messageArgs));
+    }
+
     public void debug(String message) {
         log(Level.DEBUG, message);
     }
 
-    public void debug(String messageTemplate, Object ... args) {
-        log(Level.DEBUG, messageTemplate, args);
+    public void debug(String template, Object... messageArgs) {
+        log(Level.DEBUG, template, messageArgs);
     }
 
     public void info(String message) {
         log(Level.INFO, message);
     }
 
-    public void info(String messageTemplate, Object ... args) {
-        log(Level.INFO, messageTemplate, args);
+    public void info(String template, Object... messageArgs) {
+        log(Level.INFO, template, messageArgs);
     }
 
     public void warning(String message) {
         log(Level.WARNING, message);
     }
 
-    public void warning(String messageTemplate, Object ... args) {
-        log(Level.WARNING, messageTemplate, args);
+    public void warning(String template, Object... messageArgs) {
+        log(Level.WARNING, template, messageArgs);
     }
 
     public void error(String message) {
         log(Level.ERROR, message);
     }
 
-    public void error(String messageTemplate, Object ... args) {
-        log(Level.ERROR, messageTemplate, args);
+    public void error(String template, Object... messageArgs) {
+        log(Level.ERROR, template, messageArgs);
     }
 }
