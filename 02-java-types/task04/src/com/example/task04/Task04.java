ccaa3f38ecc7f71423785fd1ce4939f165173124
Weak <skandal_dota@mail.ru>
2020-12-12 11:03:22 +0500

    Task 2.04

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -1,12 +1,21 @@
 package com.example.task04;
-
 public class Task04 {
-
     public static float calculate(int a, int b, String operation) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
         return 0;
+        float first = a, second = b;
+        if (operation == "+")
+            return first+second;
+        else if (operation == "-")
+            return first-second;
+        else if (operation == "/")
+            return first/second;
+        else if (operation == "*")
+            return first*second;
+        else
+            return 0;
     }
 
     public static void main(String[] args) {
@@ -14,6 +23,8 @@ public class Task04 {
         // например вот так:
         /*
         float result = calculate(-25, 5, "/");
+
+        float result = calculate(13, 4, "/");
         System.out.println(result);
         */
     }
