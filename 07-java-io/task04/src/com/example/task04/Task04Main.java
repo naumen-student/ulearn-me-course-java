b759fd4f17a2461a09e7bdc8d65d9e4d975633be
Snilcher <m.mo.80@mail.ru>
2020-12-12 18:50:16 +0500

    Павленко Сергей 7.4

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,19 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+    public static void main(String[] args) {
+        double sumRes = 0.0;
+        Scanner scanner = new Scanner(System.in).useDelimiter("[\\s\\n]+").useLocale(Locale.ENGLISH);
 
-        System.out.println("0.0");
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble()) {
+                sumRes += scanner.nextDouble();
+            } else scanner.next();
+        }
+        System.out.printf(Locale.ENGLISH, "%8.6f", sumRes);
     }
 }
