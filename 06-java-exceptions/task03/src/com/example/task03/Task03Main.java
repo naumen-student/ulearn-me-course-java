12c544f3aa70fb841190bb2b4f333735ead39a68
Snilcher <m.mo.80@mail.ru>
2020-12-12 18:04:15 +0500

    Павленко Сергей 6.3

--- 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
+++ 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
@@ -1,16 +1,18 @@
 package com.example.task03;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
 public class Task03Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+    public static void main(String[] args) throws IOException {
+
         throwCheckedException();
-         */
+
     }
 
-    //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
+    public static void throwCheckedException() throws IOException {
+        throw new IOException();
 
     }
 }
