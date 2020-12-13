35af327083db820363b273e0fd853c19d7109ebf
Dinar <fridget23@gmail.com>
2020-12-14 01:51:04 +0500

    Бикмухаметов Динар. Задача 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -7,6 +7,11 @@ public class Task02 {
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
         return "";
+        final long number = Long.parseLong(input);
+        if (Byte.MIN_VALUE <= number && number <= Byte.MAX_VALUE) return "byte";
+        if (Short.MIN_VALUE <= number && number <= Short.MAX_VALUE) return "short";
+        if (Integer.MIN_VALUE <= number && number <= Integer.MAX_VALUE) return "int";
+        return "long";
     }
 
     public static void main(String[] args) {
@@ -17,5 +22,4 @@ public class Task02 {
         System.out.println(result);
          */
     }
-
 }
