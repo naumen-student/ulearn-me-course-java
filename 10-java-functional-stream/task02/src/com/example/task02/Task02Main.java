a80339f32acffc9170ef58e25ff4f4d04624b706
fateful1 <>
2020-12-13 19:46:49 +0500

    Чиненов Александр. Задание 10.2

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -5,19 +5,16 @@ import java.util.stream.IntStream;
 public class Task02Main {
 
     public static void main(String[] args) {
-
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
-
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
-
+        if (n < 1 || n > 16) {
+            throw new IllegalArgumentException();
+        }
+        int limit = (int) Math.pow(2, n);
+        return IntStream.iterate(0, x -> x + 1).map(x -> x % limit).map(y -> y ^ (y >> 1));
     }
-
 }
