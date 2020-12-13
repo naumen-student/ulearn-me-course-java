4f4dfb1af3f6706dba90419e36b1b48ec6be8a8b
Weak <skandal_dota@mail.ru>
2020-12-13 12:53:09 +0500

    Task 2.10

--- 02-java-types/task10/src/com/example/task10/Task10.java
+++ 02-java-types/task10/src/com/example/task10/Task10.java
@@ -7,8 +7,10 @@ public class Task10 {
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
+
         return a == b;
+        return Float.isInfinite(a) || Float.isInfinite(b) ? a == b : Float.isNaN(a) && Float.isNaN(b) ||
+                Math.abs(a - b) < Math.pow(10, -precision);
 
     }
 
@@ -20,7 +22,6 @@ public class Task10 {
 
         boolean result = compare(sum, c, 2);
         System.out.println(result);
-
     }
 
 }
