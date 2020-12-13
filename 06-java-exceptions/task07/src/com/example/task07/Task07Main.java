5ef557e1326f64c225cd6caa3a543d6fac3fdcaf
nvit29 <gainetdinoa-nastya@ya.ru>
2020-12-13 18:22:52 +0500

    Гайнетдинова Анастасия. Задание 6.7

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -1,5 +1,7 @@
 package com.example.task07;
 
+import java.io.IOException;
+
 public class Task07Main {
 
     public static final String CHECKED = "checked";
@@ -16,9 +18,14 @@ public class Task07Main {
         try {
             processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
         } catch (Exception e) {
-
+            if (e instanceof IOException) {
+                return CHECKED;
+            }
+            else {
+                return UNCHECKED;
+            }
         }
-        return null;
+        return NONE;
     }
 
 }
