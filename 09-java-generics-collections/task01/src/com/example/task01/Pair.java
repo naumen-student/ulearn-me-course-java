f24fcb2781e39e4ff83a32db47dd10778722d842
evanesenze <evanesenze@gamil.com>
2020-12-13 22:20:42 +0500

    Базуев Иван.09.Task01

--- 09-java-generics-collections/task01/src/com/example/task01/Pair.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Pair.java
@@ -1,5 +1,28 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<TFirst,TSecond> {
+    private final TFirst first;
+    private final TSecond second;
+
+    private Pair(TFirst v1, TSecond v2){
+        this.first = v1;
+        this.second = v2;
+    }
+
+    public TFirst getFirst() { return first; }
+
+    public TSecond getSecond() { return second; }
+
+    public int hashCode() { return Objects.hash(first,second); }
+
+    public boolean equals(Pair<TFirst, TSecond> obj) { return this.hashCode() == obj.hashCode(); }
+
+    public static <TFirst,TSecond>Pair<TFirst,TSecond> of(TFirst v1, TSecond v2){ return new Pair<>(v1, v2); }
+
+    public void ifPresent(BiConsumer <? super TFirst, ? super TSecond> action) {
+        if(first != null && second != null) action.accept(first, second);
+    }
 }
