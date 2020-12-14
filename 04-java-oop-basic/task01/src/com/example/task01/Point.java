c3ba52a13816ffe083d2bc3975301938aaa472c3
Sergey Miropolsky <Miropolskysk@yandex.ru>
2020-12-14 23:50:33 +0500

    Миропольский Сергей. Задание 4.1

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -7,6 +7,28 @@ public class Point {
     int x;
     int y;
 
+    Point() {
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int a = x;
+        x = -y;
+        y = -a;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow(x-point.x, 2) + Math.pow(y-point.y, 2));
+    }
+
+    public String toString() {
+        return String.format("{0}, {1}", x,y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
