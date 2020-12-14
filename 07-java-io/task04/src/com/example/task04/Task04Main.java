c556ddb38a7d19bff62f3f508910d34525c035f0
nvit29 <gainetdinoa-nastya@ya.ru>
2020-12-14 18:06:04 +0500

    Гайнетдинова Анастасия. Задание 7.4

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.Locale;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +10,16 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        double sum = 0;
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble()) {
+                sum += scanner.nextDouble();
+            }
+            else {
+                scanner.next();
+            }
+        }
+        System.out.format(Locale.CANADA, " %1.6f", sum);
     }
 }
