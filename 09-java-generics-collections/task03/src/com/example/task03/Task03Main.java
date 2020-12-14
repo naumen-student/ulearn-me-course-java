5101e7eaeb044bbe63a89541fc58aff6ca4494eb
evanesenze <evanesenze@gamil.com>
2020-12-15 01:15:25 +0500

    Базуев Иван.09.Task03

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -3,22 +3,35 @@ package com.example.task03;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { }
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
-        for (Set<String> anagram : anagrams) {
-            System.out.println(anagram);
+    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
+        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset));
+        Map<String, TreeSet<String>> words = new TreeMap<>();
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine().toLowerCase();
+            if (str.length() <= 2 || !str.matches("[а-я]+")) continue;
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String key = new String(charArray);
+            if (words.containsKey(key)) words.get(key).add(str);
+            else words.put(key, new TreeSet<String>() {{ add(str); }});
         }
+        return getSortedList(words);
+
 
     }
 
-    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+    private static List<Set<String>> getSortedList(Map<String, TreeSet<String>> words) {
+        List<Set<String>> res = new ArrayList<>();
+        for (Set set: words.values())
+            if(set.size() >= 2) res.add(set);
+        return res;
     }
 }
