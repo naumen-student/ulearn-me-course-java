4e75ff48efde9fbd93aec9afb7676e7f47795c3f
Dinar <fridget23@gmail.com>
2020-12-15 17:42:08 +0500

    Бикмухаметов Динар. Задание 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -3,9 +3,21 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
+        int lengthOfX = String.valueOf(x).length();
+        int k = 0;
+        for (int i = 1; i <= lengthOfX; i++)
+        {
+            int y = x % 10;
+            if (y % 2 == 0)
+            {
+                k++;
+            }
+            x /= 10;
+        }
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
+        if (k == lengthOfX) return "TRUE";
         return "FALSE";
     }
 
@@ -14,8 +26,11 @@ public class Task05 {
         // например вот так:
         /*
         String result = solution(1234);
+
+        String result = solution(212212);
         System.out.println(result);
         */
+
     }
 
 }
