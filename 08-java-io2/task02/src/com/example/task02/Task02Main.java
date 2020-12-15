182c44cdd0b4288dfda20dcc513bd9c87b7dcf63
Sergey Miropolsky <Miropolskysk@yandex.ru>
2020-12-15 17:06:26 +0500

    Миропольский Сергей. Задание 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,33 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+        ArrayList<Path> list = new ArrayList<>();
+        if (!(rootDir.toFile().exists()))
+            return list;
+        else {
+            if (!(rootDir.toFile().isDirectory())) {
+                return null;
+            }
+            else
+                for (File file : rootDir.toFile().listFiles()) {
+                    if (file.isDirectory())
+                        list.addAll(listFiles(file.toPath()));
+                    else
+                        list.add(file.toPath());
+                }
+        }
+        return list;
     }
 }
