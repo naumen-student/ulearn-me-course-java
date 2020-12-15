61b88ab15869052e7e68f896c407ade795a20b58
69EHOTUK96 <raccoon.twitch@gmail.com>
2020-12-15 13:00:48 -0500

    task 04 solved

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,17 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        double res = 0;
+        while (scanner.hasNextDouble()) {
+            res += scanner.nextDouble();
+        }
+        scanner.close();
+        System.out.printf(Locale.ENGLISH, "%.6f", res);
     }
 }
