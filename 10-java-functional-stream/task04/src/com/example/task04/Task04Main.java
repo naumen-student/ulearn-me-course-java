008fb9b181277822133a42342d90c221dc97a54d
Frezyx <youngfrezyx@gmail.com>
2020-12-16 18:16:02 +0500

    Ильин Станислав. Задание 10.4

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,46 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        List<Map.Entry<String, Long>> listToSort = new ArrayList<> ( );
+        for (Map.Entry<String, Long> stringLongEntry : getStringStream ( reader )
+                .collect ( Collectors.groupingBy ( t -> t , Collectors.counting ( ) ) )
+                .entrySet ( )) {
+            listToSort.add ( stringLongEntry );
+        }
+        listToSort.sort ( Map.Entry.comparingByKey ( ) );
+        List<Map.Entry<String, Long>> toSort = new ArrayList<> ( );
+        for (Map.Entry<String, Long> stringLongEntry : listToSort) {
+            toSort.add ( stringLongEntry );
+        }
+        toSort.sort ( Map.Entry.comparingByValue ( Comparator.reverseOrder ( ) ) );
+        long limit = 10;
+        for (Map.Entry<String, Long> stringLongEntry : toSort) {
+            if ( limit-- == 0 ) break;
+            String key = stringLongEntry.getKey ( );
+            System.out.print ( key + '\n' );
+        }
+
+    }
+
+    private static Stream<String> getStringStream(BufferedReader reader) {
+        return reader.lines ( )
+                .flatMap ( x -> Stream.of ( x.split ( "[\\P{L}]" ) )
+                        .filter ( t -> !t.isEmpty ( ) )
+                        .map ( String::toLowerCase ) );
     }
 
 }
