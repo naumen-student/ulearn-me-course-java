39e022e0c87ae7a24dbda2ad5ed3a666a7810201
k1n5-bt <k1n5lobal@gmail.com>
2020-12-16 10:49:32 +0500

    Андреев Иван. Задача 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,18 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Task02Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
+    public static void main(String[] args) throws IOException {
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+    public static List<Path> listFiles(Path rootDir) throws IOException {
+        return Files.walk(rootDir).filter(Files::isRegularFile).collect(Collectors.toList());
     }
 }
