58634fa2bdaf9c3ca81768f82b3ae214d056916b
Арман <Arman.Koshaev@urfu.me>
2020-12-17 17:21:28 +0500

    Кошаев Арман. Задание 10

--- 02-java-types/task10/src/com/example/task10/Task10.java
+++ 02-java-types/task10/src/com/example/task10/Task10.java
@@ -7,18 +7,17 @@ public class Task10 {
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        if (a == (int)a && b==(int)b)
-        {
-            return a == b;
-        }
         if (Float.isNaN(a) && Float.isNaN(b))
             return true;
-        if ((Float.isNaN(a) && b == 0.0f) || (Float.isNaN(b) && a == 0.0f))
+        if ((Float.isNaN(a) && !Float.isNaN(b)) || (Float.isNaN(b) && !Float.isNaN(a)))
             return false;
-        float k = (float) Math.pow(10, precision);
-        float x = (Math.round(a * k / 10) / k);
-        float y = (Math.round(b * k / 10) / k);
-        return x == y;
+        float round = 10;
+        if(precision != 0)
+            round = (float) Math.pow(10, precision);
+        float x = (Math.round(a * round / 10) );
+        float y = (Math.round(b * round / 10) );
+        int comp = Float.compare(x, y);
+        return comp == 0;
 
     }
 
