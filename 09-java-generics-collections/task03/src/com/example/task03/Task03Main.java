ed4376c937de827c69dcd67e4bc57e977b53deab
k1n5-bt <k1n5lobal@gmail.com>
2020-12-17 07:58:44 +0500

    Андреев Иван. Задача 9.3

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -4,21 +4,35 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
-
         List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
-
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
-    }
+        TreeMap<String, TreeSet<String>> anagrams = new TreeMap<>();
+        Scanner scanner = new Scanner(inputStream, charset.name());
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine().toLowerCase();
+                if (line.matches("[а-я]{3,}")) {
+                    char[] lineSymbols = line.toCharArray();
+                    Arrays.sort(lineSymbols);
+                    String sorted = new String(lineSymbols);
+                    if (!anagrams.containsKey(sorted)) {
+                        anagrams.put(sorted, new TreeSet<>());
+                    }
+                    anagrams.get(sorted).add(line);
+                }
+            }
+            return anagrams.values().stream()
+                    .filter(value -> value.size() > 1)
+                    .collect(Collectors.toList());
+        }
 }
