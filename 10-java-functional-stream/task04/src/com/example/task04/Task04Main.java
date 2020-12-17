1aef3e7d6f3d8a5b102947440d5d151be0f17739
ssss123-byte <55254118+ssss123-byte@users.noreply.github.com>
2020-12-17 14:55:08 +0300

    Шардаков Егор. Задание 10.4

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,10 +1,35 @@
 package com.example.task04;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
+        Map<String, Integer> result = new HashMap<>();
+        new Scanner(System.in).useDelimiter(Pattern.compile("[.,:;'!?_ \n\\-\"]"))
+                .tokens()
+                .map(String::toLowerCase)
+                .filter(x -> !x.equals(""))
+                .forEach(x -> {
+                    if (result.containsKey(x)) result.put(x, result.get(x) + 1);
+                    else result.put(x, 1);
+                });
+        result
+                .entrySet()
+                .stream()
+                .sorted((x, y) ->
+                        (x.getValue().equals(y.getValue())
+                                ?
+                                CharSequence.compare(x.getKey(), y.getKey())
+                                :
+                                Integer.compare(y.getValue(), x.getValue())))
+                .limit(10)
+                .forEachOrdered(x -> System.out.print(x.getKey() + "\n"));
+    }
 
     }
 
