5b3e1d98d0c78cbb8497b4f053bb952c3b3144e3
theonethei <57193492+theonethei@users.noreply.github.com>
2020-12-19 14:29:56 +0400

    Update Task04Main.java

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,10 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Point p1 = new Point (0, 0);
+        Point p2 = new Point (1, 1);
+        int px = p1.getX ();
+        Line line = new Line (p1, p2);
+        Boolean b = line.isCollinearLine (new Point (0, 1));
     }
 }
