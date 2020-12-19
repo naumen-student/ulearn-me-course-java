e0a35d269f3bbff009e784c696641c92f881ce38
theonethei <57193492+theonethei@users.noreply.github.com>
2020-12-19 14:26:24 +0400

    Update Task03Main.java

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber number1 = new ComplexNumber(2.0, 8.6);
+        ComplexNumber number2 = new ComplexNumber(7.1, 4.3);
+        ComplexNumber sum = number1.findSum(number2);
+        ComplexNumber multiply = number1.findMultiply(number2);
     }
 }
