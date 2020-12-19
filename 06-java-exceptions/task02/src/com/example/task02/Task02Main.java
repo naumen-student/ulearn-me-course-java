0e37b3b406bf303046598c356ac6811743248e84
JackkDaniels <56221190+JackkDaniels@users.noreply.github.com>
2020-12-19 19:12:27 +0500

    Update Task02Main.java

--- 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
+++ 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
@@ -20,6 +20,6 @@ public class Task02Main {
         if(monthNumber >= 3 && monthNumber <= 5 ) return "весна";
         if(monthNumber >= 6 && monthNumber <= 8) return  "лето";
         if(monthNumber >= 9 && monthNumber <= 11) return "осень";
-        return "зима";
+        return "Зима";
     }
 }
