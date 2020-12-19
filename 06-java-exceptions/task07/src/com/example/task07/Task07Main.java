004981a6e9e0c3413f360a3c5f42399e09ed5ca7
theonethei <57193492+theonethei@users.noreply.github.com>
2020-12-19 14:07:50 +0400

    Update Task07Main.java

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -15,10 +15,13 @@ public class Task07Main {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
             processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+        } catch (RuntimeException e) {
+            return UNCHECKED;
         } catch (Exception e) {
 
+            return CHECKED;
         }
-        return null;
+        return NONE;
     }
 
 }
