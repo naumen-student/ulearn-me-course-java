abe9cfa45c5d2c0c38d8bdc5c65f12840258ea0e
Rogozhnikov Dmitriy <flames@pop-os.localdomain>
2020-12-19 17:31:19 +0500

    Рогожников Дмитрий. Задание 01

--- 09-java-generics-collections/task01/src/com/example/task01/Pair.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Pair.java
@@ -1,5 +1,45 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<T1, T2> {
+    private final T1 value1;
+    private final T2 value2;
+
+    private Pair(T1 value1, T2 value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    public static <T1, T2> Pair<T1, T2> of(T1 value1, T2 value2) {
+        return new Pair<>(value1, value2);
+    }
+
+    public T1 getFirst() {
+        return value1;
+    }
+
+    public T2 getSecond() {
+        return value2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(value1, pair.value1) &&
+                Objects.equals(value2, pair.value2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value1, value2);
+    }
+
+    public void ifPresent(BiConsumer<? super T1, ? super T2> consumer) {
+        if (this.value1 != null && this.value2 != null)
+            consumer.accept(this.value1, this.value2);
+    }
 }
