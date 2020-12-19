dca4d2879be1f499aa128a4144af0b1db186d7a9
Dima <d.d.demin@urfu.me>
2020-12-19 19:05:50 +0500

    10_task02

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -16,8 +16,13 @@ public class Task02Main {
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
-
+        if (n < 1 || n > 16)
+            throw new IllegalArgumentException("N must belong to range (1, 16)");
+        int size = (int) Math.pow(2, n);
+        return IntStream
+                .iterate(0, val -> val + 1)
+                .map(val -> val % size)
+                .map(val -> val ^ (val >> 1));
     }
 
 }
