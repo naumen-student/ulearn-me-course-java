460d80e5dc35bef0feb35715f77458dcbbb4e731
yegoreal <pk37fr1z>
2020-12-20 20:56:06 +0500

    Якимов Егор. Задание 4.05

--- 04-java-oop-basic/task05/src/com/example/task05/PolygonalLine.java
+++ 04-java-oop-basic/task05/src/com/example/task05/PolygonalLine.java
@@ -1,9 +1,13 @@
 package com.example.task05;
 
+import java.util.ArrayList;
+
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
+
+    private final ArrayList<Point> points = new ArrayList<>();
 
     /**
      * Устанавливает точки ломаной линии
@@ -12,35 +16,40 @@ public class PolygonalLine {
      */
     public void setPoints(Point[] points) {
         // TODO: реализовать
+        for (Point p : points)
+            this.points.add(new Point(p.getX(), p.getY()));
     }
 
     /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param point точка, которую нужно добавить к ломаной
+     @@ -20,7 +25,7 @@ public void setPoints(Point[] points) {
+      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
         // TODO: реализовать
+        points.add(new Point(point.getX(), point.getY()));
     }
 
     /**
-     * Добавляет точку к ломаной линии
-     *
-     * @param x координата по оси абсцисс
-     * @param y координата по оси ординат
+     @@ -30,7 +35,7 @@ public void addPoint(Point point) {
+      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
         // TODO: реализовать
+        addPoint(new Point(x, y));
     }
 
     /**
-     * Возвращает длину ломаной линии
-     *
-     * @return длину ломаной линии
+     @@ -39,8 +44,11 @@ public void addPoint(double x, double y) {
+      * @return длину ломаной линии
      */
     public double getLength() {
         // TODO: реализовать
         throw new AssertionError();
+        double sum = 0;
+        for (int i = 1; i < points.size(); i++) {
+            sum += points.get(i - 1).getLength(points.get(i));
+        }
+        return sum;
     }
 
 }
