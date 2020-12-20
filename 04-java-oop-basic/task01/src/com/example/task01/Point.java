fd4c7ab58710c7a11804fd3544d8d323f48966e8
Leo4343 <chernihov.l@ya.ru>
2020-12-20 16:09:50 +0500

    4.1 Чернихов Леонид

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -7,8 +7,30 @@ public class Point {
     int x;
     int y;
 
+    public  Point(){}
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip(){
+        int z = x;
+        x = -y;
+        y = -z;
+    }
+
+    double distance(Point point){
+        return Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2));
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
     }
 }
