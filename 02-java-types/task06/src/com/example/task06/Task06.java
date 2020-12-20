8fa93b4d384b38626be5c179a2dca4cfaa7ff4dc
Leo4343 <chernihov.l@ya.ru>
2020-12-20 15:43:25 +0500

    2.6 Чернихов Леонид

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -4,18 +4,22 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int z = x + y;
+        z = Math.abs(z);
+        int count = 0;
 
-        return 0;
+        while (z > 0){
+            z = z / 10;
+            count++;
+        }
+
+        if (count == 0)
+            return 1;
+        return count;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
+        int result = solution(777, -777);
         System.out.println(result);
-        */
     }
-
 }
