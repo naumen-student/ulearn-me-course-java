1b7f32092f4abe8b5692074335f0f2eba6de64ff
suppor14 <suppor14@yandex.ru>
2020-12-21 21:48:33 +0500

    Лицин Владислав. Задача 10.2

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -16,7 +16,15 @@ public class Task02Main {
 
     public static IntStream cycleGrayCode(int n) {
 
+        if (n <= 0 || n >= 17)
+            throw new IllegalArgumentException();
+
         return null; // your implementation here
+
+        int bound = (int) Math.pow(2, n);
+        return IntStream.iterate(0, x -> x + 1)
+                .map(x -> x % bound)
+                .map(x -> x ^ (x >> 1));
 
     }
 
