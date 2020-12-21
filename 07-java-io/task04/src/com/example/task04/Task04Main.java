c532210a6e4d4b4e74897daee31dea5538bc7f65
yegoreal <pk37fr1z>
2020-12-21 21:15:50 +0500

    Якимов Егор. Задание 7.4

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -10,5 +12,13 @@ public class Task04Main {
         // - проверить, что получилось 351.731900
 
         System.out.println("0.0");
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+            double res = 0;
+            while (scanner.hasNextDouble()) {
+                res += scanner.nextDouble();
+            }
+            scanner.close();
+            System.out.printf(Locale.ENGLISH, "%.6f", res);
+        }
     }
-}
