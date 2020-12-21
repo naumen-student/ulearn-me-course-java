5b813bc761bc22de4ec5e8f2de2ed442419ad143
Weak <skandal_dota@mail.ru>
2020-12-21 19:40:15 +0500

    Task 5.3

--- 05-java-oop-principles/task03/src/com/example/task03/Seconds.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Seconds.java
@@ -6,7 +6,6 @@ package com.example.task03;
 public class Seconds implements TimeUnit {
 
     private final long amount;
-
     public Seconds(long amount) {
         this.amount = amount;
     }
@@ -24,5 +23,11 @@ public class Seconds implements TimeUnit {
     @Override
     public long toMinutes() {
         return Math.round(amount / 60);
+        return Math.round((double) amount / 60);
+    }
+
+    @Override
+    public long toHours() {
+        return Math.round((double) amount / (60 * 60));
     }
 }
