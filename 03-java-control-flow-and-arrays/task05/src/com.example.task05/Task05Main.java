aa73ad4af9b21639185392f9e39b6c6ca6e5997f
Dinar <fridget23@gmail.com>
2020-12-21 21:57:42 +0500

    Бикмухаметов Динар. Задание 3.05

--- 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
+++ 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
@@ -1,7 +1,7 @@
-package com.example.task05;
-
-public class Task05Main {
-    public static void main(String[] args) {
+public class Task05Main
+{
+    public static void main(String[] args)
+    {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
         /*
@@ -9,8 +9,15 @@ public class Task05Main {
          */
     }
 
-    static int getMin(int a, int b, int c) {
+    static int getMin(int a, int b, int c)
+    {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         return 0;
+        if (a<b && a<c)
+            return a;
+        if (b<a & b <c)
+            return  b;
+        else
+            return c;
     }
 }
