aa93943bb39d89507cd27668faf058caf34b85a6
k1n5-bt <64440450+k1n5-bt@users.noreply.github.com>
2020-12-22 03:27:26 +0500

    Update Point.java

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -1,38 +1,34 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
 public class Point {
     int x;
     int y;
 
-    public Point(){ }
+    public Point() {
 
-    public Point(int x, int y){
+    }
+
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public void flip(){
-        x = x + y;
-        y = x - y;
-        x = -(x - y);
-        y *= -1;
-    }
-
-    double distance(Point point){
-        int dX = point.x - x;
-        int dY = point.y - y;
-        return Math.sqrt(dX * dX + dY * dY);
-    }
-
-    public String toString() {
-        return String.format("(%d %d)", x, y);
-    }
-
     void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+        System.out.println(toString());
+    }
+
+    void flip() {
+        int temporary = x;
+        x = -y;
+        y = -temporary;
+    }
+
+    double distance(Point other) {
+        return Math.sqrt(Math.pow(other.x - x, 2) + Math.pow(other.y - y, 2));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
     }
 }
