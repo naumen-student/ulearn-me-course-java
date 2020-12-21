eaa861dd0d1bfe46ced6b62e8f4a6ea79484df40
Dinar <fridget23@gmail.com>
2020-12-21 21:19:55 +0500

    Бикмухаметов Динар. Задание 3.02

--- 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
+++ 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
@@ -5,11 +5,22 @@ public class Task02Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
         /*
+
         System.out.println(getSeason(12));
          */
+
     }
 
     static String getSeason(int monthNumber) {
         return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber >= 3 && monthNumber <= 5)
+            return "весна";
+        else if(monthNumber >= 6 && monthNumber <= 8)
+            return "лето";
+        else if(monthNumber >= 9 && monthNumber <= 11)
+            return "осень";
+        else
+            return "зима";
+
     }
 }
