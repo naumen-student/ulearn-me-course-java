911baa923da30ba918311e8d186190e229ca3205
nikovOne <58881824+nikovOne@users.noreply.github.com>
2020-12-21 14:43:29 +0500

    Update Task02.java

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -1,21 +1,24 @@
 package com.example.task02;
 
+
+
 public class Task02 {
 
     public static String solution(String input) {
+        long value = Long.parseLong(input);
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if (value <= Byte.MAX_VALUE && value >= Byte.MIN_VALUE)  {
+            return "byte";
+        }
 
-        return "";
+        if (value <= Short.MAX_VALUE && value >= Short.MIN_VALUE) {
+            return "short";
+        }
+
+        if (value <= Integer.MAX_VALUE && value >= Integer.MIN_VALUE) {
+            return "int";
+        }
+
+        return "long";
     }
-
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
-    }
-
 }
