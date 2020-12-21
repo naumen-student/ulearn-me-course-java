2dbf363153067d9a342a2ee0e1e515b35ba84037
nikovOne <58881824+nikovOne@users.noreply.github.com>
2020-12-21 15:02:25 +0500

    Update Task05.java

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -4,18 +4,14 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        while (x > 0) {
+            if ((x % 10) % 2 != 0) {
+                return "FALSE";
+            }
 
-        return "FALSE";
+            x /= 10;
+        }
+
+        return "TRUE";
     }
-
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
-        System.out.println(result);
-        */
-    }
-
 }
