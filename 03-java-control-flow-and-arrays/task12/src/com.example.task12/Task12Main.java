3681df48d8d10626760fdcf7d9ff03b59fec8845
Dinar <fridget23@gmail.com>
2020-12-21 22:18:17 +0500

    Бикмухаметов Динар. Задание 3.12

--- 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
+++ 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
@@ -1,5 +1,3 @@
-package com.example.task12;
-
 public class Task12Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -13,5 +11,17 @@ public class Task12Main {
 
     static void selectionSort(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (!(arr == null || arr.length == 0)) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                for (int j = arr.length - 1; j > i; j--) {
+                    if (arr[j - 1] > arr[j]) {
+                        int tmp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = tmp;
+                    }
+                }
+            }
+        }
     }
+
 }
