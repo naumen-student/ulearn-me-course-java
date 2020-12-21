92f350031e1399dc5ed27bafb4e9ebd7cfc37be2
Seeryoga <seeryoga@gmail.com>
2020-12-21 15:04:11 +0500

    Савченко Сергей 9.2

--- 09-java-generics-collections/task02/src/com/example/task02/Task02Main.java
+++ 09-java-generics-collections/task02/src/com/example/task02/Task02Main.java
@@ -1,21 +1,62 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Task02Main {
+public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private List<E> list = new ArrayList<>();
+    private final File file;
 
-    public static void main(String[] args) throws IOException {
+    public SavedList(File file) throws IOException {
 
-        File file = new File("./testlist.dat");
-
-        SavedList<String> list1 = new SavedList<>(file);
-        list1.add("Z");
-        System.out.println("list1: " + list1);
-
-        SavedList<String> list2 = new SavedList<>(file);
-        System.out.println("list2: " + list2);
-
+        this.file = file;
+        if (file.exists()) {
+            try (ObjectInputStream data = (new ObjectInputStream(new FileInputStream(file)))) {
+                list = (List<E>) data.readObject();
+            } catch (Exception ignored) {
+            }
+        } else {
+            this.file.createNewFile();
+        }
     }
 
+    @Override
+    public E get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public E set(int index, E element) {
+        list.set(index, element);
+        updateFile();
+        return element;
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public void add(int index, E element) {
+        list.add(index, element);
+        updateFile();
+    }
+
+    @Override
+    public E remove(int index) {
+        E result = list.get(index);
+        list.remove(index);
+        updateFile();
+        return result;
+    }
+
+    private void updateFile() {
+        try (ObjectOutputStream data = (new ObjectOutputStream(new FileOutputStream(file)))) {
+            data.writeObject(list);
+        } catch (Exception ignored) {
+        }
+    }
 }
