c88ff6b12acbcdcc853eb274a20daf0166059cc6
yegoreal <pk37fr1z>
2020-12-21 21:20:09 +0500

    Якимов Егор. Задание 8.3

--- 08-java-io2/task03/src/com/example/task03/SampleData.java
+++ 08-java-io2/task03/src/com/example/task03/SampleData.java
@@ -1,42 +1,40 @@
-package com.example.task03;
+ackage com.example.task03;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
 public class SampleData {
-    static final long serialVersionUID = 132706691457162967L;
+    public class SampleData implements Serializable {
+        static final long serialVersionUID = 132706691457162967L;
 
-    String name;
-    int value;
-    Date date;
-
-    public SampleData(String name, int value, Date date) {
-        this.name = name;
-        this.value = value;
-        this.date = date;
+        String name;
+        int value;
+        Date date;
+        public SampleData(String name, int value, Date date) {
+            this.name = name;
+            this.value = value;
+            this.date = date;
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            SampleData that = (SampleData) o;
+            return value == that.value &&
+                    Objects.equals(name, that.name) &&
+                    Objects.equals(date, that.date);
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, value, date);
+        }
+        @Override
+        public String toString() {
+            return "SampleData{" +
+                    "name='" + name + '\'' +
+                    ", value=" + value +
+                    ", date=" + date +
+                    '}';
+        }
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SampleData that = (SampleData) o;
-        return value == that.value &&
-                Objects.equals(name, that.name) &&
-                Objects.equals(date, that.date);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, value, date);
-    }
-
-    @Override
-    public String toString() {
-        return "SampleData{" +
-                "name='" + name + '\'' +
-                ", value=" + value +
-                ", date=" + date +
-                '}';
-    }
-}
