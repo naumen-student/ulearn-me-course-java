e9083a71c2ce79efbdd3312af2225ba91d682c89
LilStupidPig20 <mistersaxar@gmail.com>
2020-12-22 21:20:28 +0500

    Волгарев Артем. Задание 6.1

--- 06-java-exceptions/task01/src/com/example/task01/Task01Main.java
+++ 06-java-exceptions/task01/src/com/example/task01/Task01Main.java
@@ -4,12 +4,11 @@ public class Task01Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         codeWithNPE();
-         */
     }
 
     static void codeWithNPE() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String line = null;
+        System.out.println(line.toCharArray());
     }
 }
