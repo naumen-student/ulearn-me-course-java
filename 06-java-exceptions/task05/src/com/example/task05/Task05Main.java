516b3d3ac08aa696d04f041845381fcddc25a5b5
nikovOne <58881824+nikovOne@users.noreply.github.com>
2020-12-23 16:01:30 +0500

    Update Task05Main.java

--- 06-java-exceptions/task05/src/com/example/task05/Task05Main.java
+++ 06-java-exceptions/task05/src/com/example/task05/Task05Main.java
@@ -1,22 +1,30 @@
 package com.example.task05;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Task05Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String pathToFile = args[0]; // "/home/user/file.txt"
 
-        String s = readFile(pathToFile);
-        System.out.println(s);
+        try {
+            String s = readFile(pathToFile);
+            System.out.println(s);
+        } catch (FileNotFoundException e) {
+            System.out.printf("файл \"%s\" не найден\n", pathToFile);
+        } catch (IOException e) {
+            System.out.printf("произошла ошибка при чтении файла \"%s\"\n", pathToFile);
+        }
     }
 
     public static String readFile(String pathToFile) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+
         FileReader fileReader = new FileReader(pathToFile);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        StringBuilder stringBuilder = new StringBuilder();
         String currentLine;
         while ((currentLine = bufferedReader.readLine()) != null) {
             stringBuilder.append(currentLine);
