a4687569a6cde3bd6a28f0c9601ae61b55eacf73
nikovOne <58881824+nikovOne@users.noreply.github.com>
2020-12-23 16:37:07 +0500

    Update Task02Main.java

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int first, next = System.in.read();
+        while (next != -1) {
+            first = next;
+            next = System.in.read();
+            if (first == 13 && next == 10) {
+                continue;
+            } else {
+                System.out.write(first);
+            }
+        }
     }
 }
