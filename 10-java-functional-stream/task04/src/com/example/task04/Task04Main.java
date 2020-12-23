b9290129cc794a59058fd0ebb64cd639e6c51de6
nikovOne <58881824+nikovOne@users.noreply.github.com>
2020-12-23 17:09:52 +0500

    Update Task04Main.java

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,10 +1,33 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Integer> frequency = new HashMap<>();
+        in.lines().map(String::toLowerCase)
+                .flatMap(x -> Stream.of(x.split("[,.\\-! ]")))
+                .filter(s -> !s.isEmpty())
+                .forEach(s -> {
+                    if (!frequency.containsKey(s))
+                        frequency.put(s, 1);
+                    else
+                        frequency.put(s, frequency.get(s) + 1);
+                });
+        frequency.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach(x -> System.out.print(x.getKey() + "\n"));
 
     }
 
