2500f5c6bfc70331b4d2a61fb7e73700aed77073
nikovOne <58881824+nikovOne@users.noreply.github.com>
2020-12-23 16:22:18 +0500

    Update Task01Main.java

--- 07-java-io/task01/src/com/example/task01/Task01Main.java
+++ 07-java-io/task01/src/com/example/task01/Task01Main.java
@@ -15,7 +15,17 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) {
+            throw new IllegalArgumentException("Stream is null");
+        }
+
+        int result = 0;
+        int readByte;
+
+        while((readByte = inputStream.read()) != -1) {
+            result = Integer.rotateLeft(result, 1) ^ readByte;
+        }
+
+        return result;
     }
 }
