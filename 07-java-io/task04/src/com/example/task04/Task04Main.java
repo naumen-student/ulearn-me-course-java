468fa7466913fc58322f20467b4b49e04104fd82
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-12-23 23:54:34 +0500

    Update Task04Main.java

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +10,15 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        double res = 0;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.CANADA);
 
-        System.out.println("0.0");
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble())
+                res += scanner.nextDouble();
+            else
+                scanner.next();
+        }
+        System.out.format(Locale.CANADA," %1.6f", res);
     }
 }
