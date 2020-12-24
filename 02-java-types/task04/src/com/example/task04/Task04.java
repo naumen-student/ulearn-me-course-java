284b8499fabb5792f721e193eaec65f5f2c76ed0
ALeksHertz <sashahertz@bk.ru>
2020-12-25 01:47:02 +0500

    Герц Александр. Задача 2.4

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -1,12 +1,19 @@
 package com.example.task04;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
-
+        Map<String, BiFunction<Float, Float, Float>> operations = new HashMap<>();
+        operations.put("+", (x ,y) -> x + y);
+        operations.put("-", (x, y) -> x - y);
+        operations.put("/", (x, y) -> x / y);
+        operations.put("*", (x, y) -> x * y);
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        if (!operations.containsKey(operation))
+            throw new IllegalArgumentException("Operation does not support");
+        return operations.get(operation).apply((float) a, (float) b);
     }
 
     public static void main(String[] args) {
