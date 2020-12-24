1917aae7cc8619971fd85e1f4ca34a9f8581183f
ALeksHertz <sashahertz@bk.ru>
2020-12-25 03:29:23 +0500

    Герц Александр. Задача 3.13

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -1,5 +1,5 @@
 package com.example.task13;
-
+import java.util.Arrays;
 public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -13,7 +13,9 @@ public class Task13Main {
 
     static int[] removeMoreThen1000(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if (arr == null)
+            return null;
+        return Arrays.stream(arr).filter(x -> x <= 1e3).toArray();
     }
 
 }
