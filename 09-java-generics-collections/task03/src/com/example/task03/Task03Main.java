4f73bb1adad2020ff4519a6ff8d2b68adc02ebdc
dnrkvn <diyllina@mail.ru>
2020-12-24 18:59:25 +0500

    Хабибуллина Диана. Задача 9.3

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -1,17 +1,16 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
+        List<Set<String>> anagrams = findAnagrams(new FileInputStream(
+                "C:\\Users\\diyll\\IdeaProjects\\ulearn-me-course-java\\09-java-generics-collections\\task03\\build\\resources\\main"),
+                Charset.forName("windows-1251"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
@@ -19,6 +18,35 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset));
+        Map<String, TreeSet<String>> anagrams = new TreeMap<>();
+
+        while (scanner.hasNext()) {
+            String current = scanner.next().toLowerCase();
+
+            if (current.matches("[а-я]+") && current.length() > 2) {
+                char[] word = current.toCharArray();
+                Arrays.sort(word);
+                String key = new String(word);
+
+                if (!anagrams.containsKey(key))
+                    anagrams.put(key, new TreeSet<String>() {{
+                        add(current); }});
+                else {
+                    anagrams.get(key).add(current);
+                }
+            }
+        }
+        return sortAnagrams(anagrams);
     }
+
+    private static List<Set<String>> sortAnagrams(Map<String, TreeSet<String>> anagrams) {
+        List<Set<String>> result = new ArrayList<>();
+        for (Set set : anagrams.values()) {
+            if (set.size() >= 2)
+                result.add(set);
+        }
+        return result;
+    }
+
 }
