1616f540b82d44ff9f7b5c6348a9fa9b8622168b
vlad2001g <Vladislav.Sadykov@urfu.me>
2020-12-24 13:49:26 +0500

    Садыков Владислав. Задание 4.2

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -1,56 +1,78 @@
 package com.example.task02;
 
-public class Task02Main {
-    private int hour;
-    private int minute;
-    private int second;
+public class TimeSpan {
+    private int hours;
+    private int minutes;
+    private int seconds;
 
-            public TimeSpan(int hours, int minutes, int seconds) {
-                this.hour = hours;
-                this.minute = minutes;
-                this.second = seconds;
-            }
+    public TimeSpan(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
 
-            public int getHour() {
-                return hour;
-            }
+    public void getHours(int hours) {
+        this.hours = hours;
+    }
 
-            public void setHour(int hour) {
-                this.hour = hour;
-            }
+    public int setHours() {
+        return this.hours;
+    }
 
-            public int getMinute() {
-                return minute;
-            }
+    public void getMinutes(int minutes) {
+        this.minutes = minutes;
+    }
 
-            public void setMinute(int minute) {
-                this.minute = minute;
-            }
+    public int setMinutes() {
+        return this.minutes;
+    }
 
-            public int getSeconds() {
-                return second;
-            }
+    public void getSeconds(int seconds) {
+        this.seconds = seconds;
+    }
 
-            public void setSecond(int second) {
-                this.second = second;
-            }
+    public int setSeconds() {
+        return this.seconds;
+    }
 
-            public void add(TimeSpan timeSpan) {
-                hour += timeSpan.hour;
-                minute += timeSpan.minute;
-                second += timeSpan.second;
-            }
+    public void add(TimeSpan time) {
+        change(time, 1);
+    }
 
-            public void subtract(TimeSpan timeSpan) {
-                hour -= timeSpan.hour;
-                minute -= timeSpan.minute;
-                second -= timeSpan.second;
-            }
+    public void subtract(TimeSpan time) {
+        change(time, -1);
+    }
 
-            @Override
-            public String toString() {
-                return String.format("%d, %d, %d", second, minute, hour);
-            }
+    private void change(TimeSpan time, int sign) {
+        this.hours += sign * time.hours;
+        this.minutes += sign * time.minutes;
+        this.seconds += sign * time.seconds;
+
+        if (this.seconds < 0) {
+            this.minutes -= 1;
+            this.seconds += 60;
         }
+
+        if (this.seconds > 59) {
+            this.minutes += 1;
+            this.seconds -= 60;
+        }
+
+        if (this.minutes < 0) {
+            this.hours -= 1;
+            this.minutes += 60;
+        }
+
+        if (this.minutes > 59) {
+            this.hours += 1;
+            this.minutes -= 60;
+        }
+    }
+
+    public String toString() {
+        String h = hours > 9 ? String.valueOf(hours) : "0" + hours;
+        String m = minutes > 9 ? String.valueOf(minutes) : "0" + minutes;
+        String s = seconds > 9 ? String.valueOf(seconds) : "0" + seconds;
+        return String.format("%s:%s:%s", h, m, s);
     }
 }
