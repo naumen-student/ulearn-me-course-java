a09dbf09af035427c41878c094c996d165baaeb8
AARegina <regina.agafonova.2001@gmail.com>
2020-12-24 18:43:39 +0500

    Агафонова Р.А. 08_task02

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,32 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
 
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+        List<Path> result = new ArrayList<>();
+        FillFilesList(rootDir, result);
+        return result;
+    }
 
-        return null;
+    private static void FillFilesList(Path rootDir, List<Path> paths) throws IOException {
+        DirectoryStream<Path> dirStream = Files.newDirectoryStream(rootDir);
+        for (Path child : dirStream) {
+            if (Files.isDirectory(child))
+                FillFilesList(child, paths);
+            else
+                paths.add(child);
+        }
     }
 }
