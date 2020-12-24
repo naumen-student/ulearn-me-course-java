2c395598122bbabc3e17567280bc98bc3aff4849
PolinaSvyazhina <68431429+PolinaSvyazhina@users.noreply.github.com>
2020-12-24 20:25:03 +0500

    09_task_01

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -2,7 +2,6 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -15,15 +14,7 @@ public class Task01Main {
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        ProcessBuilder builder = new ProcessBuilder("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
-        Scanner scanner = new Scanner(builder.start().getInputStream());
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (line.contains("format.tags.title")) {
-                return line.split("\"")[1];
-            }
-        }
-        return null;
+        // your implementation here
+        return "sound name";
     }
 }
-
