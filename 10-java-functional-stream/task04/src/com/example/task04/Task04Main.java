d235435b705f4ce898486c27c106ed53c0f7c4e9
evanesenze <evanesenze@gamil.com>
2020-12-24 18:33:07 +0500

    Базуев Иван.10.Task04

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,31 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Task04Main {
 
     public static void main(String[] args) {
-
-        // your implementation here
-
+        Map<String, Integer> res = new HashMap<>();
+        new Scanner(System.in)
+                .useDelimiter(Pattern.compile("[.,:;'!?_ \n\\-\"]"))
+                .tokens()
+                .map(String::toLowerCase)
+                .filter(x -> !x.equals(""))
+                .forEach(x -> res.put(x, res.containsKey(x) ? res.get(x) + 1 : 1));
+        res
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .limit(10)
+                .forEachOrdered(x -> System.out.print(x.getKey() + "\n"));
     }
-
 }
