5e9a2d99005e1ae37fdcc5d5dcc4679c1e06f660
rabbit21xx13 <rabbit21xx13@users.noreply.github.com>
2020-12-24 14:19:44 +0500

    Шевлякова Алёна. Задание 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,25 +1,35 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
         System.out.println(listFiles(Paths.get("08-java-io2/task02/src/main/resources/")));
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException {
-        List<Path> result = new ArrayList<>();
-        Files.walkFileTree(rootDir, new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-                result.add(file);
-                return FileVisitResult.CONTINUE;
-            }
-        });
-        return result;
+    public static List<Path> listFiles(Path rootDir) {
+        List<Path> res = new ArrayList<>();
+        for (File f : Objects.requireNonNull(rootDir.toFile().listFiles())) {
+            if (f.isDirectory())
+                listFiles(f.toPath(), res);
+            else
+                res.add(f.toPath());
+        }
+        return res;
+    }
+
+    private static void listFiles(Path rootDir, List<Path> paths) {
+        for (File file : (Objects.requireNonNull(rootDir.toFile().listFiles()))) {
+            if (file.isDirectory())
+                listFiles(file.toPath(), paths);
+            else
+                paths.add(file.toPath());
+
+        }
     }
 }
