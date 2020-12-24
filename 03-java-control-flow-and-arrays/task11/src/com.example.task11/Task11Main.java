84b08cdc4e0f941b164cba7a3f29cbfca1082764
Sivert01 <dodab@mail.ru>
2020-12-25 03:34:31 +0500

    Сиверт Дмитрий Задача 3.11

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,15 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0)
+            return;
+        for (int i = 1; i < arr.length; i++){
+            if(arr[i] < arr[0]){
+                int tmp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = tmp;
+            }
+        }
     }
 
 }
