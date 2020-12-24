aa0aabc0fef90150b96aacfc341347045bfc0851
cmxYZ <71211560+cmxYZ@users.noreply.github.com>
2020-12-24 10:23:39 +0500

    Create Point.java

---
+++ 04-java-oop-basic/task03/src/com/example/task03/Point.java
@@ -0,0 +1,15 @@
+package com.example.task04;
+
+public class Point {
+    final int x;
+    final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return String.format("(%d, %d", x, y);
+    }
+}
