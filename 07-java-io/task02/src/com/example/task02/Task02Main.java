edf8f61f1bb3f3ec4b95a8cc0b7a54e30efea518
viha4ka <vikusha2012@mail.ru>
2020-12-24 21:59:46 +0500

    Буторина Виктория Задание 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -4,18 +4,13 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        byte[] allBytes = System.in.readAllBytes();
-        for(int i = 0; i < allBytes.length; i++) {
-            if (i == allBytes.length - 1) {
-                System.out.write(allBytes[i]);
-                break;
-            }
-            if (allBytes[i] == 13 && allBytes[i + 1] == 10) {
-                System.out.write(10);
-                i++;
-            }
-            else System.out.write(allBytes[i]);
+        int current = System.in.read();
+        while (current >= 0) {
+            int next = System.in.read();
+            if (!(next == 10 && current == 13))
+                System.out.write(current);
+            current = next;
+            System.out.flush();
         }
-        System.out.flush();
     }
 }
