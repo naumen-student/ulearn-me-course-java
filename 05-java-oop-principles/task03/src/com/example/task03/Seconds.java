718f7e621acf48cbe00940306b7283c3ee2802cf
ssss123-byte <55254118+ssss123-byte@users.noreply.github.com>
2020-12-24 13:52:11 +0300

    Шардаков Егор. Задание 5.3

--- 05-java-oop-principles/task03/src/com/example/task03/Seconds.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Seconds.java
@@ -8,21 +8,30 @@ public class Seconds implements TimeUnit {
     private final long amount;
 
     public Seconds(long amount) {
+
         this.amount = amount;
     }
 
     @Override
     public long toMillis() {
+
         return amount * 1000;
     }
 
     @Override
     public long toSeconds() {
+
         return amount;
     }
 
     @Override
     public long toMinutes() {
-        return Math.round(amount / 60);
+
+        return Math.round(amount / 60.0);
+    }
+
+    @Override
+    public long toHours() {
+        return Math.round(amount / 60.0 / 60.0);
     }
 }
