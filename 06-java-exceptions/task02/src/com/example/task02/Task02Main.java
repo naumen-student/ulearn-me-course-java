088cdd3145aba054d4911fb7b2ebda748f73ad07
grekova-web <71182152+grekova-web@users.noreply.github.com>
2020-12-24 12:39:17 +0500

    Грекова Елена. Задание 6.2

--- 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
+++ 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
@@ -1,5 +1,7 @@
 package com.example.task02;
 
+import java.text.MessageFormat;
+
 public class Task02Main {
 
     public static void main(String[] args) {
@@ -11,27 +13,10 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        switch (monthNumber) {
-            case 12:
-            case 1:
-            case 2:
-                return "зима";
-            case 3:
-            case 4:
-            case 5:
-                return "весна";
-            case 6:
-            case 7:
-            case 8:
-                return "лето";
-            case 9:
-            case 10:
-            case 11:
-                return "осень";
-            default:
-                throw new IllegalArgumentException
-                        (String.format("monthNumber %s is invalid, month number should be between 1..12",
-                                monthNumber));
+        String[] seasons = new String[] {"зима", "весна", "лето", "осень"};
+        if(monthNumber < 1 || monthNumber > 12) {
+            throw new IllegalArgumentException(MessageFormat.format("monthNumber {0} is invalid, month number should be between 1..12", monthNumber));
         }
+        return seasons[monthNumber % 12 / 3];
     }
 }
