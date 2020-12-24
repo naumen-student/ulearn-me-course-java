cfb2bff9004d541b7327758fce46651d8f4585bf
LekoevNVK <lekoev.nvk@gmail.com>
2020-12-24 18:26:42 +0500

    Лебедкин Константин. Задача 9.3

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -3,15 +3,17 @@ package com.example.task03;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
+        List<Set<String>> anagrams = findAnagrams(new FileInputStream("09-java-generics-collections/task03/resources/singular.txt"), Charset.forName("windows-1251"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
@@ -19,6 +21,32 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset));
+        Map<String, TreeSet<String>> allWords = new TreeMap<>();
+
+        while (scanner.hasNextLine()) {
+            String current = scanner.nextLine().toLowerCase();
+            if (current.matches("[а-я]+") && current.length() > 2) {
+                char[] arrWord = current.toCharArray();
+                Arrays.sort(arrWord);
+                String currentKey = new String(arrWord);
+                if (!allWords.containsKey(currentKey))
+                    allWords.put(currentKey, new TreeSet<String>() {{
+                        add(current);
+                    }});
+                else
+                    allWords.get(currentKey).add(current);
+            }
+        }
+        return sortResultList(allWords);
+    }
+
+    private static List<Set<String>> sortResultList(Map<String, TreeSet<String>> allSets) {
+        List<Set<String>> setsFilter = new ArrayList<>();
+        for (Set set : allSets.values()) {
+            if (set.size() >= 2)
+                setsFilter.add(set);
+        }
+        return setsFilter;
     }
 }
