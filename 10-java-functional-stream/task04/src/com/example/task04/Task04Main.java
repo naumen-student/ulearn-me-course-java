c924507f1f87717caae0c69a3b58500941c45a7f
ssss123-byte <55254118+ssss123-byte@users.noreply.github.com>
2020-12-24 14:32:02 +0300

    Шардаков Егор. Задание 10.4

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,36 +1,26 @@
 package com.example.task04;
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.Map;
-import java.util.Scanner;
-import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        Map<String, Integer> result = new HashMap<>();
-        new Scanner(System.in).useDelimiter(Pattern.compile("[.,:;'!?_ \n\\-\"]"))
-                .tokens()
-                .map(String::toLowerCase)
-                .filter(x -> !x.equals(""))
-                .forEach(x -> {
-                    if (result.containsKey(x)) result.put(x, result.get(x) + 1);
-                    else result.put(x, 1);
-                });
-        result
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        reader.lines()
+                .flatMap(x -> Stream.of(x.split("[,.\\-! ]")).filter(y -> !y.isEmpty())
+                        .map(String::toLowerCase))
+                .collect(Collectors.groupingBy(x -> x, Collectors.counting()))
                 .entrySet()
                 .stream()
-                .sorted((x, y) ->
-                        (x.getValue().equals(y.getValue())
-                                ?
-                                CharSequence.compare(x.getKey(), y.getKey())
-                                :
-                                Integer.compare(y.getValue(), x.getValue())))
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .limit(10)
-                .forEachOrdered(x -> System.out.print(x.getKey() + "\n"));
+                .forEach(x -> System.out.print(x.getKey() + "\n"));
     }
-
-    }
-
 }
