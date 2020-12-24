066c385eef75debad50dfc6313a9f6e2630ddde5
Sivert01 <dodab@mail.ru>
2020-12-25 03:28:37 +0500

    Сиверт Дмитрий Задача 3.7

--- 03-java-control-flow-and-arrays/task07/src/com.example.task07/Task07Main.java
+++ 03-java-control-flow-and-arrays/task07/src/com.example.task07/Task07Main.java
@@ -1,5 +1,5 @@
 package com.example.task07;
-
+import java.util.Arrays;
 public class Task07Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -15,8 +15,7 @@ public class Task07Main {
     }
 
     static int sum(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return Arrays.stream(arr).sum();
     }
 
 }
