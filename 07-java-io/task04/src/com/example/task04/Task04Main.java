85efec68c6b3d3a3a6179c8163457e537868f638
theSweater23 <geg.gegre123@gmail.com>
2020-12-24 15:44:38 +0500

    Сулейманов Эмиль. 7.4

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+        double sum = 0.0;
+        java.util.Scanner scanner = new java.util.Scanner(System.in).useDelimiter("[\\s\\n]+").useLocale(java.util.Locale.ENGLISH);
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble()) {
+                sum += scanner.nextDouble();
+            }
+            else
+                scanner.next();
+        }
 
-        System.out.println("0.0");
+        System.out.printf(java.util.Locale.ENGLISH, "%8.6f", sum);
     }
 }
