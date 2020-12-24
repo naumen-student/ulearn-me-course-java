e9b8034215973a0cd05c50db9f3a0705e421fe66
Лялин Артём <maii122@mail.ru>
2020-12-24 22:06:49 +0500

    09_Task01 Done

--- 09-java-generics-collections/task01/src/com/example/task01/Pair.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Pair.java
@@ -1,5 +1,47 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<T1, T2> {
+
+    private final T1 firstElement;
+    private final T2 secondElement;
+
+    private Pair(T1 first, T2 second){
+        firstElement = first;
+        secondElement = second;
+    }
+
+    public static <T1, T2> Pair<T1, T2> of(T1 first, T2 second){
+        return new Pair<>(first, second);
+    }
+
+    public T1 getFirst(){
+        return firstElement;
+    }
+
+    public T2 getSecond(){
+        return secondElement;
+    }
+
+    public void ifPresent(BiConsumer<? super T1, ? super T2> biConsumer) {
+        if (firstElement != null && secondElement != null)
+            biConsumer.accept(firstElement, secondElement);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstElement, secondElement);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Pair))
+            return false;
+        if (this == obj)
+            return true;
+        Pair<T1, T2> pair = (Pair<T1, T2>) obj;
+        return firstElement.equals(pair.firstElement) && secondElement.equals(pair.secondElement);
+    }
 }
