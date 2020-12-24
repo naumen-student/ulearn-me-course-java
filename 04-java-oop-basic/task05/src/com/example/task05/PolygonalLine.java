c131dc197bbd24b3479d2ea335c24861f2e36e15
ALeksHertz <sashahertz@bk.ru>
2020-12-25 03:59:40 +0500

    Герц Александр. Задача 4.5

--- 04-java-oop-basic/task05/src/com/example/task05/PolygonalLine.java
+++ 04-java-oop-basic/task05/src/com/example/task05/PolygonalLine.java
@@ -1,19 +1,23 @@
 package com.example.task05;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
-
+    private final ArrayList<Point> points = new ArrayList<>();
     /**
      * Устанавливает точки ломаной линии
      *
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        for (Point point : points) {
+            addPoint(point);
+        }
     }
-
     /**
      * Добавляет точку к ломаной линии
      *
@@ -21,6 +25,7 @@ public class PolygonalLine {
      */
     public void addPoint(Point point) {
         // TODO: реализовать
+            points.add(new Point(point.getX(), point.getY()));
     }
 
     /**
@@ -31,6 +36,7 @@ public class PolygonalLine {
      */
     public void addPoint(double x, double y) {
         // TODO: реализовать
+            points.add(new Point(x, y));
     }
 
     /**
@@ -40,7 +46,11 @@ public class PolygonalLine {
      */
     public double getLength() {
         // TODO: реализовать
-        throw new AssertionError();
+            double length = 0;
+            for (int i = 0; i < points.size() - 1; i++) {
+                length += points.get(i).getLength(points.get(i + 1));
+            }
+            return length;
     }
 
 }
