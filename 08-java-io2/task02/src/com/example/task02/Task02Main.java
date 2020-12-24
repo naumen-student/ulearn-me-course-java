2ae800d007c445d7f97d95ac69bbcd60ee0d47ea
Arnaud225K <kouakanarnaud@gmail.com>
2020-12-24 13:51:56 +0500

    Аданэн куакан задание 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,45 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
+//    public static void main(String[] args) throws IOException, InterruptedException {
+        public static void main(String[] args) {
+            //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+            // например вот так:
 
         /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
         */
 
-    }
+        }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+//        public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
+//            // your implementation here
+            public static List<Path> listFiles(Path rootDir) throws IOException
+            {
+                MyVisitor visitor = new MyVisitor();
+                Files.walkFileTree(rootDir, visitor);
+                return visitor.allVisitedFiles;
+            }
+        }
 
-        return null;
-    }
-}
+        class  MyVisitor extends SimpleFileVisitor<Path>
+        {
+            public  final List<Path> allVisitedFiles = new ArrayList<>();
+
+//        return null;
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
+            {
+                allVisitedFiles.add(( file.normalize()));
+                return super.visitFile(file, attrs);
+            }
+        }
