64d2151f91dca7877ef5b146c0a31870c8253356
Weak <skandal_dota@mail.ru>
2020-12-24 15:24:35 +0500

    Задание 7.4

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,19 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+        double res = 0;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENG);
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble())
+                res += scanner.nextDouble();
+            else
+                scanner.next();
+        }
+        System.out.format(Locale.ENG," %1.6f", res);
     }
 }
