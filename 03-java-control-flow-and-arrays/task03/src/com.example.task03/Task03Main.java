9551a1767c52a42b61c7e6cde71affcbd6fb8237
AShamala <shamala406@gmail.com>
2020-12-25 17:58:56 +0600

    Шамала Александр 03_03

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -10,6 +10,16 @@ public class Task03Main {
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String res = "";
+        switch (weekDaysNumber) {
+            case 1: res = "понедельник"; break;
+            case 2: res = "вторник"; break;
+            case 3: res = "среда"; break;
+            case 4: res = "четверг"; break;
+            case 5: res = "пятница"; break;
+            case 6: res = "суббота"; break;
+            case 7: res = "воскресенье"; break;
+        }
+        return res;
     }
 }
