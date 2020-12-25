958a8cead864a0f6198db44935400abe5f8aea4e
ReyyL <_Galaxy5500>
2020-12-25 12:51:48 +0500

    Лазарев Андрей. Задание 4.2

--- 04-java-oop-basic/task02/src/com/example/task02/TimeSpan.java
+++ 04-java-oop-basic/task02/src/com/example/task02/TimeSpan.java
@@ -7,52 +7,59 @@ public class TimeSpan {
     private int minutes;
     private int seconds;
 
-    public TimeSpan() {
-    }
-
     public TimeSpan(int hours, int minutes, int seconds) {
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
     }
 
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
-
     public int getHours() {
         return hours;
     }
 
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
+    public void setHours(int hours) {
+        this.hours = hours;
     }
 
     public int getMinutes() {
         return minutes;
     }
 
-    public void setSeconds(int seconds) {
-        this.seconds = seconds;
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
     }
 
     public int getSeconds() {
         return seconds;
     }
 
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    private void normalizeTimeSpan() {
+        long temp = hours * 3600 + minutes * 60 + seconds;
+        hours = (int) (temp / 3600);
+        temp %= 3600;
+        minutes = (int) (temp / 60);
+        seconds = (int) (temp % 60);
+    }
+
     void add(TimeSpan time) {
         hours += time.hours;
         minutes += time.minutes;
         seconds += time.seconds;
+        normalizeTimeSpan();
     }
 
     void subtract(TimeSpan time) {
         hours -= time.hours;
         minutes -= time.minutes;
         seconds -= time.seconds;
+        normalizeTimeSpan();
     }
 
     public String toString() {
-        return String.format("Hours: %d, Minutes: %d, Seconds: %d", hours, minutes, seconds);
+        return String.format("h: %d, m: %d, s: %d", hours, minutes, seconds);
     }
 }
