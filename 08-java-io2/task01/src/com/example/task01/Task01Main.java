a957e56a15889fbdea08b1f38cc9834616ac49f1
ReyyL <_Galaxy5500>
2020-12-25 13:46:22 +0500

    Лазарев Андрей. Задание 8.1

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -6,18 +6,20 @@ import java.util.Scanner;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
-        Scanner scanner = new Scanner(processBuilder.start().getInputStream());
-        while (scanner.hasNextLine()) {
-            String currentLine = scanner.nextLine();
-            if (currentLine.contains("format.tags.title")) {
-                return currentLine.subSequence(19, currentLine.length() - 1).toString();
-            }
+        String result = "";
+
+        Scanner s = new Scanner(processBuilder.start().getInputStream());
+        while (s.hasNextLine()) {
+            String current = s.nextLine();
+            if (current.startsWith("format.tags.title"))
+                result = current.replace("format.tags.title=", "");
         }
-        return null;
+        return result.substring(1, result.length() - 1);
     }
 }
