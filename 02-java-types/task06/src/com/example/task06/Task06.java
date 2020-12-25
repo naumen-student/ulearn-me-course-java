f14e91b3f57c78b495a77b5a71abfaa6a2f49950
AShamala <shamala406@gmail.com>
2020-12-25 14:07:07 +0600

    Шамала Александр 02_06

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,10 +3,14 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = x + y;
+        int count = 0;
+        if (sum == 0) return 1;
+        while (sum != 0) {
+            count ++;
+            sum /= 10;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
