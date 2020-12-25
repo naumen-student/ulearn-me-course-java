0085dad5fac83cf59b618b041e377c03aa608ba5
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-12-25 19:02:59 +0500

    Update Task04Main.java

--- 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
+++ 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
@@ -6,7 +6,7 @@ public class Task04Main {
 
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
+        // например вот так: 
         /*
         System.out.println(getSeason(-5));
          */
