e100eaf0bd754118df38f9a774255ae0ccefefc8
Заид Омар <omar.m.zaid@hotmail.com>
2020-12-25 16:42:05 +0500

    Заид Омар Задача 7.3

--- 07-java-io/task03/src/com/example/task03/Task03Main.java
+++ 07-java-io/task03/src/com/example/task03/Task03Main.java
@@ -1,21 +1,21 @@
 package com.example.task03;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
+        System.out.println(readAsString(new FileInputStream("07-java-io/task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
     }
 
-    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+    public static String readAsString(InputStream inputStream, Charset charset) {
+        if (inputStream == null || charset == null) throw new IllegalArgumentException();
+        StringWriter result = new StringWriter();
+        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset));
+        while (scanner.hasNext()) {
+            result.write(scanner.next());
+        }
+        return result.toString();
     }
 }
