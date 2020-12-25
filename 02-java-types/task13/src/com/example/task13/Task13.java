2e57aebe9d2001a8cfa64b4d6a915a2a46fa42ac
Заид Омар <omar.m.zaid@hotmail.com>
2020-12-25 12:45:54 +0500

    Заид Омар 02_Task13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -3,17 +3,12 @@ package com.example.task13;
 public class Task13 {
 
     public static char toUpperCase(char c) {
-
-        return (char) (c - 32);
+        return Character.toUpperCase(c);
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        char result = solution('x');
+        char result = toUpperCase('x');
         System.out.println(result);
-         */
     }
 
 }
