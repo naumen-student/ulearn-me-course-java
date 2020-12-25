e6571c49280d3dc6820a099c088b9f96c03910b3
JIiCeHoK <foxs29546@gmail.com>
2020-12-26 00:18:30 +0500

    Моисеев Михаил. 10.1

--- 10-java-functional-stream/task01/src/com/example/task01/Task01Main.java
+++ 10-java-functional-stream/task01/src/com/example/task01/Task01Main.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+import jdk.nashorn.internal.ir.TernaryNode;
+import org.assertj.core.error.ShouldNotBeNull;
+
+import java.beans.Expression;
+
 public class Task01Main {
     public static void main(String[] args) throws IOException {
 
@@ -22,8 +27,9 @@ public class Task01Main {
             Predicate<? super T> condition,
             Function<? super T, ? extends U> ifTrue,
             Function<? super T, ? extends U> ifFalse) {
-
-        return null; // your implementation here
+        if (condition == null || ifFalse == null || ifTrue == null)
+            throw new NullPointerException();
+        return (x) -> condition.test(x) ? ifTrue.apply(x) : ifFalse.apply(x);
 
     }
 }
