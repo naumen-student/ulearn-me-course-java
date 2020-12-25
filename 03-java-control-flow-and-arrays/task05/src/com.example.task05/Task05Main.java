2ff086e257252bf794a18d6b4eb92ebf65028664
kalibri1995 <gainatullinalba@gmail.com>
2020-12-26 02:58:15 +0300

    Гайнатуллин Альберт Задание 3.5

--- 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
+++ 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
@@ -2,15 +2,11 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         System.out.println(getMin(5,4,3));
-         */
     }
 
     static int getMin(int a, int b, int c) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return Math.min(Math.min(a, b), c);
     }
 }
