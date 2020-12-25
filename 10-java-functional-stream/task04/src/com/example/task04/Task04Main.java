ae9a87535cb9ee77e49772b72b9b09922748e3e4
MaximFrancevich <FranzuaAirMax@yandex.ru>
2020-12-26 03:55:35 +0500

    Францевич Максим. Задание 10-04

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,39 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task04Main {
 
     public static void main(String[] args) {
-
-        // your implementation here
-
+        Map<String, Integer> wordsCount = new HashMap<>();
+        new BufferedReader(new InputStreamReader(System.in))
+                .lines()
+                .flatMap(line -> Arrays.stream(line.split("[\\s-!,.]+")))
+                .map(String::toLowerCase)
+                .forEach(word -> {
+                    if (word.length() > 0) {
+                        if (wordsCount.containsKey(word)) {
+                            wordsCount.put(word, wordsCount.get(word) + 1);
+                        } else {
+                            wordsCount.put(word, 1);
+                        }
+                    }
+                });
+        wordsCount
+                .entrySet()
+                .stream()
+                .sorted((entry1, entry2) -> {
+                    int result = entry2.getValue() - entry1.getValue();
+                    if (result == 0) {
+                        return entry1.getKey().compareTo(entry2.getKey());
+                    }
+                    return result;
+                })
+                .limit(10)
+                .forEach(entry -> System.out.printf("%s\n", entry.getKey()));
     }
-
 }
