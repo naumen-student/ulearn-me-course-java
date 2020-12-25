30ca5903c5aeed8a193fd8951e136e2cfd6e1e8a
fitsuli <danilyuk_2011@bk.ru>
2020-12-25 21:24:37 +0500

    Данилюк Дмитрий. Задача 5.3

--- 05-java-oop-principles/task03/src/com/example/task03/TimeUnitUtils.java
+++ 05-java-oop-principles/task03/src/com/example/task03/TimeUnitUtils.java
@@ -24,4 +24,16 @@ public class TimeUnitUtils {
     public static Seconds toSeconds(Milliseconds millis) {
         return new Seconds(millis.toSeconds());
     }
+
+    public static Milliseconds toMillis(TimeUnit timeUnit) {
+        return new Milliseconds(timeUnit.toMillis());
+    }
+
+    public static Seconds toSeconds(TimeUnit timeUnit) {
+        return new Seconds(timeUnit.toSeconds());
+    }
+
+    public static Hours toHours(TimeUnit timeUnit) {
+        return new Hours(timeUnit.toHours());
+    }
 }
