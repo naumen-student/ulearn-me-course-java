7be9f85e97b1bc74d6e99ce277a826422ea132d0
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-12-25 19:03:02 +0500

    Update MyException.java

--- 06-java-exceptions/task04/src/com/example/task04/MyException.java
+++ 06-java-exceptions/task04/src/com/example/task04/MyException.java
@@ -10,7 +10,7 @@ public class MyException extends IllegalArgumentException {
     }
 
     public MyException(String message, Throwable cause) {
-        super(message, cause);
+        super(message, cause); 
     }
 
     public MyException(Throwable cause) {
