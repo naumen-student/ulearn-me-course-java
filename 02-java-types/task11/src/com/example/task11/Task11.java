5fd361d1312a4a364cde5b0cff76e3cb0efa20e7
flint-afk <ivan.ivanenko55779@gmail.com>
2020-12-25 19:34:20 +0500

    Update Task11.java

--- 02-java-types/task11/src/com/example/task11/Task11.java
+++ 02-java-types/task11/src/com/example/task11/Task11.java
@@ -7,15 +7,12 @@ public class Task11 {
         // TODO исправьте функцию, чтобы избежать накопления ошибки
 
         // Считаем проценты за год
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
+        return (float)(sum*Math.pow(1.0+percent,12));
     }
 
     public static void main(String[] args) {
 
-        float sum = 500; // 500 руб. на счете
+        float sum = 500f; // 500 руб. на счете
         float percent = 0.00000001f; // 0.000001% ежемесячно
 
         sum = benefit(sum, percent);
