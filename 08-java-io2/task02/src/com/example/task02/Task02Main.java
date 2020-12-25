3a3fff57c0bafc22606fb9beff82158b92499abc
ALeksHertz <sashahertz@bk.ru>
2020-12-25 22:33:07 +0500

    Герц Александр. Задача 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,10 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
@@ -18,6 +21,10 @@ public class Task02Main {
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
         // your implementation here
 
-        return null;
+        ArrayList<Path> pathArrayList = new ArrayList<>();
+        Files.walk(rootDir)
+                .filter(Files::isRegularFile)
+                .forEach(pathArrayList::add);
+        return pathArrayList;
     }
 }
