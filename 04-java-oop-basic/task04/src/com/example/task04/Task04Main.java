340a84abc3b6142cedafe78a1f3a7f5d4e513bb6
LekoevNVK <lekoev.nvk@gmail.com>
2020-12-25 12:25:27 +0500

    Лебедкин Константин. Задача 4.4

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,10 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Points p1 = new Points (0, 0);
+        Points p2 = new Points (1, 1);
+        int px = p1.getX ();
+        Lines line = new Lines (p1, p2);
+        Boolean b = line.isCollinearLine (new Points (0, 1));
     }
 }
