9ae6d0b63b8ddc0824bf1b0b705bd7bc0ade62c0
PolinaUskova <polina.uskova1535@gmail.com>
2020-12-26 20:58:37 +0500

    Ускова Полина. Задача 5.1

--- 05-java-oop-principles/task02/src/com/example/task02/Task02Main.java
+++ 05-java-oop-principles/task02/src/com/example/task02/Task02Main.java
@@ -3,19 +3,21 @@ package com.example.task02;
 public class Task02Main {
 
     private static final Item ITEM1 = new Item("Товар 1", 10);
-    private static final Item ITEM2 = new Item("Товар 2", 20);
     private static final Item ITEM3 = new Item("Товар 3", 30);
-    private static final Item ITEM4 = new Item("Товар 4", 40);
-    private static final Item ITEM5 = new Item("Товар 5", 50);
     private static final Item ITEM6 = new Item("Товар 6", 60);
 
     public static void main(String[] args) {
-        Bill bill = new Bill();
-        bill.add(ITEM1, 10);
+        DiscountBill bill = null;
+        try {
+            bill = new DiscountBill(30);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        bill.add(ITEM1, 2);
         bill.add(ITEM3, 3);
-        bill.add(ITEM6, 1);
-        System.out.println(bill);
-        bill.add(ITEM3, 3);
-        System.out.println(bill);
+        bill.add(ITEM6, 4);
+        System.out.format("Price: %s%n", bill.getPrice());
+        System.out.format("Price with discount: %s%n", bill.getFinalPrice());
     }
 }
