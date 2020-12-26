a3b90c39c12cd4421973c0eaa647c45d24a8c462
Mohamed <mohamedtarek21763@gmail.com>
2020-12-27 02:55:19 +0500

    Хассан Мохамед Тарек Хассан 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,25 @@
 package com.example.task03;
 
 public class Task03Main {
-    public static void main(String[] args) {
+    private double realAxis;
+    private double imaginaryAxis;
 
+    public Task03Main(double realAxis, double imaginaryAxis) {
+        this.imaginaryAxis = imaginaryAxis;
+        this.realAxis = realAxis;
+    }
+
+    public Task03Main sum(Task03Main number) {
+        return new Task03Main(realAxis + number.realAxis, imaginaryAxis + number.imaginaryAxis);
+    }
+
+    public Task03Main multiply(Task03Main number) {
+        return new Task03Main(
+                realAxis * number.realAxis - imaginaryAxis * number.imaginaryAxis,
+                realAxis * number.imaginaryAxis + imaginaryAxis * number.realAxis);
+    }
+
+    public String toString() {
+        return String.format("%f + %fi", realAxis, imaginaryAxis);
     }
 }
