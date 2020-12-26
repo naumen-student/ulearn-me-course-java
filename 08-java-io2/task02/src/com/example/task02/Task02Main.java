eeaf4f9c7898e1ea4605648829c154d5f2c64589
Chocolatelis <elizavetab123@mail.ru>
2020-12-26 18:13:40 +0500

    Быкова Елизавета Задание 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -3,21 +3,28 @@ package com.example.task02;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
+import java.nio.file.*;
+import java.util.ArrayList;
 
 public class Task02Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
 
-        /*
-        System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
+    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
+
+        List<Path> res = new ArrayList<>();
+        FillFilesList(rootDir, res);
+        return res;
 
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+    public static void FillFilesList(Path rootDir, List<Path> paths) throws IOException, InterruptedException {
 
-        return null;
+        DirectoryStream<Path> dirStream = Files.newDirectoryStream(rootDir);
+
+        for (Path e : dirStream) {
+            if (Files.isDirectory(e))
+                FillFilesList(e, paths);
+            else
+                paths.add(e);
+        }
     }
 }
