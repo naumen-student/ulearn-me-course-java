19bc5f6a29f1c2b1be294820276321c9dafe1f08
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-26 13:46:45 +0500

    Колесников Александр 2.10

--- 02-java-types/task10/src/com/example/task10/Task10.java
+++ 02-java-types/task10/src/com/example/task10/Task10.java
@@ -1,15 +1,17 @@
 package com.example.task10;
-
 public class Task10 {
-
     public static boolean compare(float a, float b, int precision) {
-
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
 
+
+
+        if (Float.isNaN(a) && Float.isNaN(b))
+            return true;
+        else if (Float.isInfinite(a) && Float.isInfinite(b))
+            return a * b > 0;
+        return Math.abs(a - b) < Math.pow(10, -precision);
     }
 
     public static void main(String[] args) {
@@ -17,10 +19,7 @@ public class Task10 {
         float b = 0.4f;
         float sum = a + b;
         float c = 0.7f;
-
         boolean result = compare(sum, c, 2);
         System.out.println(result);
-
     }
-
 }
