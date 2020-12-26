d75153a8e541fa295a97c388c4039d27f315e932
ilya_res <top-toptt@yandex.ru>
2020-12-26 19:51:27 +0500

    Реснянский Илья. 7-04

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +10,25 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        Scanner in = new Scanner(System.in);
+        double res = 0;
+        String str;
+        while(in.hasNextLine()){
+            str = in.nextLine();
+            String[] arr = str.split(" ");
+            for(int i = 0; i < arr.length; i++){
+                try {
+                    res += Double.parseDouble(arr[i]);
+                }
+                catch (NumberFormatException e){
+                    continue;
+                }
+                catch (NullPointerException e){
 
-        System.out.println("0.0");
+                }
+            }
+        }
+
+        System.out.printf("%.6f", res);
     }
 }
