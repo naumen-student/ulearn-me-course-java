4fd4078c05daa9e67fffce5e7047d10629a71de8
Арман <Arman.Koshaev@urfu.me>
2020-12-26 19:15:17 +0500

    Кошаев Арман. Задание 8.3

---
+++ 08-java-io2/task03/src/com/example/task03/Task03Main.java
@@ -0,0 +1,23 @@
+package com.example.task03;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+
+public class Task03Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        System.out.println(deserialize(new FileInputStream("task03/src/main/resources/example1.bin")));
+
+
+    }
+
+    public static SampleData deserialize(InputStream inputStream) throws IOException, ClassNotFoundException {
+        SampleData deserialized;
+        try (ObjectInputStream ois = new ObjectInputStream(inputStream)) {
+            deserialized = (SampleData) ois.readObject();
+        }
+        return deserialized;
+    }
+}
