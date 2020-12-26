e6c35ca8dcb759be10db7e0bd69855184c52817e
S1ide <kopotev.nick@icloud.com>
2020-12-26 20:27:20 +0500

    Копотев Никита, задание 10.5

---
+++ 10-java-functional-stream/task05/src/com/example/task05/MailService.java
@@ -0,0 +1,26 @@
+package com.example.task05;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class MailService<T> implements Consumer<IContent<T>> {
+    private final Map<String, List<T>> mailBox;
+
+    public MailService() {
+        mailBox = new MyMap<>();
+    }
+
+    public Map<String, List<T>> getMailBox() {
+        return mailBox;
+    }
+
+    @Override
+    public void accept(IContent<T> content) {
+        if (mailBox.containsKey(content.getTo())) mailBox.get (content.getTo ()).add (content.getContent ());
+        else mailBox.put (content.getTo (), new ArrayList<T>() {{
+            add (content.getContent ());
+        }});
+    }
+} 
