e26b250a07516b4d0c17f770412a216d449b91d1
tenfrs <podaand@icloud.com>
2020-12-26 12:01:33 +0500

    Поспелова Дарья Задание 02.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -7,20 +7,16 @@ public class Task02 {
         long line = Long.parseLong(input);
         if (Byte.MIN_VALUE <= line && line <= Byte.MAX_VALUE)
             return "byte";
-        if (Integer.MIN_VALUE <= line && line <= Integer.MAX_VALUE)
-            return "int";
         if (Short.MIN_VALUE <= line && line <= Short.MAX_VALUE)
             return "short";
+        if (Integer.MIN_VALUE <= line && line <= Integer.MAX_VALUE)
+            return "int";
         return "long";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
     }
 
 }
