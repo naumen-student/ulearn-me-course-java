e9c75f33b7d7a73968589bfbc248149d40b26aa8
Mohamed <mohamedtarek21763@gmail.com>
2020-12-26 16:23:54 +0500

    Хассан Мохамед Тарек Хассан 2.13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -4,18 +4,7 @@ public class Task13 {
 
     public static char toUpperCase(char c) {
 
-        // TODO привести букву к верхнему регистру
-
-        return c;
-    }
-
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        char result = solution('x');
-        System.out.println(result);
-         */
+        return Character.toUpperCase(c);
     }
 
 }
