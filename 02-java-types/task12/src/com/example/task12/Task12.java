90d7f8c463abf7c4337db678a9d5dd05c9ee3284
PolinaUskova <polina.uskova1535@gmail.com>
2020-12-26 18:36:57 +0500

    Ускова Полина. Задача 2.12

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -16,19 +16,11 @@ public class Task12 {
         }
         return sum;
         */
-
-        return BigDecimal.ZERO;
+        int monthCount = 12;
+        return new BigDecimal(1)
+                .add(percent)
+                .pow(monthCount)
+                .multiply(sum)
+                .setScale(9,BigDecimal.ROUND_HALF_UP);
     }
-
-    public static void main(String[] args) {
-
-        BigDecimal sum = new BigDecimal(500).setScale(9, BigDecimal.ROUND_HALF_UP); // 500 руб. на счете
-        BigDecimal percent = new BigDecimal(0.00000001f).setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
-
-        sum = benefit(sum, percent);
-
-        System.out.println("Сумма на счете через год: " + sum);
-
-    }
-
 }
