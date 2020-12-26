2ca9a689149b1e1db73f2a8229131729a91d7b02
RoyalPiano <zelyoniyoleg@yandex.ru>
2020-12-27 00:37:05 +0500

    ыва

--- 09-java-generics-collections/task01/src/com/example/task01/Pair.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Pair.java
@@ -1,5 +1,37 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.function.BiConsumer;
+
+public class Pair<First, Second> {
+    First firstValue;
+    Second secondValue;
+    private Pair(First first, Second second){
+        this.firstValue = first;
+        this.secondValue = second;
+    }
+
+    public First getFirst(){
+        return firstValue;
+    }
+
+    public Second getSecond(){
+        return secondValue;
+    }
+
+    public boolean equals(Pair<First, Second> pair){
+        return this.hashCode() == pair.hashCode();
+    }
+
+    public int hashCode(){
+        return this.secondValue.hashCode() + this.secondValue.hashCode();
+    }
+
+    public void ifPresent(BiConsumer<? super First, ? super Second> biConsumer) {
+        if (firstValue != null && secondValue != null)
+            biConsumer.accept(firstValue, secondValue);
+    }
+
+    public static <First, Second> Pair<First, Second> of(First firstValue, Second secondValue) {
+        return new Pair<>(firstValue, secondValue);
+    }
 }
