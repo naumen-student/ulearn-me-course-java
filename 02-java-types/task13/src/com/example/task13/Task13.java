d16349283a5d2e02264d237f8396b8e3b7f20742
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-26 14:00:44 +0500

    Колесников Александр 2.13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -1,12 +1,12 @@
 package com.example.task13;
-
 public class Task13 {
 
     public static char toUpperCase(char c) {
 
         // TODO привести букву к верхнему регистру
 
-        return c;
+
+        return (char) (c - 32);
     }
 
     public static void main(String[] args) {
@@ -17,5 +17,4 @@ public class Task13 {
         System.out.println(result);
          */
     }
-
 }
