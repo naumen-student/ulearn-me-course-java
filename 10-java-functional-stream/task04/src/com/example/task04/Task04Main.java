b84b56ff72d0b275d4e38205c7cf711878e1ea2e
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-27 03:13:07 +0500

    Колесников Александр 10.4

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,30 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
         // your implementation here
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        reader.lines()
+                .map(String::toLowerCase)
+                .flatMap(l -> Stream.of(l.split("[^a-zёа-я0-9]+")))
+                .filter(w -> !w.isEmpty())
+                .collect(Collectors.groupingBy(w -> w, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach(x -> System.out.print(x.getKey() + '\n'));
     }
 
 }
