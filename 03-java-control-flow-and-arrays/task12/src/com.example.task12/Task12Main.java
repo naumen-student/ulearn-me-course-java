00daeefc457b83f5780fecd125d2c1df5f6f51e5
tenfrs <podaand@icloud.com>
2020-12-26 19:05:25 +0500

    Задание 3.13 Поспелова Дарья

--- 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
+++ 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
@@ -1,18 +1,16 @@
 package com.example.task12;
+import java.util.Arrays;
 
 public class Task12Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {9, 11, 7, 8};
-        selectionSort(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
 
+        if (arr == null)
+            return null;
+        return Arrays.stream(arr).filter(x -> x <= 1e3).toArray();
+    }
 }
