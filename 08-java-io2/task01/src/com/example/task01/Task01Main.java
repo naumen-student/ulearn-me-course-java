ff2198bb3b2cde6d1c132e43d6f36eb777c01647
Арман <Arman.Koshaev@urfu.me>
2020-12-26 19:03:44 +0500

    Кошаев Арман. Задание 8.1

---
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -0,0 +1,26 @@
+package com.example.task01;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task01Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
+
+    }
+
+    public static String extractSoundName(File file) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+        try (Scanner scanner = new Scanner(processBuilder.start().getInputStream())){
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if (line.contains("format.tags.title=")){
+                    return line.split("\"")[1];
+                }
+            }
+        }
+        return null;
+    }
+}
