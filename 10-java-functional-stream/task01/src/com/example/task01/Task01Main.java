a1fa5899e69391a2998f2555fe2abd9d6dee6912
AARegina <regina.agafonova.2001@gmail.com>
2020-12-26 19:36:50 +0500

    Агафонова Регина 10_task01

--- 10-java-functional-stream/task01/src/com/example/task01/Task01Main.java
+++ 10-java-functional-stream/task01/src/com/example/task01/Task01Main.java
@@ -1,21 +1,16 @@
 package com.example.task01;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-
-        // TODO С корректно реализованным методом ternaryOperator должен компилироваться и успешно работать следующий код:
-
-        /*
         Predicate<Object> condition = Objects::isNull;
         Function<Object, Integer> ifTrue = obj -> 0;
         Function<CharSequence, Integer> ifFalse = CharSequence::length;
         Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
-        */
-
     }
 
     public static <T, U> Function<T, U> ternaryOperator(
@@ -23,7 +18,10 @@ public class Task01Main {
             Function<? super T, ? extends U> ifTrue,
             Function<? super T, ? extends U> ifFalse) {
 
-        return null; // your implementation here
+        if (condition == null || ifTrue == null || ifFalse == null) {
+            throw new NullPointerException();
+        }
+        return x -> condition.test (x) ? ifTrue.apply (x) : ifFalse.apply (x);
 
     }
 }
