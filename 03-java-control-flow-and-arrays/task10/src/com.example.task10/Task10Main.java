9c2780facc60dbd842b024895e5c04a6b2c6384a
Mohamed <mohamedtarek21763@gmail.com>
2020-12-26 22:33:00 +0500

    Хассан Мохамед Тарек Хассан 3.10

--- 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
+++ 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,16 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+
+        int min = arr[0];
+        int result = 0;
+        for ( int i = 0; i < arr.length; i++ ) {
+            if (arr[i] <= min) {
+                min = arr[i];
+                result = i;
+            }
+        }
+        return result;
     }
 
 }
