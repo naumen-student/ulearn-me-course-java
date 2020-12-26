2536646884cf4a9db399fe09687444227b279388
Mohamed <mohamedtarek21763@gmail.com>
2020-12-27 02:52:54 +0500

    Хассан Мохамед Тарек Хассан 4.2

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,51 @@
 package com.example.task02;
 
-public class Task02Main {
-    public static void main(String[] args) {
+class TimeSpan{
+    private int seconds, minutes, hours;
 
+    public TimeSpan(int seconds, int minutes, int hours){
+        this.seconds = seconds;
+        this.minutes = minutes;
+        this.hours = hours;
+    }
+
+    public void setSeconds(int seconds){
+        this.seconds = seconds;
+    }
+
+    public void setMinutes(int minutes){
+        this.minutes = minutes;
+    }
+
+    public void setHours(int hours){
+        this.hours = hours;
+    }
+
+    public int getSeconds(){
+        return seconds;
+    }
+
+    public int getMinutes(){
+        return minutes;
+    }
+
+    public int getHours(){
+        return hours;
+    }
+
+    public void add(TimeSpan time){
+        seconds += time.seconds;
+        minutes += time.minutes;
+        hours += time.hours;
+    }
+
+    public void subtract(TimeSpan time){
+        seconds -= time.seconds;
+        minutes -= time.minutes;
+        hours -= time.hours;
+    }
+
+    public String toString() {
+        return String.format("", seconds, minutes, hours);
     }
 }
