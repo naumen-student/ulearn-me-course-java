0ed2f5905a54d16af6112e19a0af0c3b3c72f35c
PolinaUskova <polina.uskova1535@gmail.com>
2020-12-26 19:18:54 +0500

    Ускова Полина. Задача 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -14,7 +14,11 @@ public class Task08Main {
 
     static long mult(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        if (arr.length == 0)
+            return 0;
+        long result = 1L;
+        for (int number : arr) result *= number;
+        return result;
     }
 
 }
