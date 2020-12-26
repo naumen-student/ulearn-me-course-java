5334f1e28e825f8ace46103e2855331cb7cb688b
ihatememore <a.i.iakovleva@urfu.me>
2020-12-26 21:26:13 +0500

    Яковлева Анастасия. Задание 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -4,18 +4,19 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        boolean flag = true;
+        for (int i = 0; i < 5; i++) {
+            if ((x%10)%2 != 0 )
+                flag = false;
+            x /= 10;
+        }
+        return flag ? "TRUE" : "FALSE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
     }
 
 }
