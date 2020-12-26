92f445465dc920e9dd34e65743e7556d084eced8
PolinaUskova <polina.uskova1535@gmail.com>
2020-12-26 21:46:27 +0500

    Ускова Полина. Задача 6.6

--- 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
+++ 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
@@ -1,16 +1,9 @@
 package com.example.task06;
 
 public class Task06Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        new Task06Main().printMethodName();
-         */
-    }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        StackTraceElement element = new Exception().getStackTrace()[1];
+        System.out.print(element.getMethodName());
     }
-
 }
