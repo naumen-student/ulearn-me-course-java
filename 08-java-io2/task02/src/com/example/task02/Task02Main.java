7a30d9244fbebb95258c41680a32494342266f5f
Сухраб <Sukhrab2001@mail.ru>
2020-12-26 23:54:36 +0500

    Курбанов Сухраб, задача 8 02

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -3,17 +3,13 @@ package com.example.task02;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;import java.util.List;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
@@ -21,6 +17,7 @@ public class Task02Main {
         Files.walk(rootDir)
                 .filter(Files::isRegularFile)
                 .forEach(pathArrayList::add);
+
         return pathArrayList;
     }
 }
