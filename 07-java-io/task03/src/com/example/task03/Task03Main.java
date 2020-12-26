2d8c15d5f12934f8f349e10c8b8b3acbea9324c3
fitsuli <danilyuk_2011@bk.ru>
2020-12-27 02:43:23 +0500

    Данилюк Дмитрий. Задача 7.3

--- 07-java-io/task03/src/com/example/task03/Task03Main.java
+++ 07-java-io/task03/src/com/example/task03/Task03Main.java
@@ -3,6 +3,7 @@ package com.example.task03;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
@@ -14,8 +15,12 @@ public class Task03Main {
         */
     }
 
-    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+    public static String readAsString(InputStream inputStream, Charset charset) {
+        if (inputStream == null || charset == null)
+            throw new IllegalArgumentException();
+        StringBuilder stringBuilder = new StringBuilder();
+        Scanner scanner = new Scanner(inputStream, charset.name());
+        while (scanner.hasNext()) stringBuilder.append(scanner.next());
+        return stringBuilder.toString();
     }
 }
