c5f5295e74590846096676baae0643a0a5288be5
ihatememore <a.i.iakovleva@urfu.me>
2020-12-26 23:43:32 +0500

    Яковлева Анастасия. Задание 5.3

--- 05-java-oop-principles/task03/src/com/example/task03/Seconds.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Seconds.java
@@ -6,16 +6,13 @@ package com.example.task03;
 public class Seconds implements TimeUnit {
 
     private final long amount;
-
     public Seconds(long amount) {
         this.amount = amount;
     }
-
     @Override
     public long toMillis() {
         return amount * 1000;
     }
-
     @Override
     public long toSeconds() {
         return amount;
@@ -23,6 +20,11 @@ public class Seconds implements TimeUnit {
 
     @Override
     public long toMinutes() {
-        return Math.round(amount / 60);
+        return Math.round((double) amount / 60);
+    }
+
+    @Override
+    public long getHours() {
+        return Math.round((double) amount / 60 / 60);
     }
 }
