f128aebf3ae5fb9dc8ca172f37a9d16ce858adcf
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-26 09:38:46 +0500

    Колесников Александр задача 2.6

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -1,21 +1,23 @@
 package com.example.task05;
-
 public class Task05 {
-
     public static String solution(int x) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "FALSE";
+        int number = x;
+        while (number != 0) {
+            if ((number % 10) % 2 != 0)
+                return "FALSE";
+            number = number / 10;
+        }
+        return "TRUE";
     }
-
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution(1234);
+        String result = solution(20);
         System.out.println(result);
-        */
+
     }
 
 }
--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -1,12 +1,23 @@
 package com.example.task06;
-
 public class Task06 {
 
     public static int solution(int x, int y) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int z = x + y;
+        z = Math.abs(z);
+        int count = 0;
 
-        return 0;
+
+        while (z > 0)
+        {
+            z = z / 10;
+            count++;
+        }
+
+        if (count == 0)
+            return 1;
+        return count;
     }
 
     public static void main(String[] args) {
@@ -14,8 +25,8 @@ public class Task06 {
         // например вот так:
         /*
         int result = solution(12, 34);
+        int result = solution(777, -777);
         System.out.println(result);
         */
     }
-
 }
