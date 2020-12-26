1a899fbad2e0cc2574a1d0495c492c5552798435
Task-1.1 <apzigaziga@gmail.com>
2020-12-27 04:09:13 +0500

    Задание 3.3

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -1,15 +1,44 @@
 package com.example.task03;
 
-public class Task03Main {
-    public static void main(String[] args) {
+public class Task03Main
+{
+    public static void main(String[] args)
+    {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
         /*
+public class Task03Main
+{
+    public static void main(String[] args)
+    {
         System.out.println(getNameOfWeekDays(7));
          */
     }
 
-    static String getNameOfWeekDays(int weekDaysNumber) {
+    static String getNameOfWeekDays(int weekDaysNumber)
+    {
         return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        static String getNameOfWeekDays ( int weekDaysNumber)
+        {
+            switch (weekDaysNumber)
+            {
+                case 1:
+                    return "понедельник";
+                case 2:
+                    return "вторник";
+                case 3:
+                    return "среда";
+                case 4:
+                    return "четверг";
+                case 5:
+                    return "пятница";
+                case 6:
+                    return "суббота";
+                case 7:
+                    return "воскресенье";
+                default:
+                    return "такого дня недели не существует";
+            }
+        }
     }
 }
