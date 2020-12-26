dfd69645d43c9aace21777da5410f0e1c19365f0
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-26 10:16:19 +0500

    Колесников Александр 2.11

--- 02-java-types/task11/src/com/example/task11/Task11.java
+++ 02-java-types/task11/src/com/example/task11/Task11.java
@@ -7,21 +7,13 @@ public class Task11 {
         // TODO исправьте функцию, чтобы избежать накопления ошибки
 
         // Считаем проценты за год
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
+        return (float) (Math.pow((percent + 1.0), 12) * sum);
     }
 
     public static void main(String[] args) {
-
         float sum = 500; // 500 руб. на счете
         float percent = 0.00000001f; // 0.000001% ежемесячно
-
         sum = benefit(sum, percent);
-
         System.out.println("Сумма на счете через год: " + sum);
-
     }
-
 }
