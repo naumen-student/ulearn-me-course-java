43f8c3c00647d76896152a38efead252796997c9
tenfrs <podaand@icloud.com>
2020-12-26 17:18:59 +0500

    Задание 3.02 Поспелова Дарья

--- 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
+++ 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
@@ -10,13 +10,13 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        if (monthNumber < 3 || monthNumber == 12)
-            return "зима";
-        else if (monthNumber < 6)
-            return "осень";
-        else if (monthNumber < 9)
+
+        if (monthNumber > 2 && monthNumber < 6)
+            return "весна";
+        else if (monthNumber > 5 && monthNumber < 9)
             return "лето";
-        else
+        else if (monthNumber > 8 && monthNumber < 12)
             return "осень";
+        return "зима";
     }
 }
