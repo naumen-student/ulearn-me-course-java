3671842ed0ca90b48035dc7442285dbe06cec025
s17v3rr <oleg.gubanov@urfu.me>
2020-12-26 16:48:43 +0500

    Губанов Олег. Задание 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,25 @@
 package com.example.task02;
 
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+        ArrayList<Path> files = new ArrayList<>();
+        SimpleFileVisitor<Path> fileVisitor = new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) {
+                files.add(file);
+                return FileVisitResult.CONTINUE;
+            }
+        };
+        Files.walkFileTree(rootDir, fileVisitor);
+        return files;
     }
 }
