a78168cc7c18caffbf517ec623a0f5d67dc24da5
Арман <Arman.Koshaev@urfu.me>
2020-12-26 17:50:23 +0500

    Кошаев Арман. Задание 6.6

---
+++ 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
@@ -0,0 +1,17 @@
+package com.example.task06;
+
+public class Task06Main {
+    public static void main(String[] args) {
+        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
+        new Task06Main().printMethodName();
+         */
+    }
+
+    void printMethodName() {
+        StackTraceElement element = new Exception().getStackTrace()[1];
+        System.out.print(element.getMethodName());
+    }
+
+}
