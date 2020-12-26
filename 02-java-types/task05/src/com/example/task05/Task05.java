b1205618687462273e85efed62b7f7fb21bb86f0
valer <valeria.ssergeevna@gmail.com>
2020-12-27 00:36:38 +0500

    Валерия Семихатка 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -4,9 +4,16 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        boolean flag = true;
+        while (x > 0) {
+            int n = x % 10;
+            if (n % 2 != 0) {
+                flag = false;
+                break;
+        }
+        x /= 10;
+    }
+        return flag ? "TRUE" : "FALSE";
     }
 
     public static void main(String[] args) {
