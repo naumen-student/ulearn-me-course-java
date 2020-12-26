fd9fe47c66d6dc31c17cf54caa73e836e5f29979
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-26 18:33:07 +0500

    Колесников Александр 6.3

--- 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
+++ 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
@@ -1,5 +1,4 @@
 package com.example.task03;
-
 public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -10,7 +9,9 @@ public class Task03Main {
     }
 
     //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
 
+
+        public static void throwCheckedException() throws Exception {
+            throw new Exception();
+        }
     }
-}
