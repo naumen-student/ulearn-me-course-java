fe911d22558659d3f3ee4ecd422201d8a64db4be
Арман <Arman.Koshaev@urfu.me>
2020-12-26 17:32:51 +0500

    Кошаев Арман. Задание 6.3

---
+++ 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
@@ -0,0 +1,16 @@
+package com.example.task03;
+
+public class Task03Main {
+    public static void main(String[] args) {
+        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
+        throwCheckedException();
+         */
+    }
+
+    //todo напишите здесь свою корректную реализацию задания
+    public static void throwCheckedException() throws Exception {
+        throw new Exception("Checked exception");
+    }
+}
