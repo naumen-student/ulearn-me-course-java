949e89df0fccd68d658192769d2536b5720dffa6
Arseny Smirnov <Arseny.Smirnov@urfu.me>
2020-12-26 20:20:13 +0500

    Смирнов Арсений 10.2

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -16,7 +16,13 @@ public class Task02Main {
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
+        if (n < 1 || n > 16) {
+            throw new IllegalArgumentException();
+        }
+
+        return IntStream.iterate(0, x -> x + 1)
+                .map(x -> x % (int) Math.pow(2, n))
+                .map(x -> x^(x>>1));
 
     }
 
