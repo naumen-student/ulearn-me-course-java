197b012bd106ebbe6e3398cf502db39d9757f798
Арман <Arman.Koshaev@urfu.me>
2020-12-26 19:08:17 +0500

    Кошаев Арман. Задание 8.2

---
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -0,0 +1,23 @@
+package com.example.task02;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task02Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
+    }
+
+    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
+        ArrayList<Path> pathArrayList = new ArrayList<>();
+        Files.walk(rootDir)
+                .filter(Files::isRegularFile)
+                .forEach(pathArrayList::add);
+
+        return pathArrayList;
+    }
+}
