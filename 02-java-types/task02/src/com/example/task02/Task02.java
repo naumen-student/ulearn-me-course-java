5b3e8a68c7c62ae991e82908e24f2823f3887091
valer <valeria.ssergeevna@gmail.com>
2020-12-27 00:24:19 +0500

    Валерия Семихатка 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -6,7 +6,15 @@ public class Task02 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "";
+        long inp = Long.parseLong(input);
+        if (inp >= Byte.MIN_VALUE && inp <= Byte.MAX_VALUE)
+            return "byte";
+        else if (inp <= Short.MAX_VALUE && inp >= Short.MIN_VALUE)
+            return "short";
+        else if (inp <= Integer.MAX_VALUE && inp >= Integer.MIN_VALUE)
+            return "int";
+        else
+            return "long";
     }
 
     public static void main(String[] args) {
@@ -15,7 +23,7 @@ public class Task02 {
         /*
         String result = solution("12345");
         System.out.println(result);
-         */
+        */
     }
 
 }
