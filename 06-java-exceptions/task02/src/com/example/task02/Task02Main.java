584ecb019c7b965a8ed05aaea88fdf911ef447bc
ihatememore <a.i.iakovleva@urfu.me>
2020-12-26 23:53:25 +0500

    Яковлева Анастасия. Задание 6.2

--- 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
+++ 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
@@ -1,16 +1,22 @@
 package com.example.task02;
-
 public class Task02Main {
 
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         System.out.println(getSeason(-5));
-         */
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber < 1 || monthNumber > 12)
+            throw new IllegalArgumentException(String.format("monthNumber %d is invalid, month number should be between 1..12",
+                    monthNumber));
+        else if (monthNumber >= 3 && monthNumber <= 5) {
+            return "весна";
+        } else if (monthNumber >= 6 && monthNumber <= 8) {
+            return "лето";
+        } else if (monthNumber >= 9 && monthNumber <= 11) {
+            return "осень";
+        } else {
+            return "зима";
+        }
     }
 }
