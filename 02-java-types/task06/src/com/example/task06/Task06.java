3742cbb926736516957a945bbfa586054ddb125e
tenfrs <podaand@icloud.com>
2020-12-26 14:23:03 +0500

    Задание 2.6 Поспелова Дарья

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -4,18 +4,17 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        String num = Integer.toString(x + y);
+        return x + y < 0 ? num.length() - 1 : num.length();
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
