e29f7a6e695704d507b564ddf07fc1195b09f6ad
amooq <amooq786@gmail.com>
2020-12-27 04:38:54 +0500

    Коновалов Александр. Задание 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -4,18 +4,16 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while (x != 0){
+            int digit = x % 10;
+            if (digit % 2 != 0)
+                return "FALSE";
+            x /= 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
-        System.out.println(result);
-        */
     }
 
 }
