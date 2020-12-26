dc4f034d3a1a5342b8e8e0271580caa6d3a864ec
AARegina <regina.agafonova.2001@gmail.com>
2020-12-26 19:25:12 +0500

    Агафонова Регина 09_task03

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -1,24 +1,39 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
 
+import java.nio.charset.Charset;
+import java.io.*;
+import java.util.*;
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
-        for (Set<String> anagram : anagrams) {
-            System.out.println(anagram);
-        }
+        List<Set<String>> anagrams = findAnagrams(new FileInputStream("09-java-generics-collections/task03/resources/singular.txt"), Charset.forName("windows-1251"));
+        System.out.println(anagrams);
 
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        ArrayList<Set<String>> list = new ArrayList<> ();
+        Scanner scanner = new Scanner(inputStream);
+        TreeMap<String, TreeSet<String>> strings= new TreeMap<> ();
+        while (scanner.hasNext()){
+            String word = scanner.nextLine().toLowerCase();
+            if (word.matches("[а-я]+") && word.length() > 3){
+                char[] chars = word.toCharArray();
+                Arrays.sort(chars);
+                String sortStr = new String(chars);
+                if (!strings.containsKey(sortStr))
+                    strings.put (sortStr, new TreeSet<String> () {
+                        {
+                            add (word);
+                        }
+                    });
+                else strings.get(sortStr).add(word);
+            }
+        }
+        for(TreeSet<String> combos : strings.values()) if (combos.size () > 1) list.add (combos);
+        return list;
     }
+
 }
