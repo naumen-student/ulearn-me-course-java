c40d184b257c3db5055fe2eb3ea2da6458c770e6
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-26 18:18:16 +0500

    Колесников Александр 5.3

--- 05-java-oop-principles/task03/src/com/example/task03/Milliseconds.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Milliseconds.java
@@ -1,28 +1,25 @@
 package com.example.task03;
-
 /**
  * Интервал в миллисекундах
  */
 public class Milliseconds implements TimeUnit {
-
     private final long amount;
-
     public Milliseconds(long amount) {
         this.amount = amount;
     }
-
     @Override
     public long toMillis() {
         return amount;
     }
 
     @Override
-    public long toSeconds() {
-        return amount / 1000;
-    }
+
+    public long toSeconds() { return Math.round((float) toMillis() / 1000); }
 
     @Override
-    public long toMinutes() {
-        return amount / 1000 * 60;
-    }
+
+    public long toMinutes() { return Math.round((float) amount / 1000 / 60); }
+
+    @Override
+    public long getHours(){ return Math.round((float) toMinutes() / 60); }
 }
--- 05-java-oop-principles/task03/src/com/example/task03/Minutes.java
+++ 05-java-oop-principles/task03/src/com/example/task03/Minutes.java
@@ -1,27 +1,22 @@
 package com.example.task03;
 
 public class Minutes implements TimeUnit {
+    private final long amount;
 
-    public Minutes(long amount) {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
-    }
 
-    @Override
-    public long toMillis() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
-    }
+    public Minutes(long amount) { this.amount = amount; }
 
     @Override
-    public long toSeconds() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
-    }
+
+    public long toMillis() { return toSeconds() * 1000; }
 
     @Override
-    public long toMinutes() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
-    }
+    public long toSeconds() { return toMinutes() * 60; }
+
+    @Override
+
+    public long toMinutes() { return amount; }
+
+    @Override
+    public long getHours(){ return Math.round((float) amount / 60); }
 }
