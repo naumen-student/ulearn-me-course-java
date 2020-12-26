fe1bb1ae9c585b97ee7ab9d75a0173c8b485e688
Chocolatelis <elizavetab123@mail.ru>
2020-12-27 04:07:05 +0500

    Быкова Елизавета Задание 10.2

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -16,7 +16,12 @@ public class Task02Main {
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
+        if (n < 1 || n > 16)
+            throw new IllegalArgumentException();
+        int max = (int) Math.pow(2, n);
+
+        return IntStream.iterate(0, i -> i + 1)
+                .map(x -> (x % max) ^ ((x % max) >> 1));
 
     }
 
