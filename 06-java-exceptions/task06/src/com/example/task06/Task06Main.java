550a50b95dc01a0411444c0c94a5084412d67db2
LilStupidPig20 <mistersaxar@gmail.com>
2020-12-26 21:45:21 +0500

    Волгарев Артем. Задание 6.6

--- 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
+++ 06-java-exceptions/task06/src/com/example/task06/Task06Main.java
@@ -2,15 +2,11 @@ package com.example.task06;
 
 public class Task06Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         new Task06Main().printMethodName();
-         */
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
     }
 
 }
