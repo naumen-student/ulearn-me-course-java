72699fec332e8b422a3f117fb54983ea6b521fd8
Mohamed <mohamedtarek21763@gmail.com>
2020-12-26 15:36:11 +0500

    Хассан Мохамед Тарек Хассан 2.9

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Tests.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Tests.java
@@ -1,19 +1,28 @@
 package com.example.task03;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-public class Tests {
-
-    @Test
-    public void testGetNameOfWeekDays() {
-        Assert.assertEquals("1 - понедельник", "понедельник", Task03Main.getNameOfWeekDays(1));
-        Assert.assertEquals("2 - вторник", "вторник", Task03Main.getNameOfWeekDays(2));
-        Assert.assertEquals("3 - среда", "среда", Task03Main.getNameOfWeekDays(3));
-        Assert.assertEquals("4 - четверг", "четверг", Task03Main.getNameOfWeekDays(4));
-        Assert.assertEquals("5 - пятница", "пятница", Task03Main.getNameOfWeekDays(5));
-        Assert.assertEquals("6 - суббота", "суббота", Task03Main.getNameOfWeekDays(6));
-        Assert.assertEquals("7 - воскресенье", "воскресенье", Task03Main.getNameOfWeekDays(7));
+public class Task03Main {
+    public static void main(String[] args) {
+        System.out.println(getNameOfWeekDays(7));
     }
 
+    static String getNameOfWeekDays(int weekDaysNumber) {
+        switch (weekDaysNumber){
+            case (1):
+                return "понедельник";
+            case (2):
+                return "вторник";
+            case (3):
+                return "среда";
+            case (4):
+                return "четверг";
+            case(5):
+                return "пятница";
+            case(6):
+                return "суббота";
+            case(7):
+                return "воскресенье";
+            default:
+                return "такого дня недели не существует";
+        }
+    }
 }
