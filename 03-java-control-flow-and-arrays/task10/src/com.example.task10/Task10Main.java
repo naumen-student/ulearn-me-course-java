e56d65c5fcabd946e91b2336a19c48647813296d
tenfrs <podaand@icloud.com>
2020-12-26 18:51:10 +0500

    Задание 3.10 Поспелова Дарья

--- 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
+++ 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
@@ -4,15 +4,16 @@ public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
-        System.out.println(numMin(arr));
-         */
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int result = 0, min = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] <= min){
+                result = i;
+                min = arr[i];
+            }
+        }
+        return result;
     }
-
 }
