6d89430b72dc90886076ea82730a0af074f2b2ac
PolinaUskova <polina.uskova1535@gmail.com>
2020-12-26 22:54:37 +0500

    Ускова Полина. Задача 8.1

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,7 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -15,6 +16,16 @@ public class Task01Main {
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
         // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+        Scanner scanner = new Scanner(processBuilder.start().getInputStream());
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.contains("format.tags.title=")) {
+                return line.split("\"")[1];
+            }
+        }
+
+        return null;
     }
 }
