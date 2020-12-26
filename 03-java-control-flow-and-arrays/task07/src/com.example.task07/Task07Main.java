c568957cc494f080b1f19933058110b80fd6f66b
ilya_res <top-toptt@yandex.ru>
2020-12-26 19:03:02 +0500

    Реснянский Илья. 3-07

--- 03-java-control-flow-and-arrays/task07/src/com.example.task07/Task07Main.java
+++ 03-java-control-flow-and-arrays/task07/src/com.example.task07/Task07Main.java
@@ -1,4 +1,5 @@
 package com.example.task07;
+import java.util.Arrays;
 
 public class Task07Main {
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class Task07Main {
 
     static int sum(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return Arrays.stream(arr).sum();
     }
 
 }
