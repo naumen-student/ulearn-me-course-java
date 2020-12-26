d8bcfacfe72deb3dbc8c356968dffc6b2ef4a55d
Task-1.1 <apzigaziga@gmail.com>
2020-12-27 03:31:08 +0500

    Задача 2.7

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -1,21 +1,10 @@
-package com.example.task07;
-
-public class Task07 {
-
-    public static int solution(int n, int m, int k) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+public static int solution(int n, int m, int k)
+        {
         return 0;
-    }
-
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+        return (n / k) * (m / k);
+        }
+public static void main(String[] args)
+        {
         int result = solution(10, 20, 5);
         System.out.println(result);
-        */
-    }
-
-}
+        }
