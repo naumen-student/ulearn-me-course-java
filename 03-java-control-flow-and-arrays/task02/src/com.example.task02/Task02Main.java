3a445808b06906000b5ed9c11f41fef158f1c038
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-26 14:13:59 +0500

    Колесников Александр 3.2

--- 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
+++ 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
@@ -10,6 +10,19 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber >= 1 && monthNumber <= 2) {
+            return "зима";
+        }
+        else if (monthNumber >= 3 && monthNumber <= 5) {
+            return "весна";
+        }
+        else if (monthNumber >= 6 && monthNumber <= 8) {
+            return "лето";
+        }
+        else if (monthNumber >= 9 && monthNumber <= 11) {
+            return "осень";
+        }
+        else return "зима";
     }
 }
