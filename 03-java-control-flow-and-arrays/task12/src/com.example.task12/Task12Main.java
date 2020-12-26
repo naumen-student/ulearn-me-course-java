8601ec6f78de214a3e01ec117fd588cf9faac343
Mohamed <mohamedtarek21763@gmail.com>
2020-12-27 02:31:34 +0500

    Хассан Мохамед Тарек Хассан 3.12

--- 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
+++ 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
@@ -12,7 +12,22 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
 
+        if (arr == null || arr.length == 0)
+            return;
+
+        for (int i = 0; i < arr.length; i++) {
+            int min = Integer.MAX_VALUE;
+            int a = 0;
+
+            for (int j = i; j < arr.length; j++) {
+                if (min > arr[j]) min = arr[j];
+                if (arr[j] == min) a = j;
+            }
+
+            int b = arr[i];
+            arr[i] = min;
+            arr[a] = b;
+        }
+    }
 }
