47b07815f0e65cee57d3714015d6db034d87934d
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-26 13:18:57 +0500

    Колесников Александр 2.3

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -1,12 +1,12 @@
 package com.example.task03;
-
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return 0;
+
+        return centimetre / 100;
     }
 
     public static void main(String[] args) {
