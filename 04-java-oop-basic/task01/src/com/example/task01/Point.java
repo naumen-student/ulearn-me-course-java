ef8ed45eb527b899cee5655cc5c9b981a77d0db0
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-26 17:46:15 +0500

    Колесников Александр 4.1

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -1,11 +1,34 @@
 package com.example.task01;
-
 /**
  * Класс точки на плоскости
  */
 public class Point {
+
+    public Point(){}
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
     int x;
     int y;
+
+    public void flip(){
+        int temp = this.x;
+        this.x = -this.y;
+        this.y = -temp;
+    }
+
+    public double distance(Point point){
+        double dx = Math.pow (point.x - this.x, 2);
+        double dy = Math.pow (point.y - this.y, 2);
+        return Math.sqrt (dx + dy);
+    }
+
+    public String toString(){
+        return String.format ("{0}, {1}", this.x, this.y);
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
