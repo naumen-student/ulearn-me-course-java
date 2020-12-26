c5cb724f27c6e3e27ebd80b90838e31d3e484d4d
zukenrol <paltvlad@mail.ru>
2020-12-26 21:11:54 +0500

    Палтусов Владислав. 3-6

--- 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
+++ 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
@@ -1,17 +1,16 @@
 package com.example.task06;
 
+import java.util.Arrays;
+
 public class Task06Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         System.out.println(getMax(1, 2, 3, 4));
-         */
+
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return Arrays.stream(new int[] {a, b, c, d}).max().getAsInt();
     }
 
 }
