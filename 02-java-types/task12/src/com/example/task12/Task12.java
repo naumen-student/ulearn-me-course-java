c92c1272e54c5504ef03b65b25838051da407402
tenfrs <podaand@icloud.com>
2020-12-26 15:24:38 +0500

    Задание 2.12 Поспелова Дарья

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -6,18 +6,11 @@ public class Task12 {
 
     public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
 
-        // TODO раскомментируйте и исправьте код
 
-        // Считаем проценты за год
-
-        /*
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+            sum = sum.add(sum.multiply(percent));
         }
-        return sum;
-        */
-
-        return BigDecimal.ZERO;
+        return sum.setScale(9, BigDecimal.ROUND_HALF_UP);
     }
 
     public static void main(String[] args) {
