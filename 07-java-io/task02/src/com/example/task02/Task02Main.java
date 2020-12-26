92921417461d3077078bb5f71d55bcfe0ecfc423
ihatememore <a.i.iakovleva@urfu.me>
2020-12-27 00:18:41 +0500

    Яковлева Анастасия. Задание 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,16 @@
 package com.example.task02;
-
 import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int previous = System.in.read();
+        int current;
+        while (previous >= 0) {
+            current = System.in.read();
+            if (current != 10  ||  previous != 13)
+                System.out.write(previous);
+            previous = current;
+        }
+        System.out.flush();
     }
 }
