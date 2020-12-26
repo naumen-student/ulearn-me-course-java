514e4b01eaeae97c66b51bdf9fbcc1eb6e0174b8
Dinar <fridget23@gmail.com>
2020-12-26 16:12:21 +0500

    Бикмухаметов Динар. Задание 7.1

--- 07-java-io/task01/src/com/example/task01/Task01Main.java
+++ 07-java-io/task01/src/com/example/task01/Task01Main.java
@@ -2,24 +2,26 @@ package com.example.task01;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ByteArrayInputStream;
-
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        @@ -15,7 +16,14 @@ public static void main(String[] args) throws IOException {
-        }
-
-        public static int checkSumOfStream(InputStream inputStream) throws IOException {
-            // your implementation here
-            return 0;
-            if (inputStream == null)
-                throw new IllegalArgumentException();
-            int sum = 0;
-            int read = inputStream.read();
-            while (read > -1) {
-                sum = Integer.rotateLeft(sum, 1)^read;
-                read = inputStream.read();
-            }
-            return sum;
-        }
+        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
+        System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
+        */
     }
+
+    public static int checkSumOfStream(InputStream inputStream) throws IOException {
+        // your implementation here
+        return 0;
+        if (inputStream == null)
+            throw new IllegalArgumentException();
+        int currentByte = inputStream.read();
+        int count = 0;
+        while (currentByte != -1) {
+            count = Integer.rotateLeft(count, 1) ^ currentByte;
+            currentByte = inputStream.read();
+        }
+        return count;
+    }
+}
