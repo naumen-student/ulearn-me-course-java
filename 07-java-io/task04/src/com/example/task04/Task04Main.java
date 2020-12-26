e61653119e83308e8e9fd21ca00857dbd7593c51
LilStupidPig20 <mistersaxar@gmail.com>
2020-12-26 22:14:22 +0500

    Волгарев Артем. Задание 7.4

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,20 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        double sum = 0;
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble())
+                sum += scanner.nextDouble();
+            else {
+                scanner.next();
+            }
+        }
+        System.out.format(Locale.CANADA," %1.6f", sum);
     }
 }
