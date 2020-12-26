28e56a12cde7e9f30c53217fb2e4f110dbba49a2
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-27 01:25:02 +0500

    Колесников Александр 8.1

--- 08-java-io2/task01/src/com/example/task01/Task01Main.java
+++ 08-java-io2/task01/src/com/example/task01/Task01Main.java
@@ -2,12 +2,12 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-
         /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
         */
@@ -15,6 +15,16 @@ public class Task01Main {
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
         // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String res = "";
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+        Scanner scanner = new Scanner(processBuilder.start().getInputStream());
+        while (scanner.hasNextLine()) {
+            String currentString = scanner.nextLine();
+            if (currentString.startsWith("format.tags.title")) {
+                return currentString.split("\"")[1];
+            }
+        }
+        return res;
     }
 }
