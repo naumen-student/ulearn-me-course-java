4149f593487964bfc65ce4303f58fa3ea4405f96
Mohamed <mohamedtarek21763@gmail.com>
2020-12-27 03:59:33 +0500

    Хассан Мохамед Тарек Хассан 4.3

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,46 @@
 package com.example.task03;
+import java.util.concurrent.Callable;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumber firstNumber = new ComplexNumber(3,4);
+        ComplexNumber secondNumber = new ComplexNumber(1,2);
+        System.out.println(firstNumber.Add(secondNumber));
+        System.out.println(firstNumber.Multiply(secondNumber));
+    }
+}
 
+class ComplexNumber{
+    private double realPartNumber;
+    private double imagPartNumber;
+
+    public ComplexNumber(double realPartNumber, double imagPartNumber){
+        this. realPartNumber = realPartNumber;
+        this.imagPartNumber = imagPartNumber;
+    }
+
+    public double getRealPartNumber() {
+        return realPartNumber;
+    }
+
+    public double getImagPartNumber() {
+        return imagPartNumber;
+    }
+
+    public ComplexNumber Add(ComplexNumber number){
+        return new ComplexNumber(realPartNumber + number.realPartNumber,
+                imagPartNumber + number.imagPartNumber);
+    }
+
+    public ComplexNumber Multiply(ComplexNumber number){
+        return new ComplexNumber(realPartNumber * number.realPartNumber
+                - imagPartNumber * number.imagPartNumber,
+                realPartNumber * number.imagPartNumber
+                        + imagPartNumber * number.realPartNumber);
+
+    }
+
+    public String toString(){
+        return realPartNumber + " + " + imagPartNumber + "i";
     }
 }
