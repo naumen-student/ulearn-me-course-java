337f26f848b400504570a3017a2f0b48834ade7d
Mohamed <mohamedtarek21763@gmail.com>
2020-12-27 04:55:24 +0500

    Хассан Мохамед Тарек Хассан 6.1

--- 05-java-oop-principles/task02/src/com/example/task02/Task02Main.java
+++ 05-java-oop-principles/task02/src/com/example/task02/Task02Main.java
@@ -1,21 +1,23 @@
 package com.example.task02;
 
-public class Task02Main {
+public class DiscountBill extends Bill{
 
-    private static final Item ITEM1 = new Item("Товар 1", 10);
-    private static final Item ITEM2 = new Item("Товар 2", 20);
-    private static final Item ITEM3 = new Item("Товар 3", 30);
-    private static final Item ITEM4 = new Item("Товар 4", 40);
-    private static final Item ITEM5 = new Item("Товар 5", 50);
-    private static final Item ITEM6 = new Item("Товар 6", 60);
+    private final int discount;
 
-    public static void main(String[] args) {
-        Bill bill = new Bill();
-        bill.add(ITEM1, 10);
-        bill.add(ITEM3, 3);
-        bill.add(ITEM6, 1);
-        System.out.println(bill);
-        bill.add(ITEM3, 3);
-        System.out.println(bill);
+    public DiscountBill(int discount){
+        this.discount = discount;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    @Override
+    public long getPrice() {
+        return super.getPrice() - super.getPrice() * discount / 100;
+    }
+
+    public long getAbsoluteDiscount(){
+        return super.getPrice() - getPrice();
     }
 }
