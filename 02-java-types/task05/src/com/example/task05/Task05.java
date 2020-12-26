3cc84530dadcbf0e6d39842bc5ba160d07c332cc
PolinaUskova <polina.uskova1535@gmail.com>
2020-12-26 18:10:43 +0500

    Ускова Полина. Задача 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -5,8 +5,12 @@ public class Task05 {
     public static String solution(int x) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while (x != 0){
+            if ((x % 10) % 2 != 0)
+                return "FALSE";
+            x/=10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
