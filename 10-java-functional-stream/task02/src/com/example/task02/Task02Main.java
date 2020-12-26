e8745735a8f637289d191ee1e5ec1677e4610587
AARegina <regina.agafonova.2001@gmail.com>
2020-12-26 19:43:20 +0500

    Агафонова Регина 10_task02

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -5,19 +5,17 @@ import java.util.stream.IntStream;
 public class Task02Main {
 
     public static void main(String[] args) {
-
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
-
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
-
+        if ( n < 1 || n > 16) throw new IllegalArgumentException();
+        return IntStream
+                .iterate(0, x -> x + 1)
+                .map(x -> (int) (x % (Math.pow(2, n))))
+                .map(x -> x ^ (x >> 1));
     }
 
 }
