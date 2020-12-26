f481823fac1dd99d91c689b0598c9b82fe4d9436
Task-1.1 <apzigaziga@gmail.com>
2020-12-27 03:32:49 +0500

    Задача 2.7

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -1,10 +1,10 @@
 public static int solution(int n, int m, int k)
         {
-        return 0;
-        return (n / k) * (m / k);
+         return 0;
+         return (n / k) * (m / k);
         }
 public static void main(String[] args)
         {
-        int result = solution(10, 20, 5);
-        System.out.println(result);
+         int result = solution(10, 20, 5);
+         System.out.println(result);
         }
