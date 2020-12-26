c37d7957e63114ff0ff432b788087dc223b70618
zukenrol <paltvlad@mail.ru>
2020-12-26 20:45:24 +0500

    Палтусов Владислав. 2-11

--- 02-java-types/task11/src/com/example/task11/Task11.java
+++ 02-java-types/task11/src/com/example/task11/Task11.java
@@ -1,16 +1,8 @@
 package com.example.task11;
-
 public class Task11 {
 
     public static float benefit(float sum, float percent) {
-
-        // TODO исправьте функцию, чтобы избежать накопления ошибки
-
-        // Считаем проценты за год
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
+        return (float) (sum * Math.pow((double) percent + 1, 12));
     }
 
     public static void main(String[] args) {
@@ -21,7 +13,5 @@ public class Task11 {
         sum = benefit(sum, percent);
 
         System.out.println("Сумма на счете через год: " + sum);
-
     }
-
 }
