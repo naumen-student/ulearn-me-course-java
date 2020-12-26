63e2f31120b6321d3aed0ad33aa152f885bd61dd
Арман <Arman.Koshaev@urfu.me>
2020-12-26 14:25:02 +0500

    Кошаев Арман. Задание 4.5

---
+++ 04-java-oop-basic/task05/src/com/example/task05/Point.java
@@ -0,0 +1,51 @@
+package com.example.task05;
+
+/**
+ * Точка в двумерном пространстве
+ */
+public class Point {
+    private double x;
+    private double y;
+
+    /**
+     * Конструктор, инициализирующий координаты точки
+     *
+     * @param x координата по оси абсцисс
+     * @param y координата по оси ординат
+     */
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Возвращает координату точки по оси абсцисс
+     *
+     * @return координату точки по оси X
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * Возвращает координату точки по оси ординат
+     *
+     * @return координату точки по оси Y
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * Подсчитывает расстояние от текущей точки до точки, переданной в качестве параметра
+     *
+     * @param point вторая точка отрезка
+     * @return расстояние от текущей точки до переданной
+     */
+    public double getLength(Point point) {
+        double dx = point.x - x;
+        double dy = point.y - y;
+        return Math.sqrt(dx * dx + dy + dy);
+    }
+
+}
---
+++ 04-java-oop-basic/task05/src/com/example/task05/PolygonalLine.java
@@ -0,0 +1,55 @@
+package com.example.task05;
+
+import java.util.ArrayList;
+
+/**
+ * Ломаная линия
+ */
+public class PolygonalLine {
+    private final ArrayList<Point> points = new ArrayList<>();
+
+    /**
+     * Устанавливает точки ломаной линии
+     *
+     * @param points массив точек, которыми нужно проинициализировать ломаную линию
+     */
+    public void setPoints(Point[] points) {
+        for (Point point : points) {
+            addPoint(point);
+        }
+    }
+
+    /**
+     * Добавляет точку к ломаной линии
+     *
+     * @param point точка, которую нужно добавить к ломаной
+     */
+    public void addPoint(Point point) {
+        points.add(point);
+    }
+
+    /**
+     * Добавляет точку к ломаной линии
+     *
+     * @param x координата по оси абсцисс
+     * @param y координата по оси ординат
+     */
+    public void addPoint(double x, double y) {
+        Point p = new Point(x, y);
+        addPoint(p);
+    }
+
+    /**
+     * Возвращает длину ломаной линии
+     *
+     * @return длину ломаной линии
+     */
+    public double getLength() {
+        double l = 0;
+        for (int i = 1; i < points.size(); i++) {
+            l += points.get(i - 1).getLength(points.get(i));
+        }
+        return l;
+    }
+
+}
