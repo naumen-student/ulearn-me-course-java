b4e45136477293c8b878291c6aec09030f3d48b6
L0rd47 <sanya.kolesnikov.01@bk.ru>
2020-12-26 14:25:53 +0500

    Колесников Александр 3.4

--- 03-java-control-flow-and-arrays/task04/src/com.example.task04/Task04Main.java
+++ 03-java-control-flow-and-arrays/task04/src/com.example.task04/Task04Main.java
@@ -1,5 +1,4 @@
 package com.example.task04;
-
 public class Task04Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -11,6 +10,8 @@ public class Task04Main {
 
     static void from0to10000() {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        for(int i = 0; i < 10000; i++)
+            System.out.println(i);
     }
 
 }
