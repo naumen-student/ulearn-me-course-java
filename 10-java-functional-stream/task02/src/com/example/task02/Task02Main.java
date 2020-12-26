aeb3d66ece98b2848de126098c2df0d61fd72a14
ihatememore <a.i.iakovleva@urfu.me>
2020-12-27 00:34:18 +0500

    Яковлева Анастасия. Задание 10.2

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,23 @@
 package com.example.task02;
-
 import java.util.stream.IntStream;
-
 public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
+
 
     }
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
-
+        if (n < 1 || n > 16)
+            throw new IllegalArgumentException();
+        int max = (int) Math.pow(2, n);
+        return IntStream.iterate(0, i -> i + 1)
+                .map(x -> (x % max) ^ ((x % max) >> 1));
     }
 
 }
