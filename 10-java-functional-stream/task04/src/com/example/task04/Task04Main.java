47879631d85449c6cd8e6d68e23514348af9013b
Лялин Артём <maii122@mail.ru>
2020-12-27 18:04:22 +0500

    10_Task04 Done

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,37 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
+        InputStream in = System.in;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        Map<String, Integer> frequency = new HashMap<>();
+        reader.lines()
+                .map(String::toLowerCase)
+                .flatMap(x -> Stream.of(x.split("[,.\\-! ]")))
+                .filter(x -> !x.isEmpty())
+                .forEach(key -> {
+                    if (!frequency.containsKey(key))
+                        frequency.put(key,1);
+                    else
+                        frequency.put(key, frequency.get(key) + 1);
+                });
 
-        // your implementation here
-
+        frequency.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach(x -> System.out.print(x.getKey() + '\n'));
     }
 
 }
