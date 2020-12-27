db5b4525bf61ca49a74a0573d05b7dd711991624
theonethei <57193492+theonethei@users.noreply.github.com>
2020-12-27 14:28:07 +0400

    Update Task04Main.java

--- 07-java-io/task04/src/com/example/task04/Task04Main.java
+++ 07-java-io/task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,8 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +11,13 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        double sum = 0;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        while (scanner.hasNext())
+            if (scanner.hasNextDouble())
+                sum += scanner.nextDouble();
+            else
+                scanner.next();
+        System.out.format(Locale.US," %1.6f", sum);
     }
 }
