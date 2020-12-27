321e46bb1d6ee4a9c74236a36314b9a523424f0a
flint-afk <ivan.ivanenko55779@gmail.com>
2020-12-27 16:52:57 +0500

    Update Task02Main.java

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -6,18 +6,22 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
+
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
+
 
     }
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
+        if (n < 1 || n > 16)
+            throw new IllegalArgumentException();
+        return IntStream.iterate(0, i -> (i + 1) % ((int)Math.pow(2, n))).map(i -> i ^ (i >> 1));
 
     }
 
 }
+
+
