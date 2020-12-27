693e051f1181e2b7c01f2947a48a6e90d99e6ea9
Mohamed <mohamedtarek21763@gmail.com>
2020-12-27 05:02:03 +0500

    Хассан Мохамед Тарек Хассан 6.3

--- 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
+++ 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
@@ -1,16 +1,13 @@
 package com.example.task03;
 
+import java.io.IOException;
+
 public class Task03Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+    public static void main (String[]args) throws IOException {
         throwCheckedException();
-         */
     }
 
-    //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
-
+    public static void throwCheckedException () throws IOException {
+        throw new IOException("");
     }
 }
