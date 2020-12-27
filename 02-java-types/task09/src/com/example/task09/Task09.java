b367f3e807a76e154d5f3b054f9490a2bfa8fdcb
Polina <kiramik55@gmail.com>
2020-12-27 19:57:22 +0500

    02_task_19 new

--- 02-java-types/task09/src/com/example/task09/Task09.java
+++ 02-java-types/task09/src/com/example/task09/Task09.java
@@ -3,8 +3,6 @@ package com.example.task09;
 public class Task09 {
 
     public static double solution() {
-        // TODO Устраните ошибку вычисления выражения, не изменяя типы данных у переменных
-
         float a = 1.0f;
         float b = 3.0f;
         double x = (a / b - 1.0f / 3.0f) * 1.0e9;
@@ -13,10 +11,7 @@ public class Task09 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение
-        /*
         System.out.println(solution());
-        */
     }
 
 }
