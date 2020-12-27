1498c660d25729b82110f9cad135f66fe8881923
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 11:47:34 +0500

    Зотов Данила. Задание 2.3

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,19 +3,10 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-        long value = Long.parseLong(input);
-        String result = "long";
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-        if (value >= -128 && value <= 127)
-            result = "byte";
-        else if (value >= -32768 && value <= 32767)
-            result = "short";
-        else if (value >= -2147483648 && value <= 2147483647)
-            result = "int";
 
         return "";
-        return result;
     }
 
     public static void main(String[] args) {
