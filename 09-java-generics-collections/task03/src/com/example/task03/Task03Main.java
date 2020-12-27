59fbdf041ed0396972971ec2f666b3faf8bcac2b
tenfrs <podaand@icloud.com>
2020-12-27 15:14:04 +0500

    Задание 9.3 Поспелова Дарья

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -14,29 +14,28 @@ public class Task03Main {
 
         List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
         System.out.println(anagrams);
-        }
-
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        ArrayList<Set<String>> list = new ArrayList<> ();
+        ArrayList<Set<String>> list = new ArrayList<>();
         Scanner scanner = new Scanner(inputStream);
-        TreeMap<String, TreeSet<String>> strings= new TreeMap<> ();
-        while (scanner.hasNext()){
+        TreeMap<String, TreeSet<String>> strings = new TreeMap<>();
+        while (scanner.hasNext()) {
             String word = scanner.nextLine().toLowerCase();
-            if (word.matches("[а-я]+") && word.length() > 3){
+            if (word.matches("[а-я]+") && word.length() > 3) {
                 char[] chars = word.toCharArray();
                 Arrays.sort(chars);
                 String sortStr = new String(chars);
                 if (!strings.containsKey(sortStr))
-                    strings.put (sortStr, new TreeSet<String> () {
+                    strings.put(sortStr, new TreeSet<String>() {
                         {
-                            add (word);
+                            add(word);
                         }
                     });
                 else strings.get(sortStr).add(word);
             }
         }
-        for(TreeSet<String> combos : strings.values()) if (combos.size () > 1) list.add (combos);
+        for (TreeSet<String> combos : strings.values()) if (combos.size() > 1) list.add(combos);
         return list;
     }
+}
