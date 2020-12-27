3b1bceba93b2009e241a14e4028c3a703c3fde7c
nukit181 <nik_babin@inbox.ru>
2020-12-27 08:48:19 +0500

    Бабин Никита Задание 6.7

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -14,11 +14,13 @@ public class Task07Main {
     public String getExceptionType() {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+            processor.process();
+        } catch (RuntimeException e) {
+            return "unchecked";
         } catch (Exception e) {
-
+            return "checked";
         }
-        return null;
+        return "none";
     }
 
 }
