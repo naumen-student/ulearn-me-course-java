8cef3f295961ae3e09c5376d4385b85f0c7f0874
wizzardw1z <alex.lekontsev@mail.ru>
2020-12-27 22:32:44 +0300

    Леконцев Александр.Задание2.7

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -3,19 +3,12 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return (n / k) * (m / k);
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(10, 20, 5);
         System.out.println(result);
-        */
     }
 
 }
