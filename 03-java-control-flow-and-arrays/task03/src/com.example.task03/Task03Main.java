bbcd1c6122d689343fc99d261b11657074bb759d
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:22:58 +0500

    Зотов Данила. Задание 3.3

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -2,11 +2,10 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        System.out.println(getNameOfWeekDays(7));
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";
         if (weekDaysNumber < 1 || weekDaysNumber > 7)
             return "такого дня недели не существует";
         return new String[] {
