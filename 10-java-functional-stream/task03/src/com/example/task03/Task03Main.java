9a12faf374bc2b96feaa66043afb8bb1b5c58af8
Mohamed <mohamedtarek21763@gmail.com>
2020-12-27 21:45:02 +0500

    Хассан Мохамед Тарек Хассан 10.3

--- 10-java-functional-stream/task03/src/com/example/task03/Task03Main.java
+++ 10-java-functional-stream/task03/src/com/example/task03/Task03Main.java
@@ -7,21 +7,32 @@ import java.util.stream.Stream;
 public class Task03Main {
 
     public static void main(String[] args) {
-
         findMinMax(
                 Stream.of(2, 9, 5, 4, 8, 1, 3),
                 Integer::compareTo,
                 (min, max) ->
                         System.out.println("min: " + min + " / max: " + max)
         );
-
     }
 
     public static <T> void findMinMax(
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-
-        // your implementation here
+        Object[] max = {null};
+        Object[] min = {null};
+        final boolean[] flag = {true};
+        stream.forEach(w -> {
+            if (flag[0]) {
+                max[0] = w;
+                min[0] = w;
+                flag[0] = false;
+            } else if (order.compare(w, (T) max[0]) > 0) {
+                max[0] = w;
+            } else if (order.compare(w, (T) min[0]) < 0) {
+                min[0] = w;
+            }
+        });
+        minMaxConsumer.accept((T) min[0], (T) max[0]);
     }
 }
