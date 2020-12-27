5fea28e2570d20a87dc4f42f244f25da653d45a3
flint-afk <ivan.ivanenko55779@gmail.com>
2020-12-27 16:38:26 +0500

    Update Task03Main.java

--- 08-java-io2/task03/src/com/example/task03/Task03Main.java
+++ 08-java-io2/task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,9 @@
 package com.example.task03;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -15,7 +17,8 @@ public class Task03Main {
     }
 
     public static SampleData deserialize(InputStream inputStream) throws IOException, ClassNotFoundException {
-        // your implementation here
-        return null;
+        try (ObjectInputStream deserializationStream = new ObjectInputStream(inputStream)) {
+            return (SampleData) deserializationStream.readObject();
+        }
     }
 }
