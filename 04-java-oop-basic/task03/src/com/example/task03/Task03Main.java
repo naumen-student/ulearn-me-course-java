86f7a5bbcd3b28630a3d7d6cc4a7fd1c98fc650e
tenfrs <podaand@icloud.com>
2020-12-27 11:15:09 +0500

    Задание 4.3 Поспелова Дарья

--- 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
+++ 04-java-oop-basic/task03/src/com/example/task03/Task03Main.java
@@ -3,5 +3,9 @@ package com.example.task03;
 public class Task03Main {
     public static void main(String[] args) {
 
+        CNumber number1 = new CNumber(2.0, 8.6);
+        CNumber number2 = new CNumber(7.1, 4.3);
+        CNumber sum = number1.Sum(number2);
+        CNumber multiply = number1.Multiply(number2);
     }
 }
