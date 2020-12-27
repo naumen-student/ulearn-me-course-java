5f0664aa7a088a94f427b220e9e580efa15ae685
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:25:38 +0500

    Зотов Данила. Задание 3.6

--- 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
+++ 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
@@ -4,12 +4,10 @@ import java.util.Arrays;
 
 public class Task06Main {
     public static void main(String[] args) {
-
+        System.out.println(getMax(1, 2, 3, 4));
     }
 
     static int getMax(int a, int b, int c, int d) {
-        return 0;
         return Arrays.stream(new int[] {a, b, c, d}).max().getAsInt();
     }
-
 }
