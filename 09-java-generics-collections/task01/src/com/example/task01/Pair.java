2ad7fa6ab571b950ef33e4140785d793fdb3854e
Konstantin <super1414@list.ru>
2020-12-27 17:34:02 +0500

    Князев Константин. Задание 9.01

--- 09-java-generics-collections/task01/src/com/example/task01/Pair.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Pair.java
@@ -1,5 +1,36 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+public class Pair<T, G> {
+    private final T first;
+    private final G second;
+
+    private Pair(T first, G second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public static<T, G> Pair<T, G> of(T first, G second){
+        return new Pair<>(first, second);
+    }
+
+    public T getFirst(){
+        return first;
+    }
+
+    public G getSecond(){
+        return second;
+    }
+
+    public boolean equals(Pair<T,G> value){
+        return this.hashCode() == value.hashCode();
+    }
+
+    public int hashCode(){
+        return (first.hashCode() * 13) ^ (second.hashCode() * 13);
+    }
+
+    public void ifPresent(java.util.function.BiConsumer func){
+        if(!(first == null) && !(second == null))
+        func.accept(first,second);
+    }
 }
