e5a21b1d8d051106eda95b0c98764e2d0f30e1ed
Danil Harlamtsev <harlamtsev.danil@gmail.com>
2020-12-27 18:57:05 +0500

    Харламцев Данил задача 7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int prevByte = System.in.read();
+        int currByte;
+        while (prevByte > -1){
+            currByte = System.in.read();
+            if(prevByte != 13 || currByte != 10){
+                System.out.write(prevByte);
+            }
+            prevByte = currByte;
+            System.out.flush();
+        }
     }
 }
