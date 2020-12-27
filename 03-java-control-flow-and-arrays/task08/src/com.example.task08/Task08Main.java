7d44df447480ac3d3d0b4c7893416d4cfdc61127
Дмитрий <dimichru@yandex.ru>
2020-12-27 12:26:56 +0500

    Карасик Дмитрий задание 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -14,7 +14,11 @@ public class Task08Main {
 
     static long mult(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        long multi = 1;
+        for (int i = 0; i < arr.length; i++){
+            multi *= arr[i];
+        }
+        return arr.length == 0 ? 0 : multi;
     }
 
 }
