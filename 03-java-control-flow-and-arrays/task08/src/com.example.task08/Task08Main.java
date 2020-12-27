187e9244d41ddbed3c3dbdeaa9beeb204df09272
DaemonMarshall <eminem.marshallbrusemathers3@gmail.com>
2020-12-27 20:14:18 +0500

    Давлетшин Илья. Задание 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -13,8 +13,11 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        long a = 1;
+        if (arr.length == 0) return 0;
+        for (int i : arr)
+            a *= i;
+        return a;
     }
 
 }
