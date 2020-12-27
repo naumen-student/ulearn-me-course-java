1f83f4ed1104ebb580ed81889e031009a2091616
Mckupe <65507746+Mckupe@users.noreply.github.com>
2020-12-27 22:50:43 +0500

    Update PolygonalLine.java

--- 04-java-oop-basic/task05/src/com/example/task05/PolygonalLine.java
+++ 04-java-oop-basic/task05/src/com/example/task05/PolygonalLine.java
@@ -3,9 +3,11 @@ package com.example.task05;
 import java.util.ArrayList;
 
 public class PolygonalLine {
+
     private final ArrayList<Point> points = new ArrayList<>();
 
     public void setPoints(Point[] points) {
+
         for (Point point : points) {
             this.points.add(new Point(point.getX(), point.getY()));
         }
@@ -13,21 +15,23 @@ public class PolygonalLine {
 
 
     public void addPoint(Point point) {
-        this.points.add(new Point(point.getX(), point.getY()));
+
+        points.add(new Point(point.getX(), point.getY()));
     }
 
-
     public void addPoint(double x, double y) {
+
         points.add(new Point(x, y));
     }
 
 
     public double getLength() {
-        double result = 0.0;
-        for (int i = 0; i < points.size() - 1; i++) {
-            result = result + this.points.get(i).getLength(this.points.get(i + 1));
+
+        double length = 0;
+        for(int i = 1; i < points.size(); i++) {
+            length += points.get(i).getLength(points.get(i - 1));
         }
-        return result;
+        return length;
     }
 
 }
