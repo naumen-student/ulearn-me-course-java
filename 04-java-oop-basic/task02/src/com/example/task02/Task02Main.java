57cb9d46b27e55f56f8afb20e823712ba712b7cc
tenfrs <podaand@icloud.com>
2020-12-27 11:00:16 +0500

    Задание 4.2 Поспелова Дарья

--- 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
+++ 04-java-oop-basic/task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,47 @@
 package com.example.task02;
 
-public class Task02Main {
-    public static void main(String[] args) {
+public class TimeSpan {
+    private int seconds, minets, hours;
 
+    public TimeSpan(int sec, int minute, int hours) {
+        this.sec = sec;
+        this.minute = minute;
+        this.hours = hours;
+    }
+
+    public void add(TimeSpan time) {
+        sec = sec + time.getSeconds();
+        minute = minute + time.getMinets();
+        hours = hours + time.getHours();
+    }
+
+    public void subtract(TimeSpan time) {
+        sec = sec - time.getSeconds();
+        minute = minute - time.getMinets();
+        hours = hours - time.getHours();
+    }
+
+    public String toString() {
+        return String.format("Hours: %d, Minets: %d, Seconds: %d", hours, minute, sec);
+    }
+
+    public int getSeconds() {
+        return sec;
+    }
+
+    public void setSeconds(int sec) {
+        this.sec = sec;
+    }
+
+    public int getMinets() {
+        return minute;
+    }
+
+    public void setMinets(int minute) {
+        this.minute = minute;
+    }
+
+    public int getHours() {
+        return hours;
     }
 }
