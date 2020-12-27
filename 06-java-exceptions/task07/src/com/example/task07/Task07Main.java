2a4c7df046e288d2bf90971c5278ca2778a45cfe
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 14:30:54 +0500

    Зотов Данила. Задание 6.7

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -1,24 +1,27 @@
-package com.example.task07;
+package com.example.task01;
 
-public class Task07Main {
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-    public static final String CHECKED = "checked";
-    public static final String UNCHECKED = "unchecked";
-    public static final String NONE = "none";
+public class Task01Main {
+    public static void main(String[] args) throws IOException {
+        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
 
-    public static void main(String[] args) {
+        System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
     }
 
-    public Processor processor;
+    public static int checkSumOfStream(InputStream inputStream) throws IOException {
+        if (inputStream == null)
+            throw new IllegalArgumentException();
 
-    public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
-
+        int lastByte = inputStream.read();
+        int sum = 0;
+        while (lastByte != -1) {
+            sum = Integer.rotateLeft(sum, 1) ^ lastByte;
+            lastByte = inputStream.read();
         }
-        return null;
+        return sum;
     }
-
 }
