ddc7fc6b55aa697669a6212db3b650eb5d9910d9
Nestki <kirill-nesterov21@mail.ru>
2020-12-27 22:34:04 +0500

    Нестеров Кирилл 7.1

--- 07-java-io/task01/src/com/example/task01/Task01Main.java
+++ 07-java-io/task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,7 @@ package com.example.task01;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
@@ -15,7 +16,14 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null)
+            throw new IllegalArgumentException();
+        int sum = 0;
+        int read = inputStream.read();
+        while (read > -1) {
+            sum = Integer.rotateLeft(sum, 1)^read;
+            read = inputStream.read();
+        }
+        return sum;
     }
 }
