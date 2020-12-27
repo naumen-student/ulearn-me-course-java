83263d1718001954795a535e5fceef3c869249ad
Task-1.1 <apzigaziga@gmail.com>
2020-12-27 05:21:10 +0500

    Задание 5.4 (4)

--- 05-java-oop-principles/task04/src/com/example/task04/Task04Main.java
+++ 05-java-oop-principles/task04/src/com/example/task04/Task04Main.java
@@ -1,121 +1,30 @@
 package com.example.task04;
 
-import java.io.PrintStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Hashtable;
 
-public class Logger
+public class MemoryHandler implements MessageHandler
 {
+    private final long size;
+    private final ArrayList<String> messageList = new ArrayList<>();
+    private final MessageHandler handler;
 
-    private final String name;
-    private static final Hashtable<String, Logger> loggers = new Hashtable<>();
-    private static ArrayList<MessageHandler> handlers = new ArrayList<>();
-    private logLevel level;
-
-    enum logLevel
+    public MemoryHandler(long size, MessageHandler handler)
     {
-        DEBUG,
-        INFO,
-        WARNING,
-        ERROR
+        this.size = size;
+        this.handler = handler;
     }
 
-    private Logger(String name)
+    @Override
+    public void logMes(String message)
     {
-        this.name = name;
-    }
-
-    public String getName()
-    {
-        return this.name;
-    }
-
-    public static Logger getLogger(String name)
-    {
-        if (!loggers.containsKey(name)) {
-            loggers.put(name, new Logger(name));
-        }
-        return new Logger(name);
-    }
-
-    public void setLevel(logLevel level)
-    {
-        this.level = level;
-    }
-
-    public logLevel getLevel()
-    {
-        return this.level;
-    }
-
-    public void debug(String message)
-    {
-        this.log(logLevel.DEBUG, message);
-    }
-
-    public void debug(String format, Object... params)
-    {
-        this.log(logLevel.DEBUG, format, params);
-    }
-
-    public void info(String message)
-    {
-        this.log(logLevel.INFO, message);
-    }
-
-    public void info(String format, Object... params)
-    {
-        this.log(logLevel.INFO, format, params);
-    }
-
-    public void warning(String message)
-    {
-        this.log(logLevel.WARNING, message);
-    }
-
-    public void warning(String format, Object... params)
-    {
-        this.log(logLevel.WARNING, format, params);
-    }
-
-    public void error(String message)
-    {
-        this.log(logLevel.ERROR, message);
-    }
-
-    public void error(String format, Object... params)
-    {
-        this.log(logLevel.ERROR, format, params);
-    }
-
-    public void addHandler(MessageHandler handler)
-    {
-        handlers.add(handler);
-    }
-
-    public void log(logLevel level, String message)
-    {
-        if (level.ordinal() >= getLevel().ordinal())
+        messageList.add(message);
+        if (messageList.size() >= size)
         {
-            Date date = new Date();
-            DateFormat formattedDate = new SimpleDateFormat("yyyy:MM:dd hh:mm:ss");
-            String resultMessage = String.format("[{0}] {1} {2} - {3}%n",
-                    level,
-                    formattedDate,
-                    this.name,
-                    message);
-            for (MessageHandler handler: handlers)
+            for (String savedMessage : messageList)
             {
-                handler.logMes(resultMessage);
+                handler.logMes(savedMessage);
             }
+            messageList.clear();
         }
-    }
-
-    public void log(logLevel level, String format, Object... params)
-    {
-        log(level, String.format(format, params));
     }
 }
