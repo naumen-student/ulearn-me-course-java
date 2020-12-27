c093ee0f9a038eb87ebe5109486afdcf8c229eae
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:28:08 +0500

    Зотов Данила. Задание 3.10

--- 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
+++ 03-java-control-flow-and-arrays/task10/src/com.example.task10/Task10Main.java
@@ -2,15 +2,16 @@ package com.example.task10;
 
 public class Task10Main {
     public static void main(String[] args) {
-
+        int[] arr = {7, 5, 9};
+        System.out.println(numMin(arr));
     }
 
     static int numMin(int[] arr) {
-        return 0;
         int imin = 0;
         for (int i = 1; i < arr.length; i++)
             if (arr[i] <= arr[imin])
                 imin = i;
         return imin;
     }
+
 }
