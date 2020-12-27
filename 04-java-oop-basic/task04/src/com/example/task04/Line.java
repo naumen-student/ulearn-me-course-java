6a3bbb29395a39a88e937cc47bf4fd1b8e323689
amooq <amooq786@gmail.com>
2020-12-27 17:57:34 +0500

    Коновалов Александр. Задание 4.4

---
+++ 04-java-oop-basic/task04/src/com/example/task04/Line.java
@@ -0,0 +1,35 @@
+package com.example.task04;
+
+
+public class Line {
+    private final Point p1;
+    private final Point p2;
+
+    public Line(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+    public String toString() {
+        return String.format("(%d, %d) - (%d, %d)", p1.x, p1.y, p2.x, p2.y);
+    }
+
+    public boolean isCollinear(Point point) {
+
+        int dx = point.x - p1.x;
+        int dy = point.y - p1.y;
+
+        int dx1 = p2.y - p1.y;
+        int dy1 = p2.y - p1.y;
+
+        return dx1 * dy - dx * dy1 == 0;
+    }
+}
