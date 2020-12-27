18d72f19c629c17c4443529b826777c0033ffaf1
tenfrs <podaand@icloud.com>
2020-12-27 12:00:54 +0500

    Задание 6.2 Поспелова Дарья

--- 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
+++ 06-java-exceptions/task02/src/com/example/task02/Task02Main.java
@@ -11,6 +11,26 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        witch (monthNumber){
+            case 1:
+            case 2:
+            case 12:
+                return "зима";
+            case 3:
+            case 4:
+            case 5:
+                return "весна";
+            case 6:
+            case 7:
+            case 8:
+                return "лето";
+            case 9:
+            case 10:
+            case 11:
+                return "осень";
+            default:
+                throw new IllegalArgumentException ("monthNumber " + monthNumber +
+                        " is invalid, month number should be between 1..12");
+        }
     }
 }
