e9f52e2d0ca37a44a60c26a4505b01da165606cc
Task-1.1 <apzigaziga@gmail.com>
2020-12-27 05:22:59 +0500

    Задание 5.4 (6)

--- 05-java-oop-principles/task04/src/com/example/task04/Task04Main.java
+++ 05-java-oop-principles/task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,33 @@
 package com.example.task04;
 
-public interface MessageHandler
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class RotationFileHandler implements MessageHandler
 {
-    void logMes(String message);
+    String filePath;
+    ChronoUnit rotation;
+
+    public RotationFileHandler(String filepath, ChronoUnit rotation) throws IOException
+    {
+        this.filePath = filepath;
+        this.rotation = rotation;
+    }
+
+    @Override
+    public void logMes(String message)
+    {
+        LocalDateTime now = LocalDateTime.now().truncatedTo(rotation);
+        try
+        {
+            FileWriter fileWriter = new FileWriter(String.format("log %s.txt", now), false);
+            fileWriter.append(message + "\n");
+            fileWriter.flush();
+        } catch (IOException x)
+        {
+            System.out.print(x.toString());
+        }
+    }
 }
