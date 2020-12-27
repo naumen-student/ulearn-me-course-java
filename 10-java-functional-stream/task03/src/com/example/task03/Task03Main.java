1b386df7fa2e9b600359879def04ca2f2133766b
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-12-27 16:24:46 +0500

    Update Task03Main.java

--- 10-java-functional-stream/task03/src/com/example/task03/Task03Main.java
+++ 10-java-functional-stream/task03/src/com/example/task03/Task03Main.java
@@ -1,39 +1,34 @@
 package com.example.task03;
+
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
+
 public class Task03Main {
-
-    public static void main(String[] args) {
-
-        findMinMax(
-                Stream.of(2, 9, 5, 4, 8, 1, 3),
-                Integer::compareTo,
-                (min, max) ->
-                        System.out.println("min: " + min + " / max: " + max)
-        );
-
-    }
 
     public static <T> void findMinMax(
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-        Object[] min = {null};
-        Object[] max = {null};
-        boolean[] firstElement = {true};
 
         // your implementation here
-        stream.forEach(current -> {
-            if (firstElement[0]) {
-                min[0] = current;
-                max[0] = current;
-                firstElement[0] = false;
-            } else if (order.compare(current, (T) min[0]) < 0)
-                min[0] = current;
-            else if (order.compare(current, (T) max[0]) > 0)
-                max[0] = current;
-        });
-        minMaxConsumer.accept((T) min[0], (T) max[0]);
+        Iterator<? extends T> iterator = stream.iterator();
+        if (!iterator.hasNext()){
+            minMaxConsumer.accept(null, null);
+            return;
+        }
+        T min = iterator.next();
+        T max = min;
+        while (iterator.hasNext()){
+            T elem = iterator.next();
+            if (order.compare(elem, min) < 0) {
+                min = elem;
+            }
+            if (order.compare(elem, max) > 0){
+                max = elem;
+            }
+        }
+        minMaxConsumer.accept(min, max);
     }
 }
