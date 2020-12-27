3ea73e1d24d295a7cb2c93a27be8cc10744748c2
s17v3rr <oleg.gubanov@urfu.me>
2020-12-27 17:38:16 +0500

    Губанов Олег. Задание 9.3

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -3,22 +3,38 @@ package com.example.task03;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
+    }
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
-        for (Set<String> anagram : anagrams) {
-            System.out.println(anagram);
+    private static List<Set<String>> sortResult(TreeMap<String, TreeSet<String>> sets) {
+        List<Set<String>> result = new ArrayList<>();
+        for (Set set : sets.values()) {
+            if (set.size() >= 2)
+                result.add(set);
         }
-
+        return result;
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset));
+        TreeMap<String, TreeSet<String>> anagrams = new TreeMap<>();
+        while (scanner.hasNextLine()) {
+            String current = scanner.nextLine().toLowerCase();
+            if (current.length() > 2 && current.matches("[а-я]+")) {
+                char[] chars = current.toCharArray();
+                Arrays.sort(chars);
+                String key = new String(chars);
+                if (!anagrams.containsKey(key))
+                    anagrams.put(key, new TreeSet<>());
+                anagrams.get(key).add(current);
+            }
+        }
+        return sortResult(anagrams);
     }
 }
