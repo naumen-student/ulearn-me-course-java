7a2d93f2b84f91df896ba5fe1606dadf97f4c932
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 13:28:19 +0500

    Зотов Данила. Задание 2.3

--- 02-java-types/task03/src/com/example/task03/Task03.java
+++ 02-java-types/task03/src/com/example/task03/Task03.java
@@ -3,12 +3,12 @@ package com.example.task03;
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
-
-        return 0;
         return centimetre / 100;
     }
 
     public static void main(String[] args) {
-
+        int result = getMetreFromCentimetre(345);
+        System.out.println(result);
     }
+
 }
