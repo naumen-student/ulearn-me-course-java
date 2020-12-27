7288f6df6fa44c32fbbc9d95c3b0d8581edd56a2
fitsuli <danilyuk_2011@bk.ru>
2020-12-27 23:51:40 +0500

    Данилюк Дмитрий. Задача 10.4

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,27 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
-
+        new BufferedReader(new InputStreamReader(System.in)).lines()
+                .flatMap(w -> Arrays.stream(w.toLowerCase().split("[,.\\-! ]").clone()))
+                .filter(str -> !str.isEmpty())
+                .collect(Collectors.groupingBy(w -> w, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .forEach(w -> System.out.print(w + "\n"));
     }
 
 }
