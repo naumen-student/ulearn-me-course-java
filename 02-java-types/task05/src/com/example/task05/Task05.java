f6973ad837320079bcd9d27d0933be27befea1a2
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 12:19:14 +0500

    Зотов Данила. Задание 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -4,18 +4,16 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
         return "FALSE";
+        for (char c : String.valueOf(x).toCharArray()) {
+            if ((c - '0') % 2 != 0)
+                return "FALSE";
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
-        System.out.println(result);
-        */
+
     }
 
 }
