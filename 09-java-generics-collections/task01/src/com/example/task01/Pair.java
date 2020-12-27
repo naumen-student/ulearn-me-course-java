5b238a14eb67afbd5c421d3aff65fa1f419bea41
OperationIntruder313 <71594631+OperationIntruder313@users.noreply.github.com>
2020-12-27 23:28:56 +0500

    Курбанов Джафар. Задача 9.1

--- 09-java-generics-collections/task01/src/com/example/task01/Pair.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Pair.java
@@ -1,5 +1,46 @@
 package com.example.task01;
+import java.util.function.BiConsumer;
 
-public class Pair {
-    // TODO напишите реализацию
+public class Pair<F, S> {
+
+    private F first; //first member of pair
+    private S second; //second member of pair
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public void setFirst(F first) {
+        this.first = first;
+    }
+
+    public void setSecond(S second) {
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public int hashCode(){ return  first.hashCode() + second.hashCode(); }
+
+    public boolean equals(Pair value){
+        return  first.equals(value.getFirst()) && second.equals(value.getSecond());
+    }
+
+    public void ifPresent(BiConsumer pair) {
+        if (first != null && second != null) {
+            pair.accept(first, second);
+        }
+    }
+
+    public static <F, S> Pair<F, S> of(F firstValue, S secondValue) {
+        Pair<F, S> pair = new Pair(firstValue, secondValue);
+        return pair;
+    }
 }
