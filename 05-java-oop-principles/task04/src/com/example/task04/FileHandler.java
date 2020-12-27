aff18e78d343e9f2e0d07832658ad1ae7972bcd5
Kozlovsky17 <Kozlovskiy.2001@mail,ru>
2020-12-27 20:55:52 +0300

    Козловский Игорь. Задание 5.4

---
+++ 05-java-oop-principles/task04/src/com/example/task04/FileHandler.java
@@ -0,0 +1,16 @@
+package com.example.task04;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler implements MessageHandler {
+    @Override
+    public void log(String message) {
+        try {
+            FileWriter fileWriter = new FileWriter("log.txt", false);
+            fileWriter.write(message + "\n");
+            fileWriter.flush();
+        } catch (IOException x) {
+            System.out.print(x.toString());
+        }
+    }
+}
