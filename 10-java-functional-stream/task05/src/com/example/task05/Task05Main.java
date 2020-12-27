9e93a5ddb56a3e8a99bcd5f1dd9a556e2fa969d0
Konovalova Lera <lera.penguin01@mail.ru>
2020-12-27 23:00:36 +0500

    Коновалова Лера. Задание 10.5

--- 10-java-functional-stream/task05/src/com/example/task05/Task05Main.java
+++ 10-java-functional-stream/task05/src/com/example/task05/Task05Main.java
@@ -9,8 +9,6 @@ public class Task05Main {
 
     public static void main(String[] args) {
 
-        /*
-
         // Random variables
         String randomFrom = "..."; // Некоторая случайная строка. Можете выбрать ее самостоятельно.
         String randomTo = "...";  // Некоторая случайная строка. Можете выбрать ее самостоятельно.
@@ -87,8 +85,6 @@ public class Task05Main {
         assert salaries.get(salary2.getTo()).equals(Arrays.asList(Integer.MAX_VALUE)) : "wrong salaries mailbox content (2)";
         assert salaries.get(randomTo).equals(Arrays.asList(randomSalary)) : "wrong salaries mailbox content (3)";
 
-
-        */
 
     }
 
