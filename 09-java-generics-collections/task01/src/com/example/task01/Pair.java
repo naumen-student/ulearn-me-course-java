0be6353794ee073021dd5612b3d8d39bc3b8cc86
RoyalPiano <71338871+RoyalPiano@users.noreply.github.com>
2020-12-27 17:36:44 +0500

    Update Pair.java

--- 09-java-generics-collections/task01/src/com/example/task01/Pair.java
+++ 09-java-generics-collections/task01/src/com/example/task01/Pair.java
@@ -1,37 +1,5 @@
 package com.example.task01;
 
-import java.util.function.BiConsumer;
-
-public class Pair<First, Second> {
-    First firstValue;
-    Second secondValue;
-    private Pair(First first, Second second){
-        this.firstValue = first;
-        this.secondValue = second;
-    }
-
-    public First getFirst(){
-        return firstValue;
-    }
-
-    public Second getSecond(){
-        return secondValue;
-    }
-
-    public boolean equals(Pair<First, Second> pair){
-        return this.hashCode() == pair.hashCode();
-    }
-
-    public int hashCode(){
-        return this.secondValue.hashCode() + this.secondValue.hashCode();
-    }
-
-    public void ifPresent(BiConsumer<? super First, ? super Second> biConsumer) {
-        if (firstValue != null && secondValue != null)
-            biConsumer.accept(firstValue, secondValue);
-    }
-
-    public static <First, Second> Pair<First, Second> of(First firstValue, Second secondValue) {
-        return new Pair<>(firstValue, secondValue);
-    }
+public class Pair {
+    // TODO напишите реализацию
 }
