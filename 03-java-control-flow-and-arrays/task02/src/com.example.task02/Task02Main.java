82ed4e25c8de4b233aa0e2ea07800e1a837af537
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:22:11 +0500

    Зотов Данила. Задание 3.2

--- 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
+++ 03-java-control-flow-and-arrays/task02/src/com.example.task02/Task02Main.java
@@ -2,11 +2,10 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
-
+        System.out.println(getSeason(12));
     }
 
     static String getSeason(int monthNumber) {
-        return "";
         return new String[] {"зима", "весна", "лето", "осень"}[monthNumber % 12 / 3];
     }
 }
