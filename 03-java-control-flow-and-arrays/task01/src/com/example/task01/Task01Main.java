520d37ecff3cb2d26d1cc16d4808f9facdb17e11
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:21:16 +0500

    Зотов Данила. Задание 3.1

--- 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
+++ 03-java-control-flow-and-arrays/task01/src/com/example/task01/Task01Main.java
@@ -2,11 +2,11 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-
+        boolean b = canGetDriverLicense(100);
+        System.out.println(b);
     }
 
     static boolean canGetDriverLicense(int age) {
-        return true;
         return age >= 18;
     }
 }
