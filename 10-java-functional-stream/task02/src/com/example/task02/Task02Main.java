72e643096ab938f8586b302ccd01b67f3ae5c8fb
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-12-27 15:30:11 +0500

    Update Task02Main.java

--- 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
+++ 10-java-functional-stream/task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,39 @@
-package com.example.task02;
-
-import java.util.stream.IntStream;
-
-public class Task02Main {
+package com.example.task03;
+import java.util.Comparator;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+public class Task03Main {
 
     public static void main(String[] args) {
 
-        /*
-        cycleGrayCode(2)
-                .limit(10)
-                .forEach(System.out::println);
-        */
+        findMinMax(
+                Stream.of(2, 9, 5, 4, 8, 1, 3),
+                Integer::compareTo,
+                (min, max) ->
+                        System.out.println("min: " + min + " / max: " + max)
+        );
 
     }
 
-    public static IntStream cycleGrayCode(int n) {
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+        Object[] min = {null};
+        Object[] max = {null};
+        boolean[] firstElement = {true};
 
-        return null; // your implementation here
-
+        // your implementation here
+        stream.forEach(current -> {
+            if (firstElement[0]) {
+                min[0] = current;
+                max[0] = current;
+                firstElement[0] = false;
+            } else if (order.compare(current, (T) min[0]) < 0)
+                min[0] = current;
+            else if (order.compare(current, (T) max[0]) > 0)
+                max[0] = current;
+        });
+        minMaxConsumer.accept((T) min[0], (T) max[0]);
     }
-
 }
