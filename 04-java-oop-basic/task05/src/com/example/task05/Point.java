d5b35f8b4ab641a0cb3ecdb0370593b7e51ed28a
flint-afk <ivan.ivanenko55779@gmail.com>
2020-12-27 13:21:36 +0500

    Update Point.java

--- 04-java-oop-basic/task05/src/com/example/task05/Point.java
+++ 04-java-oop-basic/task05/src/com/example/task05/Point.java
@@ -4,6 +4,8 @@ package com.example.task05;
  * Точка в двумерном пространстве
  */
 public class Point {
+    private final double x;
+    private final double y;
 
     /**
      * Конструктор, инициализирующий координаты точки
@@ -12,7 +14,8 @@ public class Point {
      * @param y координата по оси ординат
      */
     public Point(double x, double y) {
-        throw new AssertionError();
+        this.x = x;
+        this.y = y;
     }
 
     /**
@@ -21,8 +24,7 @@ public class Point {
      * @return координату точки по оси X
      */
     public double getX() {
-        // TODO: реализовать
-        throw new AssertionError();
+        return x;
     }
 
     /**
@@ -31,8 +33,8 @@ public class Point {
      * @return координату точки по оси Y
      */
     public double getY() {
-        // TODO: реализовать
-        throw new AssertionError();
+        return y;
+
     }
 
     /**
@@ -42,8 +44,7 @@ public class Point {
      * @return расстояние от текущей точки до переданной
      */
     public double getLength(Point point) {
-        // TODO: реализовать
-        throw new AssertionError();
+        return (Math.sqrt(Math.pow((point.x - x), 2) + Math.pow((point.y - y), 2)));
     }
 
 }
