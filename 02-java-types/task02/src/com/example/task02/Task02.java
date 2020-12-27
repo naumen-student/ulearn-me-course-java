636c92d83c64493556c92e101b792c0218b2ae79
DanilaZotov <71969881+DanilaZotov@users.noreply.github.com>
2020-12-27 13:21:34 +0500

    Update Task02.java

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,28 +3,19 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-        long value = Long.parseLong(input);
-        String result = "long";
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-        if (value >= -128 && value <= 127)
-            result = "byte";
-        else if (value >= -32768 && value <= 32767)
-            result = "short";
-        else if (value >= -2147483648 && value <= 2147483647)
-            result = "int";
-
-        return "";
-        return result;
+        long val = Long.parseLong(input);
+        if (val <= Byte.MAX_VALUE && val >= Byte.MIN_VALUE) {
+            return "byte";
+        } else if (val <= Short.MAX_VALUE && val >= Short.MIN_VALUE) {
+            return "short";
+        } else if (val <= Integer.MAX_VALUE && val >= Integer.MIN_VALUE) {
+            return "int";
+        } else return "long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
