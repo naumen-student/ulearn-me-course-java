cda4ee4484f2e876aa8f8e95a66d5ec2dd992bad
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:04:34 +0500

    Зотов Данила. Задание 10.5

---
+++ 10-java-functional-stream/task05/src/com/example/task05/IContent.java
@@ -0,0 +1,10 @@
+package com.example.task05;
+
+public interface IContent<T> {
+
+    String getFrom();
+
+    String getTo();
+
+    T getContent();
+}
---
+++ 10-java-functional-stream/task05/src/com/example/task05/MyMap.java
@@ -0,0 +1,16 @@
+package com.example.task05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MyMap<T> extends HashMap<String, List<T>> {
+    @Override
+    public List<T> get(Object key) {
+        if (super.containsKey(key))
+            return super.get(key);
+        else {
+            return new ArrayList<>();
+        }
+    }
+}
