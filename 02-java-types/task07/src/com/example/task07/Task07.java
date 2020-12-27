b240481002c577178f799b6bea4c1202e5b115b6
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:12:14 +0500

    Зотов Данила. Задание 2.7

--- 02-java-types/task07/src/com/example/task07/Task07.java
+++ 02-java-types/task07/src/com/example/task07/Task07.java
@@ -3,12 +3,12 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-        return 0;
         return (n / k) * (m / k);
     }
 
     public static void main(String[] args) {
-
+        int result = solution(10, 20, 5);
+        System.out.println(result);
     }
 
 }
