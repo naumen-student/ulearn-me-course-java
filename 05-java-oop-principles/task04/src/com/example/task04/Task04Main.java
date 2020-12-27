6435887dc3fd16b2d30ebbc716b96c4f51754165
Task-1.1 <apzigaziga@gmail.com>
2020-12-27 05:22:10 +0500

    Задание 5.4 (5)

--- 05-java-oop-principles/task04/src/com/example/task04/Task04Main.java
+++ 05-java-oop-principles/task04/src/com/example/task04/Task04Main.java
@@ -1,30 +1,6 @@
 package com.example.task04;
 
-import java.util.ArrayList;
-
-public class MemoryHandler implements MessageHandler
+public interface MessageHandler
 {
-    private final long size;
-    private final ArrayList<String> messageList = new ArrayList<>();
-    private final MessageHandler handler;
-
-    public MemoryHandler(long size, MessageHandler handler)
-    {
-        this.size = size;
-        this.handler = handler;
-    }
-
-    @Override
-    public void logMes(String message)
-    {
-        messageList.add(message);
-        if (messageList.size() >= size)
-        {
-            for (String savedMessage : messageList)
-            {
-                handler.logMes(savedMessage);
-            }
-            messageList.clear();
-        }
-    }
+    void logMes(String message);
 }
