8fcc6ce30840ee7151bbfb1c1bd576477d0c7b11
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 14:16:33 +0500

    Зотов Данила. Задание 5.4

---
+++ 05-java-oop-principles/task04/src/com/example/task04/MemoryHandler.java
@@ -0,0 +1,32 @@
+
+package com.example.task04;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MemoryHandler implements MessageHandler{
+    private final MessageHandler messageHandler;
+    private final ArrayList<String> messages;
+    private final int memorySize;
+
+    public MemoryHandler(MessageHandler handler, int memorySize){
+        messageHandler = handler;
+        messages = new ArrayList<>();
+        this.memorySize = memorySize;
+    }
+
+    @Override
+    public void log(Level severityLevel, String message) {
+        if (memorySize <= (long) messages.size()) {
+            for (String currentMessage : messages)
+                messageHandler.log(severityLevel, currentMessage);
+            messages.clear();
+        }
+    }
+
+    @Override
+    public void log(Level severityLevel, String template, Object... args) {
+        log(severityLevel, String.format(template, args));
+    }
+}
