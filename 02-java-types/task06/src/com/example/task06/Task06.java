16c8eed7673d8f1cd681c15fbb12d5ab5e9f74cb
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:11:26 +0500

    Зотов Данила. Задание 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -1,14 +1,15 @@
+
 package com.example.task06;
 
 public class Task06 {
 
     public static int solution(int x, int y) {
-        return 0;
         return x + y == 0 ? 1 : (int) Math.floor(Math.log10(Math.abs(x + y))) + 1;
     }
 
     public static void main(String[] args) {
-
+        int result = solution(12, 34);
+        System.out.println(result);
     }
 
 }
