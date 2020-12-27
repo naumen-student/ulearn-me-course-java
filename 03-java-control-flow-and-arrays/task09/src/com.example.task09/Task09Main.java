831426f8c73f324844b78cdd5aedc205b5c53b8b
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:27:28 +0500

    Зотов Данила. Задание 3.9

--- 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
+++ 03-java-control-flow-and-arrays/task09/src/com.example.task09/Task09Main.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class Task09Main {
     public static void main(String[] args) {
-
+        int[] arr = new int[3];
+        arr[0] = 2;
+        arr[1] = -1;
+        arr[2] = 3;
+        System.out.println(min(arr));
     }
 
     static int min(int[] arr) {
-        return 0;
         return Arrays.stream(arr).min().getAsInt();
     }
-
 }
