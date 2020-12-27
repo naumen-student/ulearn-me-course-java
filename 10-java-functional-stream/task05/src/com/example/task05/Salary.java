7632494c2043fcf51bab98e58ec6b2b9584e6d6e
Polina <kiramik55@gmail.com>
2020-12-27 06:25:18 +0500

    10_task_05

---
+++ 10-java-functional-stream/task05/src/com/example/task05/Salary.java
@@ -0,0 +1,7 @@
+package com.example.task05;
+
+public class Salary extends Content<Integer> {
+    public Salary(String from, String to, Integer content) {
+        super(from, to, content);
+    }
+}
