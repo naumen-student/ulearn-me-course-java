9710b415531653b3adcd3f5e59b45a6d6bb8f45d
Kozlovsky17 <Kozlovskiy.2001@mail,ru>
2020-12-27 21:44:44 +0300

    Козловский Игорь. Задание 10.4

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,31 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
 public class Task04Main {
 
     public static void main(String[] args) {
+        HashMap<String, Integer> countWords = new HashMap<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
 
-        // your implementation here
+        reader.lines().map(String::toLowerCase)
+                .flatMap(y -> Arrays.stream(y.split("[^a-zа-яё0-9]+").clone()))
+                .filter(str -> !str.isEmpty())
+                .forEach(key -> {
+                    if (!countWords.containsKey(key))
+                        countWords.put(key, 1);
+                    else
+                        countWords.put(key, countWords.get(key) + 1);
+                });
 
+        countWords.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .limit(10)
+                .forEach(x -> System.out.print(x.getKey() + '\n'));
     }
-
 }
