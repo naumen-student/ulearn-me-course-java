ce92a1269d19dbb44f67247b384b1529925eaf69
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 12:49:01 +0500

    Зотов Данила. Задание 3.7

--- 03-java-control-flow-and-arrays/task07/src/com.example.task07/Task07Main.java
+++ 03-java-control-flow-and-arrays/task07/src/com.example.task07/Task07Main.java
@@ -1,22 +1,15 @@
 package com.example.task07;
 
+import java.util.Arrays;
+
 public class Task07Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] myArray = new int[4];
-        myArray[0] = 2;
-        myArray[1] = 10;
-        myArray[2] = 10;
-        myArray[3] = 0;
-        System.out.println(sum(myArray));
-         */
+
     }
 
     static int sum(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         return 0;
+        return Arrays.stream(arr).sum();
     }
 
 }
