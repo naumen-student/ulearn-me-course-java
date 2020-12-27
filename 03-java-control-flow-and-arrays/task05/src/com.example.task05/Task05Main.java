bdac9a4ce7f9dd9d5fb9b863b3bcce5fe54f3739
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:24:56 +0500

    Зотов Данила. Задание 3.5

--- 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
+++ 03-java-control-flow-and-arrays/task05/src/com.example.task05/Task05Main.java
@@ -4,11 +4,10 @@ import java.util.Arrays;
 
 public class Task05Main {
     public static void main(String[] args) {
-
+        System.out.println(getMin(5,4,3));
     }
 
     static int getMin(int a, int b, int c) {
-        return 0;
         return Arrays.stream(new int[] {a, b, c}).min().getAsInt();
     }
 }
