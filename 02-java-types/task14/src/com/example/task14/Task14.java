73e4985dfa1e024bde8a9fd80f0f9d588dc5773e
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:20:15 +0500

    Зотов Данила. Задание 2.14

--- 02-java-types/task14/src/com/example/task14/Task14.java
+++ 02-java-types/task14/src/com/example/task14/Task14.java
@@ -2,15 +2,13 @@ package com.example.task14;
 
 public class Task14 {
 
-
     public static int reverse(int value) {
-        return 0;
         return Integer.parseInt(new StringBuilder(String.valueOf(value)).reverse().toString());
     }
 
     public static void main(String[] args) {
-
+        int result = reverse(345);
+        System.out.println(result);
     }
-
 
 }
