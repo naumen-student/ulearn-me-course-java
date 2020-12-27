9349e1752e08d6cc1c55212c9ebdf37e9edc5784
Mohamed <mohamedtarek21763@gmail.com>
2020-12-27 05:46:56 +0500

    Хассан Мохамед Тарек Хассан 8.3

--- 08-java-io2/task03/src/com/example/task03/Task03Main.java
+++ 08-java-io2/task03/src/com/example/task03/Task03Main.java
@@ -2,20 +2,14 @@ package com.example.task03;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.*;
 
 public class Task03Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(deserialize(new FileInputStream("task03/src/main/resources/example1.bin")));
-        */
-
-    }
-
     public static SampleData deserialize(InputStream inputStream) throws IOException, ClassNotFoundException {
         // your implementation here
-        return null;
+
+        try (ObjectInputStream stream = new ObjectInputStream(inputStream)) {
+            return (SampleData) stream.readObject();
+        }
     }
 }
