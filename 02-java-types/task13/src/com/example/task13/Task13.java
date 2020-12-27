28ddb0d8a4a0e0bf7a506c8fb8866e10ac5806bd
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:19:31 +0500

    Зотов Данила. Задание 2.13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -3,12 +3,12 @@ package com.example.task13;
 public class Task13 {
 
     public static char toUpperCase(char c) {
-        return c;
         return Character.toUpperCase(c);
     }
 
     public static void main(String[] args) {
-
+        char result = toUpperCase('x');
+        System.out.println(result);
     }
 
 }
