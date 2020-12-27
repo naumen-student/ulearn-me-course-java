8af7666d16ad24ce71bbd27c2f3b96c18e2de2f6
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:26:55 +0500

    Зотов Данила. Задание 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -4,15 +4,16 @@ import java.util.Arrays;
 
 public class Task08Main {
     public static void main(String[] args) {
-
+        int[] arr = new int[2];
+        arr[0] = 2;
+        arr[1] = 3;
+        System.out.println(mult(arr));
     }
 
     static long mult(int[] arr) {
-        return 1;
         if (arr == null || arr.length == 0) {
             return 0;
         }
         return Arrays.stream(arr).mapToLong(e -> e).reduce(1, (a, b) -> a * b);
     }
-
 }
