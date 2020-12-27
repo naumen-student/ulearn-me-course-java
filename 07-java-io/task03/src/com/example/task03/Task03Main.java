8f263bacbbd3359ffc5810d8808c821f9362bd0b
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2020-12-27 12:28:21 +0500

    Update Task03Main.java

--- 07-java-io/task03/src/com/example/task03/Task03Main.java
+++ 07-java-io/task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,8 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.util.Scanner;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
@@ -15,16 +12,10 @@ public class Task03Main {
         /*
         System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
         */
-    }
+    } 
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
         // your implementation here
-        if (inputStream == null || charset == null)
-            throw new IllegalArgumentException();
-        StringBuilder stringBuilder = new StringBuilder();
-        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset));
-        while (scanner.hasNext())
-            stringBuilder.append(scanner.next());
-        return stringBuilder.toString();
+        return "";
     }
 }
