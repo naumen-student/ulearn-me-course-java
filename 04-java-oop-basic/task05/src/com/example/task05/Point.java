afe28725f9d56451a5279b6e2dd3f3303f4a4ec6
Mckupe <65507746+Mckupe@users.noreply.github.com>
2020-12-27 22:49:40 +0500

    Update Point.java

--- 04-java-oop-basic/task05/src/com/example/task05/Point.java
+++ 04-java-oop-basic/task05/src/com/example/task05/Point.java
@@ -1,28 +1,23 @@
 package com.example.task05;
 
-
 public class Point {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
-    }
-
-
+    } 
     public double getX() {
-        return x;
+          return x;
     }
-
 
     public double getY() {
         return y;
     }
 
-
-    public double getLength(Point point) {
-        return Math.sqrt(point.x - x * point.x - x + point.y - y * point.y - y);
+    public double getLength(Point point) {   
+        return (Math.sqrt(Math.pow((point.x - x), 2) + Math.pow((point.y - y), 2)));
     }
 
 }
