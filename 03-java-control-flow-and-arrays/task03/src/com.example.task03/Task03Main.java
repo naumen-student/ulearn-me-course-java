2945b33dd444efd90b71d71ef81bb1109bb55657
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 12:42:20 +0500

    Зотов Данила. Задание 3.3

--- 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
+++ 03-java-control-flow-and-arrays/task03/src/com.example.task03/Task03Main.java
@@ -2,14 +2,15 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getNameOfWeekDays(7));
-         */
+
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        return "";
+        if (weekDaysNumber < 1 || weekDaysNumber > 7)
+            return "такого дня недели не существует";
+        return new String[] {
+                "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"
+        }[weekDaysNumber - 1];
     }
 }
