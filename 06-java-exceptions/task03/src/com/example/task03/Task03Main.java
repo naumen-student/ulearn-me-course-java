2a95aac4365db6fd542e79ddfa21bec342fdf391
Konovalova Lera <lera.penguin01@mail.ru>
2020-12-27 17:24:21 +0500

    6.3

--- 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
+++ 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
@@ -9,7 +9,8 @@ public class Task03Main {
          */
     }
 
-    public static void throwCheckedException() throws Exception{
-        throw new Exception("checked исключение");
+    //todo напишите здесь свою корректную реализацию задания
+    public static void throwCheckedException() {
+
     }
 }
