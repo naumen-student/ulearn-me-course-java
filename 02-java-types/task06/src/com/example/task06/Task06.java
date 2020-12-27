917197b9f0db785c79813c996182b4c48b90b5d4
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 12:21:23 +0500

    Зотов Данила. Задание 2.5

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,12 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
         return 0;
+        return x + y == 0 ? 1 : (int) Math.floor(Math.log10(Math.abs(x + y))) + 1;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
-        System.out.println(result);
-        */
+
     }
 
 }
