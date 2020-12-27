90ae6725d95b2f83fa43d78e0a3bd9908f683e94
dzotov2113@mail.ru <dzotov2113@mail.ru>
2020-12-27 21:29:51 +0500

    Зотов Данила. Задание 3.13

--- 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
+++ 03-java-control-flow-and-arrays/task13/src/com.example.task13/Task13Main.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 public class Task13Main {
     public static void main(String[] args) {
-
+        int[] arr = {9, 1100, 7, 8};
+        removeMoreThen1000(arr);
+        System.out.println(java.util.Arrays.toString(arr));
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        return  null;
         if (arr == null)
             return null;
         return Arrays.stream(arr).filter(a -> a <= 1000).toArray();
     }
-
 }
