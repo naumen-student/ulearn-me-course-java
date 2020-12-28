f02760f09e141cdeff8a7cfbec29d0346cc0de2f
avfsdfasdf <sasha.2509009@gmail.com>
2020-12-28 12:01:39 +0500

    Смоляков Александр. Задание 10.04

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,42 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-        // your implementation here
-
+        List<Map.Entry<String, Long>> listToSort = new ArrayList<>();
+        for (Map.Entry<String, Long> stringLongEntry : bufferedReader.lines()
+                .flatMap(x -> Stream.of(x.split("[,.\n\\-! ]")))
+                .map(String::toLowerCase)
+                .filter(x -> !x.isEmpty())
+                .collect(Collectors.groupingBy(x -> x, Collectors.counting()))
+                .entrySet()) {
+            listToSort.add(stringLongEntry);
+        }
+        listToSort.sort(Map.Entry.comparingByKey());
+        List<Map.Entry<String, Long>> toSort = new ArrayList<>();
+        for (Map.Entry<String, Long> stringLongEntry : listToSort) {
+            toSort.add(stringLongEntry);
+        }
+        toSort.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        long limit = 10;
+        for (Map.Entry<String, Long> stringLongEntry : toSort) {
+            if (limit-- == 0) break;
+            String key = stringLongEntry.getKey();
+            System.out.print(key + "\n");
+        }
     }
 
 }
