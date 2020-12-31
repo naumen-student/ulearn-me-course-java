4d9744e682262a01f183b71ad47f05477b2cd583
AdIVIuH <42569158+AdIVIuH@users.noreply.github.com>
2020-12-31 16:54:58 +0500

    Квашнин Дмитрий. Задание 4.2

--- 04-java-oop-basic/task02/src/com/example/task02/TimeSpan.java
+++ 04-java-oop-basic/task02/src/com/example/task02/TimeSpan.java
@@ -35,20 +35,45 @@ public class TimeSpan {
         this.seconds = seconds;
     }
 
-    void add(TimeSpan time) {
-        this.hours += time.hours;
-        this.minutes += time.minutes;
-        this.seconds += time.seconds;
+    public void add(TimeSpan time) {
+        change(time, 1);
     }
 
-    void subtract(TimeSpan time) {
-        this.hours -= time.hours;
-        this.minutes -= time.minutes;
-        this.seconds -= time.seconds;
+    public void subtract(TimeSpan time) {
+        change(time, -1);
+    }
+
+    private void change(TimeSpan time, int sign) {
+        this.hours += sign * time.hours;
+        this.minutes += sign * time.minutes;
+        this.seconds += sign * time.seconds;
+
+        if (this.seconds < 0) {
+            this.minutes -= 1;
+            this.seconds += 60;
+        }
+
+        if (this.seconds >= 60) {
+            this.minutes += 1;
+            this.seconds -= 60;
+        }
+
+        if (this.minutes < 0) {
+            this.hours -= 1;
+            this.minutes += 60;
+        }
+
+        if (this.minutes >= 60) {
+            this.hours += 1;
+            this.minutes -= 60;
+        }
     }
 
     @Override
     public String toString() {
-        return hours + "h " + minutes + "m " + seconds + "s ";
+        String h = hours > 9 ? String.valueOf(hours) : "0" + hours;
+        String m = minutes > 9 ? String.valueOf(minutes) : "0" + minutes;
+        String s = seconds > 9 ? String.valueOf(seconds) : "0" + seconds;
+        return String.format("%s:%s:%s", h, m, s);
     }
 }
