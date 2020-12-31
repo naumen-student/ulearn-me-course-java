c2369bc01c4811d91c76bd4ccf92cac9aeda60a1
Kindgam <artur.52mail@mail.ru>
2020-12-31 23:07:22 +0500

    Гаскаров Артур 8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,35 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Task02Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
+    public static void main(String[] args) throws IOException {
+        System.out.println(listFiles(Paths.get("08-java-io2/task02/src/main/resources/")));
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+    public static List<Path> listFiles(Path rootDir) {
+        List<Path> res = new ArrayList<>();
+        for (File f : Objects.requireNonNull(rootDir.toFile().listFiles())) {
+            if (f.isDirectory())
+                listFiles(f.toPath(), res);
+            else
+                res.add(f.toPath());
+        }
+        return res;
+    }
 
-        return null;
+    private static void listFiles(Path rootDir, List<Path> paths) {
+        for (File file : (Objects.requireNonNull(rootDir.toFile().listFiles()))) {
+            if (file.isDirectory())
+                listFiles(file.toPath(), paths);
+            else
+                paths.add(file.toPath());
+
+        }
     }
 }
