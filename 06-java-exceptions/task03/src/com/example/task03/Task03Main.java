78ba0c0c0c57a85acb5415dea8975e16a9bca31c
AdIVIuH <42569158+AdIVIuH@users.noreply.github.com>
2020-12-31 17:04:22 +0500

    Квашнин Дмитрий. Задание 6.3

--- 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
+++ 06-java-exceptions/task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,12 @@
 package com.example.task03;
 
+import java.io.IOException;
+
 public class Task03Main {
     public static void main(String[] args) {
-//        throwCheckedException();
     }
 
-    public static void throwCheckedException() throws Exception {
-        throw new Exception("Checked Exception");
+    public static void throwCheckedException() throws IOException {
+        throw new IOException();
     }
 }
