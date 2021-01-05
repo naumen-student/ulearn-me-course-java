fa9a00c911328c950e70ad1dc6400789138944f1
Арман <Arman.Koshaev@urfu.me>
2021-01-05 18:20:26 +0500

    Кошаев Арман. Задание 10.1

---
+++ 10-java-functional-stream/task01/src/com/example/task01/Task01Main.java
@@ -0,0 +1,27 @@
+package com.example.task01;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Task01Main {
+    public static void main(String[] args) throws IOException {
+
+        Predicate<Object> condition = Objects::isNull;
+        Function<Object, Integer> ifTrue = obj -> 0;
+        Function<CharSequence, Integer> ifFalse = CharSequence::length;
+        Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+
+    }
+
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+        if (condition == null || ifTrue == null || ifFalse == null)
+            throw new NullPointerException();
+
+        return t -> condition.test(t) ? ifTrue.apply(t) : ifFalse.apply(t);
+    }
+}
