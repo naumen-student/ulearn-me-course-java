3c4c3e49dc99aafe45386260f9c5641f8d8e781b
ElifZelal <75887054+ElifZelal@users.noreply.github.com>
2021-01-11 07:39:41 +0300

    Мудуроглу Элиф Зэлал Задача6.7

--- 06-java-exceptions/task07/src/com/example/task07/Processor.java
+++ 06-java-exceptions/task07/src/com/example/task07/Processor.java
@@ -1,10 +1,19 @@
 package com.example.task07;
 
-public class Processor {
+public Processor processor;
 
-    public Object process() throws Exception {
-        //todo вы можете заменить реализацию этого метода для ручного дебага
+public String getExceptionType() {
+        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String result = NONE;
+        try {
+        processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+        processor.process();
+        } catch (Exception e) {
+
+        result = e instanceof RuntimeException ? UNCHECKED : CHECKED;
+        }
         return null;
-    }
+        return result;
+        }
 
-}
+        }
