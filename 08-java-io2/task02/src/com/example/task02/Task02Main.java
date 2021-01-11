45a37f9e83cd4e833b48a1d15738af18a90c8388
ElifZelal <75887054+ElifZelal@users.noreply.github.com>
2021-01-11 07:51:38 +0300

    Мудуроглу Элиф Зэлал Задача8.2

--- 08-java-io2/task02/src/com/example/task02/Task02Main.java
+++ 08-java-io2/task02/src/com/example/task02/Task02Main.java
@@ -1,8 +1,12 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -12,12 +16,32 @@ public class Task02Main {
         /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
         */
+        public static void main(String[] args) throws IOException {
+            System.out.println(listFiles(Paths.get("08-java-io2/task02/src/main/resources/")));
+        }
 
+        public static List<Path> listFiles(Path rootDir) {
+            List<Path> res = new ArrayList<>();
+            for (File f : Objects.requireNonNull(rootDir.toFile().listFiles())) {
+                if (f.isDirectory())
+                    listFiles(f.toPath(), res);
+                else
+                    res.add(f.toPath());
+            }
+            return res;
+        }
+
+        public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
+            // your implementation here
+            private static void listFiles(Path rootDir, List<Path> paths) {
+                for (File file : (Objects.requireNonNull(rootDir.toFile().listFiles()))) {
+                    if (file.isDirectory())
+                        listFiles(file.toPath(), paths);
+                    else
+                        paths.add(file.toPath());
+
+                    return null;
+                }
+            }
+        }
     }
-
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
-    }
-}
