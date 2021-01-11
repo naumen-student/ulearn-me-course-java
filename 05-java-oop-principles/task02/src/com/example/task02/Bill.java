c2542b5e926402ce73a5e42cac483581dafcc246
ElifZelal <75887054+ElifZelal@users.noreply.github.com>
2021-01-11 07:21:42 +0300

    Мудуроглу Элиф Зэлал Задача5.2

--- 05-java-oop-principles/task02/src/com/example/task02/Bill.java
+++ 05-java-oop-principles/task02/src/com/example/task02/Bill.java
@@ -1,65 +1,31 @@
 package com.example.task02;
 
-import java.util.ArrayList;
-import java.util.List;
+public class DiscountBill extends Bill {
+    private final long discount;
 
-/**
- * Счет к оплате
- */
-public class Bill {
-    private List<BillItem> items = new ArrayList<>();
-
-    /**
-     * Добавляет товар в корзину
-     *
-     * @param item товар
-     * @param amount количество
-     */
-    public void add(Item item, int amount) {
-        boolean found = false;
-        for (BillItem billItem: items) {
-            if (billItem.item.equals(item)) {
-                found = true;
-                billItem.amount += amount;
-                break;
-            }
+    public DiscountBill(long discount) {
+        super();
+        if (discount < 0 || discount > 100) throw new ArithmeticException(
+                String.format("Data normalize not possible. Input: %s", discount));
+        if (discount > 1){
+            this.discount = discount / 100;
         }
-        if (!found) {
-            items.add(new BillItem(item, amount));
+        else
+        {
+            this.discount = discount;
         }
-    }
-
-    /**
-     * Подсчитывает общую сумму покупки
-     *
-     * @return общую стоимость покупки
-     */
-    public long getPrice() {
-        long price = 0;
-        for (BillItem item: items) {
-            price += item.item.getPrice() * item.amount;
-        }
-        return price;
     }
 
     @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("Счет к оплате\n");
-        for(BillItem item : items) {
-            sb.append(item.item.getName()).append('(').append(item.amount).append(')');
-            sb.append('\n');
-        }
-        sb.append("Сумма к оплате: ").append(getPrice());
-        return sb.toString();
+    public long getPrice() {
+        return super.getPrice() * (1 - discount);
     }
 
-    private static class BillItem {
-        final Item item;
-        int amount;
+    public long getAbsoluteDiscount(){
+        return super.getPrice() - getPrice();
+    }
 
-        BillItem(Item item, int amount) {
-            this.item = item;
-            this.amount = amount;
-        }
+    public long getPercentDiscount(){
+        return discount * 100;
     }
 }
