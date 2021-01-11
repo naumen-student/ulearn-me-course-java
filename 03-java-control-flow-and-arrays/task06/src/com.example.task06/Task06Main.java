e287b3a32f704851e273e9e2e6790bc128f9abc3
ElifZelal <75887054+ElifZelal@users.noreply.github.com>
2021-01-11 06:22:54 +0300

    Мудуроглу Элиф Зэлал Задача3.6

--- 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
+++ 03-java-control-flow-and-arrays/task06/src/com.example.task06/Task06Main.java
@@ -6,12 +6,15 @@ public class Task06Main {
         // например вот так:
         /*
         System.out.println(getMax(1, 2, 3, 4));
+        System.out.println(getMax(7, 5, 2, 9));
+        System.out.println(getMax(9, 9, 8, 10));
          */
     }
 
     static int getMax(int a, int b, int c, int d) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         return 0;
+        return Arrays.stream(new int[] {a, b, c, d}).max().getAsInt();
     }
 
 }
