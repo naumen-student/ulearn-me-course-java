c08b6d4a48d4835d591f70af7a361c6e779fac01
ElifZelal <75887054+ElifZelal@users.noreply.github.com>
2021-01-11 05:44:21 +0300

    Мудуроглу Элиф Зэлал Задача2.11

--- 02-java-types/task11/src/com/example/task11/Task11.java
+++ 02-java-types/task11/src/com/example/task11/Task11.java
@@ -10,6 +10,7 @@ public class Task11 {
         for (int i = 1; i <= 12; i++) {
             sum += sum * percent;
         }
+        sum *= Math.pow(percent + 1.0, 12);
         return sum;
     }
 
@@ -18,10 +19,11 @@ public class Task11 {
         float sum = 500; // 500 руб. на счете
         float percent = 0.00000001f; // 0.000001% ежемесячно
 
+        float sum = 500;
+        float percent = 0.000_000_01f;
         sum = benefit(sum, percent);
 
         System.out.println("Сумма на счете через год: " + sum);
 
     }
-
 }
