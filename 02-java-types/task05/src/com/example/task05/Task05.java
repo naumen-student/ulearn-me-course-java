6b944e7b6622f84d0901980520cca0d5db97f7ea
ElifZelal <75887054+ElifZelal@users.noreply.github.com>
2021-01-11 04:27:23 +0300

    Мудуроглу Элиф Зэлал Задание2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -4,18 +4,21 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        if ( (x / 10000) % 2 != 0  ) return "FALSE";
+        else if ( (x / 1000) % 2 != 0 ) return "FALSE";
+        else if ( (x / 100) % 2 != 0 ) return "FALSE";
+        else if ( (x / 10) % 2 != 0 ) return "FALSE";
+        else if ( (x % 2 != 0)) return "FALSE";
+        else return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
