b124fd43fd483a784adbae539c95d3e0e06b2891
ElifZelal <75887054+ElifZelal@users.noreply.github.com>
2021-01-11 04:36:18 +0300

    Мудуроглу Элиф Зэлал Задание2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,19 +3,27 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int result = x + y;
+        //int sum = 0;
+        //while(result >= 0){
+        //Суммирование цифр числа
+        //   sum++;
+        //    result/=10;
+        //}
+        //return sum;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        //return (int)Math.log10(result)+1; не работает с отрицательными
+        if (result >= 0) return String.valueOf(result).split("").length ;
+        else return String.valueOf(result).split("").length -1;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(12, 34);
+
+        int result = solution(1, 1);
         System.out.println(result);
-        */
+
     }
 
 }
