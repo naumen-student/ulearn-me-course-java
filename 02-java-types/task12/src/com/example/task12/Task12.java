6e3464dacccdd4591e15c4e622a298308db16acf
ElifZelal <75887054+ElifZelal@users.noreply.github.com>
2021-01-12 19:46:08 +0300

    Мудуроглу Элиф Зэлал 2.12

--- 02-java-types/task12/src/com/example/task12/Task12.java
+++ 02-java-types/task12/src/com/example/task12/Task12.java
@@ -1,6 +1,7 @@
 package com.example.task12;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Task12 {
 
@@ -18,17 +19,14 @@ public class Task12 {
         */
 
         return BigDecimal.ZERO;
+        BigDecimal x = percent.add(BigDecimal.valueOf(1));
+        return sum.multiply(x.pow(12)).setScale(9, RoundingMode.HALF_UP);
     }
 
     public static void main(String[] args) {
-
         BigDecimal sum = new BigDecimal(500).setScale(9, BigDecimal.ROUND_HALF_UP); // 500 руб. на счете
         BigDecimal percent = new BigDecimal(0.00000001f).setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
-
         sum = benefit(sum, percent);
-
         System.out.println("Сумма на счете через год: " + sum);
-
     }
-
 }
