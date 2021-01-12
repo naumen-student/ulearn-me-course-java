6cceecd98883e67bf3989fb764d66e26bcf4c2c0
ElifZelal <75887054+ElifZelal@users.noreply.github.com>
2021-01-12 21:40:01 +0300

    Мудуроглу Элиф Зэлал 6.5

--- 06-java-exceptions/task05/src/com/example/task05/Task05Main.java
+++ 06-java-exceptions/task05/src/com/example/task05/Task05Main.java
@@ -3,13 +3,22 @@ package com.example.task05;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import static java.lang.System.*;
 
 public class Task05Main {
-    public static void main(String[] args) throws IOException {
-        String pathToFile = args[0]; // "/home/user/file.txt"
-
-        String s = readFile(pathToFile);
-        System.out.println(s);
+    public static void main(String[] args){
+        String pathToFile = args[0];
+        try {
+            out.println(readFile(pathToFile));
+        }
+        catch (FileNotFoundException e){
+            out.printf("файл \"%s\" не найден\n", pathToFile);
+        }
+        catch (IOException e){
+            out.printf("произошла ошибка при чтении файла %s\n", pathToFile);
+        }
     }
 
     public static String readFile(String pathToFile) throws IOException {
