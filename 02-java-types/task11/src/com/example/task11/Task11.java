496b3c966d5577d164773d3ab70dc4be3720bc8f
ElifZelal <75887054+ElifZelal@users.noreply.github.com>
2021-01-12 20:01:46 +0300

    Мудуроглу Элиф Зэлал 2.11

--- 02-java-types/task11/src/com/example/task11/Task11.java
+++ 02-java-types/task11/src/com/example/task11/Task11.java
@@ -3,15 +3,7 @@ package com.example.task11;
 public class Task11 {
 
     public static float benefit(float sum, float percent) {
-
-        // TODO исправьте функцию, чтобы избежать накопления ошибки
-
-        // Считаем проценты за год
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        sum *= Math.pow(percent + 1.0, 12);
-        return sum;
+        return (float) (sum * Math.pow(percent + 1.0, 12));
     }
 
     public static void main(String[] args) {
@@ -19,11 +11,10 @@ public class Task11 {
         float sum = 500; // 500 руб. на счете
         float percent = 0.00000001f; // 0.000001% ежемесячно
 
-        float sum = 500;
-        float percent = 0.000_000_01f;
         sum = benefit(sum, percent);
 
         System.out.println("Сумма на счете через год: " + sum);
 
     }
+
 }
