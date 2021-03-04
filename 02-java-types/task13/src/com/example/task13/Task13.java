98f437438c955d6d2e2d917199226c975383fcff
kalibri1995 <gainatullinalba@gmail.com>
2021-03-05 02:10:28 +0300

    Гайнатуллин Альберт Задание 2.13

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -3,10 +3,7 @@ package com.example.task13;
 public class Task13 {
 
     public static char toUpperCase(char c) {
-
-        // TODO привести букву к верхнему регистру
-
-        return c;
+        return (char)(c - 32);
     }
 
     public static void main(String[] args) {
