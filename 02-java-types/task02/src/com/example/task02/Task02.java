27a7111556339009c3adb0a9685ee8b44c5b33d4
Yazan <yazanalnajm19@gmail.com>
2021-03-31 14:36:26 +0500

    Алнажм Язан Задание 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -4,18 +4,30 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        try {
+            byte b = Byte.parseByte(input);
+            return "Byte";
+        } catch (Exception e0) {
+            try {
+                short b = Short.parseShort(input);
+                return "Short";
+            } catch (Exception e) {
+                try {
+                    int b = Integer.parseInt(input);
+                    return "int";
+                } catch (Exception e2) {
+                    try {
+                        long b = Long.parseLong(input);
+                        return "Long";
+                    } catch (Exception e3) {
+                        return "String";
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
     }
 
 }
