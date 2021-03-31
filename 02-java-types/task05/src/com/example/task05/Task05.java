8554e1cc53f4a5f302b7eeef3666a9fe63075d6f
Yazan <yazanalnajm19@gmail.com>
2021-03-31 14:16:32 +0500

    Алнажм Язан Задание 2.5

--- 02-java-types/task05/src/com/example/task05/Task05.java
+++ 02-java-types/task05/src/com/example/task05/Task05.java
@@ -4,18 +4,27 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if(x % 2 == 0)
+        {
+            x = x / 10;
+            if(x % 2 == 0 ) {
+                x = x / 10;
+                if(x % 2 == 0) {
+                    x = x / 10;
+                    if(x % 2 == 0){
+                        x = x / 10;
+                        if(x % 2 == 0)
+                            return "True";
+                    }
+                }
+            }
+        }
 
         return "FALSE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
-        System.out.println(result);
-        */
+
     }
 
 }
