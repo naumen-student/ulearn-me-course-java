2de5d620d53a1a8fb42959ae39bcedd2d15984c3
Yazan <yazanalnajm19@gmail.com>
2021-03-31 20:14:16 +0500

    Алнажм Язан Задание 3.11

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,19 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null || arr.length > 0) {
+            int min = arr[0];
+            int numMin = 0;
+            for (int i = 1; i < arr.length; i++) {
+                if (min >= arr[i]) {
+                    min = arr[i];
+                    numMin = i;
+                }
+            }
+            int holder = arr[0];
+            arr[0] = arr[numMin];
+            arr[numMin] = holder;
+        }
     }
 
 }
