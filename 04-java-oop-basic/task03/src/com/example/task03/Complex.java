bd422728ed0503d1569a80b5e664436274137f19
Yazan <yazanalnajm19@gmail.com>
2021-04-01 20:11:44 +0500

    Алнажм Язан Задание 4.3

---
+++ 04-java-oop-basic/task03/src/com/example/task03/Complex.java
@@ -0,0 +1,29 @@
+package com.example.task03;
+
+public static class Complex{
+    private final int rl;
+    private final int im;
+
+    public Complex(int rl, int im){
+        this.rl = rl;
+        this.im = im;
+    }
+
+    public void setRl(int rl){this.rl = rl;}
+    public void setIm(int im){this.im = im;}
+
+    public int getRl(){return rl;}
+    public int getIm(){return im;}
+
+    public Complex Sum(Complex complex){
+        return new Complex(rl + complex.rl, im + complex.im);
+    }
+
+    public Complex mult(Complex complix){
+        return new Complex(rl * complix.rl, im * complix.im);
+    }
+
+    public String toString(){
+        return rl+" + " + im + "i";
+    }
+}
