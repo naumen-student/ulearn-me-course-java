07262d2b4569e45fd9f0552c30d63aedf3010f24
Yazan <yazanalnajm19@gmail.com>
2021-04-05 15:46:21 +0500

    Алнажм Язан Задание 6.4

--- 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
+++ 06-java-exceptions/task04/src/com/example/task04/Task04Main.java
@@ -3,15 +3,16 @@ package com.example.task04;
 public class Task04Main {
 
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getSeason(-5));
-         */
+
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        static String getSeason(int monthNumber) {
+            String[] months = {"зима", "зима", "весна", "весна", "весна", "лето", "лето", "лето", "осень", "осень", "осень", "зима"};
+            if (monthNumber < 1 || monthNumber > 12)
+                throw new MyException(monthNumber);
+            return months[monthNumber - 1];
+        }
     }
 
 }
