2938dea6d92e21d95eaecd2d1fbc1c3fb4fbc5ed
Yazan <yazanalnajm19@gmail.com>
2021-04-08 08:29:25 +0500

    Алнажм Язан Задание 6.7

--- 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
+++ 06-java-exceptions/task07/src/com/example/task07/Task07Main.java
@@ -12,13 +12,14 @@ public class Task07Main {
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
 
+        try {
+            processor.process();
+        } catch (Exception e) {
+            if(e instanceof IOException) return CHECKED;
+            else return UNCHECKED;
         }
-        return null;
+        return NONE;
     }
 
 }
