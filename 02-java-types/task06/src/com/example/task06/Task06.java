f755595f0a3b3924a9d4776cec7fdda352f598a1
veles <velesmeister@gmail.com>
2021-04-18 14:25:26 +0500

    feat: solve task 2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -3,10 +3,14 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int sum = x + y;
+        int numeral = 0;
+        do {
+            numeral++;
+            sum /= 10;
+        } while (sum != 0);
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return numeral;
     }
 
     public static void main(String[] args) {
