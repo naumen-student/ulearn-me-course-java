1cceb8f77776bada68b623071efffb4139ea4ede
veles <velesmeister@gmail.com>
2021-04-18 16:00:27 +0500

    rollback

--- 05-java-oop-principles/task04/src/com/example/task04/ConsoleHandler.java
+++
@@ -1,8 +0,0 @@
-package com.example.task04;
-
-public class ConsoleHandler implements MessageHandler {
-    @Override
-    public void log(String message) {
-        System.out.println(message);
-    }
-}
--- 05-java-oop-principles/task04/src/com/example/task04/MessageHandler.java
+++
@@ -1,5 +0,0 @@
-package com.example.task04;
-
-public interface MessageHandler {
-    void log(String message);
-}
