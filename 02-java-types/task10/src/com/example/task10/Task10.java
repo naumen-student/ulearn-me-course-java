2b1845a94dab56d35b85d11e96b4b8714003ddb1
veles <velesmeister@gmail.com>
2021-04-18 14:35:25 +0500

    feat: task 2.10 solve

--- 02-java-types/task10/src/com/example/task10/Task10.java
+++ 02-java-types/task10/src/com/example/task10/Task10.java
@@ -2,14 +2,19 @@ package com.example.task10;
 
 public class Task10 {
 
+
+    private static boolean IsEqualsInfinity(float a, float b) {
+        return Float.isInfinite(a) && Float.isInfinite(b) && a == b;
+    }
+
+    private static boolean IsBothNaN(float a, float b) {
+        return Float.isNaN(a) && Float.isNaN(b);
+    }
+
     public static boolean compare(float a, float b, int precision) {
-
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
-
+        return IsEqualsInfinity(a, b)
+                || IsBothNaN(a, b)
+                || Math.abs(a - b) < Math.pow(0.1, precision);
     }
 
     public static void main(String[] args) {
