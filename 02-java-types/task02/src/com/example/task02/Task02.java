345c43b352d6961842a98cddb1ab6336606ec34a
NazarKazancev <nazarkazancev@gmail.com>
2021-04-19 15:48:02 +0500

    Казанцев Назар. Задание 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -1,38 +1,26 @@
+
 package com.example.task02;
 
 public class Task02 {
 
     public static String solution(String input) {
-        try {
-            byte b = Byte.parseByte(input);
+
+        long number = Long.parseLong(input);
+
+        if (number >= Byte.MIN_VALUE && number <= Byte.MAX_VALUE)
             return "byte";
-        } catch (Exception e0) {
-            try {
-                short b = Short.parseShort(input);
-                return "short";
-            } catch (Exception e) {
-                try {
-                    int b = Integer.parseInt(input);
-                    return "int";
-                } catch (Exception e2) {
-                    try {
-                        long b = Long.parseLong(input);
-                        return "long";
-                    } catch (Exception e3) {
-                        return "String";
-                    }
-                }
-            }
-        }
+        else if (number >= Short.MIN_VALUE && number <= Short.MAX_VALUE)
+            return "short";
+        else if (number >= Integer.MIN_VALUE && number <= Integer.MAX_VALUE)
+            return "int";
+        else
+            return "long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
