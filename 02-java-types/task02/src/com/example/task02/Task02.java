ca7b0214e94f19578d051a0b42f7c33180425eda
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2021-04-25 13:22:58 +0300

    Самсоненков Никита 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,13 +3,13 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-        long x=Long.parseLong(input);
+        long x = Long.parseLong(input);
 
-        if (x>=-128 && x<=127)
+        if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE)
             return "byte";
-        if (x>=-32768 && x<=32767)
+        if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE)
             return "short";
-        if (x>=-2147483648 && x<=2147483647)
+        if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE)
             return "int";
         else
             return "long";
