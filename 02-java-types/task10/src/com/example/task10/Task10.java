189317dba9e1d2a3cb2724a38e5d7b97d7159bd5
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2021-08-22 14:16:30 +0300

    Nikita Samsonenkov №2.10

--- 02-java-types/task10/src/com/example/task10/Task10.java
+++ 02-java-types/task10/src/com/example/task10/Task10.java
@@ -7,8 +7,13 @@ public class Task10 {
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
+
+        precision = precision == 0 ? 1 : precision;
+        boolean isNan = Double.isNaN(a) && Double.isNaN(b);
+        boolean isInfinity = Double.isInfinite(a) && Double.isInfinite(b);
+        if (isInfinity && Math.signum(a * b) == 1)
+            return true;
+        return Math.abs(a - b) <= Math.pow(10, -precision) || isNan;
 
     }
 
