ab1e5ff917fa6fccfb77a6e02e060af774923341
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2021-08-22 14:04:53 +0300

    Nikita Samsonenkov №2.6

--- 02-java-types/task06/src/com/example/task06/Task06.java
+++ 02-java-types/task06/src/com/example/task06/Task06.java
@@ -6,7 +6,11 @@ public class Task06 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return 0;
+        String res = Integer.toString(x + y);
+        if (res.contains("-"))
+            return res.length() - 1;
+        else
+            return res.length();
     }
 
     public static void main(String[] args) {
