7ec057d57dee07bc049c38a98e3b6385c0ca2cae
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2021-09-12 16:19:20 +0300

    Nikita Samsonenkov №7.2

--- 07-java-io/task02/src/com/example/task02/Task02Main.java
+++ 07-java-io/task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,7 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +10,15 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        InputStream inputStream = System.in;
+        int inputChar = inputStream.read();
+        while (inputChar != -1) {
+            int nextChar = inputStream.read();
+            if (!(inputChar == 13 && nextChar == 10)) {
+                System.out.write(inputChar);
+            }
+            inputChar = nextChar;
+        }
+        System.out.flush();
     }
 }
