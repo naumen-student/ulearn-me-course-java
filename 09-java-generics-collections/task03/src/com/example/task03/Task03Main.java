57d1b2e4112c1219203acd42c14dd08d959d2efb
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2021-09-12 16:42:08 +0300

    Nikita Samsonenkov №9.3

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -3,22 +3,39 @@ package com.example.task03;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
-
         List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
-
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Scanner scanner = new Scanner(new InputStreamReader(inputStream, charset)).useDelimiter("\n");
+        Map<String, TreeSet<String>> anagrams = new TreeMap<>();
+        while (scanner.hasNext()) {
+            String word = scanner.next().toLowerCase();
+            if (word.matches("[а-я]+") && word.length() > 2) {
+                char[] arr = word.toCharArray();
+                Arrays.sort(arr);
+                String sortedWord = new String(arr);
+                if (!anagrams.containsKey(sortedWord)) {
+                    anagrams.put(sortedWord, new TreeSet<>());
+                }
+                anagrams.get(sortedWord).add(word);
+            }
+        }
+        return anagrams
+                .values()
+                .stream()
+                .filter(v -> v.size() > 1)
+                .collect(Collectors.toList());
     }
 }
