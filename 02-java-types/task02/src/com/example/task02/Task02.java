63277d415541cdfc24f10f77dc39bc11f708a1d2
Андрей <brookenlp1@gmail.com>
2021-09-19 18:02:58 +0500

    Task 2.2

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -1,21 +1,22 @@
 package com.example.task02;
+
 import static java.lang.Long.parseLong;
 
 public class Task02 {
 
     public static String solution(String input) {
         long num = parseLong(input);
-        if(num >= -128 && num <= 127) {
+        if (num >= -128 && num <= 127) {
             return ("byte");
-        } else if(num <= Math.pow(2, 15) - 1 && num >= Math.pow(2, 15) * -1) {
+        } else if (num <= Math.pow(2, 15) - 1 && num >= Math.pow(2, 15) * -1) {
             return ("short");
-        } else if(num <= Math.pow(2, 31) - 1 && num >= Math.pow(2, 31) * -1) {
+        } else if (num <= Math.pow(2, 31) - 1 && num >= Math.pow(2, 31) * -1) {
             return ("int");
-        } else if(num <= Math.pow(2, 63) - 1 && num >= Math.pow(2, 63) * -1) {
+        } else if (num <= Math.pow(2, 63) - 1 && num >= Math.pow(2, 63) * -1) {
             return ("long");
         }
 
-        return "";
+        return "error";
     }
 
     public static void main(String[] args) {
