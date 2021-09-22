5bfee077bfb3718f45117e890089a354d7703d4d
Андрей <brookenlp1@gmail.com>
2021-09-22 15:48:27 +0500

    Task 2.4

--- 02-java-types/task04/src/com/example/task04/Task04.java
+++ 02-java-types/task04/src/com/example/task04/Task04.java
@@ -3,9 +3,22 @@ package com.example.task04;
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        switch (operation) {
+            case "+": {
+                return a + b;
+            }
+            case "-": {
+                return a - b;
+            }
+            case "*": {
+                return a * b;
+            }
+            case "/": {
+                if (b != 0) {
+                    return (float) a / b;
+                }
+            }
+        }
         return 0;
     }
 
