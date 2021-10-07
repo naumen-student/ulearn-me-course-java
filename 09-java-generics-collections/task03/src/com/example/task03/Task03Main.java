5e3c3e36ba57e0808b3ee9cae5792fdbb99f82f0
artem13082001 <temagroz-2@yandex.ru>
2021-10-07 05:43:28 +0500

    Groznov Artem 10 task 04

--- 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
+++ 09-java-generics-collections/task03/src/com/example/task03/Task03Main.java
@@ -1,24 +1,44 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
+        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.defaultCharset());
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
-
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        HashMap<String, SortedSet<String>> dict = new HashMap<>();
+        new BufferedReader(new InputStreamReader(inputStream, charset))
+                .lines()
+                .map(String::toLowerCase)
+                .filter(it -> it.length() >= 3 && it.matches("[а-я]*"))
+                .distinct()
+                .forEach(it -> {
+                    char[] sorted = it.toCharArray();
+                    Arrays.sort(sorted);
+                    String key = Arrays.toString(sorted);
+                    if(dict.containsKey(key)){
+                        dict.get(key).add(it);
+                    } else {
+                        SortedSet<String> list = new TreeSet<>();
+                        list.add(it);
+                        dict.put(key, list);
+                    }
+                });
+
+        return dict.values()
+                .stream()
+                .filter(it -> it.size() > 1)
+                .sorted(Comparator.comparing(SortedSet::first))
+                .collect(Collectors.toList());
     }
 }
