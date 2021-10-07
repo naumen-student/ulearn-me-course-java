66c7c573ee3868447f841d360ab886ca1dafd476
artem13082001 <temagroz-2@yandex.ru>
2021-10-07 05:57:37 +0500

    Groznov Artem 09 task 02

--- 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
+++ 09-java-generics-collections/task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,74 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private List<E> data;
+    private final File currentFile;
 
     public SavedList(File file) {
+        currentFile = file;
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+                data = new ArrayList<>();
+            }
+            catch (Exception e) {
+                e.getStackTrace();
+            }
+        }
+        else loadFromFile();
+    }
+
+    private void loadFromFile() {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(currentFile))){
+            data = (List<E>) inputStream.readObject();
+        }
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    private void saveToFile() {
+        try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(currentFile))){
+            stream.writeObject(data);
+        }
+        catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return data.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E value = data.set(index, element);
+        saveToFile();
+        return value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return data.size();
     }
 
     @Override
     public void add(int index, E element) {
+        data.add(index, element);
+        saveToFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E value = data.remove(index);
+        saveToFile();
+        return value;
     }
 }
