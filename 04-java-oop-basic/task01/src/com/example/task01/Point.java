021e8a7701f502fd477031ca583ab9b77f079fbe
Андрей <brookenlp1@gmail.com>
2021-10-10 13:47:54 +0500

    .

--- 04-java-oop-basic/task01/src/com/example/task01/Point.java
+++ 04-java-oop-basic/task01/src/com/example/task01/Point.java
@@ -7,8 +7,34 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+
+    public void flip() {
+        int tempX = -x;
+        x = -y;
+        y = tempX;
+    }
+
+    public double distance(Point p) {
+        int xDistance = Math.abs(x - p.x);
+        int yDistance = Math.abs(y - p.y);
+
+        return Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
     }
 }
