e56250c3f84a58f507be327de560467b7397004f
KonshinaAlexandra1 <sashakonshina69@gmail.com>
2022-05-12 03:01:10 +0300

    Konshina Alexandra

--- 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
+++ 10-java-functional-stream/task04/src/com/example/task04/Task04Main.java
@@ -1,10 +1,29 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
+        new BufferedReader(new InputStreamReader(System.in))
+                .lines()
+                .filter(x -> !x.isEmpty())
+                .map(String::toLowerCase)
+                .flatMap(x -> Stream.of(x.split("\\P{LD}+")))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach(x -> System.out.print(x.getKey() + "\n"));
 
     }
 
