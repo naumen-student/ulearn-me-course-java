582b9544c159540b409d28f5c198351aaf30ce65
Mimika <115660264+mimikamaxim@users.noreply.github.com>
2023-10-01 20:12:22 +0500

    task 02_02 done

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,19 +3,18 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long inputValue = Long.parseLong(input);
+        if (inputValue > Integer.MAX_VALUE || inputValue < Integer.MIN_VALUE)
+            return "long";
+        if (inputValue > Short.MAX_VALUE || inputValue < Short.MIN_VALUE)
+            return "int";
+        if (inputValue > Byte.MAX_VALUE || inputValue < Byte.MIN_VALUE)
+            return "short";
+        return "byte";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
-
 }
