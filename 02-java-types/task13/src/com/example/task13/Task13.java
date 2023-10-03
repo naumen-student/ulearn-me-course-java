56e08e31109ecd4ade7a1c87cf84243b8e57294b
Mimika <115660264+mimikamaxim@users.noreply.github.com>
2023-10-03 22:56:55 +0500

    task 02_13 done

--- 02-java-types/task13/src/com/example/task13/Task13.java
+++ 02-java-types/task13/src/com/example/task13/Task13.java
@@ -2,20 +2,15 @@ package com.example.task13;
 
 public class Task13 {
 
-    public static char toUpperCase(char c) {
-
-        // TODO привести букву к верхнему регистру
-
-        return c;
+    public static char toUpperCase(char c) throws IllegalArgumentException {
+        if (c >= 'A' && c <= 'Z') return c;
+        if (c < 'a' || c > 'z') throw new IllegalArgumentException("This is not an English letter: " + c);
+        return Character.toUpperCase(c);
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        char result = solution('x');
+        char result = toUpperCase('x');
         System.out.println(result);
-         */
     }
 
 }
